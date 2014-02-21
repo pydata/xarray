9488463c3388fbda04419208a794ef2f6ff49959
akleeman <akleeman@gmail.com>
2014-02-20 16:12:58 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
 setup(name='xray',
       version='0.1-dev',
       description='Objects for holding self describing scientific data in python',
-      author='Stephan Hoyer, Alex Kleeman',
+      author='Stephan Hoyer, Alex Kleeman, Eugene Brevdo',
       author_email='TODO',
       install_requires=['scipy >= 0.10.0', 'numpy >= 1.8', 'netCDF4 >= 1.0.6',
                         'pandas >= 0.13.1'],
