e6a83280c56bcc7708808dbf44e7f4d695bb4b5c
akleeman <akleeman@gmail.com>
2014-03-03 10:00:41 -0800

    Update dataset_array.py
    Minor doc update.

--- src/xray/dataset_array.py
+++ src/xray/dataset_array.py
@@ -302,7 +302,8 @@ class DatasetArray(AbstractArray):
         ----------
         dimensions : None or str or tuple of str, optional
             Selects a subset of the length one dimensions. If a dimension is
-            selected with length greater than one, an error is raised.
+            selected with length greater than one, an error is raised. If
+            None, all length one dimensions are squeezed.
 
         Returns
         -------
