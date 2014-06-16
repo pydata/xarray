667f26fad6af902fb0508693326bc3c313d7847d
Stephan Hoyer <shoyer@climate.com>
2014-06-15 21:27:03 -0700

    Use a contextmanager for roundtripping in test_backends
    With the current implementation, it looks like might end up deleting
    the path to files opened with netCDF4 before we are done looking
    at the values, because roundtrip (for netCDF4) returned after exiting
    the create_tmp_file context manager. I'm actually not entirely sure
    how we managed to get away with that...

--- test/test_backends.py
+++ test/test_backends.py
@@ -73,21 +73,21 @@ class DatasetIOTestCases(object):
 
     def test_roundtrip_test_data(self):
         expected = create_test_data()
-        actual = self.roundtrip(expected)
-        self.assertDatasetAllClose(expected, actual)
+        with self.roundtrip(expected) as actual:
+            self.assertDatasetAllClose(expected, actual)
 
     def test_load_data(self):
         expected = create_test_data()
 
         @contextlib.contextmanager
         def assert_loads():
-            actual = self.roundtrip(expected)
-            for v in actual.variables.values():
-                self.assertFalse(v._in_memory())
-            yield actual
-            for v in actual.variables.values():
-                self.assertTrue(v._in_memory())
-            self.assertDatasetAllClose(expected, actual)
+            with self.roundtrip(expected) as actual:
+                for v in actual.variables.values():
+                    self.assertFalse(v._in_memory())
+                yield actual
+                for v in actual.variables.values():
+                    self.assertTrue(v._in_memory())
+                self.assertDatasetAllClose(expected, actual)
 
         with self.assertRaises(AssertionError):
             # make sure the contextmanager works!
@@ -102,42 +102,44 @@ class DatasetIOTestCases(object):
 
     def test_roundtrip_None_variable(self):
         expected = Dataset({None: (('x', 'y'), [[0, 1], [2, 3]])})
-        actual = self.roundtrip(expected)
-        self.assertDatasetAllClose(expected, actual)
+        with self.roundtrip(expected) as actual:
+            self.assertDatasetAllClose(expected, actual)
 
     def test_roundtrip_string_data(self):
         expected = Dataset({'x': ('t', ['abc', 'def', np.nan],
                                   {}, {'_FillValue': ''})})
-        actual = self.roundtrip(expected)
-        self.assertDatasetAllClose(expected, actual)
+        with self.roundtrip(expected) as actual:
+            self.assertDatasetAllClose(expected, actual)
 
     def test_roundtrip_mask_and_scale(self):
         decoded = create_masked_and_scaled_data()
         encoded = create_encoded_masked_and_scaled_data()
-        self.assertDatasetAllClose(decoded, self.roundtrip(decoded))
-        self.assertDatasetAllClose(encoded,
-                                   self.roundtrip(decoded, decode_cf=False))
-        self.assertDatasetAllClose(decoded, self.roundtrip(encoded))
-        self.assertDatasetAllClose(encoded,
-                                   self.roundtrip(encoded, decode_cf=False))
+        with self.roundtrip(decoded) as actual:
+            self.assertDatasetAllClose(decoded, actual)
+        with self.roundtrip(decoded, decode_cf=False) as actual:
+            self.assertDatasetAllClose(encoded, actual)
+        with self.roundtrip(encoded) as actual:
+            self.assertDatasetAllClose(decoded, actual)
+        with self.roundtrip(encoded, decode_cf=False) as actual:
+            self.assertDatasetAllClose(encoded, actual)
 
     def test_roundtrip_example_1_netcdf(self):
         expected = open_example_dataset('example_1.nc')
-        actual = self.roundtrip(expected)
-        self.assertDatasetIdentical(expected, actual)
+        with self.roundtrip(expected) as actual:
+            self.assertDatasetIdentical(expected, actual)
 
     def test_orthogonal_indexing(self):
         in_memory = create_test_data()
-        on_disk = self.roundtrip(in_memory)
-        indexers = {'dim1': np.arange(3), 'dim2': np.arange(4),
-                    'dim3': np.arange(5)}
-        expected = in_memory.indexed(**indexers)
-        actual = on_disk.indexed(**indexers)
-        self.assertDatasetAllClose(expected, actual)
-        # do it twice, to make sure we're switched from orthogonal -> numpy
-        # when we cached the values
-        actual = on_disk.indexed(**indexers)
-        self.assertDatasetAllClose(expected, actual)
+        with self.roundtrip(in_memory) as on_disk:
+            indexers = {'dim1': np.arange(3), 'dim2': np.arange(4),
+                        'dim3': np.arange(5)}
+            expected = in_memory.indexed(**indexers)
+            actual = on_disk.indexed(**indexers)
+            self.assertDatasetAllClose(expected, actual)
+            # do it twice, to make sure we're switched from orthogonal -> numpy
+            # when we cached the values
+            actual = on_disk.indexed(**indexers)
+            self.assertDatasetAllClose(expected, actual)
 
     def test_pickle(self):
         on_disk = open_example_dataset('bears.nc')
@@ -162,24 +164,23 @@ class NetCDF4DataTest(DatasetIOTestCases, TestCase):
         with create_tmp_file() as tmp_file:
             yield backends.NetCDF4DataStore(tmp_file, mode='w')
 
+    @contextlib.contextmanager
     def roundtrip(self, data, **kwargs):
         with create_tmp_file() as tmp_file:
             data.dump(tmp_file)
-            roundtrip_data = open_dataset(tmp_file, **kwargs)
-        return roundtrip_data
+            yield open_dataset(tmp_file, **kwargs)
 
     def test_open_encodings(self):
         # Create a netCDF file with explicit time units
         # and make sure it makes it into the encodings
         # and survives a round trip
         with create_tmp_file() as tmp_file:
-            ds = nc4.Dataset(tmp_file, 'w')
-            ds.createDimension('time', size=10)
-            ds.createVariable('time', np.int32, dimensions=('time',))
-            units = 'days since 1999-01-01'
-            ds.variables['time'].setncattr('units', units)
-            ds.variables['time'][:] = np.arange(10) + 4
-            ds.close()
+            with nc4.Dataset(tmp_file, 'w') as ds:
+                ds.createDimension('time', size=10)
+                ds.createVariable('time', np.int32, dimensions=('time',))
+                units = 'days since 1999-01-01'
+                ds.variables['time'].setncattr('units', units)
+                ds.variables['time'][:] = np.arange(10) + 4
 
             expected = Dataset()
 
@@ -244,58 +245,53 @@ class NetCDF4DataTest(DatasetIOTestCases, TestCase):
         # and make sure it makes it into the encodings
         # and survives a round trip
         with create_tmp_file() as tmp_file:
-            ds = nc4.Dataset(tmp_file, 'w')
-            ds.createDimension('time', size=10)
-            ds.createVariable('time', np.int32, dimensions=('time',))
-            units = 'days since 1999-01-01'
-            ds.variables['time'].setncattr('units', units)
-            ds.variables['time'][:] = np.arange(10) + 4
-            ds.close()
+            with nc4.Dataset(tmp_file, 'w') as ds:
+                ds.createDimension('time', size=10)
+                ds.createVariable('time', np.int32, dimensions=('time',))
+                units = 'days since 1999-01-01'
+                ds.variables['time'].setncattr('units', units)
+                ds.variables['time'][:] = np.arange(10) + 4
 
             xray_dataset = open_dataset(tmp_file)
 
-        with create_tmp_file() as tmp_file:
-            xray_dataset.dump(tmp_file)
+            with create_tmp_file() as tmp_file2:
+                xray_dataset.dump(tmp_file2)
 
-            ds = nc4.Dataset(tmp_file, 'r')
-
-            self.assertEqual(ds.variables['time'].getncattr('units'), units)
-            self.assertArrayEqual(ds.variables['time'], np.arange(10) + 4)
-
-            ds.close()
+                with nc4.Dataset(tmp_file2, 'r') as ds:
+                    self.assertEqual(ds.variables['time'].getncattr('units'), units)
+                    self.assertArrayEqual(ds.variables['time'], np.arange(10) + 4)
 
     def test_compression_encoding(self):
         data = create_test_data()
         data['var2'].encoding.update({'zlib': True,
                                       'chunksizes': (10, 10),
                                       'least_significant_digit': 2})
-        actual = self.roundtrip(data)
-        for k, v in iteritems(data['var2'].encoding):
-            self.assertEqual(v, actual['var2'].encoding[k])
+        with self.roundtrip(data) as actual:
+            for k, v in iteritems(data['var2'].encoding):
+                self.assertEqual(v, actual['var2'].encoding[k])
 
         # regression test for #156
         expected = data.indexed(dim1=0)
-        actual = self.roundtrip(expected)
-        self.assertDatasetEqual(expected, actual)
+        with self.roundtrip(expected) as actual:
+            self.assertDatasetEqual(expected, actual)
 
     def test_mask_and_scale(self):
         with create_tmp_file() as tmp_file:
-            nc = nc4.Dataset(tmp_file, mode='w')
-            nc.createDimension('t', 5)
-            nc.createVariable('x', 'int16', ('t',), fill_value=-1)
-            v = nc.variables['x']
-            v.set_auto_maskandscale(False)
-            v.add_offset = 10
-            v.scale_factor = 0.1
-            v[:] = np.array([-1, -1, 0, 1, 2])
-            nc.close()
+            with nc4.Dataset(tmp_file, mode='w') as nc:
+                nc.createDimension('t', 5)
+                nc.createVariable('x', 'int16', ('t',), fill_value=-1)
+                v = nc.variables['x']
+                v.set_auto_maskandscale(False)
+                v.add_offset = 10
+                v.scale_factor = 0.1
+                v[:] = np.array([-1, -1, 0, 1, 2])
 
             # first make sure netCDF4 reads the masked and scaled data correctly
-            nc = nc4.Dataset(tmp_file, mode='r')
-            expected = np.ma.array([-1, -1, 10, 10.1, 10.2],
-                                   mask=[True, True, False, False, False])
-            actual = nc.variables['x'][:]
-            self.assertArrayEqual(expected, actual)
+            with nc4.Dataset(tmp_file, mode='r') as nc:
+                expected = np.ma.array([-1, -1, 10, 10.1, 10.2],
+                                       mask=[True, True, False, False, False])
+                actual = nc.variables['x'][:]
+                self.assertArrayEqual(expected, actual)
 
             # now check xray
             ds = open_dataset(tmp_file)
@@ -306,10 +302,9 @@ class NetCDF4DataTest(DatasetIOTestCases, TestCase):
         # This fix verifies our work-around to this netCDF4-python bug:
         # https://github.com/Unidata/netcdf4-python/pull/220
         with create_tmp_file() as tmp_file:
-            nc = nc4.Dataset(tmp_file, mode='w')
-            v = nc.createVariable('x', 'int16')
-            v[...] = 123
-            nc.close()
+            with nc4.Dataset(tmp_file, mode='w') as nc:
+                v = nc.createVariable('x', 'int16')
+                v[...] = 123
 
             ds = open_dataset(tmp_file)
             expected = Dataset({'x': ((), 123)})
@@ -319,11 +314,10 @@ class NetCDF4DataTest(DatasetIOTestCases, TestCase):
         with create_tmp_file() as tmp_file:
             values = np.array(['foo', 'bar', 'baz'], dtype=object)
 
-            nc = nc4.Dataset(tmp_file, mode='w')
-            nc.createDimension('x', 3)
-            v = nc.createVariable('x', str, ('x',))
-            v[:] = values
-            nc.close()
+            with nc4.Dataset(tmp_file, mode='w') as nc:
+                nc.createDimension('x', 3)
+                v = nc.createVariable('x', str, ('x',))
+                v[:] = values
 
             expected = Dataset({'x': ('x', values)})
             for kwargs in [{}, {'decode_cf': True}]:
@@ -346,8 +340,8 @@ class NetCDF4DataTest(DatasetIOTestCases, TestCase):
             self.assertDatasetIdentical(expected, actual)
 
             # regression test for #157
-            roundtripped = self.roundtrip(actual)
-            self.assertDatasetIdentical(expected, roundtripped)
+            with self.roundtrip(actual) as roundtripped:
+                self.assertDatasetIdentical(expected, roundtripped)
 
 
 @requires_netCDF4
@@ -358,9 +352,10 @@ class ScipyDataTest(DatasetIOTestCases, TestCase):
         fobj = BytesIO()
         yield backends.ScipyDataStore(fobj, 'w')
 
+    @contextlib.contextmanager
     def roundtrip(self, data, **kwargs):
         serialized = data.dumps()
-        return open_dataset(BytesIO(serialized), **kwargs)
+        yield open_dataset(BytesIO(serialized), **kwargs)
 
 
 @requires_netCDF4
@@ -371,11 +366,11 @@ class NetCDF3ViaNetCDF4DataTest(DatasetIOTestCases, TestCase):
             yield backends.NetCDF4DataStore(tmp_file, mode='w',
                                             format='NETCDF3_CLASSIC')
 
+    @contextlib.contextmanager
     def roundtrip(self, data, **kwargs):
         with create_tmp_file() as tmp_file:
             data.dump(tmp_file, format='NETCDF3_CLASSIC')
-            roundtrip_data = open_dataset(tmp_file, **kwargs)
-        return roundtrip_data
+            yield open_dataset(tmp_file, **kwargs)
 
 
 @requires_netCDF4
