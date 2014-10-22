982276ceb6df7230a733e4e64d587f837de4d723
Stephan Hoyer <shoyer@climate.com>
2014-10-21 23:39:52 -0700

    Add broadcast_arrays

--- xray/__init__.py
+++ xray/__init__.py
@@ -1,4 +1,4 @@
-from .core.alignment import align, concat
+from .core.alignment import align, broadcast_arrays, concat
 from .core.variable import Variable, Coordinate
 from .core.dataset import Dataset, open_dataset
 from .core.dataarray import DataArray
