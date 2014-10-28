fix silently upcasting float32 to float64 in reindex/align
Fixes #264

xray/core/common.py
@@ -115,12 +115,14 @@ def squeeze(xray_obj, dims, dim=None):
 def _maybe_promote(dtype):
     """Simpler equivalent of pandas.core.common._maybe_promote"""
     # N.B. these casting rules should match pandas
-    if np.issubdtype(dtype, np.datetime64):
-        fill_value = np.datetime64('NaT')
-    elif any(np.issubdtype(dtype, t) for t in (int, float)):
+    if np.issubdtype(dtype, float):
+        fill_value = np.nan
+    elif np.issubdtype(dtype, int):
         # convert to floating point so NaN is valid
         dtype = float
         fill_value = np.nan
+    elif np.issubdtype(dtype, np.datetime64):
+        fill_value = np.datetime64('NaT')
     else:
         dtype = object
         fill_value = np.nan