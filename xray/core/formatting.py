ad35bd24cb1e55716d42231a5012306e2678e55f
Stephan Hoyer <shoyer@climate.com>
2015-09-10 18:35:42 -0700

    Don't quote strings in FacetGrid titles
    `model = foo` looks better than `model = 'foo'` or `model = u'foo'`.
    This is also consistent with what Seaborn does.

--- xray/core/formatting.py
+++ xray/core/formatting.py
@@ -89,14 +89,14 @@ def format_timedelta(t, timedelta_format=None):
             return timedelta_str
 
 
-def format_item(x, timedelta_format=None):
+def format_item(x, timedelta_format=None, quote_strings=True):
     """Returns a succinct summary of an object as a string"""
     if isinstance(x, (np.datetime64, datetime)):
         return format_timestamp(x)
     if isinstance(x, (np.timedelta64, timedelta)):
         return format_timedelta(x, timedelta_format=timedelta_format)
     elif isinstance(x, (unicode_type, bytes_type)):
-        return repr(x)
+        return repr(x) if quote_strings else x
     elif isinstance(x, (float, np.float)):
         return '{0:.4}'.format(x)
     else:
