550e4b80b22494c50d0bff97db03bb49e5c44a87
Stephan Hoyer <shoyer@climate.com>
2015-10-05 10:52:02 -0700

    More specific error catching in decode_cf_datetime

--- xray/conventions.py
+++ xray/conventions.py
@@ -136,12 +136,16 @@ def decode_cf_datetime(num_dates, units, calendar=None):
             raise OutOfBoundsDatetime
 
         delta = _netcdf_to_numpy_timeunit(delta)
-        ref_date = pd.Timestamp(ref_date)
+        try:
+            ref_date = pd.Timestamp(ref_date)
+        except ValueError:
+            # ValueError is raised by pd.Timestamp for non-ISO timestamp
+            # strings, in which case we fall back to using netCDF4
+            raise OutOfBoundsDatetime
 
         dates = (pd.to_timedelta(flat_num_dates, delta) + ref_date).values
-    # ValueError is raised by pd.Timestamp for non-ISO timestamp strings,
-    # in which case we fall back to using netCDF4
-    except (OutOfBoundsDatetime, ValueError, OverflowError):
+
+    except (OutOfBoundsDatetime, OverflowError):
         dates = _decode_datetime_with_netcdf4(flat_num_dates, units, calendar)
 
     return dates.reshape(num_dates.shape)
