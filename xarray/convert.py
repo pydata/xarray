e7f9cb1028ab5ffaf2b0c7de5093a673f5b8a311
Neil Parley <mail@neilparley.co.uk>
2016-05-25 12:00:43 +0100

    Move helper functions outside of main functions

--- xarray/convert.py
+++ xarray/convert.py
@@ -24,8 +24,10 @@ def encode(var):
     return maybe_encode_timedelta(maybe_encode_datetime(var.variable))
 
 
-def filter_attrs(_attrs, ignored_attrs):
-    return dict((k, v) for k, v in _attrs.items() if k not in ignored_attrs)
+def _filter_attrs(attrs, ignored_attrs):
+    """ Return attrs that are not in ignored_attrs
+    """
+    return dict((k, v) for k, v in attrs.items() if k not in ignored_attrs)
 
 
 def from_cdms2(variable):
@@ -34,9 +36,9 @@ def from_cdms2(variable):
     values = np.asarray(variable)
     name = variable.id
     coords = [(v.id, np.asarray(v),
-               filter_attrs(v.attributes, cdms2_ignored_attrs))
+               _filter_attrs(v.attributes, cdms2_ignored_attrs))
               for v in variable.getAxisList()]
-    attrs = filter_attrs(variable.attributes, cdms2_ignored_attrs)
+    attrs = _filter_attrs(variable.attributes, cdms2_ignored_attrs)
     dataarray = DataArray(values, coords=coords, name=name, attrs=attrs)
     return decode_cf(dataarray.to_dataset())[dataarray.name]
 
@@ -47,9 +49,9 @@ def to_cdms2(dataarray):
     # we don't want cdms2 to be a hard dependency
     import cdms2
 
-    def set_cdms2_attrs(_var, attrs):
+    def set_cdms2_attrs(var, attrs):
         for k, v in attrs.items():
-            setattr(_var, k, v)
+            setattr(var, k, v)
 
     axes = []
     for dim in dataarray.dims:
@@ -64,9 +66,28 @@ def to_cdms2(dataarray):
     return cdms2_var
 
 
+def _pick_attrs(attrs, keys):
+    """ Return attrs with keys in keys list
+    """
+    return dict((k, v) for k, v in attrs.items() if k in keys)
+
+
+def _get_iris_args(attrs):
+    """ Converts the xarray attrs into args that can be passed into Iris
+    """
+    # iris.unit is deprecated in Iris v1.9
+    import cf_units
+    args = {'attributes': _filter_attrs(attrs, iris_forbidden_keys)}
+    args.update(_pick_attrs(attrs, ('standard_name', 'long_name',)))
+    unit_args = _pick_attrs(attrs, ('calendar',))
+    if 'units' in attrs:
+        args['units'] = cf_units.Unit(attrs['units'], **unit_args)
+    return args
+
+
 # TODO: Add converting bounds from xarray to Iris and back
 def to_iris(dataarray):
-    """Convert a DataArray into a Iris Cube
+    """ Convert a DataArray into a Iris Cube
     """
     # Iris not a hard dependency
     import iris
@@ -77,26 +98,12 @@ def to_iris(dataarray):
         from iris.fileformats._pyke_rules.compiled_krb.fc_rules_cf_fc \
             import _parse_cell_methods as parse_cell_methods
 
-    # iris.unit is deprecated in Iris v1.9
-    import cf_units
-
-    def check_attrs(attrs, keys):
-        return dict((k, v) for k, v in attrs.items() if k in keys)
-
-    def get_args(attrs):
-        _args = {'attributes': filter_attrs(attrs, iris_forbidden_keys)}
-        _args.update(check_attrs(attrs, ('standard_name', 'long_name',)))
-        _unit_args = check_attrs(coord.attrs, ('calendar',))
-        if 'units' in attrs:
-            _args['units'] = cf_units.Unit(attrs['units'], **_unit_args)
-        return _args
-
     dim_coords = []
     aux_coords = []
 
     for coord_name in dataarray.coords:
         coord = encode(dataarray.coords[coord_name])
-        coord_args = get_args(coord.attrs)
+        coord_args = _get_iris_args(coord.attrs)
         coord_args['var_name'] = coord_name
         axis = None
         if coord.dims:
@@ -108,46 +115,51 @@ def to_iris(dataarray):
             iris_coord = iris.coords.AuxCoord(coord.values, **coord_args)
             aux_coords.append((iris_coord, axis))
 
-    args = get_args(dataarray.attrs)
+    args = _get_iris_args(dataarray.attrs)
     args['var_name'] = dataarray.name
     args['dim_coords_and_dims'] = dim_coords
     args['aux_coords_and_dims'] = aux_coords
     if 'cell_methods' in dataarray.attrs:
-        args['cell_methods'] = \
-            parse_cell_methods(dataarray.name, dataarray.attrs['cell_methods'])
+        args['cell_methods'] = parse_cell_methods(
+            dataarray.name, dataarray.attrs['cell_methods'])
 
     cube = iris.cube.Cube(dataarray.to_masked_array(), **args)
     return cube
 
 
-def from_iris(cube):
-    """Convert a Iris cube into an DataArray
+def _iris_obj_to_attrs(obj):
+    """ Return a dictionary of attrs when given a Iris object
     """
+    attrs = {'standard_name': obj.standard_name,
+             'long_name': obj.long_name}
+    if obj.units.calendar:
+        attrs['calendar'] = obj.units.calendar
+    if obj.units.origin != '1':
+        attrs['units'] = obj.units.origin
+    attrs.update(obj.attributes)
+    return dict((k, v) for k, v in attrs.items() if v is not None)
 
-    def get_attr(_obj):
-        attrs = {'standard_name': _obj.standard_name,
-                 'long_name': _obj.long_name}
-        if _obj.units.calendar:
-            attrs['calendar'] = _obj.units.calendar
-        if _obj.units.origin != '1':
-            attrs['units'] = _obj.units.origin
-        attrs.update(_obj.attributes)
-        return dict((k, v) for k, v in attrs.items() if v is not None)
 
-    def get_cell_methods(cell_methods_obj):
-        _cell_methods = []
-        for cell_method in cell_methods_obj:
-            names = ''.join(['{}: '.format(n) for n in cell_method.coord_names])
-            intervals = ' '.join(['interval: {}'.format(interval)
-                                  for interval in cell_method.intervals])
-            comments = ' '.join(['comment: {}'.format(comment)
-                                 for comment in cell_method.comments])
-            extra = ' '.join([intervals, comments]).strip()
-            if extra:
-                extra = ' ({})'.format(extra)
-            _cell_methods.append(names + cell_method.method + extra)
-        return ' '.join(_cell_methods)
+def _iris_cell_methods_to_str(cell_methods_obj):
+    """ Converts a Iris cell methods into a string
+    """
+    cell_methods = []
+    for cell_method in cell_methods_obj:
+        names = ''.join(['{}: '.format(n) for n in cell_method.coord_names])
+        intervals = ' '.join(['interval: {}'.format(interval)
+                              for interval in cell_method.intervals])
+        comments = ' '.join(['comment: {}'.format(comment)
+                             for comment in cell_method.comments])
+        extra = ' '.join([intervals, comments]).strip()
+        if extra:
+            extra = ' ({})'.format(extra)
+        cell_methods.append(names + cell_method.method + extra)
+    return ' '.join(cell_methods)
 
+
+def from_iris(cube):
+    """ Convert a Iris cube into an DataArray
+    """
     name = cube.var_name
     dims = [dim.var_name for dim in cube.dim_coords]
     if not dims:
@@ -155,7 +167,7 @@ def from_iris(cube):
     coords = OrderedDict()
 
     for coord in cube.coords():
-        coord_attrs = get_attr(coord)
+        coord_attrs = _iris_obj_to_attrs(coord)
         coord_dims = [dims[i] for i in cube.coord_dims(coord)]
         if coord_dims:
             coords[coord.var_name] = (coord_dims, coord.points, coord_attrs)
@@ -163,8 +175,8 @@ def from_iris(cube):
             coords[coord.var_name] = ((),
                                       np.asscalar(coord.points), coord_attrs)
 
-    array_attrs = get_attr(cube)
-    cell_methods = get_cell_methods(cube.cell_methods)
+    array_attrs = _iris_obj_to_attrs(cube)
+    cell_methods = _iris_cell_methods_to_str(cube.cell_methods)
     if cell_methods:
         array_attrs['cell_methods'] = cell_methods
     dataarray = DataArray(cube.data, coords=coords, name=name,
