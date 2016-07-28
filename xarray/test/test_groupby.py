e1cd1dcbe377e6f0f6667eb18141c670ffabf402
Stephan Hoyer <shoyer@gmail.com>
2016-07-28 09:06:34 -0700

    another test

--- xarray/test/test_groupby.py
+++ xarray/test/test_groupby.py
@@ -1,3 +1,5 @@
+import numpy as np
+import xarray as xr
 from xarray.core.groupby import _consolidate_slices
 
 import pytest
@@ -5,16 +7,27 @@ import pytest
 
 def test_consolidate_slices():
 
-  assert _consolidate_slices([slice(3), slice(3, 5)]) == [slice(5)]
-  assert _consolidate_slices([slice(2, 3), slice(3, 6)]) == [slice(2, 6)]
-  assert (_consolidate_slices([slice(2, 3, 1), slice(3, 6, 1)])
+    assert _consolidate_slices([slice(3), slice(3, 5)]) == [slice(5)]
+    assert _consolidate_slices([slice(2, 3), slice(3, 6)]) == [slice(2, 6)]
+    assert (_consolidate_slices([slice(2, 3, 1), slice(3, 6, 1)])
           == [slice(2, 6, 1)])
 
-  slices = [slice(2, 3), slice(5, 6)]
-  assert _consolidate_slices(slices) == slices
+    slices = [slice(2, 3), slice(5, 6)]
+    assert _consolidate_slices(slices) == slices
 
-  with pytest.raises(ValueError):
-    _consolidate_slices([slice(3), 4])
+    with pytest.raises(ValueError):
+        _consolidate_slices([slice(3), 4])
 
+
+def test_multi_index_groupby():
+    # regression test for GH873
+    ds = xr.Dataset({'foo': (('x', 'y'), np.random.randn(3, 4))},
+                    {'x': ['a', 'b', 'c'], 'y': [1, 2, 3, 4]})
+    doubled = 2 * ds
+    group_doubled = (ds.stack(space=['x', 'y'])
+                     .groupby('space')
+                     .apply(lambda x: 2 * x)
+                     .unstack('space'))
+    assert doubled.equals(group_doubled)
 
 # TODO: move other groupby tests from test_dataset and test_dataarray over here
