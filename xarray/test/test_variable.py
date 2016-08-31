add wrong shape test to Variable.notnull_equals

xarray/test/test_variable.py
@@ -570,11 +570,14 @@ class TestVariable(TestCase, VariableSubclassTestCases):
 
         self.assertFalse(v1.notnull_equals(None))
 
-        v4 = Variable(('x'), [np.nan, 1, 3, np.nan])
+        v3 = Variable(('x'), [np.nan, 1, 3, np.nan])
+        self.assertFalse(v3.notnull_equals(v1))
+
+        v4 = Variable(('y'), [np.nan, 2, 3, np.nan])
         self.assertFalse(v4.notnull_equals(v1))
 
-        v3 = Variable(('y'), [np.nan, 2, 3, np.nan])
-        self.assertFalse(v3.notnull_equals(v1))
+        v5 = Variable(('x', 'y'), [[1, 2], [np.nan, np.nan]])
+        self.assertFalse(v1.notnull_equals(v5))
 
         d = np.array([1, 2, np.nan, np.nan])
         self.assertFalse(v1.notnull_equals(d))