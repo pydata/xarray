9d823dd061aa4b40401216a55ea29183036d0ff7
Marco Zuehlke <Marco.Zuehlke@brockmann-consult.de>
2016-09-07 09:59:33 +0200

    use pd.isnull instead of np.isnan

--- xarray/core/utils.py
+++ xarray/core/utils.py
@@ -96,16 +96,10 @@ def equivalent(first, second):
     """Compare two objects for equivalence (identity or equality), using
     array_equiv if either object is an ndarray
     """
-    def both_nan(first, second):
-        try:
-            return np.isnan(first) and np.isnan(second)
-        except TypeError:
-            return False
-
     if isinstance(first, np.ndarray) or isinstance(second, np.ndarray):
         return ops.array_equiv(first, second)
     else:
-        return first is second or first == second or both_nan(first, second)
+        return first is second or first == second or (pd.isnull(first) and pd.isnull(second))
 
 
 def peek_at(iterable):
