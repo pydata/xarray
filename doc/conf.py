988a415340c49891e0cc2ef94b0cc6e46f41d8f7
Fabien Maussion <fabien.maussion@uibk.ac.at>
2017-01-19 15:51:07 +0100

    hopefully final change

--- doc/conf.py
+++ doc/conf.py
@@ -211,11 +211,12 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# attempt to fix https://github.com/ipython/ipython/issues/8733
+# Sometimes the savefig directory doesn't exist and needs to be created
+# https://github.com/ipython/ipython/issues/8733
 ipython_savefig_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                    '_build','html','_static')
-# if not os.path.exists(ipython_savefig_dir):
-#     os.makedirs(ipython_savefig_dir)
+if not os.path.exists(ipython_savefig_dir):
+    os.makedirs(ipython_savefig_dir)
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
