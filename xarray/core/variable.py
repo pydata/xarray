c2d31fd56eecace10b103a147b0408fb856c542f
Joe Hamman <jhamman1@uw.edu>
2017-01-21 10:18:53 -0800

    require numpy version 1.10 or later for quantile

--- xarray/core/variable.py
+++ xarray/core/variable.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 from collections import defaultdict
 import functools
 import itertools
+from distutils.version import LooseVersion
 
 import numpy as np
 import pandas as pd
@@ -1072,6 +1073,9 @@ class Variable(common.AbstractArray, utils.NdimSizeLenMixin):
             TypeError("quantile does not work for arrays stored as dask "
                       "arrays. Load the data via .compute() or .load() prior "
                       "to calling this method.")
+        if LooseVersion(np.__version__) < LooseVersion('1.10.0'):
+            raise NotImplementedError(
+                'quantile requres numpy version 1.10.0 or later')
 
         q = np.asarray(q, dtype=np.float64)
 
