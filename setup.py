d759d84b010bdb4a2a2a09dddc7fd5f72f157674
Stephan Hoyer <shoyer@gmail.com>
2017-01-21 16:12:54 -0800

    add python3.6 to supported versions in setup.py

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Scientific/Engineering',
 ]
 
