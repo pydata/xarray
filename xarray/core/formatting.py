48f9da9e6f29f86b5f67cd8c15032432453d0eb8
Stephan Hoyer <shoyer@gmail.com>
2017-01-25 20:11:42 -0800

    Retrigger Travis-CI build

--- xarray/core/formatting.py
+++ xarray/core/formatting.py
@@ -90,7 +90,7 @@ def first_n_items(x, n_desired):
 
 
 def last_item(x):
-    """Returns the last item of an array, as a list of length zero or one."""
+    """Returns the last item of an array in a list or an empty list."""
     if x.size == 0:
         # work around for https://github.com/numpy/numpy/issues/5195
         return []
