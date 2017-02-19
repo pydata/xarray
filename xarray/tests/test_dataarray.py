0e8a0462f4c034aabc5c553de86ebc488e0c745e
fujiisoup <fujiisoup@gmail.com>
2017-02-19 20:56:46 +0900

    Restoring dim order in rolling.reduce(). For #1125

--- xarray/tests/test_dataarray.py
+++ xarray/tests/test_dataarray.py
@@ -2408,8 +2408,8 @@ class TestDataArray(TestCase):
 def da(request):
     if request.param == 1:
         times = pd.date_range('2000-01-01', freq='1D', periods=21)
-        values = np.random.random((21, 4))
-        da = DataArray(values, dims=('time', 'x'))
+        values = np.random.random((3,21, 4))
+        da = DataArray(values, dims=('a','time', 'x'))
         da['time'] = times
         return da
 
@@ -2428,13 +2428,12 @@ def test_rolling_iter(da):
     for i, (label, window_da) in enumerate(rolling_obj):
         assert label == da['time'].isel(time=i)
 
-
 def test_rolling_properties(da):
     pytest.importorskip('bottleneck', minversion='1.0')
 
     rolling_obj = da.rolling(time=4)
 
-    assert rolling_obj._axis_num == 0
+    assert rolling_obj._axis_num == 1
 
     # catching invalid args
     with pytest.raises(ValueError) as exception:
@@ -2464,7 +2463,7 @@ def test_rolling_wrapped_bottleneck(da, name, center, min_periods):
 
     func_name = 'move_{0}'.format(name)
     actual = getattr(rolling_obj, name)()
-    expected = getattr(bn, func_name)(da.values, window=7, axis=0, min_count=min_periods)
+    expected = getattr(bn, func_name)(da.values, window=7, axis=1, min_count=min_periods)
     assert_array_equal(actual.values, expected)
 
     # Test center
@@ -2517,3 +2516,4 @@ def test_rolling_reduce(da, center, min_periods, window, name):
     actual = rolling_obj.reduce(getattr(np, 'nan%s' % name))
     expected = getattr(rolling_obj, name)()
     assert_allclose(actual, expected)
+    assert(da.dims == expected.dims)
