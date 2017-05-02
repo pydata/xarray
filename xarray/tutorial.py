b1dabfe3b36bbacb5a2deeea6856cf81166f7f08
Matthew Gidden <matthew.gidden@gmail.com>
2017-05-02 06:47:17 -0500

    dl tutorial files to tmp directory, then move them once successful
    closes #1392

--- xarray/tutorial.py
+++ xarray/tutorial.py
@@ -10,6 +10,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os as _os
+import shutil as _shutil
 
 from .backends.api import open_dataset as _open_dataset
 from .core.pycompat import urlretrieve as _urlretrieve
@@ -42,8 +43,10 @@ def load_dataset(name, cache=True, cache_dir=_default_cache_dir,
 
     """
     longdir = _os.path.expanduser(cache_dir)
+    tmpdir = _os.sep.join((longdir, '.tmp'))
     fullname = name + '.nc'
     localfile = _os.sep.join((longdir, fullname))
+    tmpfile = _os.sep.join((longdir, fullname))
 
     if not _os.path.exists(localfile):
 
@@ -51,9 +54,16 @@ def load_dataset(name, cache=True, cache_dir=_default_cache_dir,
         # May want to add an option to remove it.
         if not _os.path.isdir(longdir):
             _os.mkdir(longdir)
+        if not _os.path.isdir(tmpdir):
+            _os.mkdir(tmpdir)
 
         url = '/'.join((github_url, 'raw', 'master', fullname))
-        _urlretrieve(url, localfile)
+        _urlretrieve(url, tmpfile)
+
+        if not _os.path.exists(tmpfile):
+            raise ValueError('File could not be downloaded, please try again')
+
+        _shutil.move(tmpfile, localfile)
 
     ds = _open_dataset(localfile, **kws).load()
 
