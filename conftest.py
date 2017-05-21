400762c0f9c80b093285f2886044b1acb1f7e2c3
Matthew Gidden <matthew.gidden@gmail.com>
2017-05-21 17:21:33 +0200

    adding tutorial dataset test, adds conditional --run-network-tests flag to pytest cli

--- conftest.py
+++ conftest.py
@@ -5,3 +5,5 @@ def pytest_addoption(parser):
     """Add command-line flags for pytest."""
     parser.addoption("--run-flaky", action="store_true",
                      help="runs flaky tests")
+    parser.addoption("--run-network-tests", action="store_true",
+                     help="runs tests requiring a network connection")
--- xarray/tests/test_tutorial.py
+++ xarray/tests/test_tutorial.py
@@ -1,23 +1,26 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 import os
+import pytest
 
 from xarray import tutorial, DataArray
 from xarray.core.pycompat import suppress
 
-from . import TestCase, unittest
+from . import TestCase, network
 
 
-@unittest.skip('TODO: make this conditional on network availability')
-class Test_load_dataset(TestCase):
+@network
+class TestLoadDataset(TestCase):
 
     def setUp(self):
         self.testfile = 'tiny'
         self.testfilepath = os.path.expanduser(os.sep.join(
             ('~', '.xarray_tutorial_data', self.testfile)))
         with suppress(OSError):
-            os.remove(self.testfilepath)
+            os.remove('{}.nc'.format(self.testfilepath))
+            os.remove('{}.md5'.format(self.testfilepath))
 
     def test_download_from_github(self):
         ds = tutorial.load_dataset(self.testfile)
