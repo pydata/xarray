42999571a5e8e23289028968ae4e308ef5ec58c0
Fabien Maussion <fabien.maussion@uibk.ac.at>
2017-06-01 11:52:04 +0200

    Change example to use an accessor

--- doc/gallery/plot_rasterio.py
+++ doc/gallery/plot_rasterio.py
@@ -6,7 +6,10 @@
 Parsing rasterio's geocoordinates
 =================================
 
-Convert cartesian coordinates into 2D longitudes and latitudes
+
+The example illustrates how to use an accessor (see :ref:`internals.accessors`)
+to  convert a projection's cartesian coordinates into 2D longitudes and
+latitudes.
 
 These new coordinates might be handy for plotting and indexing, but it should
 be kept in mind that a grid which is regular in projection coordinates will
@@ -22,6 +25,32 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 from rasterio.warp import transform
 
+
+# Define the accessor
+@xr.register_dataarray_accessor('rasterio')
+class RasterioAccessor(object):
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def add_lonlat_coords(self):
+        """Compute the lon/lat coordinates out of the dataset's crs.
+
+        This adds two non-dimension coordinates ('lon' and 'lat') to the
+        original dataarray.
+        """
+
+        ny, nx = len(self._obj['y']), len(self._obj['x'])
+        x, y = np.meshgrid(self._obj['x'], self._obj['y'])
+
+        # Rasterio works with 1D arrays
+        lon, lat = transform(self._obj.crs, {'init': 'EPSG:4326'},
+                             x.flatten(), y.flatten())
+        lon = np.asarray(lon).reshape((ny, nx))
+        lat = np.asarray(lat).reshape((ny, nx))
+        self._obj.coords['lon'] = (('y', 'x'), lon)
+        self._obj.coords['lat'] = (('y', 'x'), lat)
+
+
 # Download the file from rasterio's repository
 url = 'https://github.com/mapbox/rasterio/raw/master/tests/data/RGB.byte.tif'
 urllib.request.urlretrieve(url, 'RGB.byte.tif')
@@ -29,28 +58,16 @@ urllib.request.urlretrieve(url, 'RGB.byte.tif')
 # Read the data
 rioda = xr.open_rasterio('RGB.byte.tif')
 
-# Compute the lons and lats using rasterio
-ny, nx = len(rioda['y']), len(rioda['x'])
-x, y = np.meshgrid(rioda['x'], rioda['y'])
-
-# Rasterio works with 1D arrays
-lon, lat = transform(rioda.crs, {'init': 'EPSG:4326'},
-                     x.flatten(), y.flatten())
-lon = np.asarray(lon).reshape((ny, nx))
-lat = np.asarray(lat).reshape((ny, nx))
-
-# Convert the DataArray to a dataset and set them as non-dimension coordinates
-riods = rioda.to_dataset(name='img')
-riods.coords['lon'] = (('y', 'x'), lon)
-riods.coords['lat'] = (('y', 'x'), lat)
+# Compute the coordinates
+rioda.rasterio.add_lonlat_coords()
 
 # Compute a greyscale out of the rgb image
-riods['greyscale'] = riods.img.mean(dim='band')
+greyscale = rioda.mean(dim='band')
 
 # Plot on a map
 ax = plt.subplot(projection=ccrs.PlateCarree())
-riods.greyscale.plot(ax=ax, x='lon', y='lat', transform=ccrs.PlateCarree(),
-                     cmap='Greys_r', add_colorbar=False)
+greyscale.plot(ax=ax, x='lon', y='lat', transform=ccrs.PlateCarree(),
+               cmap='Greys_r', add_colorbar=False)
 ax.coastlines('10m', color='r')
 plt.show()
 
