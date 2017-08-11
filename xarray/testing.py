53f80b347b345bd1b2d133162ebce8e6049c3dcd
Benoit Bovy <benbovy@gmail.com>
2017-08-11 11:15:26 +0200

    more detailed AssertionError message for assert_identical

--- xarray/testing.py
+++ xarray/testing.py
@@ -1,6 +1,8 @@
 """Testing functions exposed to the user API"""
 from __future__ import absolute_import, division, print_function
 
+from io import StringIO
+
 import numpy as np
 
 from xarray.core import duck_array_ops
@@ -23,6 +25,12 @@ def _data_allclose_or_equiv(arr1, arr2, rtol=1e-05, atol=1e-08,
     else:
         return duck_array_ops.allclose_or_equiv(
             arr1, arr2, rtol=rtol, atol=atol)
+
+
+def _get_info_as_str(dataset):
+    buf = StringIO()
+    dataset.info(buf=buf)
+    return buf.getvalue()
 
 
 def assert_equal(a, b):
@@ -80,7 +88,10 @@ def assert_identical(a, b):
         assert a.name == b.name
         assert_identical(a._to_temp_dataset(), b._to_temp_dataset())
     elif isinstance(a, (xr.Dataset, xr.Variable)):
-        assert a.identical(b), '{}\n{}'.format(a, b)
+        assert a.identical(b), (
+            '{}\n{}\n---\n{}\n{}'
+            .format(a, b, _get_info_as_str(a), _get_info_as_str(b))
+        )
     else:
         raise TypeError('{} not supported by assertion comparison'
                         .format(type(a)))
