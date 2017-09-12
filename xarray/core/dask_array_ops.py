f1819dbe1224cd36eb63df158fc42e1830e23b5e
Joe Hamman <jhamman1@uw.edu>
2017-09-11 23:11:20 -0700

    cleanup

--- xarray/core/dask_array_ops.py
+++ xarray/core/dask_array_ops.py
@@ -24,20 +24,3 @@ def dask_rolling_wrapper(moving_func, a, window, min_count=None, axis=-1):
     # trim array
     result = da.ghost.trim_internal(out, depth)
     return result
-
-
-def dask_rolling_wrapper_without_min_count(moving_func, a, window, axis=-1):
-    '''wrapper to apply bottleneck moving window funcs on dask arrays'''
-    # inputs for ghost
-    if axis < 0:
-        axis = a.ndim + axis
-    depth = {d: 0 for d in range(a.ndim)}
-    depth[axis] = window - 1
-    boundary = {d: np.nan for d in range(a.ndim)}
-    # create ghosted arrays
-    ag = da.ghost.ghost(a, depth=depth, boundary=boundary)
-    # apply rolling func
-    out = ag.map_blocks(moving_func, window, axis=axis, dtype=a.dtype)
-    # trim array
-    result = da.ghost.trim_internal(out, depth)
-    return result
