993992def9afc075f52bedc2423bca03ac744672
Joe Hamman <jhamman1@uw.edu>
2017-10-04 15:57:38 -0700

    update workaround comment

--- xarray/tests/__init__.py
+++ xarray/tests/__init__.py
@@ -36,7 +36,10 @@ def _importorskip(modname, minversion=None):
                 raise ImportError('Minimum version not satisfied')
     except ImportError:
         has = False
-    # TODO: use pytest skip
+    # TODO: use pytest.skipif instead of unittest.skipUnless
+    # Using `unittest.skipUnless` is a temporary workaround for pytest#568,
+    # wherein class decorators stain inherited classes.
+    # xref: xarray#1531, implemented in xarray #1557.
     func = unittest.skipUnless(has, reason='requires {}'.format(modname))
     return has, func
 
