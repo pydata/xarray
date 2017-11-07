Bugfix in broadcast_indexes (#1693)
* A bug fix: wrong Indexer type is returned in `Variable._broadcast_indexes`

* Added a high-level test in `test_backends.py`

* Skip orthogonal_isel for PyNio backend.

* Raises NotImplementedError with indexing PyNio backend

* Remove unnecessary indexing type check.

* Fix typo

* Bugfix in LazilyIndexingArray and DaskIndexingAdapter

* np.int -> np.integer

* Update comment

* Typo in comments

* Improve tests

xarray/core/variable.py
@@ -290,7 +290,7 @@ class Variable(common.AbstractArray, utils.NdimSizeLenMixin):
     def _in_memory(self):
         return (isinstance(self._data, (np.ndarray, np.number, PandasIndexAdapter)) or
                 (isinstance(self._data, indexing.MemoryCachedArray) and
-                 isinstance(self._data.array, np.ndarray)))
+                 isinstance(self._data.array, indexing.NumpyIndexingAdapter)))
 
     @property
     def data(self):
@@ -488,9 +488,8 @@ class Variable(common.AbstractArray, utils.NdimSizeLenMixin):
                 if len(k.dims) > 1:
                     return self._broadcast_indexes_vectorized(key)
                 dims.append(k.dims[0])
-            if not isinstance(k, integer_types):
+            elif not isinstance(k, integer_types):
                 dims.append(d)
-
         if len(set(dims)) == len(dims):
             return self._broadcast_indexes_outer(key)
 

xarray/tests/test_indexing.py
@@ -11,6 +11,7 @@ import pandas as pd
 from xarray import Dataset, DataArray, Variable
 from xarray.core import indexing
 from xarray.core import nputils
+from xarray.core.pycompat import integer_types
 from . import TestCase, ReturnItem, raises_regex
 
 
@@ -169,6 +170,16 @@ class TestLazyArray(TestCase):
                         self.assertArrayEqual(expected, actual)
                         assert isinstance(actual._data,
                                           indexing.LazilyIndexedArray)
+
+                        # make sure actual.key is appropriate type
+                        if all(isinstance(k, integer_types + (slice, ))
+                               for k in v_lazy._data.key):
+                            assert isinstance(v_lazy._data.key,
+                                              indexing.BasicIndexer)
+                        else:
+                            assert isinstance(v_lazy._data.key,
+                                              indexing.OuterIndexer)
+
         # test sequentially applied indexers
         indexers = [(3, 2), (I[:], 0), (I[:2], -1), (I[:4], [0]), ([4, 5], 0),
                     ([0, 1, 2], [0, 1]), ([0, 3, 5], I[:2])]
@@ -211,7 +222,7 @@ class TestMemoryCachedArray(TestCase):
         original = indexing.LazilyIndexedArray(np.arange(10))
         wrapped = indexing.MemoryCachedArray(original)
         self.assertArrayEqual(wrapped, np.arange(10))
-        self.assertIsInstance(wrapped.array, np.ndarray)
+        self.assertIsInstance(wrapped.array, indexing.NumpyIndexingAdapter)
 
     def test_sub_array(self):
         original = indexing.LazilyIndexedArray(np.arange(10))
@@ -219,7 +230,7 @@ class TestMemoryCachedArray(TestCase):
         child = wrapped[:5]
         self.assertIsInstance(child, indexing.MemoryCachedArray)
         self.assertArrayEqual(child, np.arange(5))
-        self.assertIsInstance(child.array, np.ndarray)
+        self.assertIsInstance(child.array, indexing.NumpyIndexingAdapter)
         self.assertIsInstance(wrapped.array, indexing.LazilyIndexedArray)
 
     def test_setitem(self):

xarray/tests/test_variable.py
@@ -16,8 +16,10 @@ from xarray import Variable, IndexVariable, Coordinate, Dataset
 from xarray.core import indexing
 from xarray.core.variable import as_variable, as_compatible_data
 from xarray.core.indexing import (PandasIndexAdapter, LazilyIndexedArray,
-                                  NumpyIndexingAdapter, CopyOnWriteArray,
-                                  MemoryCachedArray, DaskIndexingAdapter)
+                                  BasicIndexer, OuterIndexer,
+                                  VectorizedIndexer, NumpyIndexingAdapter,
+                                  CopyOnWriteArray, MemoryCachedArray,
+                                  DaskIndexingAdapter)
 from xarray.core.pycompat import PY3, OrderedDict
 from xarray.core.common import full_like, zeros_like, ones_like
 from xarray.core.utils import NDArrayMixin
@@ -578,6 +580,9 @@ class VariableSubclassTestCases(object):
         v_new = v[np.array([0], dtype="uint64")]
         self.assertArrayEqual(v_new, v_data[[0], :])
 
+        v_new = v[np.uint64(0)]
+        self.assertArrayEqual(v_new, v_data[0, :])
+
     def test_getitem_0d_array(self):
         # make sure 0d-np.array can be used as an indexer
         v = self.cls(['x'], [0, 1, 2])
@@ -940,6 +945,39 @@ class TestVariable(TestCase, VariableSubclassTestCases):
         vind = Variable(('a', 'b'), [[0, 2], [1, 3]])
         _, ind, _ = v._broadcast_indexes((vind, 3))
         assert type(ind) == indexing.VectorizedIndexer
+
+    def test_indexer_type(self):
+        # GH:issue:1688. Wrong indexer type induces NotImplementedError
+        data = np.random.random((10, 11))
+        v = Variable(['x', 'y'], data)
+
+        def assert_indexer_type(key, object_type):
+            dims, index_tuple, new_order = v._broadcast_indexes(key)
+            assert isinstance(index_tuple, object_type)
+
+        # should return BasicIndexer
+        assert_indexer_type((0, 1), BasicIndexer)
+        assert_indexer_type((0, slice(None, None)), BasicIndexer)
+        assert_indexer_type((Variable([], 3), slice(None, None)), BasicIndexer)
+        assert_indexer_type((Variable([], 3), (Variable([], 6))), BasicIndexer)
+
+        # should return OuterIndexer
+        assert_indexer_type(([0, 1], 1), OuterIndexer)
+        assert_indexer_type(([0, 1], [1, 2]), OuterIndexer)
+        assert_indexer_type((Variable(('x'), [0, 1]), 1), OuterIndexer)
+        assert_indexer_type((Variable(('x'), [0, 1]), slice(None, None)),
+                            OuterIndexer)
+        assert_indexer_type((Variable(('x'), [0, 1]), Variable(('y'), [0, 1])),
+                            OuterIndexer)
+
+        # should return VectorizedIndexer
+        assert_indexer_type((Variable(('y'), [0, 1]), [0, 1]),
+                            VectorizedIndexer)
+        assert_indexer_type((Variable(('z'), [0, 1]), Variable(('z'), [0, 1])),
+                            VectorizedIndexer)
+        assert_indexer_type((Variable(('a', 'b'), [[0, 1], [1, 2]]),
+                             Variable(('a', 'b'), [[0, 1], [1, 2]])),
+                            VectorizedIndexer)
 
     def test_items(self):
         data = np.random.random((10, 11))