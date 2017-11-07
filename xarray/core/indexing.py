Bugfix in broadcast_indexes (#1693)
* A bug fix: wrong Indexer type is returned in `Variable._broadcast_indexes`

* Added a high-level test in `test_backends.py`

* Skip orthogonal_isel for PyNio backend.

* Raises NotImplementedError with indexing PyNio backend

* Remove unnecessary indexing type check.

* Fix typo

* Bugfix in LazilyIndexingArray and DaskIndexingAdapter

* np.int -> np.integer

* Update comment

* Typo in comments

* Improve tests

xarray/core/indexing.py
@@ -322,7 +322,7 @@ class LazilyIndexedArray(utils.NDArrayMixin, NDArrayIndexable):
         """
         # We need to avoid doubly wrapping.
         if isinstance(array, type(self)):
-            self.array = array.array
+            self.array = as_indexable(array.array)
             self.key = array.key
             if key is not None:
                 self.key = self._updated_key(key)
@@ -330,8 +330,8 @@ class LazilyIndexedArray(utils.NDArrayMixin, NDArrayIndexable):
         else:
             if key is None:
                 key = (slice(None),) * array.ndim
-                key = OuterIndexer(key)
-            self.array = array
+                key = BasicIndexer(key)
+            self.array = as_indexable(array)
             self.key = key
 
     def _updated_key(self, new_key):
@@ -347,6 +347,8 @@ class LazilyIndexedArray(utils.NDArrayMixin, NDArrayIndexable):
                 key.append(k)
             else:
                 key.append(_index_indexer_1d(k, next(new_key), size))
+        if all(isinstance(k, integer_types + (slice, )) for k in key):
+            return BasicIndexer(key)
         return OuterIndexer(key)
 
     @property
@@ -390,7 +392,7 @@ class CopyOnWriteArray(utils.NDArrayMixin, NDArrayIndexable):
 
     def _ensure_copied(self):
         if not self._copied:
-            self.array = np.array(self.array)
+            self.array = as_indexable(np.array(self.array))
             self._copied = True
 
     def __array__(self, dtype=None):
@@ -409,8 +411,8 @@ class MemoryCachedArray(utils.NDArrayMixin, NDArrayIndexable):
         self.array = _wrap_numpy_scalars(as_indexable(array))
 
     def _ensure_cached(self):
-        if not isinstance(self.array, np.ndarray):
-            self.array = np.asarray(self.array)
+        if not isinstance(self.array, NumpyIndexingAdapter):
+            self.array = NumpyIndexingAdapter(np.asarray(self.array))
 
     def __array__(self, dtype=None):
         self._ensure_cached()
@@ -521,8 +523,8 @@ class DaskIndexingAdapter(utils.NDArrayMixin, NDArrayIndexable):
     """Wrap a dask array to support xarray-style indexing.
     """
     def __init__(self, array):
-        """ This adapter is usually called in Variable.__getitem__ with
-        array=Variable._broadcast_indexes
+        """ This adapter is created in Variable.__getitem__ in
+        Variable._broadcast_indexes.
         """
         self.array = array
 
@@ -531,7 +533,8 @@ class DaskIndexingAdapter(utils.NDArrayMixin, NDArrayIndexable):
             # workaround for uint64 indexer (GH:1406)
             # TODO remove here after next dask release (0.15.3)
             return tuple([k.astype(int) if isinstance(k, np.ndarray)
-                          else k for k in key])
+                          else int(k) if isinstance(k, np.integer) else k
+                          for k in key])
 
         if isinstance(key, BasicIndexer):
             return self.array[to_int_tuple(key)]

xarray/tests/test_backends.py
@@ -22,7 +22,8 @@ from xarray import (Dataset, DataArray, open_dataset, open_dataarray,
 from xarray.backends.common import robust_getitem
 from xarray.backends.netCDF4_ import _extract_nc4_variable_encoding
 from xarray.core import indexing
-from xarray.core.pycompat import iteritems, PY2, ExitStack, basestring
+from xarray.core.pycompat import (iteritems, PY2, ExitStack, basestring,
+                                  dask_array_type)
 
 from . import (TestCase, requires_scipy, requires_netCDF4, requires_pydap,
                requires_scipy_or_netCDF4, requires_dask, requires_h5netcdf,
@@ -421,6 +422,51 @@ class DatasetIOTestCases(object):
     def test_vectorized_indexing(self):
         # This test should be overwritten if vindex is supported
         self._test_vectorized_indexing(vindex_support=False)
+
+    def test_isel_dataarray(self):
+        # Make sure isel works lazily. GH:issue:1688
+        in_memory = create_test_data()
+        with self.roundtrip(in_memory) as on_disk:
+            expected = in_memory.isel(dim2=in_memory['dim2'] < 3)
+            actual = on_disk.isel(dim2=on_disk['dim2'] < 3)
+            self.assertDatasetIdentical(expected, actual)
+
+    def validate_array_type(self, ds):
+        # Make sure that only NumpyIndexingAdapter stores a bare np.ndarray.
+        def find_and_validate_array(obj):
+            # recursively called function. obj: array or array wrapper.
+            if hasattr(obj, 'array'):
+                if isinstance(obj.array, indexing.NDArrayIndexable):
+                    find_and_validate_array(obj.array)
+                else:
+                    if isinstance(obj.array, np.ndarray):
+                        assert isinstance(obj, indexing.NumpyIndexingAdapter)
+                    elif isinstance(obj.array, dask_array_type):
+                        assert isinstance(obj, indexing.DaskIndexingAdapter)
+                    elif isinstance(obj.array, pd.Index):
+                        assert isinstance(obj, indexing.PandasIndexAdapter)
+                    else:
+                        raise TypeError('{} is wrapped by {}'.format(
+                                            type(obj.array), type(obj)))
+
+        for k, v in ds.variables.items():
+            find_and_validate_array(v._data)
+
+    def test_array_type_after_indexing(self):
+        in_memory = create_test_data()
+        with self.roundtrip(in_memory) as on_disk:
+            self.validate_array_type(on_disk)
+            indexers = {'dim1': [1, 2, 0], 'dim2': [3, 2, 0, 3],
+                        'dim3': np.arange(5)}
+            expected = in_memory.isel(**indexers)
+            actual = on_disk.isel(**indexers)
+            assert_identical(expected, actual)
+            self.validate_array_type(actual)
+            # do it twice, to make sure we're switched from orthogonal -> numpy
+            # when we cached the values
+            actual = on_disk.isel(**indexers)
+            assert_identical(expected, actual)
+            self.validate_array_type(actual)
 
 
 class CFEncodedDataTest(DatasetIOTestCases):
@@ -1192,6 +1238,10 @@ class H5NetCDFDataTest(BaseNetCDF4Test, TestCase):
         # doesn't work for h5py (without using dask as an intermediate layer)
         pass
 
+    def test_array_type_after_indexing(self):
+        # pynio also does not support list-like indexing
+        pass
+
     def test_complex(self):
         expected = Dataset({'x': ('y', np.ones(5) + 1j * np.ones(5))})
         with self.roundtrip(expected) as actual:
@@ -1701,6 +1751,15 @@ class TestPyNio(CFEncodedDataTest, NetCDF3Only, TestCase):
         pass
 
     def test_orthogonal_indexing(self):
+        # pynio also does not support list-like indexing
+        with raises_regex(NotImplementedError, 'Nio backend does not '):
+            super(TestPyNio, self).test_orthogonal_indexing()
+
+    def test_isel_dataarray(self):
+        with raises_regex(NotImplementedError, 'Nio backend does not '):
+            super(TestPyNio, self).test_isel_dataarray()
+
+    def test_array_type_after_indexing(self):
         # pynio also does not support list-like indexing
         pass
 