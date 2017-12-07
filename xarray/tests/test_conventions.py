a7313caeef3f6cf9e19cf0c23cb06868560d8874
Mike Neish <neishm@atmosp.physics.utoronto.ca>
2017-12-07 15:47:25 -0500

    Add whitespace and wrap long lines to pass flake8 tests.

--- xarray/tests/test_conventions.py
+++ xarray/tests/test_conventions.py
@@ -619,31 +619,34 @@ class TestEncodeCFVariable(TestCase):
         # regression test for GH1763
         # Set up test case with coordinates that have overlapping (but not
         # identical) dimensions.
-        zeros1 = np.zeros((1,5,3))
-        zeros2 = np.zeros((1,6,3))
-        zeros3 = np.zeros((1,5,4))
+        zeros1 = np.zeros((1, 5, 3))
+        zeros2 = np.zeros((1, 6, 3))
+        zeros3 = np.zeros((1, 5, 4))
         orig = Dataset({
-            'lon1': (['x1','y1'], zeros1.squeeze(0), {}),
-            'lon2': (['x2','y1'], zeros2.squeeze(0), {}),
-            'lon3': (['x1','y2'], zeros3.squeeze(0), {}),
-            'lat1': (['x1','y1'], zeros1.squeeze(0), {}),
-            'lat2': (['x2','y1'], zeros2.squeeze(0), {}),
-            'lat3': (['x1','y2'], zeros3.squeeze(0), {}),
-            'foo1': (['time','x1','y1'], zeros1, {'coordinates': 'lon1 lat1'}),
-            'foo2': (['time','x2','y1'], zeros2, {'coordinates': 'lon2 lat2'}),
-            'foo3': (['time','x1','y2'], zeros3, {'coordinates': 'lon3 lat3'}),
+            'lon1': (['x1', 'y1'], zeros1.squeeze(0), {}),
+            'lon2': (['x2', 'y1'], zeros2.squeeze(0), {}),
+            'lon3': (['x1', 'y2'], zeros3.squeeze(0), {}),
+            'lat1': (['x1', 'y1'], zeros1.squeeze(0), {}),
+            'lat2': (['x2', 'y1'], zeros2.squeeze(0), {}),
+            'lat3': (['x1', 'y2'], zeros3.squeeze(0), {}),
+            'foo1': (['time', 'x1', 'y1'], zeros1,
+                     {'coordinates': 'lon1 lat1'}),
+            'foo2': (['time', 'x2', 'y1'], zeros2,
+                     {'coordinates': 'lon2 lat2'}),
+            'foo3': (['time', 'x1', 'y2'], zeros3,
+                     {'coordinates': 'lon3 lat3'}),
             'time': ('time', [0.], {'units': 'hours since 2017-01-01'}),
         })
         orig = conventions.decode_cf(orig)
         # Encode the coordinates, as they would be in a netCDF output file.
         enc, attrs = conventions.encode_dataset_coordinates(orig)
         # Make sure we have the right coordinates for each variable.
-        foo1_coords = enc['foo1'].attrs.get('coordinates','')
-        foo2_coords = enc['foo2'].attrs.get('coordinates','')
-        foo3_coords = enc['foo3'].attrs.get('coordinates','')
-        assert set(foo1_coords.split()) == set(['time','lat1','lon1'])
-        assert set(foo2_coords.split()) == set(['time','lat2','lon2'])
-        assert set(foo3_coords.split()) == set(['time','lat3','lon3'])
+        foo1_coords = enc['foo1'].attrs.get('coordinates', '')
+        foo2_coords = enc['foo2'].attrs.get('coordinates', '')
+        foo3_coords = enc['foo3'].attrs.get('coordinates', '')
+        assert set(foo1_coords.split()) == set(['time', 'lat1', 'lon1'])
+        assert set(foo2_coords.split()) == set(['time', 'lat2', 'lon2'])
+        assert set(foo3_coords.split()) == set(['time', 'lat3', 'lon3'])
         # Should not have any global coordinates.
         assert 'coordinates' not in attrs
 
