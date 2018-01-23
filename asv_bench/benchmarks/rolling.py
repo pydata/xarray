6461f84ca1740f6991b6dd38a2693dbae4ec4639
Keisuke Fujii <fujiisoup@gmail.com>
2018-01-24 07:50:25 +0900

    Classize benchmark

--- asv_bench/benchmarks/rolling.py
+++ asv_bench/benchmarks/rolling.py
@@ -6,43 +6,48 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 
-from . import randn
-
+from . import randn, requires_dask
 
 nx = 3000
 ny = 2000
 nt = 1000
 window = 20
-ds = xr.Dataset({'var1': (('x', 'y'), randn((nx, ny), frac_nan=0.1)),
-                 'var2': (('x', 't'), randn((nx, nt))),
-                 'var3': (('t', ), randn(nt))},
-                coords={'x': np.arange(nx),
-                        'y': np.linspace(0, 1, ny),
-                        't': pd.date_range('1970-01-01', periods=nt, freq='D'),
-                        'x_coords': ('x', np.linspace(1.1, 2.1, nx))})
 
 
-def time_rolling(func, center):
-    getattr(ds.rolling(x=window, center=center), func)()
+class Rolling(object):
+    def setup(self, *args, **kwargs):
+        self.ds = xr.Dataset(
+            {'var1': (('x', 'y'), randn((nx, ny), frac_nan=0.1)),
+             'var2': (('x', 't'), randn((nx, nt))),
+             'var3': (('t', ), randn(nt))},
+            coords={'x': np.arange(nx),
+                    'y': np.linspace(0, 1, ny),
+                    't': pd.date_range('1970-01-01', periods=nt, freq='D'),
+                    'x_coords': ('x', np.linspace(1.1, 2.1, nx))})
+
+    def time_rolling(self, func, center):
+        getattr(self.ds.rolling(x=window, center=center), func)()
+
+    time_rolling.param_names = ['func', 'center']
+    time_rolling.params = (['mean', 'count'], [True, False])
+
+    def time_rolling_np(self, window_, min_periods):
+        self.ds.rolling(x=window_, center=False,
+                        min_periods=min_periods).reduce(getattr(np, 'nanmean'))
+
+    time_rolling_np.param_names = ['window_', 'min_periods']
+    time_rolling_np.params = ([20, 40], [5, None])
+
+    def time_rolling_to_dataset(self, center, stride):
+        self.ds.rolling(x=window, center=center).to_dataset(
+            'window_dim', stride=stride).mean(dim='window_dim')
+
+    time_rolling_to_dataset.param_names = ['center', 'stride']
+    time_rolling_to_dataset.params = ([True, False], [1, 200])
 
 
-time_rolling.param_names = ['func', 'center']
-time_rolling.params = (['mean', 'count'], [True, False])
-
-
-def time_rolling_np(window_, min_periods):
-    ds.rolling(x=window_, center=False, min_periods=min_periods).reduce(
-        getattr(np, 'nanmean'))
-
-
-time_rolling_np.param_names = ['window_', 'min_periods']
-time_rolling_np.params = ([20, 40], [5, None])
-
-
-def time_rolling_to_dataset(center, stride):
-    ds.rolling(x=window, center=center).to_dataset(
-        'window_dim', stride=stride).mean(dim='window_dim')
-
-
-time_rolling_to_dataset.param_names = ['center', 'stride']
-time_rolling_to_dataset.params = ([True, False], [1, 200])
+class RollingDask(Rolling):
+    def setup(self, *args, **kwargs):
+        requires_dask()
+        super(RollingDask, self).setup(**kwargs)
+        self.ds = self.ds.chunk({'x': 100, 'y': 50, 't': 50})
