ab82f756fb7135bb332233a89118533a1902eba2
Keisuke Fujii <fujiisoup@gmail.com>
2018-02-18 11:43:56 +0900

    typo

--- xarray/tests/test_variable.py
+++ xarray/tests/test_variable.py
@@ -758,7 +758,7 @@ class VariableSubclassTestCases(object):
             assert_array_equal(actual, expected)
 
     def test_rolling_window(self):
-        # Just a working test. See test_nputils fot the algorithm validation
+        # Just a working test. See test_nputils for the algorithm validation
         v = self.cls(['x', 'y', 'z'], np.arange(40*30*2).reshape(40, 30, 2))
         for (d, w) in [('x', 3), ('y', 5)]:
             v_rolling = v.rolling_window(d, w, d + '_window')
