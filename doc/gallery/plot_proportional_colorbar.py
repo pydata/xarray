3136bd57117a498b33f99f89378c69e0b3b23287
stickler-ci <support@stickler-ci.com>
2018-02-21 02:37:17 +0000

    Fixing style errors.

--- doc/gallery/plot_proportional_colorbar.py
+++ doc/gallery/plot_proportional_colorbar.py
@@ -21,7 +21,7 @@ air2d = air.isel(time=500)
 f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(14, 4))
 
 # Different levels
-levels = [200, 210, 220, 230, 240, 250, 260,270,280,290,320]
+levels = [200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 320]
 
 # Plot data
 air2d.plot(ax=ax1, levels=levels)
