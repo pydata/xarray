d1811f061b2e7242ebf951152e3ddf0a99c103a5
Maximilian Roos <m@maxroos.com>
2018-03-27 20:58:09 -0400

    cast single dim tuples to indexes

--- xarray/core/utils.py
+++ xarray/core/utils.py
@@ -49,6 +49,9 @@ def safe_cast_to_index(array):
         index = array
     elif hasattr(array, 'to_index'):
         index = array.to_index()
+    elif isinstance(array, tuple):
+        name, array = array
+        index = pd.Index(array, name=name)
     else:
         kwargs = {}
         if hasattr(array, 'dtype') and array.dtype.kind == 'O':
