pydata/xarray

commit 0cc64a08c672e6361d05acea3fea9f34308b62ed

Drop conflicted coordinate when assignment. (#2087)
* Drop conflicted coordinate when assignment.

* flake8

* Python 2 support. Avoid overwrite variables to be assigned.

* More pythonic

xarray/core/merge.py
@@ -547,6 +547,21 @@ def dataset_merge_method(dataset, other, overwrite_vars, compat, join):
 
 
 def dataset_update_method(dataset, other):
-    """Guts of the Dataset.update method"""
+    """Guts of the Dataset.update method
+
+    This drops a duplicated coordinates from `other` (GH:2068)
+    """
+    from .dataset import Dataset
+    from .dataarray import DataArray
+
+    other = other.copy()
+    for k, obj in other.items():
+        if isinstance(obj, (Dataset, DataArray)):
+            # drop duplicated coordinates
+            coord_names = [c for c in obj.coords
+                           if c not in obj.dims and c in dataset.coords]
+            if coord_names:
+                other[k] = obj.drop(*coord_names)
+
     return merge_core([dataset, other], priority_arg=1,
                       indexes=dataset.indexes)

xarray/tests/test_dataset.py
@@ -2327,6 +2327,45 @@ class TestDataset(TestCase):
         expected = Dataset({'x': ('y', [4, 5, 6])}, {'y': range(3)})
         assert_identical(ds, expected)
 
+    def test_setitem_with_coords(self):
+        # Regression test for GH:2068
+        ds = create_test_data()
+
+        other = DataArray(np.arange(10), dims='dim3',
+                          coords={'numbers': ('dim3', np.arange(10))})
+        expected = ds.copy()
+        expected['var3'] = other.drop('numbers')
+        actual = ds.copy()
+        actual['var3'] = other
+        assert_identical(expected, actual)
+        assert 'numbers' in other  # should not change other
+
+        # with alignment
+        other = ds['var3'].isel(dim3=slice(1, -1))
+        other['numbers'] = ('dim3', np.arange(8))
+        actual = ds.copy()
+        actual['var3'] = other
+        assert 'numbers' in other  # should not change other
+        expected = ds.copy()
+        expected['var3'] = ds['var3'].isel(dim3=slice(1, -1))
+        assert_identical(expected, actual)
+
+        # with non-duplicate coords
+        other = ds['var3'].isel(dim3=slice(1, -1))
+        other['numbers'] = ('dim3', np.arange(8))
+        other['position'] = ('dim3', np.arange(8))
+        actual = ds.copy()
+        actual['var3'] = other
+        assert 'position' in actual
+        assert 'position' in other
+
+        # assigning a coordinate-only dataarray
+        actual = ds.copy()
+        other = actual['numbers']
+        other[0] = 10
+        actual['numbers'] = other
+        assert actual['numbers'][0] == 10
+
     def test_setitem_align_new_indexes(self):
         ds = Dataset({'foo': ('x', [1, 2, 3])}, {'x': [0, 1, 2]})
         ds['bar'] = DataArray([2, 3, 4], [('x', [1, 2, 3])])

commit 9b58997677d7ec6d530d2d943890b35d68e6e7cf

Fix a bug introduced in #2087 (#2100)
* Bugfix introduced by #2087

* flake8

* Remove trailing whitespaces

xarray/core/merge.py
@@ -561,7 +561,7 @@ def dataset_update_method(dataset, other):
             coord_names = [c for c in obj.coords
                            if c not in obj.dims and c in dataset.coords]
             if coord_names:
-                other[k] = obj.drop(*coord_names)
+                other[k] = obj.drop(coord_names)
 
     return merge_core([dataset, other], priority_arg=1,
                       indexes=dataset.indexes)

xarray/tests/test_dataset.py
@@ -2366,6 +2366,13 @@ class TestDataset(TestCase):
         actual['numbers'] = other
         assert actual['numbers'][0] == 10
 
+        # GH: 2099
+        ds = Dataset({'var': ('x', [1, 2, 3])},
+                     coords={'x': [0, 1, 2], 'z1': ('x', [1, 2, 3]),
+                             'z2': ('x', [1, 2, 3])})
+        ds['var'] = ds['var'] * 2
+        assert np.allclose(ds['var'], [2, 4, 6])
+
     def test_setitem_align_new_indexes(self):
         ds = Dataset({'foo': ('x', [1, 2, 3])}, {'x': [0, 1, 2]})
         ds['bar'] = DataArray([2, 3, 4], [('x', [1, 2, 3])])