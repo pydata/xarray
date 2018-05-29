a6ad090ece6ad26c0d55338bfb0df72a5f63ec50
Stephan Hoyer <shoyer@google.com>
2018-05-28 21:40:47 -0700

    oops, we still need to skip that test

--- xarray/tests/test_conventions.py
+++ xarray/tests/test_conventions.py
@@ -273,6 +273,11 @@ class TestCFEncodedDataStore(CFEncodedDataTest, TestCase):
         # only relevant for on-disk file formats
         pass
 
+    def test_encoding_kwarg(self):
+        # we haven't bothered to raise errors yet for unexpected encodings in
+        # this test dummy
+        pass
+
     def test_encoding_kwarg_fixed_width_string(self):
         # CFEncodedInMemoryStore doesn't support explicit string encodings.
         pass
