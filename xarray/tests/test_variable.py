11d735fdc0b7824b199a3aad1c710d035dc85f12
fujiisoup <fujiisoup@gmail.com>
2018-06-18 21:40:00 +0900

    another flake8

--- xarray/tests/test_variable.py
+++ xarray/tests/test_variable.py
@@ -1509,7 +1509,7 @@ class TestVariable(TestCase, VariableSubclassTestCases):
 
         v = Variable('t', pd.date_range('2000-01-01', periods=3))
         v.argmax(skipna=True)
-        
+
         assert_identical(
             v.max(), Variable([], pd.Timestamp('2000-01-03')))
 
