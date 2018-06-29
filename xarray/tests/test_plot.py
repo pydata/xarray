f6ba130dc18b9d65ce82d04932f5baa5158a7c92
Yohai Bar Sinai <6164157+yohai@users.noreply.github.com>
2018-06-29 01:40:52 -0400

    BUG: unnamed args in faceted line plots

--- xarray/tests/test_plot.py
+++ xarray/tests/test_plot.py
@@ -1542,6 +1542,14 @@ class TestFacetedLinePlots(PlotTestCase):
         g = self.darray.plot(row='col', col='row', hue='hue')
         assert g.axes.shape == (len(self.darray.col), len(self.darray.row))
 
+    def test_unnamed_args(self):
+        g = self.darray.plot.line('o--', row='row', col='col', hue='hue')
+        lines = [q for q in g.axes.flat[0].get_children()
+                 if isinstance(q, mpl.lines.Line2D)]
+        # passing '.' as argument should set the marker
+        assert lines[0].get_marker() == 'o'
+        assert lines[0].get_linestyle() == '--'
+
     def test_default_labels(self):
         g = self.darray.plot(row='row', col='col', hue='hue')
         # Rightmost column should be labeled
