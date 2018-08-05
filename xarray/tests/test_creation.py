d4dd9ee0c00223e6324d0e7807b4f4b1b1d8fa8b
Mark Harfouche <mark.harfouche@gmail.com>
2018-08-05 12:36:27 -0400

    Lint (squash again)

--- xarray/tests/test_creation.py
+++ xarray/tests/test_creation.py
@@ -2,10 +2,11 @@ import xarray as xr
 import numpy as np
 import warnings
 
+
 def test_coords_are_dataset():
     # Setup an array with coordinates
     n = np.zeros(3)
-    coords={'x': np.arange(3)}
+    coords = {'x': np.arange(3)}
     c = xr.Dataset(coords=coords)
     with warnings.catch_warnings():
         warnings.filterwarnings("error", "iteration over an xarray.Dataset")
