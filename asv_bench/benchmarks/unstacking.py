bcabf781f45e9e380109e7c8309ba0824025e222
Maximilian Maahn <maximilian.maahn@colorado.edu>
2018-08-14 18:18:27 -0600

    Faster unstack (#2364)
    * Make dataset.unstack faster by skipping reindex if not necessary.
    
    * Remove prints, add comment
    
    * added asv benchmark for unstacking
    
    * Added test
    
    * Simplified test
    
    * Added whats-new entry
    
    * PEP8
    
    * Made asv test faster

---
+++ asv_bench/benchmarks/unstacking.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import xarray as xr
+
+from . import requires_dask
+
+
+class Unstacking(object):
+    def setup(self):
+        data = np.random.RandomState(0).randn(1, 1000, 500)
+        self.ds = xr.DataArray(data).stack(flat_dim=['dim_1', 'dim_2'])
+
+    def time_unstack_fast(self):
+        self.ds.unstack('flat_dim')
+
+    def time_unstack_slow(self):
+        self.ds[:, ::-1].unstack('flat_dim')
+
+
+class UnstackingDask(Unstacking):
+    def setup(self, *args, **kwargs):
+        requires_dask()
+        super(UnstackingDask, self).setup(**kwargs)
+        self.ds = self.ds.chunk({'flat_dim': 50})
