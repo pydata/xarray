07d0fd393d0830acdc629274aa7fb35e96390ff8
NotSqrt <notsqrt@gmail.com>
2018-08-16 22:31:23 +0200

    Improve maybe_promote test

--- xarray/tests/test_dtypes.py
+++ xarray/tests/test_dtypes.py
@@ -53,44 +53,36 @@ def test_inf(obj):
 
 
 @pytest.mark.parametrize("kind, expected", [
-    ('a', (np.dtype('O'), np.nan)),  # dtype('S')
-    ('b', (np.float32, np.nan)),  # dtype('int8')
-    ('B', (np.float32, np.nan)),  # dtype('uint8')
-    ('c', (np.dtype('O'), np.nan)),  # dtype('S1')
-    ('D', (np.complex128, np.complex('nan+nanj'))),  # dtype('complex128')
-    ('d', (np.float64, np.nan)),  # dtype('float64')
-    ('e', (np.float16, np.nan)),  # dtype('float16')
-    ('F', (np.complex64, np.complex('nan+nanj'))),  # dtype('complex64')
-    ('f', (np.float32, np.nan)),  # dtype('float32')
-    ('G', (np.complex256, np.complex('nan+nanj'))),  # dtype('complex256')
-    ('g', (np.float128, np.nan)),  # dtype('float128')
-    ('h', (np.float32, np.nan)),  # dtype('int16')
-    ('H', (np.float32, np.nan)),  # dtype('uint16')
-    ('i', (np.float64, np.nan)),  # dtype('int32')
-    ('I', (np.float64, np.nan)),  # dtype('uint32')
-    ('l', (np.float64, np.nan)),  # dtype('int64')
-    ('L', (np.float64, np.nan)),  # dtype('uint64')
-    ('m', (np.timedelta64, np.timedelta64('NaT'))),  # dtype('<m8')
-    ('M', (np.datetime64, np.datetime64('NaT'))),  # dtype('<M8')
-    ('O', (np.dtype('O'), np.nan)),  # dtype('O')
-    ('p', (np.float64, np.nan)),  # dtype('int64')
-    ('P', (np.float64, np.nan)),  # dtype('uint64')
-    ('q', (np.float64, np.nan)),  # dtype('int64')
-    ('Q', (np.float64, np.nan)),  # dtype('uint64')
-    ('S', (np.dtype('O'), np.nan)),  # dtype('S')
-    ('U', (np.dtype('O'), np.nan)),  # dtype('<U')
-    ('V', (np.dtype('O'), np.nan)),  # dtype('V')
+    ('a', (np.dtype('O'), 'nan')),  # dtype('S')
+    ('b', (np.float32, 'nan')),  # dtype('int8')
+    ('B', (np.float32, 'nan')),  # dtype('uint8')
+    ('c', (np.dtype('O'), 'nan')),  # dtype('S1')
+    ('D', (np.complex128, '(nan+nanj)')),  # dtype('complex128')
+    ('d', (np.float64, 'nan')),  # dtype('float64')
+    ('e', (np.float16, 'nan')),  # dtype('float16')
+    ('F', (np.complex64, '(nan+nanj)')),  # dtype('complex64')
+    ('f', (np.float32, 'nan')),  # dtype('float32')
+    ('h', (np.float32, 'nan')),  # dtype('int16')
+    ('H', (np.float32, 'nan')),  # dtype('uint16')
+    ('i', (np.float64, 'nan')),  # dtype('int32')
+    ('I', (np.float64, 'nan')),  # dtype('uint32')
+    ('l', (np.float64, 'nan')),  # dtype('int64')
+    ('L', (np.float64, 'nan')),  # dtype('uint64')
+    ('m', (np.timedelta64, 'NaT')),  # dtype('<m8')
+    ('M', (np.datetime64, 'NaT')),  # dtype('<M8')
+    ('O', (np.dtype('O'), 'nan')),  # dtype('O')
+    ('p', (np.float64, 'nan')),  # dtype('int64')
+    ('P', (np.float64, 'nan')),  # dtype('uint64')
+    ('q', (np.float64, 'nan')),  # dtype('int64')
+    ('Q', (np.float64, 'nan')),  # dtype('uint64')
+    ('S', (np.dtype('O'), 'nan')),  # dtype('S')
+    ('U', (np.dtype('O'), 'nan')),  # dtype('<U')
+    ('V', (np.dtype('O'), 'nan')),  # dtype('V')
 ])
 def test_maybe_promote(kind, expected):
-    # (np.nan,) == (np.nan,)
-    # but
-    # (np.nan +1j*np.nan,) != (np.nan +1j*np.nan,)
-    # and
-    # "In the future, 'NAT == x' and 'x == NAT' will always be False"
+    # 'g': np.float128 is not tested : not available on all platforms
+    # 'G': np.complex256 is not tested : not available on all platforms
 
     actual = dtypes.maybe_promote(np.dtype(kind))
-    if np.issubdtype(np.dtype(kind), np.complexfloating) or kind in 'mM':
-        assert actual[0] == expected[0]
-        assert str(actual[1]) == str(expected[1])
-    else:
-        assert actual == expected
+    assert actual[0] == expected[0]
+    assert str(actual[1]) == expected[1]
