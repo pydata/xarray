a4925b6398491b5249a6f3a81c1c22cf233b4317
Ryan Abernathey <rpa@ldeo.columbia.edu>
2018-09-10 14:08:30 -0400

    fix formatting bug

--- xarray/core/formatting.py
+++ xarray/core/formatting.py
@@ -273,7 +273,9 @@ def summarize_datavar(name, var, col_width):
 
 def summarize_coord(name, var, col_width):
     from .variable import IndexVariable
-    is_index = name in var.dims and isinstance(var, IndexVariable)
+    # is there a cleaner way to check if something is an index variable?
+    variable = getattr(var, 'variable', var)
+    is_index = name in var.dims and isinstance(variable, IndexVariable)
     show_values = var._in_memory
     marker = u'*' if is_index else u' '
     if is_index:
