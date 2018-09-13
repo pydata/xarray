f73f2278f3bbbed7d1a609fc5c5ef3cdfe48f2d8
David Tremouilles <dtremoui@ouragan>
2018-09-13 14:07:07 +0200

    add test for 1D object array indexing

--- xarray/tests/test_indexing.py
+++ xarray/tests/test_indexing.py
@@ -622,3 +622,7 @@ def test_create_mask_error():
 def test_posify_mask_subindexer(indices, expected):
     actual = indexing._posify_mask_subindexer(indices)
     np.testing.assert_array_equal(expected, actual)
+
+def test_indexing_1d_object_array():
+    er = DataArray(np.array((np.arange(3), np.arange(6))))
+    er[0]
