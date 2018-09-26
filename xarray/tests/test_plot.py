1f12d7b42ebb50e88f1d51eafc0557915a360392
Deepak Cherian <dcherian@users.noreply.github.com>
2018-09-26 09:55:32 +0900

    make facetgrid execute _finalize() only once (#2435)
    * only apply finalize_grid once.
    
    * Add test

--- xarray/tests/test_plot.py
+++ xarray/tests/test_plot.py
@@ -1523,7 +1523,9 @@ class TestFacetGrid(PlotTestCase):
 
     @pytest.mark.slow
     def test_map(self):
+        assert self.g._finalized is False
         self.g.map(plt.contourf, 'x', 'y', Ellipsis)
+        assert self.g._finalized is True
         self.g.map(lambda: None)
 
     @pytest.mark.slow
