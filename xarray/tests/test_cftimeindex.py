6cd5dbaebd7d3bb9d5458b6297eaa614f1275f0c
Spencer Clark <spencerkclark@gmail.com>
2018-10-04 16:56:06 -0400

    Make _parse_array_of_cftime_strings more robust

--- xarray/tests/test_cftimeindex.py
+++ xarray/tests/test_cftimeindex.py
@@ -689,9 +689,11 @@ def test_cftimeindex_shift_invalid_freq():
 def test_parse_array_of_cftime_strings():
     from cftime import DatetimeNoLeap
 
-    strings = np.array(['2000-01-01', '2000-01-02'])
-    expected = np.array([DatetimeNoLeap(2000, 1, 1),
-                         DatetimeNoLeap(2000, 1, 2)])
+    strings = np.array([['2000-01-01', '2000-01-02'],
+                        ['2000-01-03', '2000-01-04']])
+    expected = np.array(
+        [[DatetimeNoLeap(2000, 1, 1), DatetimeNoLeap(2000, 1, 2)],
+         [DatetimeNoLeap(2000, 1, 3), DatetimeNoLeap(2000, 1, 4)]])
 
     result = _parse_array_of_cftime_strings(strings, DatetimeNoLeap)
     np.testing.assert_array_equal(result, expected)
