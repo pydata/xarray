a4a2f567601dcac4ff127c29ca801dc9e148cbf8
ned haughton <ned@climaterisk.com.au>
2018-10-04 19:56:42 +1000

    Add swap_dims to relevant 'See also' sections

--- xarray/core/common.py
+++ xarray/core/common.py
@@ -344,6 +344,7 @@ class DataWithCoords(SupportsArithmetic, AttrAccessMixin):
         See also
         --------
         Dataset.assign
+        Dataset.swap_dims
         """
         data = self.copy(deep=False)
         results = self._calc_assign_results(kwargs)
