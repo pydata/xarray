6536479374645abab6b42f2584ed5ff9e2a9928f
Alessandro Amici <a.amici@bopen.eu>
2018-10-18 21:45:07 +0200

    Add test_backends_api.py to unit test private function in api.py.

---
+++ xarray/tests/test_backends_api.py
@@ -0,0 +1,20 @@
+
+from xarray.backends.api import _get_default_engine
+from . import requires_netCDF4, requires_pynio, requires_scipy
+
+
+@requires_netCDF4
+@requires_scipy
+@requires_pynio
+def test__get_default_engine():
+    engine_remote = _get_default_engine('http://example.org/test.nc')
+    assert engine_remote == 'netcdf4'
+
+    engine = _get_default_engine('/example.gz')
+    assert engine == 'scipy'
+
+    engine = _get_default_engine('/example.grib')
+    assert engine == 'pynio'
+
+    engine = _get_default_engine('/example')
+    assert engine == 'netcdf4'
