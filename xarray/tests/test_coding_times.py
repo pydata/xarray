42406e9dd253cf85fc2a7e4980188783be5e36dc
Spencer Clark <spencerkclark@gmail.com>
2018-10-28 09:50:03 -0400

    Add test for decoding dates with microsecond-resolution units
    This would have failed before including the microsecond attribute
    of each date in cftime_to_nptime in eaa4a44.

--- xarray/tests/test_coding_times.py
+++ xarray/tests/test_coding_times.py
@@ -47,7 +47,8 @@ _CF_DATETIME_NUM_DATES_UNITS = [
     ([0.5, 1.5], 'hours since 1900-01-01T00:00:00'),
     (0, 'milliseconds since 2000-01-01T00:00:00'),
     (0, 'microseconds since 2000-01-01T00:00:00'),
-    (np.int32(788961600), 'seconds since 1981-01-01')  # GH2002
+    (np.int32(788961600), 'seconds since 1981-01-01'),  # GH2002
+    (12300 + np.arange(5), 'hour since 1680-01-01 00:00:00.500000')
 ]
 _CF_DATETIME_TESTS = [num_dates_units + (calendar,) for num_dates_units,
                       calendar in product(_CF_DATETIME_NUM_DATES_UNITS,
