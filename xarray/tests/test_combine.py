58a3648cc2b4a7465f5cacdaa8e6eb67e54ff583
Thomas Nicholas <tegn500@york.ac.uk>
2018-11-30 21:21:23 +0000

    Test for getting new tile id

--- xarray/tests/test_combine.py
+++ xarray/tests/test_combine.py
@@ -517,6 +517,15 @@ def _create_tile_ids(shape):
 
 @requires_dask  # only for toolz
 class TestCombineND(object):
+    @pytest.mark.parametrize("old_id, new_id", [((3,0,1), (0,1)),
+                                                ((0, 0),  (0,)),
+                                                ((1,),    ()),
+                                                ((0,),    ()),
+                                                ((1, 0),  (0,))])
+    def test_new_tile_id(self, old_id, new_id):
+        ds = create_test_data
+        assert _new_tile_id((old_id, ds)) == new_id
+
     def test_get_new_tile_ids(self, create_combined_ids):
         shape = (1, 2, 3)
         combined_ids = create_combined_ids(shape)
@@ -642,8 +651,6 @@ class TestAutoCombineND(object):
         expected = Dataset({'x': [0, 1]})
         assert_identical(expected, actual)
 
-
-# TODO test for _new_tile_id
 
 class TestAutoCombineUsingCoords(object):
     @pytest.mark.xfail(reason="Not yet implemented")
