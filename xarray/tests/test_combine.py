c4bb8d0396498154da3521ffe7c9a81a4199cf7b
Thomas Nicholas <tegn500@york.ac.uk>
2018-11-30 20:07:54 +0000

    test_get_new_tile_ids now doesn't assume dicts are ordered

--- xarray/tests/test_combine.py
+++ xarray/tests/test_combine.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from xarray import DataArray, Dataset, Variable, auto_combine, concat
+from xarray import DataArray, Dataset, Variable, auto_combine, concat, merge
 from xarray.core.pycompat import OrderedDict, iteritems
 from xarray.core.combine import (
     _new_tile_id, _auto_combine_all_along_first_dim,
@@ -529,9 +529,9 @@ class TestCombineND(object):
         shape = (1, 2, 3)
         combined_ids = create_combined_ids(shape)
 
-        for combined, tile_id in zip(combined_ids.items(), _create_tile_ids(shape)):
-            expected_new_tile_id = tile_id[1:]
-            assert _new_tile_id(combined) == expected_new_tile_id
+        expected_tile_ids = sorted(combined_ids.keys())
+        actual_tile_ids = _create_tile_ids(shape)
+        assert expected_tile_ids == actual_tile_ids
 
     @pytest.mark.parametrize("concat_dim", ['dim1', 'new_dim'])
     def test_concat_once(self, create_combined_ids, concat_dim):
@@ -547,16 +547,36 @@ class TestCombineND(object):
         expected_ds = concat([ds(0), ds(1)], dim=concat_dim)
         assert_combined_tile_ids_equal(result, {(): expected_ds})
 
-    def test_concat_twice(self, create_combined_ids):
+    def test_concat_only_first_dim(self, create_combined_ids):
         shape = (2, 3)
         combined_ids = create_combined_ids(shape)
-        result = _combine_nd(combined_ids, concat_dims=['dim1', 'dim2'])
+        ds = create_test_data
+        result = _auto_combine_all_along_first_dim(combined_ids,
+                                                   dim='dim1',
+                                                   data_vars='all',
+                                                   coords='different',
+                                                   compat='no_conflicts')
 
         ds = create_test_data
         partway1 = concat([ds(0), ds(3)], dim='dim1')
         partway2 = concat([ds(1), ds(4)], dim='dim1')
         partway3 = concat([ds(2), ds(5)], dim='dim1')
-        expected = concat([partway1, partway2, partway3], dim='dim2')
+        expected_datasets = [partway1, partway2, partway3]
+        expected = {(i,): ds for i, ds in enumerate(expected_datasets)}
+        
+        assert_combined_tile_ids_equal(result, expected)
+
+    @pytest.mark.parametrize("concat_dim", ['dim1', 'new_dim'])
+    def test_concat_twice(self, create_combined_ids, concat_dim):
+        shape = (2, 3)
+        combined_ids = create_combined_ids(shape)
+        result = _combine_nd(combined_ids, concat_dims=['dim1', concat_dim])
+
+        ds = create_test_data
+        partway1 = concat([ds(0), ds(3)], dim='dim1')
+        partway2 = concat([ds(1), ds(4)], dim='dim1')
+        partway3 = concat([ds(2), ds(5)], dim='dim1')
+        expected = concat([partway1, partway2, partway3], dim=concat_dim)
 
         assert_equal(result, expected)
 
