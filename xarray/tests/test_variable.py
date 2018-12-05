56aeaaab2bf84d99db58aa2bc72e9fac0af81974
MBlaschek <michael.blaschek@gmail.com>
2018-12-05 10:43:28 +0100

    added test for binary_ops keep_attrs=True

--- xarray/tests/test_variable.py
+++ xarray/tests/test_variable.py
@@ -26,7 +26,7 @@ from xarray.tests import requires_bottleneck
 
 from . import (
     assert_allclose, assert_array_equal, assert_equal, assert_identical,
-    raises_regex, requires_dask, source_ndarray)
+    raises_regex, requires_dask, source_ndarray, set_options)
 
 
 class VariableSubclassobjects(object):
@@ -1544,7 +1544,24 @@ class TestVariable(VariableSubclassobjects):
         vm = v.mean(keep_attrs=True)
         assert len(vm.attrs) == len(_attrs)
         assert vm.attrs == _attrs
+        
+    def test_binary_ops_keep_attrs(self):
+        _attrs = {'units': 'test', 'long_name': 'testing'}
 
+        a = Variable(['x', 'y'], np.random.randn(3,3), _attrs)
+        b = Variable(['x', 'y'], np.random.randn(3,3), _attrs)
+
+        # Test dropped attrs
+        d = a - b   # just one operation
+        assert len(d.attrs) == 0
+        assert d.attrs == OrderedDict()
+
+        # Test kept attrs
+        with set_options(keep_attrs=True):
+            d = a - b 
+        assert len(d.attrs) == len(_attrs)
+        assert d.attrs == _attrs
+        
     def test_count(self):
         expected = Variable([], 3)
         actual = Variable(['x'], [1, 2, 3, np.nan]).count()
