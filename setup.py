a0919732bc7825aeafe1b5f262fc999cba6da85d
Joseph Hamman <jhamman@ucar.edu>
2018-12-31 08:47:21 -0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -72,6 +72,5 @@ setup(name=DISTNAME,
       setup_requires=SETUP_REQUIRES,
       tests_require=TESTS_REQUIRE,
       url=URL,
-      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
       packages=find_packages(),
       package_data={'xarray': ['tests/data/*']})
