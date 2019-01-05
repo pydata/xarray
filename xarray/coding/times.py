1e1ddb299a267c5a810de27f8f26ddae3daada36
Spencer Clark <spencerkclark@gmail.com>
2019-01-04 20:07:00 -0500

    Only convert ref_date if it is not timezone-naive

--- xarray/coding/times.py
+++ xarray/coding/times.py
@@ -357,7 +357,12 @@ def encode_cf_datetime(dates, units=None, calendar=None):
 
         delta_units = _netcdf_to_numpy_timeunit(delta)
         time_delta = np.timedelta64(1, delta_units).astype('timedelta64[ns]')
-        ref_date = pd.Timestamp(ref_date).tz_convert(None)
+        ref_date = pd.Timestamp(ref_date)
+
+        # If the ref_date Timestamp is timezone-aware, convert to UTC and
+        # make it timezone-naive (GH 2649).
+        if ref_date.tz is not None:
+            ref_date = ref_date.tz_convert(None)
 
         # Wrap the dates in a DatetimeIndex to do the subtraction to ensure
         # an OverflowError is raised if the ref_date is too far away from
