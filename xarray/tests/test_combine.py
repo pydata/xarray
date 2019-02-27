66b4c4f9b11f722dc82695a052c3342b5b26f9ec
Thomas Nicholas <tegn500@york.ac.uk>
2019-02-27 17:13:49 +0000

    Added test to guard against issue #2777

--- xarray/tests/test_combine.py
+++ xarray/tests/test_combine.py
@@ -411,6 +411,21 @@ class TestManualCombine(object):
         expected = Dataset({'a': (('x', 'y'), [[0, 1], [2, 3]])})
         assert_identical(expected, actual)
 
+    def test_concat_name_symmetry(self):
+        """Inspired by the discussion on GH issue #2777"""
+
+        da1 = DataArray(name='a', data=[[0]], dims=['x', 'y'])
+        da2 = DataArray(name='b', data=[[1]], dims=['x', 'y'])
+        da3 = DataArray(name='a', data=[[2]], dims=['x', 'y'])
+        da4 = DataArray(name='b', data=[[3]], dims=['x', 'y'])
+
+        x_first = manual_combine([[da1, da2], [da3, da4]],
+                                 concat_dim=['x', 'y'])
+        y_first = manual_combine([[da1, da3], [da2, da4]],
+                                 concat_dim=['y', 'x'])
+
+        assert_identical(x_first, y_first)
+
     def test_concat_one_dim_merge_another(self):
         data = create_test_data()
         data1 = data.copy(deep=True)
