bf7d549c02ac0fa7fb85b7f33a291accab19aa02
Thomas Nicholas <tegn500@york.ac.uk>
2019-03-03 13:49:05 +0000

    Reverted uamiv test

--- xarray/tests/test_backends.py
+++ xarray/tests/test_backends.py
@@ -2894,7 +2894,7 @@ class TestPseudoNetCDFFormat(object):
             ['example.uamiv',
              'example.uamiv'],
             engine='pseudonetcdf',
-            concat_dim=['TSTEP'],
+            concat_dim='TSTEP',
             backend_kwargs={'format': 'uamiv'})
 
         data1 = np.arange(20, dtype='f').reshape(1, 1, 4, 5)
