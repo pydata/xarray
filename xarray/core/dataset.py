68455caf35afd9fb76a803ff509e528008be0c9e
Stephan Hoyer <shoyer@gmail.com>
2019-03-24 19:32:55 -0700

    Rename _attrs -> attrs

--- xarray/core/dataset.py
+++ xarray/core/dataset.py
@@ -916,9 +916,9 @@ class Dataset(Mapping, ImplementsDatasetReduce, DataWithCoords):
             variables = OrderedDict((k, v.copy(deep=deep, data=data.get(k)))
                                     for k, v in self._variables.items())
 
-        _attrs = copy.deepcopy(self._attrs) if deep else copy.copy(self._attrs)
+        attrs = copy.deepcopy(self._attrs) if deep else copy.copy(self._attrs)
 
-        return self._replace(variables, attrs=_attrs)
+        return self._replace(variables, attrs=attrs)
 
     @property
     def _level_coords(self):
