80474531679e2d755ebbc0e19e251d4deb2fcf17
Spencer Clark <spencerkclark@gmail.com>
2019-04-08 09:13:35 -0400

    Reduce length of cftime resample tests

--- xarray/tests/test_cftimeindex_resample.py
+++ xarray/tests/test_cftimeindex_resample.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 import datetime
@@ -5,32 +7,10 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 from xarray.core.resample_cftime import CFTimeGrouper
+from xarray.coding.cftime_offsets import _PATTERN
 
 pytest.importorskip('cftime')
 pytest.importorskip('pandas', minversion='0.24')
-
-
-@pytest.fixture(
-    params=[
-        dict(start='2004-01-01T12:07:01', periods=91, freq='3D'),
-        dict(start='1892-01-03T12:07:01', periods=15, freq='41987T'),
-        dict(start='2004-01-01T12:07:01', periods=7, freq='3Q-AUG'),
-        dict(start='1892-01-03T12:07:01', periods=10, freq='3AS-JUN')
-    ],
-    ids=['3D', '41987T', '3Q_AUG', '3AS_JUN']
-)
-def time_range_kwargs(request):
-    return request.param
-
-
-@pytest.fixture()
-def datetime_index(time_range_kwargs):
-    return pd.date_range(**time_range_kwargs)
-
-
-@pytest.fixture()
-def cftime_index(time_range_kwargs):
-    return xr.cftime_range(**time_range_kwargs)
 
 
 def da(index):
@@ -38,51 +18,38 @@ def da(index):
                         coords=[index], dims=['time'])
 
 
-@pytest.mark.parametrize('freq', [
-    '700T', '8001T',
-    '12H', '8001H',
-    '8D', '8001D',
-    '2MS', '3MS',
-    '2QS-AUG', '3QS-SEP',
-    '3AS-MAR', '4A-MAY'])
-@pytest.mark.parametrize('closed', [None, 'right'])
-@pytest.mark.parametrize('label', [None, 'right'])
-@pytest.mark.parametrize('base', [24, 31])
-def test_resampler(freq, closed, label, base,
-                   datetime_index, cftime_index):
-    # Fairly extensive testing for standard/proleptic Gregorian calendar
-    # For any frequencies which are not greater-than-day and anchored
-    # at the end, the default values for closed and label are 'left'.
-    loffset = '12H'
-    try:
-        da_datetime = da(datetime_index).resample(
-            time=freq, closed=closed, label=label, base=base,
-            loffset=loffset).mean()
-    except ValueError:
-        with pytest.raises(ValueError):
-            da(cftime_index).resample(
-                time=freq, closed=closed, label=label, base=base,
-                loffset=loffset).mean()
+def _scale_freq(freq, mode):
+    """Scale input to a longer or shorter frequency string"""
+    freq_data = re.match(_PATTERN, freq).groupdict()
+    if mode == 'longer':
+        new_multiple = int(freq_data['multiple']) * 2
+    elif mode == 'shorter':
+        new_multiple = int(freq_data['multiple']) // 2
     else:
-        da_cftime = da(cftime_index).resample(time=freq, closed=closed,
-                                              label=label, base=base,
-                                              loffset=loffset).mean()
-        da_cftime['time'] = da_cftime.indexes['time'].to_datetimeindex()
-        xr.testing.assert_identical(da_cftime, da_datetime)
+        raise ValueError
+    return '{}{}'.format(new_multiple, freq_data['freq'])
 
 
 @pytest.mark.parametrize('freq', [
-    '2M', '3M',
-    '2Q-JUN', '3Q-JUL',
-    '3A-FEB', '4A-APR'])
-@pytest.mark.parametrize('closed', ['left', None])
-@pytest.mark.parametrize('label', ['left', None])
-@pytest.mark.parametrize('base', [17, 24])
-def test_resampler_end_super_day(freq, closed, label, base,
-                                 datetime_index, cftime_index):
-    # Fairly extensive testing for standard/proleptic Gregorian calendar.
-    # For greater-than-day frequencies anchored at the end, the default values
-    # for closed and label are 'right'.
+    '8001T', '12H',
+    '8D', '2MS',
+    '7M', '41QS-AUG',
+    '11Q-JUN', '3AS-MAR',
+    '4A-MAY'])
+@pytest.mark.parametrize('closed', [None, 'left', 'right'])
+@pytest.mark.parametrize('label', [None, 'left', 'right'])
+@pytest.mark.parametrize('base', [24, 31])
+@pytest.mark.parametrize(
+    'da_freq_length',
+    ['longer', 'shorter'],
+    ids=['longer_da_freq', 'shorter_da_freq']
+)
+def test_resample(freq, closed, label, base, da_freq_length):
+    da_freq = _scale_freq(freq, da_freq_length)
+    index_kwargs = dict(start='2000-01-01T12:07:01', periods=5, freq=da_freq)
+    datetime_index = pd.date_range(**index_kwargs)
+    cftime_index = xr.cftime_range(**index_kwargs)
+
     loffset = '12H'
     try:
         da_datetime = da(datetime_index).resample(
