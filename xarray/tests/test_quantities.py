f792478c4ac2f36645dca22afd70b04ab0b07d15
Keewis <keewis@posteo.de>
2019-05-13 14:47:44 +0200

    replace the single data fixture with multiple smaller ones

--- xarray/tests/test_quantities.py
+++ xarray/tests/test_quantities.py
@@ -20,28 +20,6 @@ def requires_quantities(test):
     )
 
 
-@pytest.fixture
-def sample_data():
-    from collections import namedtuple
-
-    x = (np.arange(10) + 1) * pq.A
-    y = (np.arange(20) + 1)
-    xp = (np.arange(10) + 1) * pq.J
-    v = (np.arange(10 * 20).reshape(10, 20) + 1) * pq.V
-    da = DataArray(
-        v,
-        dims=('x', 'y'),
-        coords=dict(x=x, y=y, xp=(['x'], xp)),
-    )
-
-    data = namedtuple(
-        'SampleData',
-        ['x', 'y', 'xp', 'v', 'da'],
-    )
-
-    return data(x=x, y=y, xp=xp, v=v, da=da)
-
-
 def equal_with_units(a, b):
     a = a if not isinstance(a, DataArray) else a.data
     b = b if not isinstance(b, DataArray) else b.data
@@ -53,60 +31,82 @@ def equal_with_units(a, b):
     )
 
 
+@pytest.fixture
+def data():
+    return (np.arange(10 * 20).reshape(10, 20) + 1) * pq.V
+
+
+@pytest.fixture
+def coords():
+    return {
+        'x': (np.arange(10) + 1) * pq.A,
+        'y': np.arange(20) + 1,
+        'xp': (np.arange(10) + 1) * pq.J,
+    }
+
+
+@pytest.fixture
+def data_array(data, coords):
+    coords['xp'] = (['x'], coords['xp'])
+    return DataArray(
+        data,
+        dims=('x', 'y'),
+        coords=coords,
+    )
+
+
+def with_keys(mapping, keys):
+    return {
+        key: value
+        for key, value in mapping.items()
+        if key in keys
+    }
+
+
 @requires_quantities
 class TestWithQuantities(object):
-    def test_units_in_data_and_coords(self, sample_data):
-        assert equal_with_units(sample_data.da.xp.data, sample_data.xp)
+    def test_units_in_data_and_coords(self, data_array):
+        assert equal_with_units(data_array.xp.data, data_array.xp)
 
-    def test_arithmetics(self, sample_data):
-        x = sample_data.x
-        y = sample_data.y
-        v = sample_data.v
-        da = sample_data.da
+    def test_arithmetics(self, data_array, data, coords):
+        v = data
+        da = data_array
 
         f = np.arange(10 * 20).reshape(10, 20) * pq.A
-        g = DataArray(f, dims=['x', 'y'], coords=dict(x=x, y=y))
+        g = DataArray(f, dims=['x', 'y'], coords=with_keys(coords, ['x', 'y']))
         assert equal_with_units(da * g, v * f)
 
         # swapped dimension order
         f = np.arange(20 * 10).reshape(20, 10) * pq.V
-        g = DataArray(f, dims=['y', 'x'], coords=dict(x=x, y=y))
+        g = DataArray(f, dims=['y', 'x'], coords=with_keys(coords, ['x', 'y']))
         assert equal_with_units(da + g, v + f.T)
 
         # broadcasting
         f = (np.arange(10) + 1) * pq.m
-        g = DataArray(f, dims=['x'], coords=dict(x=x))
+        g = DataArray(f, dims=['x'], coords=with_keys(coords, ['x']))
         assert equal_with_units(da / g, v / f[:, None])
 
-    def test_unit_checking(self, sample_data):
-        da = sample_data.da
-        x = sample_data.x
-        y = sample_data.y
+    def test_unit_checking(self, data_array, coords):
+        da = data_array
+
         f = np.arange(10 * 20).reshape(10, 20) * pq.A
-        g = DataArray(f, dims=['x', 'y'], coords=dict(x=x, y=y))
+        g = DataArray(f, dims=['x', 'y'], coords=with_keys(coords, ['x', 'y']))
         with pytest.raises(ValueError,
                            match="Unable to convert between units"):
             da + g
 
     @pytest.mark.xfail(reason="units in indexes not supported")
-    def test_units_in_indexes(self, sample_data):
+    def test_units_in_indexes(self, data_array, coords):
         """ Test if units survive through xarray indexes.
         Indexes are borrowed from Pandas, and Pandas does not support
         units. Therefore, we currently don't intend to support units on
         indexes either.
         """
-        x = sample_data.x
-        da = sample_data.da
-        assert equal_with_units(da.x, x)
+        assert equal_with_units(data_array.x, coords['x'])
 
-    def test_sel(self, sample_data):
-        y = sample_data.y
-        v = sample_data.v
-        da = sample_data.da
-        assert equal_with_units(da.sel(y=y[0]), v[:, 0])
+    def test_sel(self, data_array, coords, data):
+        assert equal_with_units(data_array.sel(y=coords['y'][0]), data[:, 0])
 
     @pytest.mark.xfail
-    def test_mean(self, sample_data):
-        da = sample_data.da
-        v = sample_data.v
-        assert equal_with_units(da.mean('x'), v.mean(0))
+    def test_mean(self, data_array, data):
+        assert equal_with_units(data_array.mean('x'), data.mean(0))
