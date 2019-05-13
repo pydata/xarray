0d4b543d0c29c29605620de67f3edb7e5ffd5c41
Keewis <keewis@posteo.de>
2019-05-13 15:13:47 +0200

    convert the test methods from the namespace class to functions
    The namespace class has been useful for switching off the tests using
    only one decorator, but now that the tests get skipped if necessary on
    module level, it is not required anymore.

--- xarray/tests/test_quantities.py
+++ xarray/tests/test_quantities.py
@@ -60,58 +60,63 @@ def with_keys(mapping, keys):
     }
 
 
-class TestWithQuantities(object):
-    def test_units_in_data_and_coords(self, data_array):
-        assert equal_with_units(data_array.xp.data, data_array.xp)
+def test_units_in_data_and_coords(data_array):
+    assert equal_with_units(data_array.xp.data, data_array.xp)
 
-    def test_arithmetics(self, data_array, data, coords):
-        v = data
-        da = data_array
 
-        f = np.arange(10 * 20).reshape(10, 20) * pq.A
-        g = DataArray(f, dims=['x', 'y'], coords=with_keys(coords, ['x', 'y']))
-        assert equal_with_units(da * g, v * f)
+def test_arithmetics(data_array, data, coords):
+    v = data
+    da = data_array
 
-        # swapped dimension order
-        f = np.arange(20 * 10).reshape(20, 10) * pq.V
-        g = DataArray(f, dims=['y', 'x'], coords=with_keys(coords, ['x', 'y']))
-        assert equal_with_units(da + g, v + f.T)
+    f = np.arange(10 * 20).reshape(10, 20) * pq.A
+    g = DataArray(f, dims=['x', 'y'], coords=with_keys(coords, ['x', 'y']))
+    assert equal_with_units(da * g, v * f)
 
-        # broadcasting
-        f = (np.arange(10) + 1) * pq.m
-        g = DataArray(f, dims=['x'], coords=with_keys(coords, ['x']))
-        assert equal_with_units(da / g, v / f[:, None])
+    # swapped dimension order
+    f = np.arange(20 * 10).reshape(20, 10) * pq.V
+    g = DataArray(f, dims=['y', 'x'], coords=with_keys(coords, ['x', 'y']))
+    assert equal_with_units(da + g, v + f.T)
 
-    @pytest.mark.xfail(reason="units don't survive through combining yet")
-    def test_combine(self, data_array):
-        from xarray import concat
+    # broadcasting
+    f = (np.arange(10) + 1) * pq.m
+    g = DataArray(f, dims=['x'], coords=with_keys(coords, ['x']))
+    assert equal_with_units(da / g, v / f[:, None])
 
-        a = data_array[:, :10]
-        b = data_array[:, 10:]
 
-        assert equal_with_units(concat([a, b], dim='y'), data_array)
+@pytest.mark.xfail(reason="units don't survive through combining yet")
+def test_combine(data_array):
+    from xarray import concat
 
-    def test_unit_checking(self, data_array, coords):
-        da = data_array
+    a = data_array[:, :10]
+    b = data_array[:, 10:]
 
-        f = np.arange(10 * 20).reshape(10, 20) * pq.A
-        g = DataArray(f, dims=['x', 'y'], coords=with_keys(coords, ['x', 'y']))
-        with pytest.raises(ValueError,
-                           match="Unable to convert between units"):
-            da + g
+    assert equal_with_units(concat([a, b], dim='y'), data_array)
 
-    @pytest.mark.xfail(reason="units in indexes not supported")
-    def test_units_in_indexes(self, data_array, coords):
-        """ Test if units survive through xarray indexes.
-        Indexes are borrowed from Pandas, and Pandas does not support
-        units. Therefore, we currently don't intend to support units on
-        indexes either.
-        """
-        assert equal_with_units(data_array.x, coords['x'])
 
-    def test_sel(self, data_array, coords, data):
-        assert equal_with_units(data_array.sel(y=coords['y'][0]), data[:, 0])
+def test_unit_checking(data_array, coords):
+    da = data_array
 
-    @pytest.mark.xfail
-    def test_mean(self, data_array, data):
-        assert equal_with_units(data_array.mean('x'), data.mean(0))
+    f = np.arange(10 * 20).reshape(10, 20) * pq.A
+    g = DataArray(f, dims=['x', 'y'], coords=with_keys(coords, ['x', 'y']))
+    with pytest.raises(ValueError,
+                       match="Unable to convert between units"):
+        da + g
+
+
+@pytest.mark.xfail(reason="units in indexes not supported")
+def test_units_in_indexes(data_array, coords):
+    """ Test if units survive through xarray indexes.
+    Indexes are borrowed from Pandas, and Pandas does not support
+    units. Therefore, we currently don't intend to support units on
+    indexes either.
+    """
+    assert equal_with_units(data_array.x, coords['x'])
+
+
+def test_sel(data_array, coords, data):
+    assert equal_with_units(data_array.sel(y=coords['y'][0]), data[:, 0])
+
+
+@pytest.mark.xfail
+def test_mean(data_array, data):
+    assert equal_with_units(data_array.mean('x'), data.mean(0))
