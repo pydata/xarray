5526fe40d68f83ff45ea75b026aacfac0b2b76b3
dcherian <deepak@cherian.net>
2019-05-15 13:00:11 -0600

    Make sure we iterate over coords too.

--- xarray/backends/api.py
+++ xarray/backends/api.py
@@ -724,7 +724,7 @@ def open_mfdataset(paths, chunks=None, concat_dim=_CONCAT_DIM_DEFAULT,
 
     combined._file_obj = _MultiFileCloser(file_objs)
     combined.attrs = datasets[0].attrs
-    for v in combined:
+    for v in combined.variables:
         if v in datasets[0]:
             combined[v].encoding = datasets[0][v].encoding
     return combined
