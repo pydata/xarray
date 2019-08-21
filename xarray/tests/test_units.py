663d0a25cd446ee5ef5c8873b70df49c7cbf5924
Keewis <keewis@posteo.de>
2019-08-22 01:46:18 +0200

    add tests for data array aggregation functions

---
+++ xarray/tests/test_units.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pint
+import pytest
+
+import xarray as xr
+from xarray.core.npcompat import IS_NEP18_ACTIVE
+
+pytestmark = pytest.mark.skipif(
+    not IS_NEP18_ACTIVE, reason="NUMPY_EXPERIMENTAL_ARRAY_FUNCTION is not enabled"
+)
+
+
+unit_registry = pint.UnitRegistry()
+
+
+def assert_equal_with_units(a, b):
+    a_ = a if not isinstance(a, (xr.Dataset, xr.DataArray, xr.Variable)) else a.data
+    b_ = b if not isinstance(b, (xr.Dataset, xr.DataArray, xr.Variable)) else b.data
+
+    assert np.allclose(a_, b_)
+    if hasattr(a_, "units") or hasattr(b_, "units"):
+        assert (hasattr(a_, "units") and hasattr(b_, "units")) and a_.units == b_.units
+
+
+@pytest.fixture(params=[float, int])
+def dtype(request):
+    return request.param
+
+
+class TestDataArray:
+    @pytest.mark.parametrize(
+        "func",
+        (
+            np.all,
+            np.any,
+            np.argmax,
+            np.argmin,
+            np.max,
+            np.mean,
+            np.median,
+            np.min,
+            np.prod,
+            np.sum,
+            np.std,
+            np.var,
+        ),
+    )
+    def test_aggregation(self, func, dtype):
+        array = np.arange(10).astype(dtype) * unit_registry.m
+        data_array = xr.DataArray(data=array)
+
+        result_array = func(array)
+        result_data_array = func(data_array)
+
+        assert_equal_with_units(result_array, result_data_array)
