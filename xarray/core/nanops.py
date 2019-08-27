faa7eab73f2b1189550132fa06c422e3c6b74661
Ulrich J. Herter <ulrich.herter@hsu-hh.de>
2019-08-27 16:13:35 +0200

    nanargmin/max: only set fill_value when needed

--- xarray/core/nanops.py
+++ xarray/core/nanops.py
@@ -88,8 +88,8 @@ def nanmax(a, axis=None, out=None):
 
 
 def nanargmin(a, axis=None):
-    fill_value = dtypes.get_pos_infinity(a.dtype)
     if a.dtype.kind == "O":
+        fill_value = dtypes.get_pos_infinity(a.dtype)
         return _nan_argminmax_object("argmin", fill_value, a, axis=axis)
 
     module = dask_array if isinstance(a, dask_array_type) else nputils
@@ -97,8 +97,8 @@ def nanargmin(a, axis=None):
 
 
 def nanargmax(a, axis=None):
-    fill_value = dtypes.get_neg_infinity(a.dtype)
     if a.dtype.kind == "O":
+        fill_value = dtypes.get_neg_infinity(a.dtype)
         return _nan_argminmax_object("argmax", fill_value, a, axis=axis)
 
     module = dask_array if isinstance(a, dask_array_type) else nputils
