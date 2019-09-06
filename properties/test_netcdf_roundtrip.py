3230c2f4a522d2c6b19411398f4b3b70367c5d27
Thomas Kluyver <thomas@kluyver.me.uk>
2019-09-06 11:40:16 +0200

    Reformat code with black

--- properties/test_netcdf_roundtrip.py
+++ properties/test_netcdf_roundtrip.py
@@ -14,34 +14,36 @@ settings.load_profile("ci")
 
 an_array = npst.arrays(
     dtype=st.one_of(
-        npst.unsigned_integer_dtypes(), npst.integer_dtypes(),
+        npst.unsigned_integer_dtypes(),
+        npst.integer_dtypes(),
         # NetCDF does not support float16
         # https://www.unidata.ucar.edu/software/netcdf/docs/data_type.html
-        npst.floating_dtypes(sizes=(32, 64))
+        npst.floating_dtypes(sizes=(32, 64)),
     ),
     shape=npst.array_shapes(max_side=3),  # max_side specified for performance
 )
 
 compatible_names = st.text(
     alphabet=st.characters(
-        whitelist_categories=('Ll', 'Lu', 'Nd'),
+        whitelist_categories=("Ll", "Lu", "Nd"),
         # It looks like netCDF should allow unicode names, but removing
         # this causes a failure with 'ά'
-        max_codepoint=255
+        max_codepoint=255,
     ),
-    min_size=1
+    min_size=1,
 )
+
 
 @given(st.data(), an_array)
 def test_netcdf_roundtrip(tmp_path, data, arr):
     names = data.draw(
-        st.lists(compatible_names, min_size=arr.ndim, max_size=arr.ndim, unique=True).map(
-            tuple
-        )
+        st.lists(
+            compatible_names, min_size=arr.ndim, max_size=arr.ndim, unique=True
+        ).map(tuple)
     )
     var = xr.Variable(names, arr)
-    original = xr.Dataset({'data': var})
-    original.to_netcdf(tmp_path / 'test.nc')
+    original = xr.Dataset({"data": var})
+    original.to_netcdf(tmp_path / "test.nc")
 
-    roundtripped = xr.open_dataset(tmp_path / 'test.nc')
+    roundtripped = xr.open_dataset(tmp_path / "test.nc")
     xr.testing.assert_identical(original, roundtripped)
