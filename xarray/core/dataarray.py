5bc3ff7f8c6c408a7b302f8c96d3b5fb22132479
Tony Tung <ttung@chanzuckerberg.com>
2019-09-26 22:45:26 -0700

    Fix DataArray.to_netcdf type annotation (#3325)
    It calls DataSet.to_netcdf, which returns Union[bytes, "Delayed", None].  So this should as well.

--- xarray/core/dataarray.py
+++ xarray/core/dataarray.py
@@ -2244,7 +2244,7 @@ class DataArray(AbstractArray, DataWithCoords):
         isnull = pd.isnull(values)
         return np.ma.MaskedArray(data=values, mask=isnull, copy=copy)
 
-    def to_netcdf(self, *args, **kwargs) -> Optional["Delayed"]:
+    def to_netcdf(self, *args, **kwargs) -> Union[bytes, "Delayed", None]:
         """Write DataArray contents to a netCDF file.
 
         All parameters are passed directly to `xarray.Dataset.to_netcdf`.
