42fd5fc444df51ed02b70dccc4e24ea74f743084
dcherian <deepak@cherian.net>
2019-10-01 16:05:57 -0600

    make diff smaller.

--- xarray/core/concat.py
+++ xarray/core/concat.py
@@ -315,8 +315,8 @@ def _dataset_concat(
     if variables_to_merge:
         to_merge = {var: [] for var in variables_to_merge}
 
-        for var in variables_to_merge:
-            for ds in datasets:
+        for ds in datasets:
+            for var in variables_to_merge:
                 if var in ds:
                     to_merge[var].append(ds.variables[var])
 
