67c70346d10fbb9a944f7af1838ee4c9f8c46bf7
Thomas Kluyver <thomas@kluyver.me.uk>
2019-10-11 10:05:29 +0100

    Add print_blob to config

--- properties/conftest.py
+++ properties/conftest.py
@@ -1,5 +1,5 @@
 from hypothesis import settings
 
 # Run for a while - arrays are a bigger search space than usual
-settings.register_profile("ci", deadline=None)
+settings.register_profile("ci", deadline=None, print_blob=True)
 settings.load_profile("ci")
