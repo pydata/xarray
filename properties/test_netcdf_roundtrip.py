83bbb73b968e60747098a7867ca11fa6ce283e17
Thomas Kluyver <thomas@kluyver.me.uk>
2019-10-11 09:48:39 +0100

    Remove redundant hypothesis config

--- properties/test_netcdf_roundtrip.py
+++ properties/test_netcdf_roundtrip.py
@@ -7,10 +7,6 @@ from hypothesis import given, settings
 
 import xarray as xr
 
-# Run for a while - arrays are a bigger search space than usual
-settings.register_profile("ci", deadline=None)
-settings.load_profile("ci")
-
 
 an_array = npst.arrays(
     dtype=st.one_of(
