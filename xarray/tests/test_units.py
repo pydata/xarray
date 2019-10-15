788d6ff7d955d4a7cbf336c814720bd9235b0d99
Keewis <keewis@posteo.de>
2019-10-15 02:13:20 +0200

    add tests for grouped operations

--- xarray/tests/test_units.py
+++ xarray/tests/test_units.py
@@ -1544,3 +1544,35 @@ class TestDataArray:
         result = func(data_array).mean()
 
         assert_equal_with_units(expected, result)
+
+    @pytest.mark.parametrize(
+        "func",
+        (
+            pytest.param(
+                method("assign_coords", {"z": (["x"], np.arange(5) * unit_registry.s)}),
+                marks=pytest.mark.xfail(reason="strips units"),
+            ),
+            pytest.param(method("first")),
+            pytest.param(method("last")),
+            pytest.param(
+                method("quantile", q=[0.25, 0.5, 0.75], dim="x"),
+                marks=pytest.mark.xfail(reason="strips units"),
+            ),
+        ),
+        ids=repr,
+    )
+    def test_grouped_operations(self, func, dtype):
+        array = (
+            np.linspace(0, 10, 5 * 10).reshape(5, 10).astype(dtype) * unit_registry.m
+        )
+
+        x = np.arange(array.shape[0]) * unit_registry.m
+        y = np.arange(array.shape[1]) * 3 * unit_registry.s
+
+        data_array = xr.DataArray(data=array, coords={"x": x, "y": y}, dims=("x", "y"))
+        units = extract_units(data_array)
+
+        expected = attach_units(func(strip_units(data_array).groupby("y")), units)
+        result = func(data_array.groupby("y"))
+
+        assert_equal_with_units(expected, result)
