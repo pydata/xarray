938d57f80a89cb7fae4be688d385d2b194898d8f
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2019-10-19 21:23:34 -0400

    Update xarray/tests/test_cftime_offsets.py
    Co-Authored-By: crusaderky <crusaderky@gmail.com>

--- xarray/tests/test_cftime_offsets.py
+++ xarray/tests/test_cftime_offsets.py
@@ -230,7 +230,7 @@ def test_to_offset_quarter(month_label, month_int, multiple, offset_str):
     if month_label:
         freq = "-".join([freq, month_label])
     if multiple:
-        freq = f"{multiple}" + freq
+        freq = f"{multiple}{freq}"
     result = to_offset(freq)
 
     if multiple and month_int:
