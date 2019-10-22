6cc48f110386df5a3c581bba59724441ffa3b736
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-10-22 07:15:23 -0600

    Maintain backwards compatibility
    Co-Authored-By: Spencer Clark <spencerkclark@gmail.com>

--- xarray/coding/cftime_offsets.py
+++ xarray/coding/cftime_offsets.py
@@ -237,7 +237,14 @@ def _shift_month(date, months, day_option="start"):
         day = _days_in_month(reference)
     else:
         raise ValueError(day_option)
-    return date.replace(year=year, month=month, day=day)
+    if LooseVersion(cftime.__version__) < LooseVersion("1.0.4"):
+        # dayofwk=-1 is required to update the dayofwk and dayofyr attributes of
+        # the returned date object in versions of cftime between 1.0.2 and
+        # 1.0.3.4.  It can be removed for versions of cftime greater than
+        # 1.0.3.4.
+        return date.replace(year=year, month=month, day=day, dayofwk=-1)
+    else:
+        return date.replace(year=year, month=month, day=day)
 
 
 def roll_qtrday(other, n, month, day_option, modby=3):
