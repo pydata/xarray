d464997a55d49d194528505b97eb809e5415c46e
Deepak Cherian <dcherian@users.noreply.github.com>
2019-10-25 11:19:37 -0600

    Update xarray/core/groupby.py
    Co-Authored-By: Maximilian Roos <5635139+max-sixty@users.noreply.github.com>

--- xarray/core/groupby.py
+++ xarray/core/groupby.py
@@ -30,7 +30,7 @@ def check_reduce_dims(reduce_dims, dimensions):
             reduce_dims = [reduce_dims]
         if any([dim not in dimensions for dim in reduce_dims]):
             raise ValueError(
-                "cannot reduce over dimensions %r. expected either xarray.ALL_DIMS to reduce over all dimensions or one or more of %r."
+                "cannot reduce over dimensions %r. expected either '...' to reduce over all dimensions or one or more of %r."
                 % (reduce_dims, dimensions)
             )
 
