044c67d2ec2b63c67a2554d8555bb0bd8c342614
Thomas Kluyver <thomas@kluyver.me.uk>
2019-10-29 15:26:27 +0000

    Don't require hypothesis for conftest file

--- properties/conftest.py
+++ properties/conftest.py
@@ -1,5 +1,8 @@
-from hypothesis import settings
-
-# Run for a while - arrays are a bigger search space than usual
-settings.register_profile("ci", deadline=None, print_blob=True)
-settings.load_profile("ci")
+try:
+    from hypothesis import settings
+except ImportError:
+    pass
+else:
+    # Run for a while - arrays are a bigger search space than usual
+    settings.register_profile("ci", deadline=None, print_blob=True)
+    settings.load_profile("ci")
