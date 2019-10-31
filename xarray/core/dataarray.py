320ef116cfa55f700564cab7bdaa540315ac772d
Maximilian Roos <m@maxroos.com>
2019-10-30 22:13:57 -0400

    type check sentinel values, using Enum pattern

--- xarray/core/dataarray.py
+++ xarray/core/dataarray.py
@@ -53,7 +53,7 @@ from .dataset import Dataset, merge_indexes, split_indexes
 from .formatting import format_item
 from .indexes import Indexes, default_indexes
 from .options import OPTIONS
-from .utils import ReprObject, _check_inplace, either_dict_or_kwargs
+from .utils import Default, ReprObject, __default, _check_inplace, either_dict_or_kwargs
 from .variable import (
     IndexVariable,
     Variable,
@@ -270,8 +270,6 @@ class DataArray(AbstractArray, DataWithCoords):
     _coarsen_cls = rolling.DataArrayCoarsen
     _resample_cls = resample.DataArrayResample
 
-    __default = ReprObject("<default>")
-
     dt = property(DatetimeAccessor)
 
     def __init__(
@@ -387,18 +385,18 @@ class DataArray(AbstractArray, DataWithCoords):
         self,
         variable: Variable = None,
         coords=None,
-        name: Optional[Hashable] = __default,
+        name: Union[Optional[Hashable], Default] = __default,
     ) -> "DataArray":
         if variable is None:
             variable = self.variable
         if coords is None:
             coords = self._coords
-        if name is self.__default:
+        if name is __default:
             name = self.name
         return type(self)(variable, coords, name=name, fastpath=True)
 
     def _replace_maybe_drop_dims(
-        self, variable: Variable, name: Optional[Hashable] = __default
+        self, variable: Variable, name: Union[Optional[Hashable], Default] = __default
     ) -> "DataArray":
         if variable.dims == self.dims and variable.shape == self.shape:
             coords = self._coords.copy()
@@ -2450,13 +2448,11 @@ class DataArray(AbstractArray, DataWithCoords):
         except (TypeError, AttributeError):
             return False
 
-    __default_name = object()
-
     def _result_name(self, other: Any = None) -> Optional[Hashable]:
         # use the same naming heuristics as pandas:
         # https://github.com/ContinuumIO/blaze/issues/458#issuecomment-51936356
-        other_name = getattr(other, "name", self.__default_name)
-        if other_name is self.__default_name or other_name == self.name:
+        other_name = getattr(other, "name", __default)
+        if other_name is __default or other_name == self.name:
             return self.name
         else:
             return None
