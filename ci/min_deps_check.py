f2cf7e7670524bd282b8cbb5d09964a2945b8f2a
Richard Höchenberger <richard.hoechenberger@gmail.com>
2019-12-18 07:58:24 +0100

    PKG: Remove setuptools MONTHS policy

--- ci/min_deps_check.py
+++ ci/min_deps_check.py
@@ -22,8 +22,7 @@ IGNORE_DEPS = {
     "pytest-env",
 }
 
-POLICY_MONTHS = {"python": 42, "numpy": 24, "pandas": 12, "scipy": 12,
-                 "setuptools": 12}
+POLICY_MONTHS = {"python": 42, "numpy": 24, "pandas": 12, "scipy": 12}
 POLICY_MONTHS_DEFAULT = 6
 
 has_errors = False
