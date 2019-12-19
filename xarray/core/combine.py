04b8aa33b8dd09f481e5529c10e3d1834e2db4bb
Ian Bolliger <bolliger@berkeley.edu>
2019-12-19 21:57:09 +0000

    reformat docstring

--- xarray/core/combine.py
+++ xarray/core/combine.py
@@ -116,8 +116,10 @@ def _infer_concat_order_from_coords(datasets):
 
 
 def _check_dimension_depth_tile_ids(combined_tile_ids):
-    """Check all tuples are the same length
-    # i.e. check that all lists are nested to the same depth"""
+    """
+    Check all tuples are the same length, i.e. check that all lists are 
+    nested to the same depth.
+    """
     tile_ids = combined_tile_ids.keys()
     nesting_depths = [len(tile_id) for tile_id in tile_ids]
     if not nesting_depths:
