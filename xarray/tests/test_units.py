6099585af63104deac69c8d75b969b00b653364a
Keewis <keewis@posteo.de>
2019-12-29 16:04:15 +0100

    add a test function for searchsorted and item

--- xarray/tests/test_units.py
+++ xarray/tests/test_units.py
@@ -1354,7 +1354,6 @@ class TestVariable(VariableSubclassobjects):
             method("astype", np.float32),
             method("conj"),
             method("conjugate"),
-            method("searchsorted", 5),
             method("clip", min=2, max=7),
         ),
         ids=repr,
@@ -1398,7 +1397,6 @@ class TestVariable(VariableSubclassobjects):
             for key, value in kwargs.items()
         }
 
-        print("running on:", func, args, kwargs)
         units = extract_units(func(array, *args, **kwargs))
         expected = attach_units(
             func(strip_units(variable), *converted_args, **converted_kwargs), units
@@ -1407,6 +1405,66 @@ class TestVariable(VariableSubclassobjects):
 
         assert extract_units(expected) == extract_units(actual)
         xr.testing.assert_allclose(expected, actual)
+
+    @pytest.mark.parametrize(
+        "func", (method("item", 5), method("searchsorted", 5)), ids=repr
+    )
+    @pytest.mark.parametrize(
+        "unit,error",
+        (
+            pytest.param(1, DimensionalityError, id="no_unit"),
+            pytest.param(
+                unit_registry.dimensionless, DimensionalityError, id="dimensionless"
+            ),
+            pytest.param(unit_registry.s, DimensionalityError, id="incompatible_unit"),
+            pytest.param(unit_registry.cm, None, id="compatible_unit"),
+            pytest.param(unit_registry.m, None, id="identical_unit"),
+        ),
+    )
+    def test_raw_numpy_methods(self, func, unit, error, dtype):
+        array = np.linspace(0, 1, 10).astype(dtype) * unit_registry.m
+        variable = xr.Variable("x", array)
+
+        args = [
+            item * unit
+            if isinstance(item, (int, float, list)) and func.name != "item"
+            else item
+            for item in func.args
+        ]
+        kwargs = {
+            key: value * unit
+            if isinstance(value, (int, float, list)) and func.name != "item"
+            else value
+            for key, value in func.kwargs.items()
+        }
+
+        if error is not None and func.name != "item":
+            with pytest.raises(error):
+                func(variable, *args, **kwargs)
+
+            return
+
+        converted_args = [
+            strip_units(convert_units(item, {None: unit_registry.m}))
+            if func.name != "item"
+            else item
+            for item in args
+        ]
+        converted_kwargs = {
+            key: strip_units(convert_units(value, {None: unit_registry.m}))
+            if func.name != "item"
+            else value
+            for key, value in kwargs.items()
+        }
+
+        units = extract_units(func(array, *args, **kwargs))
+        expected = attach_units(
+            func(strip_units(variable), *converted_args, **converted_kwargs), units
+        )
+        actual = func(variable, *args, **kwargs)
+
+        assert extract_units(expected) == extract_units(actual)
+        np.testing.assert_allclose(expected, actual)
 
 
 class TestDataArray:
