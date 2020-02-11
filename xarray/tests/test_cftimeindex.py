bd92d06ed0b6c1f80feb46d96a580f0e4d951b72
Spencer Clark <spencerkclark@gmail.com>
2020-02-11 08:09:16 -0500

    black

--- xarray/tests/test_cftimeindex.py
+++ xarray/tests/test_cftimeindex.py
@@ -743,9 +743,8 @@ def test_cftimeindex_sub_timedelta(index):
 @requires_cftime
 @pytest.mark.parametrize(
     "other",
-    [np.array(4 * [timedelta(days=1)]),
-     np.array(timedelta(days=1))],
-    ids=["1d-array", "scalar-array"]
+    [np.array(4 * [timedelta(days=1)]), np.array(timedelta(days=1))],
+    ids=["1d-array", "scalar-array"],
 )
 def test_cftimeindex_sub_timedelta_array(index, other):
     date_type = index.date_type
