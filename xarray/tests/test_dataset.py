d5f2392db2e46d484543652996e5e45f6126dcc9
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2020-03-13 15:17:38 +0100

    Blacked.

--- xarray/tests/test_dataset.py
+++ xarray/tests/test_dataset.py
@@ -1460,13 +1460,15 @@ class TestDataset:
 
     def test_categorical_multiindex(self):
         i1 = pd.Series([0, 0])
-        cat = pd.CategoricalDtype(categories=['foo', 'baz', 'bar'])
-        i2 = pd.Series(['baz', 'bar'], dtype=cat)
+        cat = pd.CategoricalDtype(categories=["foo", "baz", "bar"])
+        i2 = pd.Series(["baz", "bar"], dtype=cat)
 
-        df = pd.DataFrame({'i1': i1, 'i2': i2, 'values': [1, 2]}).set_index(['i1', 'i2'])
+        df = pd.DataFrame({"i1": i1, "i2": i2, "values": [1, 2]}).set_index(
+            ["i1", "i2"]
+        )
         actual = df.to_xarray()
         print(actual)
-        assert actual['values'].shape == (1, 2)
+        assert actual["values"].shape == (1, 2)
 
     def test_sel_drop(self):
         data = Dataset({"foo": ("x", [1, 2, 3])}, {"x": [0, 1, 2]})
