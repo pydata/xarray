bf5530b4995584d51f121861d055ce17a97c4906
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2020-04-03 13:06:29 +0200

    FIX: add back fixture line removed by accident

--- xarray/tests/test_dataarray.py
+++ xarray/tests/test_dataarray.py
@@ -4193,6 +4193,7 @@ class TestDataArray:
         assert_equal(y.rank("z", pct=True), y)
 
     @pytest.mark.parametrize("use_dask", [True, False])
+    @pytest.mark.parametrize("use_datetime", [True, False])
     def test_polyfit(self, use_dask, use_datetime):
         if use_dask and not has_dask:
             pytest.skip("requires dask")
