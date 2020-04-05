782e0e2a835b8f3302d3114bd9d45f6e549b9b5f
Deepak Cherian <dcherian@users.noreply.github.com>
2020-04-05 13:41:24 +0000

    Use divergent colormap if lowest and highest level span 0 (#3913)
    * Use divergent colormap if lowest and highest level span 0
    
    Fixes #3524
    
    * sort levels by default.
    
    * better levels check

--- xarray/plot/utils.py
+++ xarray/plot/utils.py
@@ -169,6 +169,9 @@ def _determine_cmap_params(
     """
     import matplotlib as mpl
 
+    if isinstance(levels, Iterable):
+        levels = sorted(levels)
+
     calc_data = np.ravel(plot_data[np.isfinite(plot_data)])
 
     # Handle all-NaN input data gracefully
@@ -216,8 +219,13 @@ def _determine_cmap_params(
         vlim = abs(vmax - center)
 
     if possibly_divergent:
+        levels_are_divergent = (
+            isinstance(levels, Iterable) and levels[0] * levels[-1] < 0
+        )
         # kwargs not specific about divergent or not: infer defaults from data
-        divergent = ((vmin < 0) and (vmax > 0)) or not center_is_none
+        divergent = (
+            ((vmin < 0) and (vmax > 0)) or not center_is_none or levels_are_divergent
+        )
     else:
         divergent = False
 
