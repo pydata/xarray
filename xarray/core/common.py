c6e75362f9dd0a83946ebfbeac0e9be96abc2385
EtienneCmb <e.combrisson@gmail.com>
2020-04-10 17:13:16 +0200

    Empty line missing for DataArray.assign_coords doc

--- xarray/core/common.py
+++ xarray/core/common.py
@@ -455,6 +455,7 @@ class DataWithCoords(SupportsArithmetic, AttrAccessMixin):
             lon_2    (lon) int64 300 289 0 1
 
         Note that the same result can also be obtained with a dict e.g.
+
         >>> _ = da.assign_coords({"lon_2": ('lon', lon_2)})
 
         Notes
