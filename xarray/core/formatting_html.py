412227b74c9c04bdf8b1ceb9fc7e1183109492f1
Ian Castleden <ian.castleden@gmail.com>
2020-04-17 18:52:03 +0800

    better attribute access

--- xarray/core/formatting_html.py
+++ xarray/core/formatting_html.py
@@ -183,7 +183,7 @@ def array_section(obj):
     # "unique" id to expand/collapse the section
     data_id = "section-" + str(uuid.uuid4())
     collapsed = ""
-    variable = obj.variable if hasattr(obj, "variable") else obj
+    variable = getattr(obj, "variable", obj)
     preview = escape(inline_variable_array_repr(variable, max_width=70))
     data_repr = short_data_repr_html(obj)
     data_icon = _icon("icon-database")
