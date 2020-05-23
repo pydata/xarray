2ddcb553c4a48a13a0b737fdcc87662db64e3fdc
Andrew Williams <williamsa3142@gmail.com>
2020-05-23 20:03:47 +0100

    removed whitespace in test_dask...oops

--- xarray/tests/test_dask.py
+++ xarray/tests/test_dask.py
@@ -1048,7 +1048,7 @@ def test_map_blocks_error(map_da, map_ds):
     with raises_regex(TypeError, "Function must return an xarray DataArray"):
         xr.map_blocks(returns_numpy, map_da)
 
-    with raises_regex(TypeError, "args must be"): 
+    with raises_regex(TypeError, "args must be"):
         xr.map_blocks(operator.add, map_da, args=10)
 
     with raises_regex(TypeError, "kwargs must be"):
