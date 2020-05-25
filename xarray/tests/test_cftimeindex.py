9752d799e5d0c8996a7c647bcba955def81c2779
Phobos <pascal.bourgault@gmail.com>
2020-05-25 17:47:25 -0400

    Fix tests for requiring cftime 1.1.0

--- xarray/tests/test_cftimeindex.py
+++ xarray/tests/test_cftimeindex.py
@@ -1049,17 +1049,18 @@ def test_asi8_distant_date():
 
 
 @requires_cftime_1_1_0
-@pytest.mark.parametrize(
-    "index",
-    [
-        xr.cftime_range("2000-01-01", periods=3, freq="D"),
-        pd.date_range("2000-01-01", periods=3, freq="D"),
-        pd.timedelta_range(start="1D", periods=3, freq="D"),
-    ],
-)
-def test_infer_freq_valid_types(index):
-    assert xr.infer_freq(index) == "D"
-    assert xr.infer_freq(xr.DataArray(index)) == "D"
+def test_infer_freq_valid_types():
+    cf_indx = xr.cftime_range("2000-01-01", periods=3, freq="D")
+    assert xr.infer_freq(cf_indx) == "D"
+    assert xr.infer_freq(xr.DataArray(cf_indx)) == "D"
+
+    pd_indx = pd.date_range("2000-01-01", periods=3, freq="D")
+    assert xr.infer_freq(pd_indx) == "D"
+    assert xr.infer_freq(xr.DataArray(pd_indx)) == "D"
+
+    pd_td_indx = pd.timedelta_range(start="1D", periods=3, freq="D")
+    assert xr.infer_freq(pd_td_indx) == "D"
+    assert xr.infer_freq(xr.DataArray(pd_td_indx)) == "D"
 
 
 @requires_cftime_1_1_0
