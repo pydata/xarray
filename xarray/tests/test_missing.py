4dcf248d4040224dcd04593da0705ce83afabab6
Phobos <pascal.bourgault@gmail.com>
2020-05-27 14:18:24 -0400

    Allow non-unique and non-monotonic in get_clean_interp_index and polyfit

--- xarray/tests/test_missing.py
+++ xarray/tests/test_missing.py
@@ -534,6 +534,16 @@ def test_get_clean_interp_index_potential_overflow():
     get_clean_interp_index(da, "time")
 
 
+@pytest.mark.parametrize("index", ([0, 2, 1], [0, 1, 1]))
+def test_get_clean_interp_index_strict(index):
+    da = xr.DataArray([0, 1, 2], dims=("x",), coords={"x": index})
+
+    with pytest.raises(ValueError):
+        get_clean_interp_index(da, "x")
+
+    get_clean_interp_index(da, "x", strict=False)
+
+
 @pytest.fixture
 def da_time():
     return xr.DataArray(
