909ae41cadc3265e1828201c056ba3b074e9c7d6
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2020-07-25 19:23:49 -0400

    xfail failing upstream plotting tests (#4271)
    * xfail failing upstream plotting tests
    
    * _
    
    * _

--- xarray/tests/test_plot.py
+++ xarray/tests/test_plot.py
@@ -2251,6 +2251,7 @@ class TestDatetimePlot(PlotTestCase):
         self.darray.plot.line()
 
 
+@pytest.mark.xfail(reason="Failing on upstream tests asof 2020-07-25")
 @requires_nc_time_axis
 @requires_cftime
 class TestCFDatetimePlot(PlotTestCase):
