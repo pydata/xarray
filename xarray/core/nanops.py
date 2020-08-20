5226ed1d39547375a6da2e37d4db4305e222c990
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2020-08-20 12:22:55 -0400

    Allow multiple dims to be passed with min_count (#4356)
    * Allow multiple dims to be passed with min_count
    
    * Add whatsnew

--- xarray/core/nanops.py
+++ xarray/core/nanops.py
@@ -26,13 +26,9 @@ def _maybe_null_out(result, axis, mask, min_count=1):
     """
     xarray version of pandas.core.nanops._maybe_null_out
     """
-    if hasattr(axis, "__len__"):  # if tuple or list
-        raise ValueError(
-            "min_count is not available for reduction with more than one dimensions."
-        )
 
     if axis is not None and getattr(result, "ndim", False):
-        null_mask = (mask.shape[axis] - mask.sum(axis) - min_count) < 0
+        null_mask = (np.take(mask.shape, axis).prod() - mask.sum(axis) - min_count) < 0
         if null_mask.any():
             dtype, fill_value = dtypes.maybe_promote(result.dtype)
             result = result.astype(dtype)
