9058879c42a6ed1d441eb3c0b86fedf1c309444d
keewis <keewis@users.noreply.github.com>
2020-08-22 16:39:45 +0200

    use the fallback_version option to avoid errors on source checkouts (#4358)

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-try:
-    setup(use_scm_version=True)
-except LookupError as e:
-    # .git has been removed, and this is not a package created by sdist
-    # This is the case e.g. of a remote deployment with PyCharm Professional
-    if not str(e).startswith("setuptools-scm was unable to detect version"):
-        raise
-    setup(version="999")
+setup(use_scm_version={"fallback_version": "999"})
