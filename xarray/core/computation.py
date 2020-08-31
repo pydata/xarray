b1194006c6769828c19951b18f53cbd690b927c4
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2020-08-31 09:38:02 +0200

    FIX: catch dask chunk mismatch ValueErrors, warn and set ``allow_rechunk=True``

--- xarray/core/computation.py
+++ xarray/core/computation.py
@@ -645,14 +645,40 @@ def apply_variable_ufunc(
             def func(*arrays):
                 import dask.array as da
 
-                res = da.apply_gufunc(
+                gufunc = functools.partial(
+                    da.apply_gufunc,
                     numpy_func,
                     signature.to_gufunc_string(exclude_dims),
                     *arrays,
                     vectorize=vectorize,
                     output_dtypes=output_dtypes,
-                    **dask_gufunc_kwargs,
                 )
+
+                try:
+                    res = gufunc(**dask_gufunc_kwargs)
+                except ValueError as exc:
+                    if "with different chunksize present" in str(exc):
+                        warnings.warn(
+                            f"Non-core dimension with different chunksize present. To fix, set "
+                            f"``allow_rechunk=True`` in the ``dask_gufunc_kwargs`` parameter. "
+                            f"Not setting will raise dask ValueError ``{str(exc)}`` in a "
+                            f"future version.",
+                            FutureWarning,
+                            stacklevel=2,
+                        )
+                    elif "consists of multiple chunks" in str(exc):
+                        warnings.warn(
+                            f"Core dimension consists of multiple chunks. To fix, rechunk "
+                            f"into a single chunk along this dimension or set "
+                            f"``allow_rechunk=True`` in the ``dask_gufunc_kwargs`` parameter. "
+                            f"Not setting will raise dask ValueError ``{str(exc)}`` in a future version.",
+                            FutureWarning,
+                            stacklevel=2,
+                        )
+                    else:
+                        raise
+                    dask_gufunc_kwargs["allow_rechunk"] = True
+                    res = gufunc(**dask_gufunc_kwargs)
 
                 # todo: covers for https://github.com/dask/dask/pull/6207
                 #  remove when minimal dask version >= 2.17.0
