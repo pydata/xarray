d18882ce08de5422cbeaf341942d7717d5e8ec48
alexamici <a.amici@bopen.eu>
2020-09-25 18:50:05 +0200

    Update xarray/backends/api.py
    Co-authored-by: Joe Hamman <jhamman1@gmail.com>

--- xarray/backends/api.py
+++ xarray/backends/api.py
@@ -179,9 +179,8 @@ def _normalize_path(path):
     if isinstance(path, Path):
         path = str(path)
 
-    if isinstance(path, str):
-        if not is_remote_uri(path):
-            path = os.path.abspath(os.path.expanduser(path))
+    if isinstance(path, str) and not is_remote_uri(path):
+        path = os.path.abspath(os.path.expanduser(path))
 
     return path
 
