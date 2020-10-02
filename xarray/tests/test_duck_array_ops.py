333e8dba55f0165ccadf18f2aaaee9257a4d716b
Mathias Hauser <mathause@users.noreply.github.com>
2020-10-02 11:28:26 +0200

    fix: min_counts for non-nan sum and prod (#4423)
    * fix: min_counts for non-nan sum and prod
    
    * use method1
    
    * typo
    
    * remove empty line
    
    * Update doc/whats-new.rst
    
    Co-authored-by: keewis <keewis@users.noreply.github.com>
    
    Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>
    Co-authored-by: keewis <keewis@users.noreply.github.com>

--- xarray/tests/test_duck_array_ops.py
+++ xarray/tests/test_duck_array_ops.py
@@ -580,15 +580,17 @@ def test_dask_gradient(axis, edge_order):
 @pytest.mark.parametrize("dask", [False, True])
 @pytest.mark.parametrize("func", ["sum", "prod"])
 @pytest.mark.parametrize("aggdim", [None, "x"])
-def test_min_count(dim_num, dtype, dask, func, aggdim):
+@pytest.mark.parametrize("contains_nan", [True, False])
+@pytest.mark.parametrize("skipna", [True, False, None])
+def test_min_count(dim_num, dtype, dask, func, aggdim, contains_nan, skipna):
     if dask and not has_dask:
         pytest.skip("requires dask")
 
-    da = construct_dataarray(dim_num, dtype, contains_nan=True, dask=dask)
+    da = construct_dataarray(dim_num, dtype, contains_nan=contains_nan, dask=dask)
     min_count = 3
 
-    actual = getattr(da, func)(dim=aggdim, skipna=True, min_count=min_count)
-    expected = series_reduce(da, func, skipna=True, dim=aggdim, min_count=min_count)
+    actual = getattr(da, func)(dim=aggdim, skipna=skipna, min_count=min_count)
+    expected = series_reduce(da, func, skipna=skipna, dim=aggdim, min_count=min_count)
     assert_allclose(actual, expected)
     assert_dask_array(actual, dask)
 
