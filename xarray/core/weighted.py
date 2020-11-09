c49d312f2fb86bcad1db338e2bcb57b605d8c754
Mathias Hauser <mathause@users.noreply.github.com>
2020-11-09 14:42:02 +0100

    Update xarray/core/weighted.py

--- xarray/core/weighted.py
+++ xarray/core/weighted.py
@@ -112,9 +112,7 @@ class Weighted:
             return w
 
         if is_duck_dask_array(weights.data):
-            weights = weights.copy(
-                data=weights.data.map_blocks(_weight_check, dtype=weights.dtype)
-            )
+            weights.data.map_blocks(_weight_check, dtype=weights.dtype)
 
         else:
             _weight_check(weights.data)
