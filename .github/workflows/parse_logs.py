9e2a634709d6bcff8c3f702de89cdae65a2496b1
Keewis <keewis@posteo.de>
2020-11-29 23:04:06 +0100

    refactor the short test summary info parsing script

--- .github/workflows/parse_logs.py
+++ .github/workflows/parse_logs.py
@@ -1,21 +1,54 @@
 # type: ignore
+import argparse
+import itertools
 import pathlib
+import textwrap
 
-files = pathlib.Path("logs").rglob("**/*-log")
-files = sorted(filter(lambda x: x.is_file(), files))
+parser = argparse.ArgumentParser()
+parser.add_argument("filepaths", nargs="+", type=pathlib.Path)
+args = parser.parse_args()
 
-message = "\n"
+filepaths = sorted(p for p in args.filepaths if p.is_file())
+
+
+def extract_short_test_summary_info(path):
+    with open(path) as f:
+        lines = (line.rstrip() for line in f)
+        up_to_start_of_section = itertools.dropwhile(
+            lambda l: "=== short test summary info ===" not in l,
+            lines,
+        )
+        up_to_section_content = itertools.islice(up_to_start_of_section, 1, None)
+        section_content = itertools.takewhile(
+            lambda l: l.startswith("FAILED"), up_to_section_content
+        )
+        content = "\n".join(section_content)
+
+    return content
+
+
+def format_log_message(path):
+    py_version = path.name.split("-")[1]
+    summary = f"Python {py_version} Test Summary Info"
+    data = extract_short_test_summary_info(path)
+    message = textwrap.dedent(
+        f"""\
+        <details><summary>{summary}</summary>
+
+        ```
+        {data}
+        ```
+
+        </details>
+        """
+    )
+
+    return message
+
 
 print("Parsing logs ...")
-for file in files:
-    with open(file) as fpt:
-        print(f"Parsing {file.absolute()}")
-        data = fpt.read().split("test summary info")[-1].splitlines()[1:-1]
-        data = "\n".join(data)
-        py_version = file.name.split("-")[1]
-        message = f"{message}\n<details>\n<summary>\nPython {py_version} Test Summary Info\n</summary>\n\n```bash\n{data}\n```\n</details>\n"
+message = "\n\n".join(format_log_message(path) for path in filepaths)
 
 output_file = pathlib.Path("pytest-logs.txt")
-with open(output_file, "w") as fpt:
-    print(f"Writing output file to: {output_file.absolute()} ")
-    fpt.write(message)
+print(f"Writing output file to: {output_file.absolute()}")
+output_file.write_text(message)
