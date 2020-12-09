097a7d9589cd4133652226c80e0d2c34ea4de85a
jbusecke <julius@ldeo.columbia.edu>
2020-12-09 11:42:26 -0500

    Satisfy isort

--- xarray/tests/test_weighted.py
+++ xarray/tests/test_weighted.py
@@ -5,7 +5,7 @@ import xarray as xr
 from xarray import DataArray
 from xarray.tests import assert_allclose, assert_equal, raises_regex
 
-from . import raise_if_dask_computes, requires_dask, requires_cftime
+from . import raise_if_dask_computes, requires_cftime, requires_dask
 
 
 @pytest.mark.parametrize("as_dataset", (True, False))
