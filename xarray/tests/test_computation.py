667adc896ff7b4dd6a41d732c45fbebe24584d11
Keewis <keewis@posteo.de>
2020-12-14 20:19:55 +0100

    update the tests

--- xarray/tests/test_computation.py
+++ xarray/tests/test_computation.py
@@ -1470,16 +1470,9 @@ def test_where():
 
 def test_where_attrs():
     cond = xr.DataArray([True, False], dims="x", attrs={"attr": "value"})
-
-    with xr.set_options(keep_attrs=False):
-        actual = xr.where(cond, 1, 0)
-        expected = xr.DataArray([1, 0], dims="x")
-        assert_identical(expected, actual)
-
-    with xr.set_options(keep_attrs=True):
-        actual = xr.where(cond, 1, 0)
-        expected = xr.DataArray([1, 0], dims="x", attrs={"attr": "value"})
-        assert_identical(expected, actual)
+    actual = xr.where(cond, 1, 0)
+    expected = xr.DataArray([1, 0], dims="x", attrs={"attr": "value"})
+    assert_identical(expected, actual)
 
 
 @pytest.mark.parametrize("use_dask", [True, False])
