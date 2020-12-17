bddd930c4aa86de4f8a06897eb8c0885c6a03c6c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-12-17 12:37:23 -0700

    Remove unmatched paranthesis

--- xarray/conventions.py
+++ xarray/conventions.py
@@ -65,7 +65,7 @@ def _infer_dtype(array, name: T_Name = None) -> np.dtype:
         raise ValueError(
             "unable to infer dtype on variable {!r}; object array "
             "contains mixed native types: {}".format(
-                name, ", ".join(x.__name__ for x in native_dtypes))
+                name, ", ".join(x.__name__ for x in native_dtypes)
             )
         )
 
