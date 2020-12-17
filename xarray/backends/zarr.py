138679748558f41cd28f82a25046bc96b1c4d1ef
aurghs <35919497+aurghs@users.noreply.github.com>
2020-12-17 17:21:57 +0100

    Port all the engines to apiv2 (#4673)
    * first draft: not working
    
    * fix and black
    
    * remove test_autoclose_future_warning
    
    * remove blanc lines
    
    * fix
    
    * reverted delete autoclose test. Added warning in apiv2
    
    * isort
    
    * revert delete test_autoclose_future_warning
    
    * for backward compatibility: add store-backend
    used in open_dataset if a store is passed instead a file_name and an engine
    
    * fix default in store.py
    
    * remove not open_dataset_paramenters definition in store.py
    
    * remove "**kwrargs" in pseudonetcdf_backend.open_dataset_parameter definition
    
    * add a comment to explain why the open_dataset_parameters are explicity defined in peseudonetcdf_.py
    
    * use store.open_backend_dataset_store to reduce duplicated code inside backends
    
    * style
    
    Co-authored-by: Alessandro Amici <a.amici@bopen.eu>

--- xarray/backends/zarr.py
+++ xarray/backends/zarr.py
@@ -2,12 +2,12 @@ import numpy as np
 
 from .. import coding, conventions
 from ..core import indexing
-from ..core.dataset import Dataset
 from ..core.pycompat import integer_types
-from ..core.utils import FrozenDict, HiddenKeyDict, close_on_error
+from ..core.utils import FrozenDict, HiddenKeyDict
 from ..core.variable import Variable
 from .common import AbstractWritableDataStore, BackendArray, _encode_variable_name
 from .plugins import BackendEntrypoint
+from .store import open_backend_dataset_store
 
 # need some special secret attributes to tell us the dimensions
 DIMENSION_KEY = "_ARRAY_DIMENSIONS"
@@ -679,28 +679,16 @@ def open_backend_dataset_zarr(
         chunk_store=chunk_store,
     )
 
-    with close_on_error(store):
-        vars, attrs = store.load()
-        file_obj = store
-        encoding = store.get_encoding()
-
-        vars, attrs, coord_names = conventions.decode_cf_variables(
-            vars,
-            attrs,
-            mask_and_scale=mask_and_scale,
-            decode_times=decode_times,
-            concat_characters=concat_characters,
-            decode_coords=decode_coords,
-            drop_variables=drop_variables,
-            use_cftime=use_cftime,
-            decode_timedelta=decode_timedelta,
-        )
-
-        ds = Dataset(vars, attrs=attrs)
-        ds = ds.set_coords(coord_names.intersection(vars))
-        ds._file_obj = file_obj
-        ds.encoding = encoding
-
+    ds = open_backend_dataset_store(
+        store,
+        mask_and_scale=mask_and_scale,
+        decode_times=decode_times,
+        concat_characters=concat_characters,
+        decode_coords=decode_coords,
+        drop_variables=drop_variables,
+        use_cftime=use_cftime,
+        decode_timedelta=decode_timedelta,
+    )
     return ds
 
 
