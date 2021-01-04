bcb6762165395008ac5746d8a7c2053421cd5f04
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-01-04 14:54:52 +0100

    Print number of variables in repr

--- xarray/core/formatting.py
+++ xarray/core/formatting.py
@@ -370,9 +370,10 @@ def _mapping_repr(mapping, title, summarizer, col_width=None, max_rows=None):
         col_width = _calculate_col_width(mapping)
     if max_rows is None:
         max_rows = OPTIONS["display_max_rows"]
-    summary = [f"{title}:"]
     if mapping:
-        if len(mapping) > max_rows:
+        len_mapping = len(mapping)
+        summary = [f"{title} ({min(max_rows, len_mapping)}/{len_mapping}):"]
+        if len_mapping > max_rows:
             first_rows = max_rows // 2 + max_rows % 2
             items = list(mapping.items())
             summary += [summarizer(k, v, col_width) for k, v in items[:first_rows]]
@@ -383,6 +384,7 @@ def _mapping_repr(mapping, title, summarizer, col_width=None, max_rows=None):
         else:
             summary += [summarizer(k, v, col_width) for k, v in mapping.items()]
     else:
+        summary = [f"{title}:"]
         summary += [EMPTY_REPR]
     return "\n".join(summary)
 
