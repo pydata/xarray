fd822cd5c66d89b8ca60df94590c0bc191bd3fc3
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-01-17 16:45:35 +0100

    Update dataset_plot.py

--- xarray/plot/dataset_plot.py
+++ xarray/plot/dataset_plot.py
@@ -1,8 +1,8 @@
 import functools
 
+import matplotlib as mpl
 import numpy as np
 import pandas as pd
-import matplotlib as mpl
 
 from ..core.alignment import broadcast
 from .facetgrid import _easy_facetgrid
