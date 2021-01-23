5a6b55197f097fa0466074345ce19a1e2b02f46c
Keewis <keewis@posteo.de>
2021-01-23 23:59:29 +0100

    also skip the concat combine_attrs variables test

--- xarray/tests/test_concat.py
+++ xarray/tests/test_concat.py
@@ -313,6 +313,7 @@ class TestConcatDataset:
 
             assert_identical(actual, expected)
 
+    @pytest.mark.skip(reason="not implemented, yet (see #4827)")
     @pytest.mark.parametrize(
         "combine_attrs, var1_attrs, var2_attrs, expected_attrs, expect_exception",
         [
