9e3ebf28e925894c6b349f731d11a35d49c9fe02
Keewis <keewis@posteo.de>
2021-01-26 22:54:05 +0100

    print the repr of a multiindex using only a subset of the coordinate values

--- xarray/core/formatting.py
+++ xarray/core/formatting.py
@@ -300,11 +300,14 @@ def _summarize_coord_multiindex(coord, col_width, marker):
 
 
 def _summarize_coord_levels(coord, col_width, marker="-"):
+    n_values = col_width // 4
+    indices = list(range(0, n_values)) + list(range(-n_values, 0))
+    subset = coord[indices]
     return "\n".join(
         summarize_variable(
-            lname, coord.get_level_variable(lname), col_width, marker=marker
+            lname, subset.get_level_variable(lname), col_width, marker=marker
         )
-        for lname in coord.level_names
+        for lname in subset.level_names
     )
 
 
