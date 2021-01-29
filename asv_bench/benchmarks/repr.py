8f20f1feca36513f166d611a0b068a73cc98d0cc
keewis <keewis@users.noreply.github.com>
2021-01-29 01:16:10 +0100

    Apply suggestions from code review
    Co-authored-by: Maximilian Roos <5635139+max-sixty@users.noreply.github.com>

--- asv_bench/benchmarks/repr.py
+++ asv_bench/benchmarks/repr.py
@@ -11,8 +11,8 @@ class ReprMultiIndex:
         series = pd.Series(range(100000000), index=index)
         self.da = xr.DataArray(series)
 
-    def repr(self):
+    def time_repr(self):
         repr(self.da)
 
-    def repr_html(self):
+    def time_repr_html(self):
         self.da._repr_html_()
