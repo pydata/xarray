2c7b7af6a7a743292b3a5bd0907e07de06b285b8
Aureliana Barghini <a.barghini@bopen.eu>
2021-02-01 14:16:50 +0100

    style

--- xarray/backends/plugins.py
+++ xarray/backends/plugins.py
@@ -9,18 +9,18 @@ import pkg_resources
 from .common import BACKEND_ENTRYPOINTS
 
 
-def remove_duplicates(backend_entrypoints):
+def remove_duplicates(pkg_entrypoints):
 
     # sort and group entrypoints by name
-    backend_entrypoints = sorted(backend_entrypoints, key=lambda ep: ep.name)
-    backend_entrypoints_grouped = itertools.groupby(
-        backend_entrypoints, key=lambda ep: ep.name
+    pkg_entrypoints = sorted(pkg_entrypoints, key=lambda ep: ep.name)
+    pkg_entrypoints_grouped = itertools.groupby(
+        pkg_entrypoints, key=lambda ep: ep.name
     )
     # check if there are multiple entrypoints for the same name
-    unique_backend_entrypoints = []
-    for name, matches in backend_entrypoints_grouped:
+    unique_pkg_entrypoints = []
+    for name, matches in pkg_entrypoints_grouped:
         matches = list(matches)
-        unique_backend_entrypoints.append(matches[0])
+        unique_pkg_entrypoints.append(matches[0])
         matches_len = len(matches)
         if matches_len > 1:
             selected_module_name = matches[0].module_name
@@ -30,7 +30,7 @@ def remove_duplicates(backend_entrypoints):
                 f"\n {all_module_names}.\n It will be used: {selected_module_name}.",
                 RuntimeWarning,
             )
-    return unique_backend_entrypoints
+    return unique_pkg_entrypoints
 
 
 def detect_parameters(open_dataset):
@@ -48,13 +48,13 @@ def detect_parameters(open_dataset):
     return tuple(parameters)
 
 
-def create_engines_dict(backend_entrypoints):
-    engines = {}
-    for backend_ep in backend_entrypoints:
-        name = backend_ep.name
-        backend = backend_ep.load()
-        engines[name] = backend
-    return engines
+def create_backends_dict(pkg_entrypoints):
+    backend_entrypoints = {}
+    for pkg_ep in pkg_entrypoints:
+        name = pkg_ep.name
+        backend = pkg_ep.load()
+        backend_entrypoints[name] = backend
+    return backend_entrypoints
 
 
 def set_missing_parameters(engines):
@@ -64,10 +64,10 @@ def set_missing_parameters(engines):
             backend.open_dataset_parameters = detect_parameters(open_dataset)
 
 
-def build_engines(entrypoints):
+def build_engines(pkg_entrypoints):
     backend_entrypoints = BACKEND_ENTRYPOINTS.copy()
-    pkg_entrypoints = remove_duplicates(entrypoints)
-    external_backend_entrypoints = create_engines_dict(pkg_entrypoints)
+    pkg_entrypoints = remove_duplicates(pkg_entrypoints)
+    external_backend_entrypoints = create_backends_dict(pkg_entrypoints)
     backend_entrypoints.update(external_backend_entrypoints)
     set_missing_parameters(backend_entrypoints)
     return backend_entrypoints
@@ -75,8 +75,8 @@ def build_engines(entrypoints):
 
 @functools.lru_cache(maxsize=1)
 def list_engines():
-    entrypoints = pkg_resources.iter_entry_points("xarray.backends")
-    return build_engines(entrypoints)
+    pkg_entrypoints = pkg_resources.iter_entry_points("xarray.backends")
+    return build_engines(pkg_entrypoints)
 
 
 def guess_engine(store_spec):
