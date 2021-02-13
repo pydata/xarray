fb1fcb90cd22c82d0818aa51e90fcadfdd680436
Keewis <keewis@posteo.de>
2021-02-13 14:53:51 +0100

    add combine_attrs="no_conflict"

--- xarray/tests/test_merge.py
+++ xarray/tests/test_merge.py
@@ -282,7 +282,7 @@ class TestMergeFunction:
 
     def test_merge_no_conflicts_preserve_attrs(self):
         data = xr.Dataset({"x": ([], 0, {"foo": "bar"})})
-        actual = xr.merge([data, data])
+        actual = xr.merge([data, data], combine_attrs="no_conflicts")
         assert_identical(data, actual)
 
     def test_merge_no_conflicts_broadcast(self):
