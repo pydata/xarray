0efbbbb9db61e8ab38aee9cb721078d762fd6e5b
Keewis <keewis@posteo.de>
2021-02-28 02:58:45 +0100

    also test sparse

--- xarray/tests/test_duckarray_testing.py
+++ xarray/tests/test_duckarray_testing.py
@@ -3,10 +3,11 @@ import pytest
 from xarray.duckarray import duckarray_module
 
 pint = pytest.importorskip("pint")
+sparse = pytest.importorskip("sparse")
 ureg = pint.UnitRegistry(force_ndarray_like=True)
 
 
-def create(data, method):
+def create_pint(data, method):
     if method in ("prod",):
         units = "dimensionless"
     else:
@@ -14,13 +15,22 @@ def create(data, method):
     return ureg.Quantity(data, units)
 
 
+def create_sparse(data, method):
+    return sparse.COO.from_numpy(data)
+
+
 TestPint = duckarray_module(
     "pint",
-    create,
+    create_pint,
     marks={
         "TestDataset.test_reduce": [pytest.mark.skip(reason="not implemented yet")],
     },
     global_marks=[
         pytest.mark.filterwarnings("error::pint.UnitStrippedWarning"),
     ],
+)
+
+TestSparse = duckarray_module(
+    "sparse",
+    create_sparse,
 )
