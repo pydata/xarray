d65143823b5100c3792cb5c382e35c09889982d9
Keewis <keewis@posteo.de>
2021-03-01 15:53:52 +0100

    move the utils tests into a different file

--- xarray/tests/test_duckarray_testing.py
+++ xarray/tests/test_duckarray_testing.py
@@ -1,65 +1,10 @@
 import pytest
 
-from xarray import duckarray
 from xarray.duckarray import duckarray_module
 
 pint = pytest.importorskip("pint")
 sparse = pytest.importorskip("sparse")
 ureg = pint.UnitRegistry(force_ndarray_like=True)
-
-
-class Module:
-    def module_test1(self):
-        pass
-
-    def module_test2(self):
-        pass
-
-    @pytest.mark.parametrize("param1", ("a", "b", "c"))
-    def parametrized_test(self, param1):
-        pass
-
-    class Submodule:
-        def submodule_test(self):
-            pass
-
-
-class TestUtils:
-    @pytest.mark.parametrize(
-        ["selector", "expected"],
-        (
-            ("test_function", (["test_function"], None)),
-            (
-                "TestGroup.TestSubgroup.test_function",
-                (["TestGroup", "TestSubgroup", "test_function"], None),
-            ),
-            ("test_function[variant]", (["test_function"], "variant")),
-            (
-                "TestGroup.test_function[variant]",
-                (["TestGroup", "test_function"], "variant"),
-            ),
-        ),
-    )
-    def test_parse_selector(self, selector, expected):
-        actual = duckarray.parse_selector(selector)
-        assert actual == expected
-
-    @pytest.mark.parametrize(
-        ["components", "expected"],
-        (
-            (["module_test1"], (Module, Module.module_test1, "module_test1")),
-            (
-                ["Submodule", "submodule_test"],
-                (Module.Submodule, Module.Submodule.submodule_test, "submodule_test"),
-            ),
-        ),
-    )
-    def test_get_test(self, components, expected):
-        module = Module
-        actual = duckarray.get_test(module, components)
-        print(actual)
-        print(expected)
-        assert actual == expected
 
 
 def create_pint(data, method):
---
+++ xarray/tests/test_duckarray_testing_utils.py
@@ -0,0 +1,55 @@
+import pytest
+
+from xarray import duckarray
+
+
+class Module:
+    def module_test1(self):
+        pass
+
+    def module_test2(self):
+        pass
+
+    @pytest.mark.parametrize("param1", ("a", "b", "c"))
+    def parametrized_test(self, param1):
+        pass
+
+    class Submodule:
+        def submodule_test(self):
+            pass
+
+
+class TestUtils:
+    @pytest.mark.parametrize(
+        ["selector", "expected"],
+        (
+            ("test_function", (["test_function"], None)),
+            (
+                "TestGroup.TestSubgroup.test_function",
+                (["TestGroup", "TestSubgroup", "test_function"], None),
+            ),
+            ("test_function[variant]", (["test_function"], "variant")),
+            (
+                "TestGroup.test_function[variant]",
+                (["TestGroup", "test_function"], "variant"),
+            ),
+        ),
+    )
+    def test_parse_selector(self, selector, expected):
+        actual = duckarray.parse_selector(selector)
+        assert actual == expected
+
+    @pytest.mark.parametrize(
+        ["components", "expected"],
+        (
+            (["module_test1"], (Module, Module.module_test1, "module_test1")),
+            (
+                ["Submodule", "submodule_test"],
+                (Module.Submodule, Module.Submodule.submodule_test, "submodule_test"),
+            ),
+        ),
+    )
+    def test_get_test(self, components, expected):
+        module = Module
+        actual = duckarray.get_test(module, components)
+        assert actual == expected
