4f24ab9caeacab85a9b4bc3bce90a974ae41d7a0
Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
2021-03-06 15:02:01 -0800

    Adjust tests to use updated pandas syntax for offsets (#4537)
    * Adjust tests to use updated pandas syntax for offsets
    
    * Revert irrelevant change
    
    * Retain calcs for `actual`
    
    * Update xarray/tests/test_dataarray.py
    
    Co-authored-by: keewis <keewis@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * fix imports
    
    Co-authored-by: keewis <keewis@users.noreply.github.com>
    Co-authored-by: Keewis <keewis@posteo.de>

--- xarray/tests/test_dataarray.py
+++ xarray/tests/test_dataarray.py
@@ -7,6 +7,7 @@ from textwrap import dedent
 import numpy as np
 import pandas as pd
 import pytest
+from pandas.tseries.frequencies import to_offset
 
 import xarray as xr
 from xarray import (
@@ -2990,9 +2991,13 @@ class TestDataArray:
         actual = array.resample(time="24H").reduce(np.mean)
         assert_identical(expected, actual)
 
+        # Our use of `loffset` may change if we align our API with pandas' changes.
+        # ref https://github.com/pydata/xarray/pull/4537
         actual = array.resample(time="24H", loffset="-12H").mean()
-        expected = DataArray(array.to_series().resample("24H", loffset="-12H").mean())
-        assert_identical(expected, actual)
+        expected_ = array.to_series().resample("24H").mean()
+        expected_.index += to_offset("-12H")
+        expected = DataArray.from_series(expected_)
+        assert_identical(actual, expected)
 
         with raises_regex(ValueError, "index must be monotonic"):
             array[[2, 0, 1]].resample(time="1D")
