19c8a910f0989ace54ce7a571d814d4638ada0eb
Todd Jennings <toddrjen@gmail.com>
2021-03-06 09:01:55 -0500

    test fixes

--- xarray/tests/test_accessor_str.py
+++ xarray/tests/test_accessor_str.py
@@ -639,39 +639,39 @@ def test_extract_extractall_name_collision_raises(dtype):
 
     value = xr.DataArray([["a"]], dims=["X", "Y"]).astype(dtype)
 
-    with pytest.raises(KeyError, match="Dimension X already present in DataArray."):
+    with pytest.raises(KeyError, match="Dimension 'X' already present in DataArray."):
         value.str.extract(pat=pat_str, dim="X")
 
-    with pytest.raises(KeyError, match="Dimension X already present in DataArray."):
+    with pytest.raises(KeyError, match="Dimension 'X' already present in DataArray."):
         value.str.extract(pat=pat_re, dim="X")
 
     with pytest.raises(
-        KeyError, match="Group dimension X already present in DataArray."
+        KeyError, match="Group dimension 'X' already present in DataArray."
     ):
         value.str.extractall(pat=pat_str, group_dim="X", match_dim="ZZ")
 
     with pytest.raises(
-        KeyError, match="Group dimension X already present in DataArray."
+        KeyError, match="Group dimension 'X' already present in DataArray."
     ):
         value.str.extractall(pat=pat_re, group_dim="X", match_dim="YY")
 
     with pytest.raises(
-        KeyError, match="Match dimension Y already present in DataArray."
+        KeyError, match="Match dimension 'Y' already present in DataArray."
     ):
         value.str.extractall(pat=pat_str, group_dim="XX", match_dim="Y")
 
     with pytest.raises(
-        KeyError, match="Match dimension Y already present in DataArray."
+        KeyError, match="Match dimension 'Y' already present in DataArray."
     ):
         value.str.extractall(pat=pat_re, group_dim="XX", match_dim="Y")
 
     with pytest.raises(
-        KeyError, match="Group dimension ZZ is the same as match dimension ZZ."
+        KeyError, match="Group dimension 'ZZ' is the same as match dimension 'ZZ'."
     ):
         value.str.extractall(pat=pat_str, group_dim="ZZ", match_dim="ZZ")
 
     with pytest.raises(
-        KeyError, match="Group dimension ZZ is the same as match dimension ZZ."
+        KeyError, match="Group dimension 'ZZ' is the same as match dimension 'ZZ'."
     ):
         value.str.extractall(pat=pat_re, group_dim="ZZ", match_dim="ZZ")
 
