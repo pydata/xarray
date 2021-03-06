2503af744caa687d6ed3b3f1f24f99ccb0238ae1
Keewis <keewis@posteo.de>
2021-03-06 22:40:43 +0100

    add a test for applying marks to test variants

--- xarray/tests/test_duckarray_testing_utils.py
+++ xarray/tests/test_duckarray_testing_utils.py
@@ -82,3 +82,33 @@ def test_apply_marks_normal(marks):
     expected = [m.mark for m in marks]
 
     assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "marks",
+    (
+        pytest.param([pytest.mark.skip(reason="arbitrary")], id="single mark"),
+        pytest.param(
+            [
+                pytest.mark.filterwarnings("error"),
+                pytest.mark.parametrize("a", (0, 1, 2)),
+            ],
+            id="multiple marks",
+        ),
+    ),
+)
+@pytest.mark.parametrize("variant", ("[a]", "[b]", "[c]"))
+def test_apply_marks_variant(marks, variant):
+    class Module:
+        @pytest.mark.parametrize("param1", ("a", "b", "c"))
+        def func(param1):
+            pass
+
+    module = Module
+    components = ["func"]
+
+    duckarray_testing_utils.apply_marks(module, components, {variant: marks})
+    marked = Module.func
+    actual = marked.pytestmark
+
+    assert len(actual) > 1 and any(mark.name == "attach_marks" for mark in actual)
