8d8021282f8e49b2514696fbff24dfe7477a87f5
Keewis <keewis@posteo.de>
2021-03-09 15:51:56 +0100

    also test dask wrapped by pint

--- xarray/tests/test_duckarray_testing.py
+++ xarray/tests/test_duckarray_testing.py
@@ -2,6 +2,7 @@ import pytest
 
 from .duckarray_testing import duckarray_module
 
+da = pytest.importorskip("dask.array")
 pint = pytest.importorskip("pint")
 sparse = pytest.importorskip("sparse")
 ureg = pint.UnitRegistry(force_ndarray_like=True)
@@ -13,6 +14,11 @@ def create_pint(data, method):
     else:
         units = "m"
     return ureg.Quantity(data, units)
+
+
+def create_pint_dask(data, method):
+    data = da.from_array(data, chunks=(2,))
+    return create_pint(data, method)
 
 
 def create_sparse(data, method):
@@ -28,6 +34,24 @@ TestPint = duckarray_module(
                 pytest.mark.skip(reason="xarray.Variable.argsort does not support dim")
             ],
             "[prod]": [pytest.mark.skip(reason="nanprod drops units")],
+        },
+    },
+    global_marks=[
+        pytest.mark.filterwarnings("error::pint.UnitStrippedWarning"),
+    ],
+)
+
+TestPintDask = duckarray_module(
+    "pint_dask",
+    create_pint_dask,
+    marks={
+        "TestVariable.test_reduce": {
+            "[argsort]": [
+                pytest.mark.skip(reason="xarray.Variable.argsort does not support dim")
+            ],
+            "[cumsum]": [pytest.mark.skip(reason="nancumsum drops the units")],
+            "[median]": [pytest.mark.skip(reason="median does not support dim")],
+            "[prod]": [pytest.mark.skip(reason="prod drops the units")],
         },
     },
     global_marks=[
