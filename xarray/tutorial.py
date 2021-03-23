719765cde58bb987d3cc85a5223549e3aff15f6b
Keewis <keewis@posteo.de>
2021-03-23 17:32:50 +0100

    allow opening rasterio files using open_dataset

--- xarray/tutorial.py
+++ xarray/tutorial.py
@@ -11,15 +11,30 @@ import pathlib
 import numpy as np
 
 from .backends.api import open_dataset as _open_dataset
+from .backends.rasterio_ import open_rasterio
 from .core.dataarray import DataArray
 from .core.dataset import Dataset
+
+
+def _open_rasterio(path, engine=None, **kwargs):
+    data = open_rasterio(path, **kwargs)
+    name = data.name if data.name is not None else "data"
+    return data.to_dataset(name=name)
+
 
 _default_cache_dir_name = "xarray_tutorial_data"
 base_url = "https://github.com/pydata/xarray-data"
 version = "master"
 
+
 external_urls = {
-    "RGB.byte": "https://github.com/mapbox/rasterio/raw/master/tests/data/RGB.byte.tif",
+    "RGB.byte": (
+        "rasterio",
+        "https://github.com/mapbox/rasterio/raw/master/tests/data/RGB.byte.tif",
+    ),
+}
+engine_overrides = {
+    "rasterio": _open_rasterio,
 }
 
 
@@ -64,19 +79,22 @@ def open_dataset(
     elif cache_dir is None:
         cache_dir = pooch.os_cache(_default_cache_dir_name)
 
-    # process the name
-    default_extension = ".nc"
-    path = pathlib.Path(name)
-    if not path.suffix:
-        path = path.with_suffix(default_extension)
+    if name in external_urls:
+        engine, url = external_urls[name]
+    else:
+        # process the name
+        default_extension = ".nc"
+        path = pathlib.Path(name)
+        if not path.suffix:
+            path = path.with_suffix(default_extension)
+        engine = None
 
+        url = f"{base_url}/raw/{version}/{path.name}"
+
+    _open = engine_overrides.get(engine, _open_dataset)
     # retrieve the file
-    filepath = pooch.retrieve(
-        url=f"{base_url}/raw/{version}/{path.name}",
-        known_hash=None,
-        path=cache_dir,
-    )
-    ds = _open_dataset(filepath, **kws)
+    filepath = pooch.retrieve(url=url, known_hash=None, path=cache_dir)
+    ds = _open(filepath, engine=engine, **kws)
     if not cache:
         ds = ds.load()
         pathlib.Path(filepath).unlink()
