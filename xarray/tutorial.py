0b60eb0986c089b97cc908265dbd4d82a0de326f
Keewis <keewis@posteo.de>
2021-03-23 16:52:38 +0100

    remove tutorial.open_rasterio

--- xarray/tutorial.py
+++ xarray/tutorial.py
@@ -11,80 +11,10 @@ import pathlib
 import numpy as np
 
 from .backends.api import open_dataset as _open_dataset
-from .backends.rasterio_ import open_rasterio as _open_rasterio
 from .core.dataarray import DataArray
 from .core.dataset import Dataset
 
 _default_cache_dir_name = "xarray_tutorial_data"
-
-
-def open_rasterio(
-    name,
-    cache=True,
-    cache_dir=None,
-    github_url="https://github.com/mapbox/rasterio",
-    branch="master",
-    **kws,
-):
-    """
-    Open a rasterio dataset from rasterio's online repository (requires internet).
-
-    If a local copy is found then always use that to avoid network traffic.
-
-    Parameters
-    ----------
-    name : str
-        Name of the file containing the dataset. If no suffix is given, assumed
-        to be TIF ('.tif' is appended)
-        e.g. 'RGB.byte'
-    cache_dir : path-like, optional
-        The directory in which to search for and write cached data.
-    cache : boolean, optional
-        If True, then cache data locally for use on subsequent calls
-    github_url : string
-        Github repository where the data is stored
-    branch : string
-        The git branch to download from
-    kws : dict, optional
-        Passed to xarray.open_dataset
-
-    See Also
-    --------
-    xarray.open_rasterio
-    xarray.tutorial.open_dataset
-
-    """
-    try:
-        import pooch
-    except ImportError:
-        raise ImportError("using the tutorial data requires pooch")
-
-    if isinstance(cache_dir, pathlib.Path):
-        cache_dir = os.fspath(cache_dir)
-    elif cache_dir is None:
-        cache_dir = pooch.os_cache(_default_cache_dir_name)
-
-    # process the name
-    default_extension = ".tif"
-    path = pathlib.Path(name)
-    if not path.suffix:
-        path = path.with_suffix(default_extension)
-    elif path.suffix == ".byte":
-        path = path.with_name(name + default_extension)
-
-    # retrieve the file
-    filepath = pooch.retrieve(
-        url=f"{github_url}/raw/{branch}/tests/data/{path.name}",
-        known_hash=None,
-        path=cache_dir,
-    )
-    ds = _open_rasterio(filepath, **kws)
-    if not cache:
-        ds = ds.load()
-        pathlib.Path(filepath).unlink()
-
-    return ds
-
 
 external_urls = {
     "RGB.byte": "https://github.com/mapbox/rasterio/raw/master/tests/data/RGB.byte.tif",
