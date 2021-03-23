b532879c42c1a6641e9571ad22e32821a3795ad7
Keewis <keewis@posteo.de>
2021-03-23 23:36:33 +0100

    typo

--- xarray/tests/test_tutorial.py
+++ xarray/tests/test_tutorial.py
@@ -14,14 +14,14 @@ class TestLoadDataset:
         self.testfile = "tiny"
 
     def test_download_from_github(self, tmp_path, monkeypatch):
-        monkeypatch.setenv("XDG_CACHE_DIR", os.fspatch(tmp_path))
+        monkeypatch.setenv("XDG_CACHE_DIR", os.fspath(tmp_path))
 
         ds = tutorial.open_dataset(self.testfile).load()
         tiny = DataArray(range(5), name="tiny").to_dataset()
         assert_identical(ds, tiny)
 
     def test_download_from_github_load_without_cache(self, tmp_path, monkeypatch):
-        monkeypatch.setenv("XDG_CACHE_DIR", os.fspatch(tmp_path))
+        monkeypatch.setenv("XDG_CACHE_DIR", os.fspath(tmp_path))
 
         ds_nocache = tutorial.open_dataset(self.testfile, cache=False).load()
         ds_cache = tutorial.open_dataset(self.testfile).load()
@@ -30,7 +30,7 @@ class TestLoadDataset:
     def test_download_rasterio_from_github_load_without_cache(
         self, tmp_path, monkeypatch
     ):
-        monkeypatch.setenv("XDG_CACHE_DIR", os.fspatch(tmp_path))
+        monkeypatch.setenv("XDG_CACHE_DIR", os.fspath(tmp_path))
 
         ds_nocache = tutorial.open_dataset("RGB.byte", cache=False).load()
         ds_cache = tutorial.open_dataset("RGB.byte", cache=True).load()
