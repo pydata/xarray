420123cdff8a4c10c9ed5e3e829dde9c62682fe5
b8raoult <53792887+b8raoult@users.noreply.github.com>
2021-03-30 17:00:11 +0100

    Disable the cfgrib backend if eccodes is not installed (#5083)
    * Disable the cfgrib backend if eccodes is not installed
    
    If cfgrib is installed, but libeccodes.so is not installed on the system, Xarray does not load as cfgrib will throw a RuntimeError(), which is not caught.
    
    * Update xarray/backends/cfgrib_.py
    
    Co-authored-by: Mathias Hauser <mathause@users.noreply.github.com>
    
    * Update xarray/backends/cfgrib_.py
    
    Co-authored-by: Mathias Hauser <mathause@users.noreply.github.com>
    
    * Update cfgrib_.py
    
    Missing import
    
    * Run black
    
    * Update whats-new.rst
    
    * Update whats-new.rst
    
    * Update xarray/backends/cfgrib_.py
    
    Co-authored-by: Mathias Hauser <mathause@users.noreply.github.com>

--- xarray/backends/cfgrib_.py
+++ xarray/backends/cfgrib_.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 
 import numpy as np
 
@@ -20,7 +21,12 @@ try:
     has_cfgrib = True
 except ModuleNotFoundError:
     has_cfgrib = False
-
+# cfgrib throws a RuntimeError if eccodes is not installed
+except RuntimeError:
+    warnings.warn(
+        "Failed to load cfgrib - most likely eccodes is missing. Try `import cfgrib` to get the error message"
+    )
+    has_cfgrib = False
 
 # FIXME: Add a dedicated lock, even if ecCodes is supposed to be thread-safe
 #   in most circumstances. See:
