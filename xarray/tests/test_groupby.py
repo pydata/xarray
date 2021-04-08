67c305f952368bcfd87e64652ddf870a2e94dcd1
Alex Marandon <contact@alexmarandon.com>
2021-04-08 16:49:14 +0200

    Remove trailing space from DatasetGroupBy repr (#5131)
    Remove trailing whitespace from DatasetGroupBy representation because
    flake8 reports it as a violation when present in doctests.
    
    Fix #5130

--- xarray/tests/test_groupby.py
+++ xarray/tests/test_groupby.py
@@ -388,7 +388,7 @@ repr_da = xr.DataArray(
 def test_groupby_repr(obj, dim):
     actual = repr(obj.groupby(dim))
     expected = "%sGroupBy" % obj.__class__.__name__
-    expected += ", grouped over %r " % dim
+    expected += ", grouped over %r" % dim
     expected += "\n%r groups with labels " % (len(np.unique(obj[dim])))
     if dim == "x":
         expected += "1, 2, 3, 4, 5."
@@ -405,7 +405,7 @@ def test_groupby_repr(obj, dim):
 def test_groupby_repr_datetime(obj):
     actual = repr(obj.groupby("t.month"))
     expected = "%sGroupBy" % obj.__class__.__name__
-    expected += ", grouped over 'month' "
+    expected += ", grouped over 'month'"
     expected += "\n%r groups with labels " % (len(np.unique(obj.t.dt.month)))
     expected += "1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12."
     assert actual == expected
