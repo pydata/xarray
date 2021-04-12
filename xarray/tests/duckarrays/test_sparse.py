8114d2af59afe126d61fa94c605e8db660c0e1ec
Keewis <keewis@posteo.de>
2021-04-12 15:59:39 +0200

    fix the sparse tests

--- xarray/tests/duckarrays/test_sparse.py
+++ xarray/tests/duckarrays/test_sparse.py
@@ -1,5 +1,6 @@
 import pytest
 
+from .. import assert_allclose
 from . import base
 from .base import utils
 
@@ -7,10 +8,27 @@ sparse = pytest.importorskip("sparse")
 
 
 def create(op):
-    return utils.numpy_array.map(sparse.COO.from_numpy)
+    def convert(arr):
+        if arr.ndim == 0:
+            return arr
+
+        return sparse.COO.from_numpy(arr)
+
+    return utils.numpy_array.map(convert)
 
 
-class TestReduceMethods(base.ReduceMethodTests):
+class TestVariableReduceMethods(base.VariableReduceTests):
     @staticmethod
     def create(op):
         return create(op)
+
+    def check_reduce(self, obj, op, *args, **kwargs):
+        actual = getattr(obj, op)(*args, **kwargs)
+
+        if isinstance(actual.data, sparse.COO):
+            actual = actual.copy(data=actual.data.todense())
+
+        dense = obj.copy(data=obj.data.todense())
+        expected = getattr(dense, op)(*args, **kwargs)
+
+        assert_allclose(actual, expected)
