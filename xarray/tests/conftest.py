2b0dcba1c6769ce1f2c0bc8d2cdc9ce3bd187da5
Keewis <keewis@posteo.de>
2021-04-22 17:58:12 +0200

    add a mark to skip individual test nodes

--- xarray/tests/conftest.py
+++ xarray/tests/conftest.py
@@ -1,26 +1,40 @@
 def pytest_configure(config):
     config.addinivalue_line(
         "markers",
-        "attach_marks(marks): function to attach marks to tests and test variants",
+        "apply_marks(marks): function to attach marks to tests and test variants",
     )
+
+
+def always_sequence(obj):
+    if not isinstance(obj, (list, tuple)):
+        obj = [obj]
+
+    return obj
 
 
 def pytest_collection_modifyitems(session, config, items):
     for item in items:
-        mark = item.get_closest_marker("attach_marks")
+        mark = item.get_closest_marker("apply_marks")
         if mark is None:
             continue
-        index = item.own_markers.index(mark)
-        del item.own_markers[index]
 
         marks = mark.args[0]
         if not isinstance(marks, dict):
             continue
 
+        possible_marks = marks.get(item.originalname)
+        if possible_marks is None:
+            continue
+
+        if not isinstance(possible_marks, dict):
+            for mark in always_sequence(possible_marks):
+                item.add_marker(mark)
+            continue
+
         variant = item.name[len(item.originalname) :]
-        to_attach = marks.get(variant)
+        to_attach = possible_marks.get(variant)
         if to_attach is None:
             continue
 
-        for mark in to_attach:
+        for mark in always_sequence(to_attach):
             item.add_marker(mark)
