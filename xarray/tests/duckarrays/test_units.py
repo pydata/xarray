2a70c380bb972ef9c3700ac79dcae0c664c92011
Keewis <keewis@posteo.de>
2021-04-23 01:24:13 +0200

    use the DataArray reduce tests with pint

--- xarray/tests/duckarrays/test_units.py
+++ xarray/tests/duckarrays/test_units.py
@@ -74,3 +74,58 @@ class TestVariableReduceMethods(base.VariableReduceTests):
 
             assert_units_equal(actual, expected)
             assert_identical(actual, expected)
+
+
+@pytest.mark.apply_marks(
+    {
+        "test_reduce": {
+            "[prod]": pytest.mark.skip(reason="inconsistent implementation in pint"),
+            "[std]": pytest.mark.skip(
+                reason="bottleneck's implementation of std is incorrect for float32"
+            ),
+        }
+    }
+)
+class TestDataArrayReduceMethods(base.DataArrayReduceTests):
+    @st.composite
+    @staticmethod
+    def create(draw, op, shape):
+        if op in ("cumprod",):
+            units = st.just("dimensionless")
+        else:
+            units = all_units
+
+        return Quantity(draw(strategies.numpy_array(shape)), draw(units))
+
+    def check_reduce(self, obj, op, *args, **kwargs):
+        if (
+            op in ("cumprod",)
+            and obj.data.size > 1
+            and obj.data.units != unit_registry.dimensionless
+        ):
+            with pytest.raises(pint.DimensionalityError):
+                getattr(obj, op)(*args, **kwargs)
+        else:
+            actual = getattr(obj, op)(*args, **kwargs)
+
+            note(f"actual:\n{actual}")
+
+            without_units = obj.copy(data=obj.data.magnitude)
+            expected = getattr(without_units, op)(*args, **kwargs)
+
+            func_name = f"nan{op}" if obj.dtype.kind in "fc" else op
+            func = getattr(np, func_name, getattr(np, op))
+            func_kwargs = kwargs.copy()
+            dim = func_kwargs.pop("dim", None)
+            axis = utils.valid_axes_from_dims(obj.dims, dim)
+            func_kwargs["axis"] = axis
+            with utils.suppress_warning(RuntimeWarning):
+                result = func(obj.data, *args, **func_kwargs)
+            units = getattr(result, "units", None)
+            if units is not None:
+                expected = expected.copy(data=Quantity(expected.data, units))
+
+            note(f"expected:\n{expected}")
+
+            assert_units_equal(actual, expected)
+            assert_identical(actual, expected)
