396c2ba8d78f0439b0e9f0e107d1809dbf46862d
Keewis <keewis@posteo.de>
2021-04-23 19:16:51 +0200

    add DataArray and Dataset tests for sparse

--- xarray/tests/duckarrays/test_sparse.py
+++ xarray/tests/duckarrays/test_sparse.py
@@ -2,6 +2,8 @@ import pytest
 
 pytest.importorskip("hypothesis")
 
+from xarray import DataArray, Dataset, Variable
+
 from .. import assert_allclose
 from . import base
 from .base import strategies
@@ -17,6 +19,29 @@ def create(op, shape):
         return sparse.COO.from_numpy(arr)
 
     return strategies.numpy_array(shape).map(convert)
+
+
+def as_dense(obj):
+    if isinstance(obj, Variable) and isinstance(obj.data, sparse.COO):
+        new_obj = obj.copy(data=obj.data.todense())
+    elif isinstance(obj, DataArray):
+        ds = obj._to_temp_dataset()
+        dense = as_dense(ds)
+        new_obj = obj._from_temp_dataset(dense)
+    elif isinstance(obj, Dataset):
+        variables = {name: as_dense(var) for name, var in obj.variables.items()}
+        coords = {
+            name: var for name, var in variables.items() if name in obj._coord_names
+        }
+        data_vars = {
+            name: var for name, var in variables.items() if name not in obj._coord_names
+        }
+
+        new_obj = Dataset(coords=coords, data_vars=data_vars, attrs=obj.attrs)
+    else:
+        new_obj = obj
+
+    return new_obj
 
 
 @pytest.mark.apply_marks(
@@ -36,12 +61,53 @@ class TestVariableReduceMethods(base.VariableReduceTests):
         return create(op, shape)
 
     def check_reduce(self, obj, op, *args, **kwargs):
-        actual = getattr(obj, op)(*args, **kwargs)
+        actual = as_dense(getattr(obj, op)(*args, **kwargs))
+        expected = getattr(as_dense(obj), op)(*args, **kwargs)
 
-        if isinstance(actual.data, sparse.COO):
-            actual = actual.copy(data=actual.data.todense())
+        assert_allclose(actual, expected)
 
-        dense = obj.copy(data=obj.data.todense())
-        expected = getattr(dense, op)(*args, **kwargs)
+
+@pytest.mark.apply_marks(
+    {
+        "test_reduce": {
+            "[cumprod]": pytest.mark.skip(reason="cumprod not implemented by sparse"),
+            "[cumsum]": pytest.mark.skip(reason="cumsum not implemented by sparse"),
+            "[median]": pytest.mark.skip(reason="median not implemented by sparse"),
+            "[std]": pytest.mark.skip(reason="nanstd not implemented by sparse"),
+            "[var]": pytest.mark.skip(reason="nanvar not implemented by sparse"),
+        }
+    }
+)
+class TestDataArrayReduceMethods(base.DataArrayReduceTests):
+    @staticmethod
+    def create(op, shape):
+        return create(op, shape)
+
+    def check_reduce(self, obj, op, *args, **kwargs):
+        actual = as_dense(getattr(obj, op)(*args, **kwargs))
+        expected = getattr(as_dense(obj), op)(*args, **kwargs)
+
+        assert_allclose(actual, expected)
+
+
+@pytest.mark.apply_marks(
+    {
+        "test_reduce": {
+            "[cumprod]": pytest.mark.skip(reason="cumprod not implemented by sparse"),
+            "[cumsum]": pytest.mark.skip(reason="cumsum not implemented by sparse"),
+            "[median]": pytest.mark.skip(reason="median not implemented by sparse"),
+            "[std]": pytest.mark.skip(reason="nanstd not implemented by sparse"),
+            "[var]": pytest.mark.skip(reason="nanvar not implemented by sparse"),
+        }
+    }
+)
+class TestDatasetReduceMethods(base.DatasetReduceTests):
+    @staticmethod
+    def create(op, shape):
+        return create(op, shape)
+
+    def check_reduce(self, obj, op, *args, **kwargs):
+        actual = as_dense(getattr(obj, op)(*args, **kwargs))
+        expected = getattr(as_dense(obj), op)(*args, **kwargs)
 
         assert_allclose(actual, expected)
