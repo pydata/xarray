969749cd55c5aff4923921cbfa49a19f41e9ce1b
Tom White <tom.e.white@gmail.com>
2021-05-05 11:10:26 +0100

    Change test_repr_file_collapsed to work on all platforms

--- xarray/tests/test_formatting.py
+++ xarray/tests/test_formatting.py
@@ -476,7 +476,7 @@ def test_large_array_repr_length():
 
 @requires_netCDF4
 def test_repr_file_collapsed(tmp_path):
-    arr = xr.DataArray(np.arange(100000), dims="test")
+    arr = xr.DataArray(np.arange(300), dims="test")
     arr.to_netcdf(tmp_path / "test.nc", engine="netcdf4")
 
     with xr.open_dataarray(tmp_path / "test.nc") as arr, xr.set_options(
@@ -485,8 +485,8 @@ def test_repr_file_collapsed(tmp_path):
         actual = formatting.array_repr(arr)
         expected = dedent(
             """\
-        <xarray.DataArray (test: 100000)>
-        [100000 values with dtype=int64]
+        <xarray.DataArray (test: 300)>
+        array([  0,   1,   2, ..., 297, 298, 299])
         Dimensions without coordinates: test"""
         )
 
