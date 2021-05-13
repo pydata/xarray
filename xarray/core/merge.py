ed055c3208488fbc4353afb88b4b401982e14bfb
keewis <keewis@users.noreply.github.com>
2021-05-13 19:25:52 +0200

    combine keep_attrs and combine_attrs in apply_ufunc (#5041)

--- xarray/core/merge.py
+++ xarray/core/merge.py
@@ -314,6 +314,7 @@ def merge_coordinates_without_align(
     objects: "List[Coordinates]",
     prioritized: Mapping[Hashable, MergeElement] = None,
     exclude_dims: AbstractSet = frozenset(),
+    combine_attrs: str = "override",
 ) -> Tuple[Dict[Hashable, Variable], Dict[Hashable, Index]]:
     """Merge variables/indexes from coordinates without automatic alignments.
 
@@ -335,7 +336,7 @@ def merge_coordinates_without_align(
     else:
         filtered = collected
 
-    return merge_collected(filtered, prioritized)
+    return merge_collected(filtered, prioritized, combine_attrs=combine_attrs)
 
 
 def determine_coords(
