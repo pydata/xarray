03e72706bfde33f6afd4f79938665d9c22a7750e
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-05-17 01:50:31 +0200

    Add version variable for optional imports in pycompat (#5314)
    * optional import versions to pycompat
    
    * Update indexing.py
    
    * move dtypes to avoid circular import
    
    * Update pycompat.py
    
    * cupy.__version__
    
    * import whole dtypes

--- xarray/core/pycompat.py
+++ xarray/core/pycompat.py
@@ -1,3 +1,5 @@
+from distutils.version import LooseVersion
+
 import numpy as np
 
 from .utils import is_duck_array
@@ -5,8 +7,11 @@ from .utils import is_duck_array
 integer_types = (int, np.integer)
 
 try:
+    import dask
     import dask.array
     from dask.base import is_dask_collection
+
+    dask_version = LooseVersion(dask.__version__)
 
     # solely for isinstance checks
     dask_array_type = (dask.array.Array,)
@@ -16,6 +21,7 @@ try:
 
 
 except ImportError:  # pragma: no cover
+    dask_version = LooseVersion("0.0.0")
     dask_array_type = ()
     is_duck_dask_array = lambda _: False
     is_dask_collection = lambda _: False
@@ -24,14 +30,18 @@ try:
     # solely for isinstance checks
     import sparse
 
+    sparse_version = LooseVersion(sparse.__version__)
     sparse_array_type = (sparse.SparseArray,)
 except ImportError:  # pragma: no cover
+    sparse_version = LooseVersion("0.0.0")
     sparse_array_type = ()
 
 try:
     # solely for isinstance checks
     import cupy
 
+    cupy_version = LooseVersion(cupy.__version__)
     cupy_array_type = (cupy.ndarray,)
 except ImportError:  # pragma: no cover
+    cupy_version = LooseVersion("0.0.0")
     cupy_array_type = ()
