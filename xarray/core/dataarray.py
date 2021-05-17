2578fc3a598140b19d499d6e6a1c6eba521619fc
Ravin Kumar <ravinsdrive@gmail.com>
2021-05-16 20:52:36 -0700

    Remove precipitation from docstring (#5321)

--- xarray/core/dataarray.py
+++ xarray/core/dataarray.py
@@ -288,7 +288,6 @@ class DataArray(AbstractArray, DataWithCoords, DataArrayArithmetic):
 
     >>> np.random.seed(0)
     >>> temperature = 15 + 8 * np.random.randn(2, 2, 3)
-    >>> precipitation = 10 * np.random.rand(2, 2, 3)
     >>> lon = [[-99.83, -99.32], [-99.79, -99.23]]
     >>> lat = [[42.25, 42.21], [42.63, 42.59]]
     >>> time = pd.date_range("2014-09-06", periods=3)
