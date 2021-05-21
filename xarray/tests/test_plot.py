9f7dcc30ef57470935a39045bc7a71e0381345a0
Santiago Soler <santiago.r.soler@gmail.com>
2021-05-21 12:18:39 -0300

    Add test class for pcolormesh with logscale on x and y
    Add a new class that creates a dummy DataArray with logspaces on both
    x and y coordinates. Add method for checking if the vertices of the
    Patches in pcolormesh Collection match the expected values.

--- xarray/tests/test_plot.py
+++ xarray/tests/test_plot.py
@@ -1710,6 +1710,52 @@ class TestPcolormesh(Common2dMixin, PlotTestCase):
         assert artist.get_array().size <= self.darray.size
 
 
+class TestPcolormeshLogscale(PlotTestCase):
+    """
+    Test pcolormesh axes when x and y are in logscale
+    """
+
+    plotfunc = staticmethod(xplt.pcolormesh)
+
+    @pytest.fixture(autouse=True)
+    def setUp(self):
+        self.boundaries = (-1, 9, -4, 3)
+        shape = (8, 11)
+        x = np.logspace(self.boundaries[0], self.boundaries[1], shape[1])
+        y = np.logspace(self.boundaries[2], self.boundaries[3], shape[0])
+        da = DataArray(
+            easy_array(shape, start=-1),
+            dims=["y", "x"],
+            coords={"y": y, "x": x},
+            name="testvar",
+        )
+        self.darray = da
+
+    def test_interval_breaks_logspace(self):
+        """
+        Check if the outer vertices of the pcolormesh are the expected values
+
+        Checks bugfix for #5333
+        """
+        artist = self.darray.plot.pcolormesh(xscale="log", yscale="log")
+
+        # Grab the coordinates of the vertices of the Patches
+        x_vertices = [p.vertices[:, 0] for p in artist.properties()["paths"]]
+        y_vertices = [p.vertices[:, 1] for p in artist.properties()["paths"]]
+
+        # Get the maximum and minimum values for each set of vertices
+        xmin, xmax = np.min(x_vertices), np.max(x_vertices)
+        ymin, ymax = np.min(y_vertices), np.max(y_vertices)
+
+        # Check if they are equal to 10 to the power of the outer value of its
+        # corresponding axis plus or minus the interval in the logspace
+        log_interval = 0.5
+        np.testing.assert_allclose(xmin, 10 ** (self.boundaries[0] - log_interval))
+        np.testing.assert_allclose(xmax, 10 ** (self.boundaries[1] + log_interval))
+        np.testing.assert_allclose(ymin, 10 ** (self.boundaries[2] - log_interval))
+        np.testing.assert_allclose(ymax, 10 ** (self.boundaries[3] + log_interval))
+
+
 @pytest.mark.slow
 class TestImshow(Common2dMixin, PlotTestCase):
 
