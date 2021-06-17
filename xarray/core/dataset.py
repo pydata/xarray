36c5956dfbe1baf1683b2c016c77f2c964fb102d
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-06-17 21:37:56 +0200

    test fixing type in to_stacked_array

--- xarray/core/dataset.py
+++ xarray/core/dataset.py
@@ -3867,7 +3867,7 @@ class Dataset(DataWithCoords, DatasetArithmetic, Mapping):
         self,
         new_dim: Hashable,
         sample_dims: Sequence[Hashable],
-        variable_dim: str = "variable",
+        variable_dim: Hashable = "variable",
         name: Hashable = None,
     ) -> "DataArray":
         """Combine variables of differing dimensionality into a DataArray
