cbf289cffdc26c8d96f790013fc604ccd550e48c
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-06-17 21:49:24 +0200

    test fixing to_stacked_array

--- xarray/core/dataset.py
+++ xarray/core/dataset.py
@@ -3866,7 +3866,7 @@ class Dataset(DataWithCoords, DatasetArithmetic, Mapping):
     def to_stacked_array(
         self,
         new_dim: Hashable,
-        sample_dims: Sequence[Hashable],
+        sample_dims: Mapping[Hashable, Sequence[Hashable]],
         variable_dim: Hashable = "variable",
         name: Hashable = None,
     ) -> "DataArray":
