c9bd39c17ca868fed9a540fc4fa61a4b81b9cbe6
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-06-30 22:20:59 +0200

    Update test_missing.py

--- xarray/tests/test_missing.py
+++ xarray/tests/test_missing.py
@@ -25,7 +25,7 @@ from xarray.tests import (
 )
 
 try:
-    import cftime  # flake8: noqa
+    import cftime  # type: ignore # noqa
 
     from xarray.tests.test_cftime_offsets import _CFTIME_CALENDARS
 except ImportError:
