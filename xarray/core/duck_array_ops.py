5b93fbdb15c7214c531804b0acbfe33addac1791
Thomas Nicholas <tegn500@york.ac.uk>
2021-07-06 14:01:47 -0400

    linting

--- xarray/core/duck_array_ops.py
+++ xarray/core/duck_array_ops.py
@@ -31,13 +31,13 @@ except ImportError:
 
 from numpy import all as array_all  # noqa
 from numpy import any as array_any  # noqa
+from numpy import zeros_like  # noqa
 from numpy import around, broadcast_to  # noqa
 from numpy import concatenate as _concatenate
 from numpy import einsum, isclose, isin, isnan, isnat, pad  # noqa
 from numpy import stack as _stack
 from numpy import take, tensordot, transpose, unravel_index  # noqa
 from numpy import where as _where
-from numpy import zeros_like  # noqa
 from numpy.ma import masked_invalid  # noqa
 
 
