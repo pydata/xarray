d8fa99fe62b9dad9ad9b1aa684d840030af4ac0a
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2021-07-14 11:50:49 +1000

    Lint

--- xarray/coding/strings.py
+++ xarray/coding/strings.py
@@ -29,7 +29,11 @@ def check_vlen_dtype(dtype):
 
 
 def is_unicode_dtype(dtype):
-    return dtype.kind == "U" or check_vlen_dtype(dtype) == str or check_vlen_dtype(dtype) == np.str_
+    return (
+        dtype.kind == "U"
+        or check_vlen_dtype(dtype) == str
+        or check_vlen_dtype(dtype) == np.str_
+    )
 
 
 def is_bytes_dtype(dtype):
