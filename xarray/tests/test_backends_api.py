14380e6b9b656809258e617c8aaa3ffdfffda327
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-08-08 00:56:23 +0200

    Test using correct signature

--- xarray/tests/test_backends_api.py
+++ xarray/tests/test_backends_api.py
@@ -26,6 +26,7 @@ def test_custom_engine():
 
     class CustomBackend(xr.backends.BackendEntrypoint):
         def open_dataset(
+            self,
             filename_or_obj,
             drop_variables=None,
             **kwargs,
