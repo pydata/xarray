d178f0f9682ed2dab79881fb9a906db681db622d
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-08-10 11:42:39 +0200

    Update test_formatting.py

--- xarray/tests/test_formatting.py
+++ xarray/tests/test_formatting.py
@@ -552,19 +552,19 @@ def test__mapping_repr(display_max_rows, n_vars, n_attr):
 
 def test__element_formatter(n_elements=100):
     expected = """\
-    Dimensions without coordinates: dim_0: 3, dim_1: 3, dim_2: 3, dim_3: 3, 
-                                    dim_4: 3, dim_5: 3, dim_6: 3, dim_7: 3, 
-                                    dim_8: 3, dim_9: 3, dim_10: 3, dim_11: 3, 
-                                    dim_12: 3, dim_13: 3, dim_14: 3, dim_15: 3, 
-                                    dim_16: 3, dim_17: 3, dim_18: 3, dim_19: 3, 
-                                    dim_20: 3, dim_21: 3, dim_22: 3, dim_23: 3, 
-                                    ...
-                                    dim_76: 3, dim_77: 3, dim_78: 3, dim_79: 3, 
-                                    dim_80: 3, dim_81: 3, dim_82: 3, dim_83: 3, 
-                                    dim_84: 3, dim_85: 3, dim_86: 3, dim_87: 3, 
-                                    dim_88: 3, dim_89: 3, dim_90: 3, dim_91: 3, 
-                                    dim_92: 3, dim_93: 3, dim_94: 3, dim_95: 3, 
-                                    dim_96: 3, dim_97: 3, dim_98: 3, dim_99: 3"""
+Dimensions without coordinates: dim_0: 3, dim_1: 3, dim_2: 3, dim_3: 3, 
+                                dim_4: 3, dim_5: 3, dim_6: 3, dim_7: 3, 
+                                dim_8: 3, dim_9: 3, dim_10: 3, dim_11: 3, 
+                                dim_12: 3, dim_13: 3, dim_14: 3, dim_15: 3, 
+                                dim_16: 3, dim_17: 3, dim_18: 3, dim_19: 3, 
+                                dim_20: 3, dim_21: 3, dim_22: 3, dim_23: 3, 
+                                ...
+                                dim_76: 3, dim_77: 3, dim_78: 3, dim_79: 3, 
+                                dim_80: 3, dim_81: 3, dim_82: 3, dim_83: 3, 
+                                dim_84: 3, dim_85: 3, dim_86: 3, dim_87: 3, 
+                                dim_88: 3, dim_89: 3, dim_90: 3, dim_91: 3, 
+                                dim_92: 3, dim_93: 3, dim_94: 3, dim_95: 3, 
+                                dim_96: 3, dim_97: 3, dim_98: 3, dim_99: 3"""
     expected = dedent(expected)
 
     intro = "Dimensions without coordinates: "
