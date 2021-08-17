ac2c0ef108e60206fa182302c51528337a7be34b
Tomas Chor <tomaschor@gmail.com>
2021-08-16 19:29:54 -0700

    Improves rendering of complex LaTeX expressions as `long_name`s when plotting (#5682)
    * try to wrap texts differently if they're latex code
    
    * added space around modulo operator
    
    * prevents breaking up of long words
    
    * added test for label_from_attrs() function with a long latex string
    
    * Update xarray/plot/utils.py
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    
    * Update xarray/tests/test_utils.py
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    
    * moved test_latex_name_isnt_split() to test_plots
    
    * Apply suggestions from code review
    
    * fixed test_latex_name_isnt_split()
    
    * ran code through pre-commit
    
    * updated whats-new.rst
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>

--- xarray/plot/utils.py
+++ xarray/plot/utils.py
@@ -490,7 +490,13 @@ def label_from_attrs(da, extra=""):
     else:
         units = _get_units_from_attrs(da)
 
-    return "\n".join(textwrap.wrap(name + extra + units, 30))
+    # Treat `name` differently if it's a latex sequence
+    if name.startswith("$") and (name.count("$") % 2 == 0):
+        return "$\n$".join(
+            textwrap.wrap(name + extra + units, 60, break_long_words=False)
+        )
+    else:
+        return "\n".join(textwrap.wrap(name + extra + units, 30))
 
 
 def _interval_to_mid_points(array):
--- xarray/tests/test_plot.py
+++ xarray/tests/test_plot.py
@@ -2950,3 +2950,10 @@ def test_datarray_scatter(x, y, z, hue, markersize, row, col, add_legend, add_co
             add_legend=add_legend,
             add_colorbar=add_colorbar,
         )
+
+
+def test_latex_name_isnt_split():
+    da = xr.DataArray()
+    long_latex_name = r"$Ra_s = \mathrm{mean}(\epsilon_k) / \mu M^2_\infty$"
+    da.attrs = dict(long_name=long_latex_name)
+    assert label_from_attrs(da) == long_latex_name
