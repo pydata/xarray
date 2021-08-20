3ea8162224ae667e42ed6b34c15b0dbaebea7108
Thomas Nicholas <thomas.nicholas@columbia.edu>
2021-08-19 20:19:57 -0400

    simplified getting and setting nodes

--- xarray/datatree_/datatree/datatree.py
+++ xarray/datatree_/datatree/datatree.py
@@ -112,17 +112,17 @@ class TreeNode(anytree.NodeMixin):
         else:
             raise ValueError(f"{address} is not a valid form of path")
 
-    def get(self, path: PathType) -> TreeNode:
+    def get_node(self, path: PathType) -> TreeNode:
         """
         Access node of the tree lying at the given path.
 
-        Raises a KeyError if not found.
+        Raises a TreeError if not found.
 
         Parameters
         ----------
         path :
-            Path names can be given as unix-like paths, or as tuples of strings
-            (where each string is known as a single "tag").
+            Paths can be given as unix-like paths, or as tuples of strings
+            (where each string is known as a single "tag"). Path includes the name of the target node.
 
         Returns
         -------
@@ -131,73 +131,73 @@ class TreeNode(anytree.NodeMixin):
         p = self._tuple_or_path_to_path(path)
         return anytree.Resolver('name').get(self, p)
 
-    def set(self, path: PathType, value: Union[TreeNode, Dataset, DataArray] = None) -> None:
+    def set_node(
+        self,
+        path: PathType = '/',
+        node: TreeNode = None,
+        new_nodes_along_path: bool = True,
+        allow_overwrite: bool = True,
+    ) -> None:
         """
         Set a node on the tree, overwriting anything already present at that path.
 
-        The new value can be an array or a DataTree, in which case it forms a new node of the tree.
+        The given value either forms a new node of the tree or overwrites an existing node at that location.
 
-        Paths are specified relative to the node on which this method was called.
+        Paths are specified relative to the node on which this method was called, and the name of the node forms the
+        last part of the path. (i.e. `.set_node(path='', TreeNode('a'))` is equivalent to `.add_child(TreeNode('a'))`.
 
         Parameters
         ----------
         path : Union[Hashable, Sequence[Hashable]]
             Path names can be given as unix-like paths, or as tuples of strings (where each string
-            is known as a single "tag").
-        value : Union[TreeNode, Dataset, DataArray, None]
+            is known as a single "tag"). Default is '/'.
+        node : TreeNode
+        new_nodes_along_path : bool
+            If true, then if necessary new nodes will be created along the given path, until the tree can reach the
+            specified location. If false then an error is thrown instead of creating intermediate nodes alang the path.
+        allow_overwrite : bool
+            Whether or not to overwrite any existing node at the location given by path. Default is True.
+
+        Raises
+        ------
+        KeyError
+            If a node already exists at the given path
         """
-        self._set_item(path=path, value=value, new_nodes_along_path=True, allow_overwrite=True)
-
-    def _set_item(self, path: PathType, value: Union[TreeNode, Dataset, DataArray, None],
-                  new_nodes_along_path: bool, allow_overwrite: bool) -> None:
-
-        if not isinstance(value, (TreeNode, Dataset, DataArray)) and value is not None:
-            raise TypeError("Can only set new nodes to TreeNode, Dataset, or DataArray instances, not "
-                            f"{type(value)}")
 
         # Determine full path of new object
         path = self._tuple_or_path_to_path(path)
-        tags = path.split(self.separator)
-        if len(tags) < 1:
-            raise ValueError("Not enough path information provided to create a new node. Please either provide a "
-                             "path containing at least one tag, or a named object for the value.")
-        *tags, last_tag = tags
 
-        # TODO: Check that dimensions/coordinates are compatible with adjacent nodes?
+        if not isinstance(node, TreeNode):
+            raise ValueError(f"Can only set nodes to be subclasses of TreeNode, but node is of type {type(node)}")
+        node_name = node.name
 
-        # Walk to location of new node, creating node objects as we go if necessary
+        # Walk to location of new node, creating intermediate node objects as we go if necessary
         parent = self
+        tags = [tag for tag in path.split(self.separator) if tag not in [self.separator, '']]
         for tag in tags:
             # TODO will this mutation within a for loop actually work?
             if tag not in [child.name for child in parent.children]:
                 if new_nodes_along_path:
-
-                    parent.add_child(TreeNode(name=tag, parent=parent))
+                    # TODO prevent this from leaving a trail of nodes if the assignment fails somehow
+                    parent.add_child(TreeNode(name=tag))
                 else:
-                    # TODO Should this also be before we walk?
                     raise KeyError(f"Cannot reach new node at path {path}: "
                                    f"parent {parent} has no child {tag}")
-            parent = next(c for c in parent.children if c.name == tag)
+            parent = parent.get_node(tag)
 
-        # Deal with anything existing at this location
-        if last_tag in [child.name for child in parent.children]:
+        # Deal with anything already existing at this location
+        if node_name in [child.name for child in parent.children]:
             if allow_overwrite:
-                child = list(parent.children)[last_tag]
+                child = parent.get_node(node_name)
                 child.parent = None
                 del child
             else:
                 # TODO should this be before we walk to the new node?
                 raise KeyError(f"Cannot set item at {path} whilst that path already points to a "
-                               f"{type(parent.get(last_tag))} object")
+                               f"{type(parent.get_node(node_name))} object")
 
-        # Create new child node and set at this location
-        if isinstance(value, TreeNode):
-            new_child = value
-        elif isinstance(value, (Dataset, DataArray)) or value is None:
-            new_child = DatasetNode(name=last_tag, data=value)
-        else:
-            raise TypeError
-        new_child.parent = parent
+        # Place new child node at this location
+        parent.add_child(node)
 
     def glob(self, path: str):
         return self._resolver.glob(self, path)
@@ -507,6 +507,11 @@ class DataTree(DatasetNode):
 
     def as_dataarray(self) -> DataArray:
         return self.ds.as_dataarray()
+
+    @property
+    def groups(self):
+        """Return all netCDF4 groups in the tree, given as a tuple of path-like strings."""
+        return tuple(node.path for node in anytree.iterators.PreOrderIter(self))
 
     def to_netcdf(self, filename: str):
         from .io import _datatree_to_netcdf
---
+++ xarray/datatree_/datatree/tests/test_treenode.py
@@ -0,0 +1,241 @@
+import pytest
+
+from anytree.node.exceptions import TreeError
+from anytree.resolver import ChildResolverError
+
+from datatree.datatree import TreeNode
+
+
+class TestFamilyTree:
+    def test_lonely(self):
+        root = TreeNode("root")
+        assert root.name == "root"
+        assert root.parent is None
+        assert root.children == ()
+
+    def test_parenting(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary", parent=john)
+
+        assert mary.parent == john
+        assert mary in john.children
+
+        with pytest.raises(KeyError, match="already has a child named"):
+            TreeNode("mary", parent=john)
+
+        with pytest.raises(TreeError, match="not of type 'NodeMixin'"):
+            mary.parent = "apple"
+
+    def test_parent_swap(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary", parent=john)
+
+        steve = TreeNode("steve")
+        mary.parent = steve
+        assert mary in steve.children
+        assert mary not in john.children
+
+    def test_multi_child_family(self):
+        mary = TreeNode("mary")
+        kate = TreeNode("kate")
+        john = TreeNode("john", children=[mary, kate])
+        assert mary in john.children
+        assert kate in john.children
+        assert mary.parent is john
+        assert kate.parent is john
+
+    def test_disown_child(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary", parent=john)
+        mary.parent = None
+        assert mary not in john.children
+
+    def test_add_child(self):
+        john = TreeNode("john")
+        kate = TreeNode("kate")
+        john.add_child(kate)
+        assert kate in john.children
+        assert kate.parent is john
+        with pytest.raises(KeyError, match="already has a child named"):
+            john.add_child(TreeNode("kate"))
+
+    def test_assign_children(self):
+        john = TreeNode("john")
+        jack = TreeNode("jack")
+        jill = TreeNode("jill")
+
+        john.children = (jack, jill)
+        assert jack in john.children
+        assert jack.parent is john
+        assert jill in john.children
+        assert jill.parent is john
+
+        evil_twin_jill = TreeNode("jill")
+        with pytest.raises(KeyError, match="already has a child named"):
+            john.children = (jack, jill, evil_twin_jill)
+
+    def test_sibling_relationships(self):
+        mary = TreeNode("mary")
+        kate = TreeNode("kate")
+        ashley = TreeNode("ashley")
+        john = TreeNode("john", children=[mary, kate, ashley])
+        assert mary in kate.siblings
+        assert ashley in kate.siblings
+        assert kate not in kate.siblings
+        with pytest.raises(AttributeError):
+            kate.siblings = john
+
+    @pytest.mark.xfail
+    def test_adoption(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_root(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_ancestors(self):
+        raise NotImplementedError
+
+    @pytest.mark.xfail
+    def test_descendants(self):
+        raise NotImplementedError
+
+
+class TestGetNodes:
+    def test_get_child(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary", parent=john)
+        assert john.get_node("mary") is mary
+        assert john.get_node(("mary",)) is mary
+
+    def test_get_nonexistent_child(self):
+        john = TreeNode("john")
+        TreeNode("jill", parent=john)
+        with pytest.raises(ChildResolverError):
+            john.get_node("mary")
+
+    def test_get_grandchild(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary", parent=john)
+        sue = TreeNode("sue", parent=mary)
+        assert john.get_node("mary/sue") is sue
+        assert john.get_node(("mary", "sue")) is sue
+
+    def test_get_great_grandchild(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary", parent=john)
+        sue = TreeNode("sue", parent=mary)
+        steven = TreeNode("steven", parent=sue)
+        assert john.get_node("mary/sue/steven") is steven
+        assert john.get_node(("mary", "sue", "steven")) is steven
+
+    def test_get_from_middle_of_tree(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary", parent=john)
+        sue = TreeNode("sue", parent=mary)
+        steven = TreeNode("steven", parent=sue)
+        assert mary.get_node("sue/steven") is steven
+        assert mary.get_node(("sue", "steven")) is steven
+
+
+class TestSetNodes:
+    def test_set_child_node(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary")
+        john.set_node('/', mary)
+
+        mary = john.children[0]
+        assert mary.name == "mary"
+        assert isinstance(mary, TreeNode)
+        assert mary.children is ()
+
+    def test_child_already_exists(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary", parent=john)
+        marys_replacement = TreeNode("mary")
+
+        with pytest.raises(KeyError):
+            john.set_node('/', marys_replacement, allow_overwrite=False)
+
+    def test_set_grandchild(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary")
+        rose = TreeNode("rose")
+        john.set_node('/', mary)
+        john.set_node('/mary/', rose)
+
+        mary = john.children[0]
+        assert mary.name == "mary"
+        assert isinstance(mary, TreeNode)
+        assert rose in mary.children
+
+        rose = mary.children[0]
+        assert rose.name == "rose"
+        assert isinstance(rose, TreeNode)
+        assert rose.children is ()
+
+    def test_set_grandchild_and_create_intermediate_child(self):
+        john = TreeNode("john")
+        rose = TreeNode("rose")
+        john.set_node("/mary/", rose)
+
+        mary = john.children[0]
+        assert mary.name == "mary"
+        assert isinstance(mary, TreeNode)
+        assert mary.children[0] is rose
+
+        rose = mary.children[0]
+        assert rose.name == "rose"
+        assert isinstance(rose, TreeNode)
+        assert rose.children is ()
+
+    def test_no_intermediate_children_allowed(self):
+        john = TreeNode("john")
+        rose = TreeNode("rose")
+        with pytest.raises(KeyError, match="Cannot reach"):
+            john.set_node(path="mary", node=rose, new_nodes_along_path=False, allow_overwrite=True)
+
+    def test_set_great_grandchild(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary", parent=john)
+        rose = TreeNode("rose", parent=mary)
+        sue = TreeNode("sue")
+        john.set_node("mary/rose", sue)
+        assert sue.parent is rose
+
+    def test_overwrite_child(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary")
+        john.set_node('/', mary)
+        assert mary in john.children
+
+        marys_evil_twin = TreeNode("mary")
+        john.set_node('/', marys_evil_twin)
+        assert marys_evil_twin in john.children
+        assert mary not in john.children
+
+    def test_dont_overwrite_child(self):
+        john = TreeNode("john")
+        mary = TreeNode("mary")
+        john.set_node('/', mary)
+        assert mary in john.children
+
+        marys_evil_twin = TreeNode("mary")
+        with pytest.raises(KeyError, match="path already points"):
+            john.set_node('', marys_evil_twin, new_nodes_along_path=True, allow_overwrite=False)
+        assert mary in john.children
+        assert marys_evil_twin not in john.children
+
+
+class TestPaths:
+    def test_relative_path(self):
+        ...
+
+
+class TestTags:
+    ...
+
+
+class TestRenderTree:
+    ...
