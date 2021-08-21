8d6af1d96ce979cc4bd34e039655fc005169887a
keewis <keewis@users.noreply.github.com>
2021-08-21 13:24:37 +0200

    extend show_versions (#5724)
    * print sparse in print_versions
    
    * add fsspec to print_versions
    
    * add cupy [skip-ci]

--- xarray/util/print_versions.py
+++ xarray/util/print_versions.py
@@ -118,7 +118,10 @@ def show_versions(file=sys.stdout):
         ("cartopy", lambda mod: mod.__version__),
         ("seaborn", lambda mod: mod.__version__),
         ("numbagg", lambda mod: mod.__version__),
+        ("fsspec", lambda mod: mod.__version__),
+        ("cupy", lambda mod: mod.__version__),
         ("pint", lambda mod: mod.__version__),
+        ("sparse", lambda mod: mod.__version__),
         # xarray setup/test
         ("setuptools", lambda mod: mod.__version__),
         ("pip", lambda mod: mod.__version__),
