d41fda7f63b3f3172051d8a0eb559bc88cfb45e3
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-09-07 19:21:55 +0200

    Update groupby.py

--- asv_bench/benchmarks/groupby.py
+++ asv_bench/benchmarks/groupby.py
@@ -8,21 +8,23 @@ from . import parameterized, requires_dask
 
 class GroupBy:
     def setup(self, *args, **kwargs):
-        self.ds = xr.Dataset({
-            "a": xr.DataArray(np.r_[np.arange(500.), np.arange(500.)]),
-            "b": xr.DataArray(np.arange(1000.)),
-        })
+        self.ds = xr.Dataset(
+            {
+                "a": xr.DataArray(np.r_[np.arange(500.0), np.arange(500.0)]),
+                "b": xr.DataArray(np.arange(1000.0)),
+            }
+        )
 
-    @parameterized(["method"], [("sum", "mean")])
+    # @parameterized(["method"], [("sum", "mean")])
     def agg(self, method):
-        getattr(self.ds.groupby("a"), method)()
+        return getattr(self.ds.groupby("a"), method)()
 
 
 class GroupByDask(GroupBy):
     def setup(self, *args, **kwargs):
         requires_dask()
         super().setup(**kwargs)
-        self.ds = self.ds.chunk({"t": 50})
+        self.ds = self.ds.chunk({"dim_0": 50})
 
 
 class GroupByDataFrame(GroupBy):
@@ -35,4 +37,4 @@ class GroupByDaskDataFrame(GroupBy):
     def setup(self, *args, **kwargs):
         requires_dask()
         super().setup(**kwargs)
-        self.ds = self.ds.chunk({"t": 50}).to_dataframe()
+        self.ds = self.ds.chunk({"dim_0": 50}).to_dataframe()
