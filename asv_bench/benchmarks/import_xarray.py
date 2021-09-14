7f402faf679ad739c57f6e1f72efffe08378ee54
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-09-14 21:28:55 +0200

    Update import_xarray.py

--- asv_bench/benchmarks/import_xarray.py
+++ asv_bench/benchmarks/import_xarray.py
@@ -1,7 +1,7 @@
 class ImportXarray:
     def setup(self, *args, **kwargs):
         def import_xr():
-            import xarray
+            import xarray  # noqa: f401
 
         self._import_xr = import_xr
 
