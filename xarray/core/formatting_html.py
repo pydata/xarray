98dec8eb822575a0fa14185dc0f45eb30d5b665c
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-10-25 21:31:58 +0200

    Use importlib.resources to load files

--- xarray/core/formatting_html.py
+++ xarray/core/formatting_html.py
@@ -2,8 +2,7 @@ import uuid
 from collections import OrderedDict
 from functools import lru_cache, partial
 from html import escape
-
-import pkg_resources
+from importlib import resources
 
 from .formatting import inline_variable_array_repr, short_data_repr
 from .options import _get_boolean_with_default
@@ -15,7 +14,7 @@ STATIC_FILES = ("static/html/icons-svg-inline.html", "static/css/style.css")
 def _load_static_files():
     """Lazily load the resource files into memory the first time they are needed"""
     return [
-        pkg_resources.resource_string("xarray", fname).decode("utf8")
+        resources.files("xarray").joinpath(fname).read_bytes().decode("utf8")
         for fname in STATIC_FILES
     ]
 
