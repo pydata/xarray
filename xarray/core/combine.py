f475f444740496cc5a2208113988f09b70d90c0b
Tom Nicholas <35968931+TomNicholas@users.noreply.github.com>
2021-10-29 15:33:42 -0400

    Update xarray/core/combine.py
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>

--- xarray/core/combine.py
+++ xarray/core/combine.py
@@ -905,6 +905,7 @@ def combine_by_coords(
         a        (x) float64 1.0 2.0 3.0 4.0
 
     If all the DataArrays are unnamed, a single DataArray will be returned, e.g.
+
     >>> unnamed_da1 = xr.DataArray(data=[1.0, 2.0], coords={"x": [0, 1]}, dims="x")
     >>> unnamed_da2 = xr.DataArray(data=[3.0, 4.0], coords={"x": [2, 3]}, dims="x")
     >>> xr.combine_by_coords([unnamed_da1, unnamed_da2])
