ff6419522bd30c71fd5778d909405186eafb8559
Illviljan <14371165+Illviljan@users.noreply.github.com>
2021-11-06 18:29:20 +0100

    Update groupby.py

--- asv_bench/benchmarks/groupby.py
+++ asv_bench/benchmarks/groupby.py
@@ -8,10 +8,11 @@ from . import parameterized, requires_dask
 
 class GroupBy:
     def setup(self, *args, **kwargs):
+        self.n = 100
         self.ds1d = xr.Dataset(
             {
-                "a": xr.DataArray(np.r_[np.arange(300), np.arange(300)]),
-                "b": xr.DataArray(np.arange(600)),
+                "a": xr.DataArray(np.r_[np.arange(self.n), np.arange(self.n)]),
+                "b": xr.DataArray(np.arange(2 * self.n)),
             }
         )
         self.ds2d = self.ds1d.expand_dims(z=10)
@@ -35,8 +36,10 @@ class GroupByDask(GroupBy):
     def setup(self, *args, **kwargs):
         requires_dask()
         super().setup(**kwargs)
-        self.ds1d = self.ds1d.sel(dim_0=slice(150)).chunk({"dim_0": 50})
-        self.ds2d = self.ds2d.sel(dim_0=slice(150)).chunk({"dim_0": 50, "z": 4})
+        self.ds1d = self.ds1d.sel(dim_0=slice(self.n * 0.5)).chunk({"dim_0": 50})
+        self.ds2d = self.ds2d.sel(dim_0=slice(self.n * 0.5)).chunk(
+            {"dim_0": 50, "z": 4}
+        )
 
 
 class GroupByDataFrame(GroupBy):
@@ -74,7 +77,7 @@ class Resample:
     @parameterized(["method", "ndim"], [("sum", "mean"), (1, 2)])
     def time_agg_large_num_groups(self, method, ndim):
         ds = getattr(self, f"ds{ndim}d")
-        getattr(ds.resample(time="12H"), method)()
+        getattr(ds.resample(time="24H"), method)()
 
 
 class ResampleDask(Resample):
