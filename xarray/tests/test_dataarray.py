45feeaba8f3f19c8740da061a33839c5b2cf2ece
Deepak Cherian <deepak@cherian.net>
2021-11-25 21:52:06 -0700

    Annotate some reduction tests.

--- xarray/tests/test_dataarray.py
+++ xarray/tests/test_dataarray.py
@@ -2392,7 +2392,7 @@ class TestDataArray:
         expected = DataArray([[-1, 0, 0], [-3, 0, 0]], coords, dims=["x", "y"])
         assert_identical(expected, actual)
 
-    def test_reduce(self):
+    def test_reduce(self) -> None:
         coords = {
             "x": [-1, -2],
             "y": ["ab", "cd", "ef"],
@@ -2433,7 +2433,7 @@ class TestDataArray:
         expected = DataArray(orig.values.astype(int), dims=["x", "y"]).mean("x")
         assert_equal(actual, expected)
 
-    def test_reduce_keepdims(self):
+    def test_reduce_keepdims(self) -> None:
         coords = {
             "x": [-1, -2],
             "y": ["ab", "cd", "ef"],
