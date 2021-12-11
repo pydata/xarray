58af567608e163a325f5ae85bfbc50235e190ea6
Christian Jauvin <cjauvin@gmail.com>
2021-12-11 12:22:20 -0500

    Expand scope of invalid q value test

--- xarray/tests/test_weighted.py
+++ xarray/tests/test_weighted.py
@@ -269,14 +269,19 @@ def test_weighted_quantile_bool():
     assert_equal(expected, result)
 
 
-def test_weighted_quantile_with_2d_q():
+@pytest.mark.parametrize("q", (-1, 1.1, ((0.2, 0.4), (0.6, 0.8))))
+def test_weighted_quantile_with_invalid_q(q):
 
     da = DataArray([1, 1.9, 2.2, 3, 3.7, 4.1, 5])
-    q = np.array([0.2, 0.4, 0.6, 0.8]).reshape(2, 2)
+    q = np.asarray(q)
     weights = xr.ones_like(da)
 
-    with pytest.raises(ValueError, match="q must be a scalar or 1d"):
-        da.weighted(weights).quantile(q)
+    if q.ndim <= 1:
+        with pytest.raises(ValueError, match="q values must be between 0 and 1"):
+            da.weighted(weights).quantile(q)
+    else:
+        with pytest.raises(ValueError, match="q must be a scalar or 1d"):
+            da.weighted(weights).quantile(q)
 
 
 @pytest.mark.parametrize(
