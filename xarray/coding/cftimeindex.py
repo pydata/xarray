c0cd6e4ac7cdbeefc77a898f254fe9c325542d6e
Aaron Spring <aaronspring@users.noreply.github.com>
2022-01-04 23:13:33 +0100

    Apply suggestions from code review

--- xarray/coding/cftimeindex.py
+++ xarray/coding/cftimeindex.py
@@ -554,10 +554,11 @@ class CFTimeIndex(pd.Index):
         elif isinstance(freq, str):
             if "us" in freq and not isinstance(n, int):
                 raise TypeError("float n cannot be combined with freq='us'.")
-            larger_than_week_freq = (
-                True if freq in ["AS", "A", "M", "MS", "Q", "QS"] else False
+            freq_no_offset = freq.split(“-“)[0] if “-“ in freq else freq
+            larger_than_day_freq = (
+                True if freq_no_offset in ["AS", "A", “YS”, "Y”, "M", "MS", "Q", "QS"] else False
             )  # todo make more robust
-            if isinstance(n, float) and larger_than_week_freq:
+            if isinstance(n, float) and larger_than_day_freq:
                 raise TypeError(
                     "If 'n' is float, 'freq' must be smaller or equal to days/D"
                 )
