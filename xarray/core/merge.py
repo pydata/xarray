48062b29d9071eb919086f0293a0234bbb3da7c6
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-01-31 11:38:15 +0100

    fix

--- xarray/core/merge.py
+++ xarray/core/merge.py
@@ -407,7 +407,7 @@ def coerce_pandas_values(objects: Iterable[CoercibleMapping]) -> list[DatasetLik
         else:
             variables = {}
             if isinstance(obj, PANDAS_TYPES):
-                obj = dict(obj.iter())
+                obj = dict(obj.iteritems())
             for k, v in obj.items():
                 if isinstance(v, PANDAS_TYPES):
                     v = DataArray(v)
