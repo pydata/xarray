9f98c9d8980ab9150940c634ed2583d3746afdcf
Mathias Hauser <mathias.hauser@env.ethz.ch>
2022-01-31 11:55:18 +0100

    move warning filter

--- xarray/tests/test_variable.py
+++ xarray/tests/test_variable.py
@@ -2612,12 +2612,15 @@ class TestNumpyCoercion:
         np.testing.assert_equal(v.to_numpy(), np.array([1, 2, 3]))
 
     @requires_pint
-    @pytest.mark.filterwarnings("ignore::pint.UnitStrippedWarning")
     def test_from_pint(self, Var):
-        from pint import Quantity
+        import pint
 
         arr = np.array([1, 2, 3])
-        v = Var("x", Quantity(arr, units="m"))
+
+        # IndexVariable strips the unit
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=pint.UnitStrippedWarning)
+            v = Var("x", pint.Quantity(arr, units="m"))
 
         assert_identical(v.as_numpy(), Var("x", arr))
         np.testing.assert_equal(v.to_numpy(), arr)
@@ -2648,14 +2651,17 @@ class TestNumpyCoercion:
 
     @requires_dask
     @requires_pint
-    @pytest.mark.filterwarnings("ignore::pint.UnitStrippedWarning")
     def test_from_pint_wrapping_dask(self, Var):
         import dask
-        from pint import Quantity
+        import pint
 
         arr = np.array([1, 2, 3])
         d = dask.array.from_array(np.array([1, 2, 3]))
-        v = Var("x", Quantity(d, units="m"))
+
+        # IndexVariable strips the unit
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=pint.UnitStrippedWarning)
+            v = Var("x", pint.Quantity(d, units="m"))
 
         result = v.as_numpy()
         assert_identical(result, Var("x", arr))
