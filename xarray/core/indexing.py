e61cbec145a3c5a46edf2845e36de1471e1c8d21
Benoit Bovy <benbovy@gmail.com>
2022-03-22 13:49:50 +0100

    add _repr_html_ for PandasMultiIndexingAdapter
    This may greatly speed-up the html repr of Xarray objects with
    multi-indexes
    
    This optimized _repr_html_ is now used for formatting the array detailed
    view of multi-index coordinates, instead of converting the full index /
    levels to numpy arrays before formatting it.

--- xarray/core/indexing.py
+++ xarray/core/indexing.py
@@ -5,6 +5,7 @@ from collections import Counter, defaultdict
 from contextlib import suppress
 from dataclasses import dataclass, field
 from datetime import timedelta
+from html import escape
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -1513,23 +1514,31 @@ class PandasMultiIndexingAdapter(PandasIndexingAdapter):
             )
             return f"{type(self).__name__}{props}"
 
-    def _repr_inline_(self, max_width) -> str:
-        # special implementation to speed-up the repr for big multi-indexes
+    def _get_array_subset(self, size: int) -> np.ndarray:
+        # used to speed-up the repr for big multi-indexes
+
+        if self.size > 200 and size < self.size:
+            n_values = size
+            indices = np.concatenate([np.arange(0, n_values), np.arange(-n_values, 0)])
+            subset = self[OuterIndexer((indices,))]
+        else:
+            subset = self
+
+        return np.asarray(subset)
+
+    def _repr_inline_(self, max_width: int) -> str:
+        from .formatting import format_array_flat
+
         if self.level is None:
             return "MultiIndex"
         else:
-            from .formatting import format_array_flat
+            return format_array_flat(self._get_array_subset(max_width), max_width)
 
-            if self.size > 100 and max_width < self.size:
-                n_values = max_width
-                indices = np.concatenate(
-                    [np.arange(0, n_values), np.arange(-n_values, 0)]
-                )
-                subset = self[OuterIndexer((indices,))]
-            else:
-                subset = self
+    def _repr_html_(self) -> str:
+        from .formatting import short_numpy_repr
 
-            return format_array_flat(np.asarray(subset), max_width)
+        array_repr = short_numpy_repr(self._get_array_subset(200))
+        return f"<pre>{escape(array_repr)}</pre>"
 
     def copy(self, deep: bool = True) -> "PandasMultiIndexingAdapter":
         # see PandasIndexingAdapter.copy
