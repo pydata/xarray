e6f4675ca323c79928ea39a8b050f74900401460
Michael Niklas <mick.niklas@gmail.com>
2022-05-01 19:12:13 +0200

    Revert "comment some code until PR#6556"
    This reverts commit 0ef3462e6b6d6ed4e926de728685b0e169e85f13.

--- xarray/tests/test_computation.py
+++ xarray/tests/test_computation.py
@@ -2003,10 +2003,8 @@ def test_polyval(use_dask, x, coeffs, expected) -> None:
             pytest.skip("requires dask")
         coeffs = coeffs.chunk({"degree": 2})
         x = x.chunk({"x": 2})
-    # uncomment with https://github.com/pydata/xarray/pull/6556
-    # with raise_if_dask_computes():
-    #     actual = xr.polyval(x, coeffs)
-    actual = xr.polyval(x, coeffs)
+    with raise_if_dask_computes():
+        actual = xr.polyval(x, coeffs)
     xr.testing.assert_allclose(actual, expected)
 
 
