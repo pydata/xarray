a824ad2b70dfedd186c4070e482119ba7234b22d
dcherian <deepak@cherian.net>
2022-05-02 10:54:05 -0600

    Minor cleanup

--- asv_bench/benchmarks/polyfit.py
+++ asv_bench/benchmarks/polyfit.py
@@ -2,25 +2,24 @@ import xarray as xr
 
 from . import parameterized, randn, requires_dask
 
-ndegs = (2, 5, 20)
-nxs = (10**2, 10**6)
-
-xs = {nx: xr.DataArray(randn((nx,)), dims="x", name="x") for nx in nxs}
-coeffs = {ndeg: xr.DataArray(randn((ndeg,)), dims="degree") for ndeg in ndegs}
+NDEGS = (2, 5, 20)
+NX = (10**2, 10**6)
 
 
 class Polyval:
     def setup(self, *args, **kwargs):
-        self.coeffs = coeffs
-        self.xs = xs
+        self.xs = {nx: xr.DataArray(randn((nx,)), dims="x", name="x") for nx in NX}
+        self.coeffs = {
+            ndeg: xr.DataArray(randn((ndeg,)), dims="degree") for ndeg in NDEGS
+        }
 
-    @parameterized(["nx", "ndeg"], [nxs, ndegs])
+    @parameterized(["nx", "ndeg"], [NX, NDEGS])
     def time_polyval(self, nx, ndeg):
         x = self.xs[nx]
         c = self.coeffs[ndeg]
         xr.polyval(x, c).compute()
 
-    @parameterized(["nx", "ndeg"], [nxs, ndegs])
+    @parameterized(["nx", "ndeg"], [NX, NDEGS])
     def peakmem_polyval(self, nx, ndeg):
         x = self.xs[nx]
         c = self.coeffs[ndeg]
@@ -31,4 +30,4 @@ class PolyvalDask(Polyval):
     def setup(self, *args, **kwargs):
         requires_dask()
         super().setup(*args, **kwargs)
-        self.xs = {nx: self.xs[nx].chunk({"x": 10000}) for nx in nxs}
+        self.xs = {k: v.chunk({"x": 10000}) for k, v in self.xs.items()}
