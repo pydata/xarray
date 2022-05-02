c4ced87b83326a4baa577f1f985667944d475bca
Deepak Cherian <dcherian@users.noreply.github.com>
2022-05-01 20:56:48 -0600

    Update asv_bench/benchmarks/polyfit.py

--- asv_bench/benchmarks/polyfit.py
+++ asv_bench/benchmarks/polyfit.py
@@ -3,7 +3,7 @@ import xarray as xr
 from . import parameterized, randn, requires_dask
 
 ndegs = (2, 5, 20)
-nxs = (10**2, 10**4, 10**6, 10**8)
+nxs = (10**2, 10**6)
 
 xs = {nx: xr.DataArray(randn((nx,)), dims="x", name="x") for nx in nxs}
 coeffs = {ndeg: xr.DataArray(randn((ndeg,)), dims="degree") for ndeg in ndegs}
