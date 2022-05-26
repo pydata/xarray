0ea1e355412088f8f4e12117eb365ac0103bab43
Joe Hamman <jhamman@ucar.edu>
2022-05-26 15:17:35 -0400

    Feature/to dict encoding (#6635)
    * add encoding option to dataset/dataarray/variable to_dict methods
    
    * Update xarray/core/dataset.py
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    
    * Update xarray/tests/test_dataarray.py
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    
    * type dict in to_dict
    
    * add whats new
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>

--- xarray/core/dataset.py
+++ xarray/core/dataset.py
@@ -5944,7 +5944,7 @@ class Dataset(DataWithCoords, DatasetReductions, DatasetArithmetic, Mapping):
 
         return df
 
-    def to_dict(self, data=True):
+    def to_dict(self, data: bool = True, encoding: bool = False) -> dict:
         """
         Convert this dataset to a dictionary following xarray naming
         conventions.
@@ -5958,21 +5958,34 @@ class Dataset(DataWithCoords, DatasetReductions, DatasetArithmetic, Mapping):
         data : bool, optional
             Whether to include the actual data in the dictionary. When set to
             False, returns just the schema.
+        encoding : bool, optional
+            Whether to include the Dataset's encoding in the dictionary.
+
+        Returns
+        -------
+        d : dict
 
         See Also
         --------
         Dataset.from_dict
+        DataArray.to_dict
         """
-        d = {
+        d: dict = {
             "coords": {},
             "attrs": decode_numpy_dict_values(self.attrs),
             "dims": dict(self.dims),
             "data_vars": {},
         }
         for k in self.coords:
-            d["coords"].update({k: self[k].variable.to_dict(data=data)})
+            d["coords"].update(
+                {k: self[k].variable.to_dict(data=data, encoding=encoding)}
+            )
         for k in self.data_vars:
-            d["data_vars"].update({k: self[k].variable.to_dict(data=data)})
+            d["data_vars"].update(
+                {k: self[k].variable.to_dict(data=data, encoding=encoding)}
+            )
+        if encoding:
+            d["encoding"] = dict(self.encoding)
         return d
 
     @classmethod
@@ -6061,6 +6074,7 @@ class Dataset(DataWithCoords, DatasetReductions, DatasetArithmetic, Mapping):
         obj = obj.set_coords(coords)
 
         obj.attrs.update(d.get("attrs", {}))
+        obj.encoding.update(d.get("encoding", {}))
 
         return obj
 
