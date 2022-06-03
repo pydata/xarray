d2e38716d41d44704d1731a4bb586d6762da3065
Michael Niklas <michael.niklas@airbus.com>
2022-06-03 10:10:40 +0200

    removed unused import Union

--- xarray/core/_typed_ops.pyi
+++ xarray/core/_typed_ops.pyi
@@ -1,7 +1,7 @@
 """Stub file for mixin classes with arithmetic operators."""
 # This file was generated using xarray.util.generate_ops. Do not edit manually.
 
-from typing import NoReturn, TypeVar, Union, overload
+from typing import NoReturn, TypeVar, overload
 
 import numpy as np
 
