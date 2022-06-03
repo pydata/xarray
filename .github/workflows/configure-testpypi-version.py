bdf8768d0f045eec129b8aaa2e8886cd3dbe94a9
Keewis <keewis@posteo.de>
2022-06-03 13:46:43 +0200

    use `"."` as path separator

--- .github/workflows/configure-testpypi-version.py
+++ .github/workflows/configure-testpypi-version.py
@@ -6,15 +6,15 @@ import tomli
 import tomli_w
 
 
-def split_path(path):
+def split_path(path, sep="/"):
     if isinstance(path, str):
-        return [part for part in path.split("/") if part]
+        return [part for part in path.split(sep) if part]
     else:
         return path
 
 
-def extract(mapping, path):
-    parts = split_path(path)
+def extract(mapping, path, sep="/"):
+    parts = split_path(path, sep=sep)
     cur = mapping
     for part in parts:
         cur = cur[part]
@@ -22,10 +22,10 @@ def extract(mapping, path):
     return cur
 
 
-def update(mapping, path, value):
+def update(mapping, path, value, sep="/"):
     new = copy.deepcopy(mapping)
 
-    parts = split_path(path)
+    parts = split_path(path, sep=sep)
     parent = extract(new, parts[:-1])
     parent[parts[-1]] = value
 
@@ -38,7 +38,9 @@ args = parser.parse_args()
 
 content = args.path.read_text()
 decoded = tomli.loads(content)
-updated = update(decoded, "tool/setuptools_scm/local_scheme", "no-local-version")
+updated = update(
+    decoded, "tool.setuptools_scm.local_scheme", "no-local-version", sep="."
+)
 
 new_content = tomli_w.dumps(updated)
 
