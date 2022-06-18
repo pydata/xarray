f8b6afa0400a8c675ae3f38452f2b13427b8829e
Michael Niklas <mick.niklas@gmail.com>
2022-06-18 11:50:02 +0200

    no type checking for coarsen reductions

--- xarray/tests/test_coarsen.py
+++ xarray/tests/test_coarsen.py
@@ -26,35 +26,35 @@ def test_coarsen_absent_dims_error(ds: Dataset) -> None:
 
 @pytest.mark.parametrize("dask", [True, False])
 @pytest.mark.parametrize(("boundary", "side"), [("trim", "left"), ("pad", "right")])
-def test_coarsen_dataset(ds: Dataset, dask: bool, boundary, side) -> None:
+def test_coarsen_dataset(ds, dask, boundary, side):
     if dask and has_dask:
         ds = ds.chunk({"x": 4})
 
-    actual = ds.coarsen(time=2, x=3, boundary=boundary, side=side).max()  # type: ignore[attr-defined]
+    actual = ds.coarsen(time=2, x=3, boundary=boundary, side=side).max()
     assert_equal(
-        actual["z1"], ds["z1"].coarsen(x=3, boundary=boundary, side=side).max()  # type: ignore[attr-defined]
+        actual["z1"], ds["z1"].coarsen(x=3, boundary=boundary, side=side).max()
     )
     # coordinate should be mean by default
     assert_equal(
-        actual["time"], ds["time"].coarsen(time=2, boundary=boundary, side=side).mean()  # type: ignore[attr-defined]
+        actual["time"], ds["time"].coarsen(time=2, boundary=boundary, side=side).mean()
     )
 
 
 @pytest.mark.parametrize("dask", [True, False])
-def test_coarsen_coords(ds: Dataset, dask: bool) -> None:
+def test_coarsen_coords(ds, dask):
     if dask and has_dask:
         ds = ds.chunk({"x": 4})
 
     # check if coord_func works
-    actual = ds.coarsen(time=2, x=3, boundary="trim", coord_func={"time": "max"}).max()  # type: ignore[attr-defined]
-    assert_equal(actual["z1"], ds["z1"].coarsen(x=3, boundary="trim").max())  # type: ignore[attr-defined]
-    assert_equal(actual["time"], ds["time"].coarsen(time=2, boundary="trim").max())  # type: ignore[attr-defined]
+    actual = ds.coarsen(time=2, x=3, boundary="trim", coord_func={"time": "max"}).max()
+    assert_equal(actual["z1"], ds["z1"].coarsen(x=3, boundary="trim").max())
+    assert_equal(actual["time"], ds["time"].coarsen(time=2, boundary="trim").max())
 
     # raise if exact
     with pytest.raises(ValueError):
-        ds.coarsen(x=3).mean()  # type: ignore[attr-defined]
+        ds.coarsen(x=3).mean()
     # should be no error
-    ds.isel(x=slice(0, 3 * (len(ds["x"]) // 3))).coarsen(x=3).mean()  # type: ignore[attr-defined]
+    ds.isel(x=slice(0, 3 * (len(ds["x"]) // 3))).coarsen(x=3).mean()
 
     # working test with pd.time
     da = xr.DataArray(
@@ -62,14 +62,14 @@ def test_coarsen_coords(ds: Dataset, dask: bool) -> None:
         dims="time",
         coords={"time": pd.date_range("1999-12-15", periods=364)},
     )
-    actual = da.coarsen(time=2).mean()  # type: ignore[attr-defined]
+    actual = da.coarsen(time=2).mean()
 
 
 @requires_cftime
-def test_coarsen_coords_cftime() -> None:
+def test_coarsen_coords_cftime():
     times = xr.cftime_range("2000", periods=6)
     da = xr.DataArray(range(6), [("time", times)])
-    actual = da.coarsen(time=3).mean()  # type: ignore[attr-defined]
+    actual = da.coarsen(time=3).mean()
     expected_times = xr.cftime_range("2000-01-02", freq="3D", periods=2)
     np.testing.assert_array_equal(actual.time, expected_times)
 
