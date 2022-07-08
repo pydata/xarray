c5f5db735f56e97aeff55ebc03b2f29ca20cb5a0
Illviljan <14371165+Illviljan@users.noreply.github.com>
2022-07-08 20:43:43 +0200

    Add read only cached_properties

--- xarray/core/utils.py
+++ xarray/core/utils.py
@@ -977,3 +977,11 @@ def contains_only_dask_or_numpy(obj) -> bool:
             for var in obj.variables.values()
         ]
     )
+
+
+class cached_property(functools.cached_property):
+    """Read only version of functools.cached_property."""
+
+    def __set__(self, instance, val):
+        """Raise an error when attempting to set a cached property."""
+        raise AttributeError("Can't set attribute")
