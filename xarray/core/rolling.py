298eb45a8bc8f4d11402937c2d902fa711084ffd
Mick <43316012+headtr1ck@users.noreply.github.com>
2022-07-14 19:41:00 +0200

    Fix `DataArrayRolling.__iter__` with `center=True` (#6744)
    * new test_rolling module
    
    * fix rolling iter with center=True
    
    * add fix to whats-new
    
    * fix DatasetRolling test names
    
    * small code simplification

--- xarray/core/rolling.py
+++ xarray/core/rolling.py
@@ -267,16 +267,21 @@ class DataArrayRolling(Rolling["DataArray"]):
         # TODO legacy attribute
         self.window_labels = self.obj[self.dim[0]]
 
-    def __iter__(self) -> Iterator[tuple[RollingKey, DataArray]]:
+    def __iter__(self) -> Iterator[tuple[DataArray, DataArray]]:
         if self.ndim > 1:
             raise ValueError("__iter__ is only supported for 1d-rolling")
-        stops = np.arange(1, len(self.window_labels) + 1)
-        starts = stops - int(self.window[0])
-        starts[: int(self.window[0])] = 0
-        for (label, start, stop) in zip(self.window_labels, starts, stops):
-            window = self.obj.isel({self.dim[0]: slice(start, stop)})
 
-            counts = window.count(dim=self.dim[0])
+        dim0 = self.dim[0]
+        window0 = int(self.window[0])
+        offset = (window0 + 1) // 2 if self.center[0] else 1
+        stops = np.arange(offset, self.obj.sizes[dim0] + offset)
+        starts = stops - window0
+        starts[: window0 - offset] = 0
+
+        for (label, start, stop) in zip(self.window_labels, starts, stops):
+            window = self.obj.isel({dim0: slice(start, stop)})
+
+            counts = window.count(dim=dim0)
             window = window.where(counts >= self.min_periods)
 
             yield (label, window)
--- xarray/tests/test_rolling.py
+++ xarray/tests/test_rolling.py
@@ -27,8 +27,10 @@ pytestmark = [
 
 class TestDataArrayRolling:
     @pytest.mark.parametrize("da", (1, 2), indirect=True)
-    def test_rolling_iter(self, da) -> None:
-        rolling_obj = da.rolling(time=7)
+    @pytest.mark.parametrize("center", [True, False])
+    @pytest.mark.parametrize("size", [1, 2, 3, 7])
+    def test_rolling_iter(self, da: DataArray, center: bool, size: int) -> None:
+        rolling_obj = da.rolling(time=size, center=center)
         rolling_obj_mean = rolling_obj.mean()
 
         assert len(rolling_obj.window_labels) == len(da["time"])
@@ -40,14 +42,7 @@ class TestDataArrayRolling:
             actual = rolling_obj_mean.isel(time=i)
             expected = window_da.mean("time")
 
-            # TODO add assert_allclose_with_nan, which compares nan position
-            # as well as the closeness of the values.
-            assert_array_equal(actual.isnull(), expected.isnull())
-            if (~actual.isnull()).sum() > 0:
-                np.allclose(
-                    actual.values[actual.values.nonzero()],
-                    expected.values[expected.values.nonzero()],
-                )
+            np.testing.assert_allclose(actual.values, expected.values)
 
     @pytest.mark.parametrize("da", (1,), indirect=True)
     def test_rolling_repr(self, da) -> None:
