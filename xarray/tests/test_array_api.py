9b7d55951a9d0632ad531f91c57419843d2d7b78
Max Jones <meghanj@alum.mit.edu>
2022-07-20 14:25:10 -0400

    Add test

--- xarray/tests/test_array_api.py
+++ xarray/tests/test_array_api.py
@@ -43,6 +43,12 @@ def test_indexing(arrays) -> None:
     assert_equal(actual, expected)
 
 
+def test_properties(arrays) -> None:
+    np_arr, xp_arr = arrays
+    assert np_arr.nbytes == 48
+    assert xp_arr.nbytes == 48
+
+
 def test_reorganizing_operation(arrays) -> None:
     np_arr, xp_arr = arrays
     expected = np_arr.transpose()
