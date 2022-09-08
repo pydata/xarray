402ac49d3343bc90a07c8c54948357c4ca9897cf
garciam <markel.garciad@gmail.com>
2022-09-08 10:21:26 +0200

    Check both netCDF4 and libnetcdf versions to see if they support the new compression arguments.

--- xarray/backends/netCDF4_.py
+++ xarray/backends/netCDF4_.py
@@ -250,8 +250,14 @@ def _extract_nc4_variable_encoding(
     if h5py_okay:
         valid_encodings.add("compression_opts")
 
-    if backend == "netCDF4" and Version(netCDF4.__version__) < Version("1.6.0"):
-        valid_encodings.remove("compression")
+    if backend == "netCDF4" and has_netcdf4:
+        # if using netCDF4 check the versions are updated enough for supporting the new
+        # compression
+        if (
+            Version(netCDF4.__version__) < Version("1.6.0") or
+            Version(netCDF4.getlibversion().split(" ")[0]) <  Version("4.9.0")
+        ):
+            valid_encodings.remove("compression")
 
     if not raise_on_invalid and encoding.get("chunksizes") is not None:
         # It's possible to get encoded chunksizes larger than a dimension size
