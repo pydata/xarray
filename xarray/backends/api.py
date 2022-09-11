a1afde8a3e06540778f7a594a905c3ae4fe545cd
ColemanTom <15375218+ColemanTom@users.noreply.github.com>
2022-09-11 13:30:11 +1000

    Ensure only string and Path filenames encode source
    Co-authored-by: Mathias Hauser <mathause@users.noreply.github.com>

--- xarray/backends/api.py
+++ xarray/backends/api.py
@@ -359,7 +359,7 @@ def _dataset_from_backend_dataset(
     ds.set_close(backend_ds._close)
 
     # Ensure source filename always stored in dataset object
-    if "source" not in ds.encoding:
+    if "source" not in ds.encoding and isinstance(filename_or_obj, (str, os.PathLike)):
         ds.encoding["source"] = _normalize_path(filename_or_obj)
 
     return ds
