1b3239c2f63125ad999bc7816b53e9389d210931
Michael Niklas <mick.niklas@gmail.com>
2022-09-18 18:34:47 +0200

    add some more tests for tuples

--- xarray/tests/test_utils.py
+++ xarray/tests/test_utils.py
@@ -300,7 +300,9 @@ def test_infix_dims_errors(supplied, all_):
         pytest.param("a", ("a",), id="str"),
         pytest.param(["a", "b"], ("a", "b"), id="list_of_str"),
         pytest.param(["a", 1], ("a", 1), id="list_mixed"),
+        pytest.param(("a", "b"), ("a", "b"), id="tuple_of_str"),
         pytest.param(["a", ("b", "c")], ("a", ("b", "c")), id="list_with_tuple"),
+        pytest.param((("b", "c"),), (("b", "c"),), id="tuple_of_tuple"),
         pytest.param(None, None, id="None"),
     ],
 )
@@ -308,13 +310,13 @@ def test_parse_dims(
     dim: str | Iterable[Hashable] | None,
     expected: tuple[Hashable, ...],
 ) -> None:
-    all_dims = ("a", "b", 1, ("b", "c"))  # different Hashables
+    all_dims = ("a", "b", 1, ("b", "c"))  # selection of different Hashables
     actual = utils.parse_dims(dim, all_dims, replace_none=False)
     assert actual == expected
 
 
 def test_parse_dims_replace_none() -> None:
-    all_dims = ("a", "b", 1, ("b", "c"))  # different Hashables
+    all_dims = ("a", "b", 1, ("b", "c"))  # selection of different Hashables
     actual = utils.parse_dims(None, all_dims, replace_none=True)
     assert actual == all_dims
 
@@ -327,8 +329,8 @@ def test_parse_dims_replace_none() -> None:
         pytest.param(["x", 2], id="list_missing_all"),
     ],
 )
-def test_parse_dims_raises(dim: str | Iterable[Hashable] | None) -> None:
-    all_dims = ("a", "b", 1, ("b", "c"))  # different Hashables
+def test_parse_dims_raises(dim: str | Iterable[Hashable]) -> None:
+    all_dims = ("a", "b", 1, ("b", "c"))  # selection of different Hashables
     with pytest.raises(ValueError, match="'x'"):
         utils.parse_dims(dim, all_dims, check=True)
 
