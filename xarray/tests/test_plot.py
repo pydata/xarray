2a2968e4cb9e076ac514c9b3d420709fa7571d3b
Michael Niklas <mick.niklas@gmail.com>
2022-09-27 21:59:52 +0200

    simplify test a bit

--- xarray/tests/test_plot.py
+++ xarray/tests/test_plot.py
@@ -2983,27 +2983,27 @@ def test_get_axis_raises():
 @pytest.mark.parametrize(
     ["figsize", "size", "aspect", "ax", "kwargs"],
     [
-        pytest.param((3, 2), None, None, None, {}, id="figsize"),
+        pytest.param((3, 2), None, None, False, {}, id="figsize"),
         pytest.param(
-            (3.5, 2.5), None, None, None, {"label": "test"}, id="figsize_kwargs"
+            (3.5, 2.5), None, None, False, {"label": "test"}, id="figsize_kwargs"
         ),
-        pytest.param(None, 5, None, None, {}, id="size"),
-        pytest.param(None, 5.5, None, None, {"label": "test"}, id="size_kwargs"),
-        pytest.param(None, 5, 1, None, {}, id="size+aspect"),
+        pytest.param(None, 5, None, False, {}, id="size"),
+        pytest.param(None, 5.5, None, False, {"label": "test"}, id="size_kwargs"),
+        pytest.param(None, 5, 1, False, {}, id="size+aspect"),
         pytest.param(None, None, None, True, {}, id="ax"),
-        pytest.param(None, None, None, None, {}, id="default"),
-        pytest.param(None, None, None, None, {"label": "test"}, id="default_kwargs"),
+        pytest.param(None, None, None, False, {}, id="default"),
+        pytest.param(None, None, None, False, {"label": "test"}, id="default_kwargs"),
     ],
 )
 def test_get_axis(
     figsize: tuple[float, float] | None,
     size: float | None,
     aspect: float | None,
-    ax: bool | None,
+    ax: bool,
     kwargs: dict[str, Any],
 ) -> None:
     with figure_context():
-        inp_ax = None if ax is None else plt.axes()
+        inp_ax = plt.axes() if ax else None
         out_ax = get_axis(
             figsize=figsize, size=size, aspect=aspect, ax=inp_ax, **kwargs
         )
