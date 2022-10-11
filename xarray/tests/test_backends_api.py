1ff1038c305b7a184e7cbae850b7c7c3cae64880
Deepak Cherian <dcherian@users.noreply.github.com>
2022-10-11 10:18:35 -0600

    Update xarray/tests/test_backends_api.py

--- xarray/tests/test_backends_api.py
+++ xarray/tests/test_backends_api.py
@@ -50,6 +50,8 @@ def test_custom_engine() -> None:
 
 
 def test_multiindex() -> None:
+    # GH7139
+    # Check that we properly handle backends that change index variables
     def _create_multiindex(**kwargs):
         return pd.MultiIndex.from_arrays(list(kwargs.values()), names=kwargs.keys())
 
