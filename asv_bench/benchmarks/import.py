24ba96cc3e34410c8b1bfeeb66b15735753b693d
Michael Niklas <mick.niklas@gmail.com>
2022-10-17 10:42:29 +0200

    fix forgotten self arg

--- asv_bench/benchmarks/import.py
+++ asv_bench/benchmarks/import.py
@@ -1,17 +1,17 @@
 class Import:
     """Benchmark importing xarray"""
 
-    def timeraw_import_xarray():
+    def timeraw_import_xarray(self):
         return """
         import xarray
         """
 
-    def timeraw_import_xarray_plot():
+    def timeraw_import_xarray_plot(self):
         return """
         import xarray.plot
         """
 
-    def timeraw_import_xarray_backends():
+    def timeraw_import_xarray_backends(self):
         return """
         from xarray.backends import list_engines
         list_engines()
