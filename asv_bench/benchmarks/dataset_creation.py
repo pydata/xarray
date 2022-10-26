3273950a3a2476ab067520d596d016663cb8daca
Mark Harfouche <mark.harfouche@gmail.com>
2022-10-25 22:12:39 -0400

    Add a benchmark for dataset element insertion

---
+++ asv_bench/benchmarks/dataset_creation.py
@@ -0,0 +1,18 @@
+import xarray as xr
+
+
+class Creation:
+    def setup(self):
+        # Everybody is lazy loading these days
+        # so lets force modules to get instantiated here, instead of
+        # in the benchmark
+        dummy_dataset = xr.Dataset()
+        dummy_dataset['a'] = 1
+        dummy_dataset['b'] = 1
+
+        self.dataset = xr.Dataset()
+
+    def time_dataset_creation(self):
+        dataset = self.dataset
+        for i in range(1000):
+            dataset[f"var{i}"] = i
