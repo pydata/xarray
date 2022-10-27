d3407a73d982646aea082c71ecd6958d3df81ebf
Pierre <pierreloicq@gmail.com>
2022-10-27 19:07:28 +0200

    assign_coords docs: see also Dataset.set_coords (#7231)

--- xarray/core/common.py
+++ xarray/core/common.py
@@ -601,6 +601,7 @@ class DataWithCoords(AttrAccessMixin):
         --------
         Dataset.assign
         Dataset.swap_dims
+        Dataset.set_coords
         """
         coords_combined = either_dict_or_kwargs(coords, coords_kwargs, "assign_coords")
         data = self.copy(deep=False)
