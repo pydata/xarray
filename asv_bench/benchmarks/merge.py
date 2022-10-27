2fdf774d51cb5d7b9e7e20b58c601b3029a09b10
Mark Harfouche <mark.harfouche@gmail.com>
2022-10-27 11:12:28 -0400

    Give the parameter a name

--- asv_bench/benchmarks/merge.py
+++ asv_bench/benchmarks/merge.py
@@ -2,16 +2,17 @@ import xarray as xr
 
 
 class DatasetAddVariable:
-    params = [0, 10, 100, 1000]
+    param_names = ["existing_elements"]
+    params = [[0, 10, 100, 1000]]
 
-    def setup(self, elements):
+    def setup(self, existing_elements):
         self.datasets = {}
         # Dictionary insertion is fast(er) than xarray.Dataset insertion
         d = {}
-        for i in range(elements):
+        for i in range(existing_elements):
             d[f"var{i}"] = i
         self.dataset = xr.merge([d])
 
-    def time_variable_insertion(self, elements):
+    def time_variable_insertion(self, existin_elements):
         dataset = self.dataset
-        dataset[f"new_var"] = 0
+        dataset["new_var"] = 0
