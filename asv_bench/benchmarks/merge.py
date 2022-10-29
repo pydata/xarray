4bacd9c6d60195cb3bbd20b6f7da72849253d05f
Mark Harfouche <mark.harfouche@gmail.com>
2022-10-29 18:28:28 -0400

    Move data set creation definition

--- asv_bench/benchmarks/merge.py
+++ asv_bench/benchmarks/merge.py
@@ -27,6 +27,10 @@ class DatasetAddVariable:
 
 
 class DatasetCreation:
+    # The idea here is to time how long it takes to go from numpy
+    # and python data types, to a full dataset
+    # See discussion
+    # https://github.com/pydata/xarray/issues/7224#issuecomment-1292216344
     param_names = ["strategy", "count"]
     params = [
         ["dict_of_DataArrays", "dict_of_Variables", "dict_of_Tuples"],
@@ -34,30 +38,32 @@ class DatasetCreation:
     ]
 
     def setup(self, strategy, count):
-        self.dataset_value = np.array(["0", "b"], dtype=str)
+        data = np.array(["0", "b"], dtype=str)
         self.dataset_coords = dict(time=np.array([0, 1]))
+        if strategy == "dict_of_DataArrays":
+            def create_data_vars():
+                return {
+                    f"long_variable_name_{i}": xr.DataArray(
+                        data=data, dims=("time")
+                    )
+                    for i in range(count)
+                }
+
+        elif strategy == "dict_of_Variables":
+            def create_data_vars():
+                return {
+                    f"long_variable_name_{i}": xr.Variable("time", data)
+                    for i in range(count)
+                }
+        elif strategy == "dict_of_Tuples":
+            def create_data_vars():
+                return {
+                    f"long_variable_name_{i}": ("time", data)
+                    for i in range(count)
+                }
+
+        self.create_data_vars = create_data_vars
 
     def time_dataset_creation(self, strategy, count):
-        # The idea here is to time how long it takes to go from numpy
-        # and python data types, to a full dataset
-        # See discussion
-        # https://github.com/pydata/xarray/issues/7224#issuecomment-1292216344
-        if strategy == "dict_of_DataArrays":
-            data_vars = {
-                f"long_variable_name_{i}": xr.DataArray(
-                    data=self.dataset_value, dims=("time")
-                )
-                for i in range(count)
-            }
-        elif strategy == "dict_of_Variables":
-            data_vars = {
-                f"long_variable_name_{i}": xr.Variable("time", self.dataset_value)
-                for i in range(count)
-            }
-        elif strategy == "dict_of_Tuples":
-            data_vars = {
-                f"long_variable_name_{i}": ("time", self.dataset_value)
-                for i in range(count)
-            }
-
+        data_vars = self.create_data_vars()
         xr.Dataset(data_vars=data_vars, coords=self.dataset_coords)
