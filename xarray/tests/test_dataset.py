c583455d568c3a6c8e68ceec90b70cc13388d24e
Benoit Bovy <benbovy@gmail.com>
2022-12-02 17:32:40 +0100

    Fix assign_coords resetting all dimension coords to default index (#7347)
    * fix merge_coords and collect_variables_and_indexes
    
    * add test
    
    * update what's new
    
    * update what's new with pull-request link

--- xarray/tests/test_dataset.py
+++ xarray/tests/test_dataset.py
@@ -4170,6 +4170,20 @@ class TestDataset:
             is not actual.xindexes["level_2"]
         )
 
+    def test_assign_coords_custom_index_side_effect(self) -> None:
+        # test that assigning new coordinates do not reset other dimension coord indexes
+        # to default (pandas) index (https://github.com/pydata/xarray/issues/7346)
+        class CustomIndex(PandasIndex):
+            pass
+
+        ds = (
+            Dataset(coords={"x": [1, 2, 3]})
+            .drop_indexes("x")
+            .set_xindex("x", CustomIndex)
+        )
+        actual = ds.assign_coords(y=[4, 5, 6])
+        assert isinstance(actual.xindexes["x"], CustomIndex)
+
     def test_merge_multiindex_level(self) -> None:
         data = create_test_multiindex()
 
