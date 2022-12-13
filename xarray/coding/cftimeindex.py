f44f15f516991af97eef202fc85375b0a5a304de
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2022-12-13 12:10:52 -0800

    Add whatsnew and change to breaking change

--- xarray/coding/cftimeindex.py
+++ xarray/coding/cftimeindex.py
@@ -463,16 +463,12 @@ class CFTimeIndex(pd.Index):
         indexer = np.where(distance <= tolerance, indexer, -1)
         return indexer
 
-    def get_loc(self, key, method=None, tolerance=None):
+    def get_loc(self, key):
         """Adapted from pandas.tseries.index.DatetimeIndex.get_loc"""
         if isinstance(key, str):
             return self._get_string_slice(key)
-        elif method is not None and tolerance is not None:
-            return super().get_loc(key)
-        elif Version(pd.__version__) < Version("2.0"):
-            return super().get_loc(key, method=method, tolerance=tolerance)
         else:
-            return super().get_indexer(self, [key], method=method, tolerance=tolerance)
+            return super().get_loc(key)
 
     def _maybe_cast_slice_bound(self, label, side, kind=None):
         """Adapted from
