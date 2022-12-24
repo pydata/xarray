19806e38748dfa0f678c1e507a560198ca0ab699
Ian Carroll <carroll.ian@gmail.com>
2022-12-24 00:07:42 +0000

    Preserve original dtype when accessing MultiIndex levels (#7393)
    * return multiindex levels as original dtype (#7250)
    
    * typing in test, contributor attribution
    
    * rst is not markdown

--- xarray/core/indexing.py
+++ xarray/core/indexing.py
@@ -1531,8 +1531,12 @@ class PandasMultiIndexingAdapter(PandasIndexingAdapter):
         self.level = level
 
     def __array__(self, dtype: DTypeLike = None) -> np.ndarray:
+        if dtype is None:
+            dtype = self.dtype
         if self.level is not None:
-            return self.array.get_level_values(self.level).values
+            return np.asarray(
+                self.array.get_level_values(self.level).values, dtype=dtype
+            )
         else:
             return super().__array__(dtype)
 
