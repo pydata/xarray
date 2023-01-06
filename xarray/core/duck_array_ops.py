c91559220ccb0c911575143186b845b87f795c4c
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-01-06 18:54:47 +0100

    Update duck_array_ops.py

--- xarray/core/duck_array_ops.py
+++ xarray/core/duck_array_ops.py
@@ -33,9 +33,9 @@ from numpy import (  # noqa
 from numpy.lib.stride_tricks import sliding_window_view  # noqa
 
 from xarray.core import dask_array_ops, dtypes, nputils
-from xarray.core.nputils import nanfirst, nanlast, get_array_namespace
+from xarray.core.nputils import nanfirst, nanlast
 from xarray.core.pycompat import array_type, is_duck_dask_array
-from xarray.core.utils import is_duck_array, module_available
+from xarray.core.utils import is_duck_array, module_available, get_array_namespace
 
 dask_available = module_available("dask")
 
