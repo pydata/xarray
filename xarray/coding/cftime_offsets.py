15f4034c47fa1c88ea757341135187cef161b4da
spencerkclark <spencerkclark@gmail.com>
2023-01-29 07:35:58 -0500

    [test-upstream] Add references to pandas PRs for _NoDefault

--- xarray/coding/cftime_offsets.py
+++ xarray/coding/cftime_offsets.py
@@ -858,6 +858,12 @@ def _generate_range(start, end, periods, offset):
 class _NoDefault(Enum):
     """Used by pandas to specify a default value for a deprecated argument.
     Copied from pandas._libs.lib._NoDefault.
+
+    See also:
+    - pandas-dev/pandas#30788
+    - pandas-dev/pandas#40684
+    - pandas-dev/pandas#40715
+    - pandas-dev/pandas#47045
     """
 
     no_default = "NO_DEFAULT"
