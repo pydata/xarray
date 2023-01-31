4a9b5d6cbf0ed8d13e1febe4194cad9b39dd84d9
Joseph Hamman <joe@earthmover.io>
2023-01-30 19:46:10 -0800

    add local scheduler test

--- xarray/tests/test_distributed.py
+++ xarray/tests/test_distributed.py
@@ -152,7 +152,7 @@ def test_open_mfdataset_can_open_files_with_cftime_index(parallel, tmp_path):
 @requires_cftime
 @requires_netCDF4
 @pytest.mark.parametrize("parallel", (True, False))
-def test_open_mfdataset_multiple_files_parallel(parallel, tmp_path):
+def test_open_mfdataset_multiple_files_parallel_distributed(parallel, tmp_path):
     lon = np.arange(100)
     time = xr.cftime_range("20010101", periods=100, calendar="360_day")
     data = np.random.random((time.size, lon.size))
@@ -166,6 +166,30 @@ def test_open_mfdataset_multiple_files_parallel(parallel, tmp_path):
 
     with cluster() as (s, [a, b]):
         with Client(s["address"]):
+            with xr.open_mfdataset(
+                fnames, parallel=parallel, concat_dim="time", combine="nested"
+            ) as tf:
+                assert_identical(tf["test"], da)
+
+
+# TODO: move this to test_backends.py
+@requires_cftime
+@requires_netCDF4
+@pytest.mark.parametrize("parallel", (True, False))
+def test_open_mfdataset_multiple_files_parallel(parallel, tmp_path):
+    lon = np.arange(100)
+    time = xr.cftime_range("20010101", periods=100, calendar="360_day")
+    data = np.random.random((time.size, lon.size))
+    da = xr.DataArray(data, coords={"time": time, "lon": lon}, name="test")
+
+    fnames = []
+    for i in range(0, 100, 10):
+        fname = tmp_path / f"test_{i}.nc"
+        da.isel(time=slice(i, i + 10)).to_netcdf(fname)
+        fnames.append(fname)
+
+    for get in [dask.threaded.get, dask.multiprocessing.get, dask.local.get_sync, None]:
+        with dask.config.set(scheduler=get):
             with xr.open_mfdataset(
                 fnames, parallel=parallel, concat_dim="time", combine="nested"
             ) as tf:
