395a3ae69ee7220d3107db7ea247ba3a0d5c5021
Thomas Nicholas <thomas.nicholas@columbia.edu>
2023-02-01 11:18:05 -0500

    importorskip whole test file

--- xarray/tests/test_datatree.py
+++ xarray/tests/test_datatree.py
@@ -1,19 +1,18 @@
+import pytest
+
 import xarray.testing as xrt
-from xarray import Dataset
-from xarray.tests import requires_datatree
+from xarray import Dataset, DataTree
+
+pytest.importorskip("datatree")
 
 
-@requires_datatree
 def test_import_datatree():
     """Just test importing datatree package from xarray-contrib repo"""
-    from xarray import DataTree
 
     DataTree()
 
 
-@requires_datatree
 def test_to_datatree():
-    from xarray import DataTree
 
     ds = Dataset({"a": ("x", [1, 2, 3])})
     dt = ds.to_datatree(node_name="group1")
