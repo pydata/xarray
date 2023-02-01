ffa53c470980623bff5fef9b1c67fda126b46edd
Thomas Nicholas <thomas.nicholas@columbia.edu>
2023-01-31 19:06:44 -0500

    also check we can import accessor

--- xarray/tests/test_datatree.py
+++ xarray/tests/test_datatree.py
@@ -4,7 +4,6 @@ from xarray.tests import requires_datatree
 @requires_datatree
 def test_import_datatree():
     """Just test importing datatree package from xarray-contrib repo"""
-    from xarray import DataTree, open_datatree
+    from xarray import DataTree, open_datatree, register_datatree_accessor
 
     DataTree()
-    
