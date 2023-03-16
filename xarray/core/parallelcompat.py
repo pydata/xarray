3ae21d9116162bedcd242b04f25dabb29a5f6c31
Thomas Nicholas <thomas.nicholas@columbia.edu>
2023-03-16 16:08:04 -0400

    use indexing adapter on cubed arrays too

--- xarray/core/parallelcompat.py
+++ xarray/core/parallelcompat.py
@@ -423,6 +423,9 @@ class CubedManager(ChunkManager["CubedArray"]):
     def from_array(self, data: np.ndarray, chunks, **kwargs) -> "CubedArray":
         from cubed import Array, from_array
 
+        from xarray.core import indexing
+
+        # cubed-specific kwargs
         spec = kwargs.pop("spec", None)
 
         if isinstance(data, Array):
@@ -430,6 +433,22 @@ class CubedManager(ChunkManager["CubedArray"]):
         elif is_duck_dask_array(data):
             raise TypeError("Trying to rechunk a dask array using cubed")
         else:
+            if isinstance(data, indexing.ExplicitlyIndexed):
+                # Unambiguously handle array storage backends (like NetCDF4 and h5py)
+                # that can't handle general array indexing. For example, in netCDF4 you
+                # can do "outer" indexing along two dimensions independent, which works
+                # differently from how NumPy handles it.
+                # da.from_array works by using lazy indexing with a tuple of slices.
+                # Using OuterIndexer is a pragmatic choice: dask does not yet handle
+                # different indexing types in an explicit way:
+                # https://github.com/dask/dask/issues/2883
+                data = indexing.ImplicitToExplicitIndexingAdapter(
+                    data, indexing.OuterIndexer
+                )
+
+            if utils.is_dict_like(chunks):
+                chunks = tuple(chunks.get(n, s) for n, s in enumerate(data.shape))
+
             data = from_array(
                 data,
                 chunks,
