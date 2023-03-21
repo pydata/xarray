f4de57769c5d45ce5647731d65a844dfec02f9b8
Thomas Nicholas <thomas.nicholas@columbia.edu>
2023-03-21 16:02:13 -0400

    add chunkmanager entrypoint

---
+++ xarray/core/daskmanager.py
@@ -0,0 +1,226 @@
+from collections.abc import Sequence
+from typing import Any, Callable, Optional, Union
+
+import numpy as np
+
+from xarray.core import utils
+from xarray.core.parallelcompat import ChunkManagerEntrypoint, T_ChunkedArray, T_Chunks
+from xarray.core.pycompat import DuckArrayModule, is_duck_dask_array
+from xarray.core.types import DaskArray
+
+
+class DaskManager(ChunkManagerEntrypoint["DaskArray"]):
+    array_cls: type["DaskArray"]
+
+    def __init__(self):
+        # TODO can we replace this with a class attribute instead?
+
+        from dask.array import Array
+
+        self.array_cls = Array
+
+    def is_chunked_array(self, data: Any) -> bool:
+        return is_duck_dask_array(data)
+
+    def chunks(self, data: "DaskArray") -> T_Chunks:
+        return data.chunks
+
+    def from_array(self, data, chunks, **kwargs) -> "DaskArray":
+        import dask.array as da
+
+        from xarray.core import indexing
+
+        # dask-specific kwargs
+        name = kwargs.pop("name", None)
+        lock = kwargs.pop("lock", False)
+        inline_array = kwargs.pop("inline_array", False)
+
+        if is_duck_dask_array(data):
+            data = self.rechunk(data, chunks)
+        elif isinstance(data, DuckArrayModule("cubed").type):
+            raise TypeError("Trying to rechunk a cubed array using dask")
+        else:
+            if isinstance(data, indexing.ExplicitlyIndexed):
+                # Unambiguously handle array storage backends (like NetCDF4 and h5py)
+                # that can't handle general array indexing. For example, in netCDF4 you
+                # can do "outer" indexing along two dimensions independent, which works
+                # differently from how NumPy handles it.
+                # da.from_array works by using lazy indexing with a tuple of slices.
+                # Using OuterIndexer is a pragmatic choice: dask does not yet handle
+                # different indexing types in an explicit way:
+                # https://github.com/dask/dask/issues/2883
+                data = indexing.ImplicitToExplicitIndexingAdapter(
+                    data, indexing.OuterIndexer
+                )
+
+                # All of our lazily loaded backend array classes should use NumPy
+                # array operations.
+                dask_kwargs = {"meta": np.ndarray}
+            else:
+                dask_kwargs = {}
+
+            if utils.is_dict_like(chunks):
+                chunks = tuple(chunks.get(n, s) for n, s in enumerate(data.shape))
+
+            data = da.from_array(
+                data,
+                chunks,
+                name=name,
+                lock=lock,
+                inline_array=inline_array,
+                **dask_kwargs,
+            )
+        return data
+
+    # TODO is simple method propagation like this necessary?
+    def rechunk(self, data: "DaskArray", chunks, **kwargs) -> "DaskArray":
+        return data.rechunk(chunks, **kwargs)
+
+    def compute(self, *data: "DaskArray", **kwargs) -> np.ndarray:
+        from dask.array import compute
+
+        return compute(*data, **kwargs)
+
+    @property
+    def array_api(self) -> Any:
+        from dask import array as da
+
+        return da
+
+    def reduction(
+        self,
+        arr: T_ChunkedArray,
+        func: Callable,
+        combine_func: Optional[Callable] = None,
+        aggregate_func: Optional[Callable] = None,
+        axis: Optional[Union[int, Sequence[int]]] = None,
+        dtype: Optional[np.dtype] = None,
+        keepdims: bool = False,
+    ) -> T_ChunkedArray:
+        from dask.array import reduction
+
+        return reduction(
+            arr,
+            chunk=func,
+            combine=combine_func,
+            aggregate=aggregate_func,
+            axis=axis,
+            dtype=dtype,
+            keepdims=keepdims,
+        )
+
+    def apply_gufunc(
+        self,
+        func,
+        signature,
+        *args,
+        axes=None,
+        axis=None,
+        keepdims=False,
+        output_dtypes=None,
+        output_sizes=None,
+        vectorize=None,
+        allow_rechunk=False,
+        meta=None,
+        **kwargs,
+    ):
+        from dask.array.gufunc import apply_gufunc
+
+        return apply_gufunc(
+            func,
+            signature,
+            *args,
+            axes=axes,
+            axis=axis,
+            keepdims=keepdims,
+            output_dtypes=output_dtypes,
+            output_sizes=output_sizes,
+            vectorize=vectorize,
+            allow_rechunk=allow_rechunk,
+            meta=meta,
+            **kwargs,
+        )
+
+    def map_blocks(
+        self,
+        func,
+        *args,
+        dtype=None,
+        chunks=None,
+        drop_axis=[],
+        new_axis=None,
+        **kwargs,
+    ):
+        from dask.array import map_blocks
+
+        # pass through name, meta, token as kwargs
+        return map_blocks(
+            func,
+            *args,
+            dtype=dtype,
+            chunks=chunks,
+            drop_axis=drop_axis,
+            new_axis=new_axis,
+            **kwargs,
+        )
+
+    def blockwise(
+        self,
+        func,
+        out_ind,
+        *args,
+        name=None,
+        token=None,
+        dtype=None,
+        adjust_chunks=None,
+        new_axes=None,
+        align_arrays=True,
+        concatenate=None,
+        meta=None,
+        **kwargs,
+    ):
+        from dask.array import blockwise
+
+        return blockwise(
+            func,
+            out_ind,
+            *args,
+            name=name,
+            token=token,
+            dtype=dtype,
+            adjust_chunks=adjust_chunks,
+            new_axes=new_axes,
+            align_arrays=align_arrays,
+            concatenate=concatenate,
+            meta=meta,
+            **kwargs,
+        )
+
+    def unify_chunks(
+        self, *args, **kwargs
+    ) -> tuple[dict[str, T_Chunks], list["DaskArray"]]:
+        from dask.array.core import unify_chunks
+
+        return unify_chunks(*args, **kwargs)
+
+    def store(
+        self,
+        sources: Union["DaskArray", Sequence["DaskArray"]],
+        targets: Any,
+        **kwargs,
+    ):
+        from dask.array import store
+
+        # TODO separate expected store kwargs from other compute kwargs?
+
+        return store(
+            sources=sources,
+            targets=targets,
+            **kwargs,
+        )
+
+
+# try:
+#     CHUNK_MANAGERS["dask"] = DaskManager
+# except ImportError:
+#     pass
--- xarray/core/parallelcompat.py
+++ xarray/core/parallelcompat.py
@@ -3,14 +3,17 @@ The code in this module is an experiment in going from N=1 to N=2 parallel compu
 It could later be used as the basis for a public interface allowing any N frameworks to interoperate with xarray,
 but for now it is just a private experiment.
 """
+import functools
+import sys
 from abc import ABC, abstractmethod
 from collections.abc import Sequence
+from importlib.metadata import entry_points
 from typing import TYPE_CHECKING, Any, Callable, Generic, Optional, TypeVar, Union
 
 import numpy as np
 
 from xarray.core import utils
-from xarray.core.pycompat import DuckArrayModule, is_chunked_array, is_duck_dask_array
+from xarray.core.pycompat import is_chunked_array, is_duck_dask_array
 
 T_ChunkedArray = TypeVar("T_ChunkedArray")
 
@@ -18,21 +21,49 @@ T_ChunkedArray = TypeVar("T_ChunkedArray")
 # T_Chunks: TypeAlias = tuple[tuple[int, ...], ...]
 T_Chunks = Any
 
-CHUNK_MANAGERS: dict[str, type["ChunkManager"]] = {}
+CHUNK_MANAGERS: dict[str, type["ChunkManagerEntrypoint"]] = {}
 
 if TYPE_CHECKING:
-    from xarray.core.types import CubedArray, DaskArray, ZarrArray
+    from xarray.core.types import CubedArray, ZarrArray
 
 
-def get_chunkmanager(name: str) -> "ChunkManager":
-    if name in CHUNK_MANAGERS:
-        chunkmanager_cls = CHUNK_MANAGERS[name]
-        return chunkmanager_cls()
+@functools.lru_cache(maxsize=1)
+def list_chunkmanagers() -> dict[str, "ChunkManagerEntrypoint"]:
+    """
+    Return a dictionary of available engines and their BackendEntrypoint objects.
+
+    Notes
+    -----
+    # New selection mechanism introduced with Python 3.10. See GH6514.
+    """
+    if sys.version_info >= (3, 10):
+        entrypoints = entry_points(group="xarray.chunkmanagers")
     else:
-        raise ImportError(f"ChunkManager {name} has not been defined")
+        entrypoints = entry_points().get("xarray.chunkmanagers", ())
+
+    # Load entrypoints and instantiate chunkmanagers only once
+    return {name: entrypoint.load()() for name, entrypoint in entrypoints}
 
 
-def get_chunked_array_type(*args) -> "ChunkManager":
+def get_chunkmanager(manager: str) -> "ChunkManagerEntrypoint":
+    """Get namespace of chunk-handling methods for a specified parallel chunk manager, e.g. dask."""
+
+    if isinstance(manager, str):
+        chunkmanagers = list_chunkmanagers()
+        if manager not in chunkmanagers:
+            raise ValueError(
+                f"unrecognized chunk manager {manager} - must be one of: {list(chunkmanagers)}"
+            )
+
+        chunkmanager = chunkmanagers[manager]
+    else:
+        # TODO should we accept type[ChunkManagerEntrypoint] too?
+        raise TypeError("manager must be a string")
+
+    return chunkmanager
+
+
+def get_chunked_array_type(*args) -> "ChunkManagerEntrypoint":
     """
     Detects which parallel backend should be used for given set of arrays.
 
@@ -59,8 +90,8 @@ def get_chunked_array_type(*args) -> "ChunkManager":
 
     # iterate over defined chunk managers, seeing if each recognises this array type
     chunked_arr = chunked_arrays[0]
-    for chunkmanager_cls in CHUNK_MANAGERS.values():
-        chunkmanager = chunkmanager_cls()
+    chunkmanagers = list_chunkmanagers()
+    for chunkmanager in chunkmanagers.values():
         if chunkmanager.is_chunked_array(chunked_arr):
             return chunkmanager
 
@@ -73,7 +104,7 @@ class ChunkManagerNotFoundError(Exception):
     ...
 
 
-class ChunkManager(ABC, Generic[T_ChunkedArray]):
+class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
     """
     Adapter between a particular parallel computing framework and xarray.
 
@@ -192,224 +223,7 @@ class ChunkManager(ABC, Generic[T_ChunkedArray]):
         raise NotImplementedError()
 
 
-class DaskManager(ChunkManager["DaskArray"]):
-    array_cls: type["DaskArray"]
-
-    def __init__(self):
-        # TODO can we replace this with a class attribute instead?
-
-        from dask.array import Array
-
-        self.array_cls = Array
-
-    def is_chunked_array(self, data: Any) -> bool:
-        return is_duck_dask_array(data)
-
-    def chunks(self, data: "DaskArray") -> T_Chunks:
-        return data.chunks
-
-    def from_array(self, data, chunks, **kwargs) -> "DaskArray":
-        import dask.array as da
-
-        from xarray.core import indexing
-
-        # dask-specific kwargs
-        name = kwargs.pop("name", None)
-        lock = kwargs.pop("lock", False)
-        inline_array = kwargs.pop("inline_array", False)
-
-        if is_duck_dask_array(data):
-            data = self.rechunk(data, chunks)
-        elif isinstance(data, DuckArrayModule("cubed").type):
-            raise TypeError("Trying to rechunk a cubed array using dask")
-        else:
-            if isinstance(data, indexing.ExplicitlyIndexed):
-                # Unambiguously handle array storage backends (like NetCDF4 and h5py)
-                # that can't handle general array indexing. For example, in netCDF4 you
-                # can do "outer" indexing along two dimensions independent, which works
-                # differently from how NumPy handles it.
-                # da.from_array works by using lazy indexing with a tuple of slices.
-                # Using OuterIndexer is a pragmatic choice: dask does not yet handle
-                # different indexing types in an explicit way:
-                # https://github.com/dask/dask/issues/2883
-                data = indexing.ImplicitToExplicitIndexingAdapter(
-                    data, indexing.OuterIndexer
-                )
-
-                # All of our lazily loaded backend array classes should use NumPy
-                # array operations.
-                dask_kwargs = {"meta": np.ndarray}
-            else:
-                dask_kwargs = {}
-
-            if utils.is_dict_like(chunks):
-                chunks = tuple(chunks.get(n, s) for n, s in enumerate(data.shape))
-
-            data = da.from_array(
-                data,
-                chunks,
-                name=name,
-                lock=lock,
-                inline_array=inline_array,
-                **dask_kwargs,
-            )
-        return data
-
-    # TODO is simple method propagation like this necessary?
-    def rechunk(self, data: "DaskArray", chunks, **kwargs) -> "DaskArray":
-        return data.rechunk(chunks, **kwargs)
-
-    def compute(self, *data: "DaskArray", **kwargs) -> np.ndarray:
-        from dask.array import compute
-
-        return compute(*data, **kwargs)
-
-    @property
-    def array_api(self) -> Any:
-        from dask import array as da
-
-        return da
-
-    def reduction(
-        self,
-        arr: T_ChunkedArray,
-        func: Callable,
-        combine_func: Optional[Callable] = None,
-        aggregate_func: Optional[Callable] = None,
-        axis: Optional[Union[int, Sequence[int]]] = None,
-        dtype: Optional[np.dtype] = None,
-        keepdims: bool = False,
-    ) -> T_ChunkedArray:
-        from dask.array import reduction
-
-        return reduction(
-            arr,
-            chunk=func,
-            combine=combine_func,
-            aggregate=aggregate_func,
-            axis=axis,
-            dtype=dtype,
-            keepdims=keepdims,
-        )
-
-    def apply_gufunc(
-        self,
-        func,
-        signature,
-        *args,
-        axes=None,
-        axis=None,
-        keepdims=False,
-        output_dtypes=None,
-        output_sizes=None,
-        vectorize=None,
-        allow_rechunk=False,
-        meta=None,
-        **kwargs,
-    ):
-        from dask.array.gufunc import apply_gufunc
-
-        return apply_gufunc(
-            func,
-            signature,
-            *args,
-            axes=axes,
-            axis=axis,
-            keepdims=keepdims,
-            output_dtypes=output_dtypes,
-            output_sizes=output_sizes,
-            vectorize=vectorize,
-            allow_rechunk=allow_rechunk,
-            meta=meta,
-            **kwargs,
-        )
-
-    def map_blocks(
-        self,
-        func,
-        *args,
-        dtype=None,
-        chunks=None,
-        drop_axis=[],
-        new_axis=None,
-        **kwargs,
-    ):
-        from dask.array import map_blocks
-
-        # pass through name, meta, token as kwargs
-        return map_blocks(
-            func,
-            *args,
-            dtype=dtype,
-            chunks=chunks,
-            drop_axis=drop_axis,
-            new_axis=new_axis,
-            **kwargs,
-        )
-
-    def blockwise(
-        self,
-        func,
-        out_ind,
-        *args,
-        name=None,
-        token=None,
-        dtype=None,
-        adjust_chunks=None,
-        new_axes=None,
-        align_arrays=True,
-        concatenate=None,
-        meta=None,
-        **kwargs,
-    ):
-        from dask.array import blockwise
-
-        return blockwise(
-            func,
-            out_ind,
-            *args,
-            name=name,
-            token=token,
-            dtype=dtype,
-            adjust_chunks=adjust_chunks,
-            new_axes=new_axes,
-            align_arrays=align_arrays,
-            concatenate=concatenate,
-            meta=meta,
-            **kwargs,
-        )
-
-    def unify_chunks(
-        self, *args, **kwargs
-    ) -> tuple[dict[str, T_Chunks], list["DaskArray"]]:
-        from dask.array.core import unify_chunks
-
-        return unify_chunks(*args, **kwargs)
-
-    def store(
-        self,
-        sources: Union["DaskArray", Sequence["DaskArray"]],
-        targets: Any,
-        **kwargs,
-    ):
-        from dask.array import store
-
-        # TODO separate expected store kwargs from other compute kwargs?
-
-        return store(
-            sources=sources,
-            targets=targets,
-            **kwargs,
-        )
-
-
-try:
-    CHUNK_MANAGERS["dask"] = DaskManager
-except ImportError:
-    pass
-
-
-class CubedManager(ChunkManager["CubedArray"]):
+class CubedManager(ChunkManagerEntrypoint["CubedArray"]):
     array_cls: type["CubedArray"]
 
     def __init__(self):
@@ -601,11 +415,3 @@ class CubedManager(ChunkManager["CubedArray"]):
             targets,
             **kwargs,
         )
-
-
-try:
-    import cubed  # noqa
-
-    CHUNK_MANAGERS["cubed"] = CubedManager
-except ImportError:
-    pass
