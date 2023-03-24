79a243827db2e9f5c110324dc02b024cd11fc628
Scott Henderson <scottyh@uw.edu>
2023-03-24 10:54:38 -0700

    remove open_rasterio

--- xarray/util/print_versions.py
+++ xarray/util/print_versions.py
@@ -108,7 +108,6 @@ def show_versions(file=sys.stdout):
         ("cftime", lambda mod: mod.__version__),
         ("nc_time_axis", lambda mod: mod.__version__),
         ("PseudoNetCDF", lambda mod: mod.__version__),
-        ("rasterio", lambda mod: mod.__version__),
         ("cfgrib", lambda mod: mod.__version__),
         ("iris", lambda mod: mod.__version__),
         ("bottleneck", lambda mod: mod.__version__),
