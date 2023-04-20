use function to fix wrong values on decoding

xarray/coding/variables.py
@@ -333,6 +333,7 @@ def _ensure_scale_offset_conformance(
             raise ValueError(f"scale_factor {scale_factor} mismatch, scalar expected.")
         else:
             scale_factor = np.asarray(scale_factor).item()
+            mapping["scale_factor"] = scale_factor
 
     add_offset = mapping.get("add_offset")
     if np.ndim(add_offset) > 0:
@@ -340,6 +341,7 @@ def _ensure_scale_offset_conformance(
             raise ValueError(f"add_offset {add_offset} mismatch, scalar expected.")
         else:
             add_offset = np.asarray(add_offset).item()
+            mapping["add_offset"] = add_offset
 
     dtype = mapping.get("dtype")
     ptype = np.dtype(dtype) if dtype is not None else None
@@ -449,18 +451,11 @@ class CFScaleOffsetCoder(VariableCoder):
             # for decoding we need the original dtype
             encoding.setdefault("dtype", data.dtype)
 
-            # only warn on decoding
+            # only warn on decoding, but fix erroneous encoding
             # we try to decode non-conforming data
             _ensure_scale_offset_conformance(encoding, strict=False)
 
             dtype = _choose_float_dtype(data.dtype, "add_offset" in encoding)
-            # if scale/offset are non-conforming, extract and overwrite
-            if np.ndim(scale_factor) > 0:
-                scale_factor = np.asarray(scale_factor).item()
-                encoding["scale_factor"] = scale_factor
-            if np.ndim(add_offset) > 0:
-                add_offset = np.asarray(add_offset).item()
-                encoding["add_offset"] = add_offset
             transform = partial(
                 _scale_offset_decoding,
                 scale_factor=scale_factor,