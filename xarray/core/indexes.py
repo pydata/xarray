dd813252144d7f5ecd833ce3b91ee8f8627c943e
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-05-14 13:42:58 +0200

    don't think id() is required here.

--- xarray/core/indexes.py
+++ xarray/core/indexes.py
@@ -1495,11 +1495,11 @@ def filter_indexes_from_coords(
     of coordinate names.
 
     """
-    filtered_indexes: dict[Any, Index] = dict(**indexes)
+    filtered_indexes: dict[Any, Index] = dict(indexes)
 
     index_coord_names: dict[Hashable, set[Hashable]] = defaultdict(set)
     for name, idx in indexes.items():
-        index_coord_names[id(idx)].add(name)
+        index_coord_names[idx].add(name)
 
     for idx_coord_names in index_coord_names.values():
         if not idx_coord_names <= filtered_coord_names:
