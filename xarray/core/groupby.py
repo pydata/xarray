0a8c91813b075f37358f0d5aed47fda75b7e4e61
Deepak Cherian <dcherian@users.noreply.github.com>
2023-06-05 18:21:31 -0600

    Fix .groupby(multi index level) (#7830)
    * Fix .groupby(multi index level)
    
    Closes #6836
    
    * Update xarray/tests/test_groupby.py
    
    * mypy: Add _DummyGroup.to_index
    
    ---------
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>

--- xarray/core/groupby.py
+++ xarray/core/groupby.py
@@ -236,6 +236,10 @@ class _DummyGroup(Generic[T_Xarray]):
             key = key[0]
         return self.values[key]
 
+    def to_index(self) -> pd.Index:
+        # could be pd.RangeIndex?
+        return pd.Index(np.arange(self.size))
+
     def copy(self, deep: bool = True, data: Any = None):
         raise NotImplementedError
 
@@ -381,7 +385,7 @@ class ResolvedGrouper(ABC, Generic[T_Xarray]):
     @property
     def group_as_index(self) -> pd.Index:
         if self._group_as_index is None:
-            self._group_as_index = safe_cast_to_index(self.group1d)
+            self._group_as_index = self.group1d.to_index()
         return self._group_as_index
 
 
