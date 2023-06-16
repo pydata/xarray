99f95591dcaafb6a5f538700c25a8c52e8a39812
mgunyho <20118130+mgunyho@users.noreply.github.com>
2023-06-16 06:15:06 +0300

    Add errors option to curvefit (#7891)
    * Add allow_failures flag to Dataset.curve_fit
    
    * Reword docstring
    
    * Add allow_failures flag also to DataArray
    
    * Add unit test for curvefit with allow_failures
    
    * Update whats-new
    
    Co-authored-by: Dominik Stańczak <stanczakdominik@gmail.com>
    
    * Add PR to whats-new
    
    * Update docstring
    
    * Rename allow_failures to errors to be consistent with other methods
    
    * Compute array so test passes also with dask
    
    * Check error message
    
    * Update whats-new
    
    ---------
    
    Co-authored-by: Dominik Stańczak <stanczakdominik@gmail.com>
    Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>

--- xarray/core/dataset.py
+++ xarray/core/dataset.py
@@ -8631,6 +8631,7 @@ class Dataset(
         p0: dict[str, float | DataArray] | None = None,
         bounds: dict[str, tuple[float | DataArray, float | DataArray]] | None = None,
         param_names: Sequence[str] | None = None,
+        errors: ErrorOptions = "raise",
         kwargs: dict[str, Any] | None = None,
     ) -> T_Dataset:
         """
@@ -8675,6 +8676,10 @@ class Dataset(
             this will be automatically determined by arguments of `func`. `param_names`
             should be manually supplied when fitting a function that takes a variable
             number of parameters.
+        errors : {"raise", "ignore"}, default: "raise"
+            If 'raise', any errors from the `scipy.optimize_curve_fit` optimization will
+            raise an exception. If 'ignore', the coefficients and covariances for the
+            coordinates where the fitting failed will be NaN.
         **kwargs : optional
             Additional keyword arguments to passed to scipy curve_fit.
 
@@ -8757,6 +8762,9 @@ class Dataset(
                             f"dimensions {preserved_dims}."
                         )
 
+        if errors not in ["raise", "ignore"]:
+            raise ValueError('errors must be either "raise" or "ignore"')
+
         # Broadcast all coords with each other
         coords_ = broadcast(*coords_)
         coords_ = [
@@ -8793,7 +8801,15 @@ class Dataset(
                     pcov = np.full([n_params, n_params], np.nan)
                     return popt, pcov
             x = np.squeeze(x)
-            popt, pcov = curve_fit(func, x, y, p0=p0_, bounds=(lb, ub), **kwargs)
+
+            try:
+                popt, pcov = curve_fit(func, x, y, p0=p0_, bounds=(lb, ub), **kwargs)
+            except RuntimeError:
+                if errors == "raise":
+                    raise
+                popt = np.full([n_params], np.nan)
+                pcov = np.full([n_params, n_params], np.nan)
+
             return popt, pcov
 
         result = type(self)()
