08c39e65d19234304995392dd4beabd101b061b5
Deepak Cherian <dcherian@users.noreply.github.com>
2023-06-16 08:13:59 -0600

    Add cfgrib,ipywidgets to doc env (#7888)
    * Add cfgrib to doc env
    
    * [skip-ci] Raise for errors in doc build
    
    * Remove rasterio examples
    
    * Add ipywidgets to silence nbsphinx warning

--- doc/conf.py
+++ doc/conf.py
@@ -58,7 +58,7 @@ except ImportError:
         ]
     )
 
-nbsphinx_allow_errors = True
+nbsphinx_allow_errors = False
 
 # -- General configuration ------------------------------------------------
 
