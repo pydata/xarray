d120fab9182169e4bcb0918e0f516c36c8760df2
Thomas Nicholas <thomas.nicholas@columbia.edu>
2023-07-05 11:07:09 -0400

    proper docstrings

--- xarray/core/parallelcompat.py
+++ xarray/core/parallelcompat.py
@@ -179,6 +179,18 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         Check if the given object is an instance of this type of chunked array.
 
         Compares against the type stored in the array_cls attribute by default.
+
+        Parameters
+        ----------
+        data : Any
+
+        Returns
+        -------
+        is_chunked : bool
+
+        See Also
+        --------
+        dask.is_dask_collection
         """
         return isinstance(data, self.array_cls)
 
@@ -187,7 +199,17 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         """
         Return the current chunks of the given array.
 
+        Returns chunks explicitly as a tuple of tuple of ints.
+
         Used internally by xarray objects' .chunks and .chunksizes properties.
+
+        Parameters
+        ----------
+        data : chunked array
+
+        Returns
+        -------
+        chunks : tuple[tuple[int, ...], ...]
 
         See Also
         --------
@@ -205,7 +227,26 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         previous_chunks: T_NormalizedChunks | None = None,
     ) -> T_NormalizedChunks:
         """
+        Normalize given chunking pattern into an explicit tuple of tuples representation.
+
+        Exposed primarily because different chunking backends may want to make different decisions about how to
+        automatically chunk along dimensions not given explicitly in the input chunks.
+
         Called internally by xarray.open_dataset.
+
+        Parameters
+        ----------
+        chunks : tuple, int, dict, or string
+            The chunks to be normalized.
+        shape : Tuple[int]
+            The shape of the array
+        limit : int (optional)
+            The maximum block size to target in bytes,
+            if freedom is given to choose
+        dtype : np.dtype
+        previous_chunks : Tuple[Tuple[int]], optional
+            Chunks from a previous array that we should use for inspiration when
+            rechunking dimensions automatically.
 
         See Also
         --------
@@ -218,11 +259,19 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         self, data: np.ndarray, chunks: T_Chunks, **kwargs
     ) -> T_ChunkedArray:
         """
-        Creates a chunked array from a non-chunked numpy-like array.
+        Create a chunked array from a non-chunked numpy-like array.
+
+        Generally input should have a ``.shape``, ``.ndim``, ``.dtype`` and support numpy-style slicing.
 
         Called when the .chunk method is called on an xarray object that is not already chunked.
         Also called within open_dataset (when chunks is not None) to create a chunked array from
         an xarray lazily indexed array.
+
+        Parameters
+        ----------
+        data : array_like
+        chunks : int, tuple
+            How to chunk the array.
 
         See Also
         --------
@@ -241,6 +290,19 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
 
         Called when the .chunk method is called on an xarray object that is already chunked.
 
+        Parameters
+        ----------
+        data : dask array
+            Array to be rechunked.
+        chunks :  int, tuple, dict or str, optional
+            The new block dimensions to create. -1 indicates the full size of the
+            corresponding dimension. Default is "auto" which automatically
+            determines chunk sizes.
+
+        Returns
+        -------
+        chunked array
+
         See Also
         --------
         dask.array.Array.rechunk
@@ -248,16 +310,28 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         return data.rechunk(chunks, **kwargs)  # type: ignore[attr-defined]
 
     @abstractmethod
-    def compute(self, *data: T_ChunkedArray, **kwargs) -> tuple[np.ndarray, ...]:
+    def compute(self, *data: T_ChunkedArray | Any, **kwargs) -> tuple[np.ndarray, ...]:
         """
         Computes one or more chunked arrays, returning them as eager numpy arrays.
 
         Called anytime something needs to computed, including multiple arrays at once.
         Used by `.compute`, `.persist`, `.values`.
 
+        Parameters
+        ----------
+        *data : object
+            Any number of objects. If an object is an instance of the chunked array type, it is computed
+            and the in-memory result returned as a numpy array. All other types should be passed through unchanged.
+
+        Returns
+        -------
+        objs
+            The input, but with all chunked arrays now computed.
+
         See Also
         --------
-        dask.array.compute
+        dask.compute
+        dask.array.Array.compute
         """
         raise NotImplementedError()
 
@@ -265,6 +339,10 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
     def array_api(self) -> Any:
         """
         Return the array_api namespace following the python array API standard.
+
+        See https://data-apis.org/array-api/latest/ . Currently used to access the array API function
+        ``full_like``, which is called within the xarray constructors ``xarray.full_like``, ``xarray.ones_like``,
+        ``xarray.zeros_like``, etc.
 
         See Also
         --------
@@ -283,7 +361,36 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         keepdims: bool = False,
     ) -> T_ChunkedArray:
         """
-        Used in some reductions like nanfirst, which is used by groupby.first.
+        A general version of array reductions along one or more axes.
+
+        Used inside some reductions like nanfirst, which is used by ``groupby.first``.
+
+        Parameters
+        ----------
+        arr : chunked array
+            Data to be reduced along one or more axes.
+        func : Callable(x_chunk, axis, keepdims)
+            First function to be executed when resolving the dask graph.
+            This function is applied in parallel to all original chunks of x.
+            See below for function parameters.
+        combine_func : Callable(x_chunk, axis, keepdims), optional
+            Function used for intermediate recursive aggregation (see
+            split_every below). If omitted, it defaults to aggregate_func.
+        aggregate_func : Callable(x_chunk, axis, keepdims)
+            Last function to be executed, producing the final output. It is always invoked, even when the reduced
+            Array counts a single chunk along the reduced axes.
+        axis : int or sequence of ints, optional
+            Axis or axes to aggregate upon. If omitted, aggregate along all axes.
+        dtype : np.dtype
+            data type of output. This argument was previously optional, but
+            leaving as ``None`` will now raise an exception.
+        keepdims : boolean, optional
+            Whether the reduction function should preserve the reduced axes,
+            leaving them at size ``output_size``, or remove them.
+
+        Returns
+        -------
+        chunked array
 
         See Also
         --------
@@ -304,7 +411,67 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         **kwargs,
     ):
         """
-        Called inside xarray.apply_ufunc, so must be supplied for vast majority of xarray computations to be supported.
+        Apply a generalized ufunc or similar python function to arrays.
+
+        ``signature`` determines if the function consumes or produces core
+        dimensions. The remaining dimensions in given input arrays (``*args``)
+        are considered loop dimensions and are required to broadcast
+        naturally against each other.
+
+        In other terms, this function is like ``np.vectorize``, but for
+        the blocks of chunked arrays. If the function itself shall also
+        be vectorized use ``vectorize=True`` for convenience.
+
+        Called inside ``xarray.apply_ufunc``, which is called internally for most xarray operations.
+        Therefore this method must be implemented for the vast majority of xarray computations to be supported.
+
+        Parameters
+        ----------
+        func : callable
+            Function to call like ``func(*args, **kwargs)`` on input arrays
+            (``*args``) that returns an array or tuple of arrays. If multiple
+            arguments with non-matching dimensions are supplied, this function is
+            expected to vectorize (broadcast) over axes of positional arguments in
+            the style of NumPy universal functions [1]_ (if this is not the case,
+            set ``vectorize=True``). If this function returns multiple outputs,
+            ``output_core_dims`` has to be set as well.
+        signature: string
+            Specifies what core dimensions are consumed and produced by ``func``.
+            According to the specification of numpy.gufunc signature [2]_
+        *args : numeric
+            Input arrays or scalars to the callable function.
+        axes: List of tuples, optional, keyword only
+            A list of tuples with indices of axes a generalized ufunc should operate on.
+            For instance, for a signature of ``"(i,j),(j,k)->(i,k)"`` appropriate for
+            matrix multiplication, the base elements are two-dimensional matrices
+            and these are taken to be stored in the two last axes of each argument. The
+            corresponding axes keyword would be ``[(-2, -1), (-2, -1), (-2, -1)]``.
+            For simplicity, for generalized ufuncs that operate on 1-dimensional arrays
+            (vectors), a single integer is accepted instead of a single-element tuple,
+            and for generalized ufuncs for which all outputs are scalars, the output
+            tuples can be omitted.
+        keepdims: bool, optional, keyword only
+            If this is set to True, axes which are reduced over will be left in the result as
+            a dimension with size one, so that the result will broadcast correctly against the
+            inputs. This option can only be used for generalized ufuncs that operate on inputs
+            that all have the same number of core dimensions and with outputs that have no core
+            dimensions , i.e., with signatures like ``"(i),(i)->()"`` or ``"(m,m)->()"``.
+            If used, the location of the dimensions in the output can be controlled with axes
+            and axis.
+        output_dtypes : Optional, dtype or list of dtypes, keyword only
+            Valid numpy dtype specification or list thereof.
+            If not given, a call of ``func`` with a small set of data
+            is performed in order to try to automatically determine the
+            output dtypes.
+        vectorize: bool, keyword only
+            If set to ``True``, ``np.vectorize`` is applied to ``func`` for
+            convenience. Defaults to ``False``.
+        **kwargs : dict
+            Extra keyword arguments to pass to `func`
+
+        Returns
+        -------
+        Single chunked array or tuple of chunked arrays
 
         See Also
         --------
@@ -323,11 +490,41 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         **kwargs,
     ):
         """
-        Called in elementwise operations, but notably not called in xarray.map_blocks.
+        Map a function across all blocks of a chunked array.
+
+        Called in elementwise operations, but notably not (currently) called within xarray.map_blocks.
+
+        Parameters
+        ----------
+        func : callable
+            Function to apply to every block in the array.
+            If ``func`` accepts ``block_info=`` or ``block_id=``
+            as keyword arguments, these will be passed dictionaries
+            containing information about input and output chunks/arrays
+            during computation. See examples for details.
+        args : dask arrays or other objects
+        dtype : np.dtype, optional
+            The ``dtype`` of the output array. It is recommended to provide this.
+            If not provided, will be inferred by applying the function to a small
+            set of fake data.
+        chunks : tuple, optional
+            Chunk shape of resulting blocks if the function does not preserve
+            shape. If not provided, the resulting array is assumed to have the same
+            block structure as the first input array.
+        drop_axis : number or iterable, optional
+            Dimensions lost by the function.
+        new_axis : number or iterable, optional
+            New dimensions created by the function. Note that these are applied
+            after ``drop_axis`` (if present).
+        **kwargs :
+            Other keyword arguments to pass to function. Values must be constants
+            (not dask.arrays)
 
         See Also
         --------
         dask.array.map_blocks
+        dask.array.blockwise : Generalized operation with control over block alignment.
+        dask.array.map_overlap : Generalized operation with overlap between neighbors.
         """
         raise NotImplementedError()
 
@@ -342,7 +539,38 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         **kwargs,
     ):
         """
-        Called by some niche functions in xarray.
+        Tensor operation: Generalized inner and outer products.
+
+        A broad class of blocked algorithms and patterns can be specified with a
+        concise multi-index notation.  The ``blockwise`` function applies an in-memory
+        function across multiple blocks of multiple inputs in a variety of ways.
+        Many chunked array operations are special cases of blockwise including
+        elementwise, broadcasting, reductions, tensordot, and transpose.
+
+        Currently only called explicitly in xarray when performing multidimensional interpolation.
+
+        Parameters
+        ----------
+        func : callable
+            Function to apply to individual tuples of blocks
+        out_ind : iterable
+            Block pattern of the output, something like 'ijk' or (1, 2, 3)
+        *args : sequence of Array, index pairs
+            You may also pass literal arguments, accompanied by None index
+            e.g. (x, 'ij', y, 'jk', z, 'i', some_literal, None)
+        **kwargs : dict
+            Extra keyword arguments to pass to function
+        adjust_chunks : dict
+            Dictionary mapping index to function to be applied to chunk sizes
+        new_axes : dict, keyword only
+            New indexes and their dimension lengths
+        align_arrays: bool
+            Whether or not to align chunks along equally sized dimensions when
+            multiple arrays are provided.  This allows for larger chunks in some
+            arrays to be broken into smaller ones that match chunk sizes in other
+            arrays such that they are compatible for block function mapping. If
+            this is false, then an error will be thrown if arrays do not already
+            have the same number of blocks in each dimension.
 
         See Also
         --------
@@ -356,7 +584,14 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         **kwargs,
     ) -> tuple[dict[str, T_NormalizedChunks], list[T_ChunkedArray]]:
         """
+        Unify chunks across a sequence of arrays.
+
         Called by xarray.unify_chunks.
+
+        Parameters
+        ----------
+        *args: sequence of Array, index pairs
+            Sequence like (x, 'ij', y, 'jk', z, 'i')
 
         See Also
         --------
@@ -371,7 +606,24 @@ class ChunkManagerEntrypoint(ABC, Generic[T_ChunkedArray]):
         **kwargs: dict[str, Any],
     ):
         """
-        Used when writing to any backend.
+        Store chunked arrays in array-like objects, overwriting data in target.
+
+        This stores chunked arrays into object that supports numpy-style setitem
+        indexing (e.g. a Zarr Store). Allows storing values chunk by chunk so that it does not have to
+        fill up memory. For best performance you likely want to align the block size of
+        the storage target with the block size of your array.
+
+        Used when writing to any registered xarray I/O backend.
+
+        Parameters
+        ----------
+        sources: Array or collection of Arrays
+        targets: array-like or collection of array-likes
+            These should support setitem syntax ``target[10:20] = ...``.
+            If sources is a single item, targets must be a single item; if sources is a
+            collection of arrays, targets must be a matching collection.
+        kwargs:
+            Parameters passed to compute/persist (only used if compute=True)
 
         See Also
         --------
