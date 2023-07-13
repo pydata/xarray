e6e5313c9004ccdeb295247c704ad4f743177585
Justus Magin <keewis@posteo.de>
2023-07-13 19:53:05 +0200

    check that the hints are constructed correctly

--- xarray/tests/test_formatting.py
+++ xarray/tests/test_formatting.py
@@ -244,6 +244,40 @@ class TestFormatting:
         assert names[0] in inline
         assert index._repr_inline_(max_width=40) in inline
 
+    @pytest.mark.parametrize(
+        "names",
+        (
+            ["x"],
+            ["x", "y"],
+            ["x", "y", "z"],
+            ["x", "y", "z", "a"],
+        ),
+    )
+    def test_index_repr_grouping(self, names) -> None:
+        from xarray.core.indexes import Index
+
+        class CustomIndex(Index):
+            def __init__(self, names):
+                self.names = names
+
+            def __repr__(self):
+                return f"CustomIndex(coords={self.names})"
+
+        index = CustomIndex(names)
+
+        normal = formatting.summarize_index(names, index, col_width=20)
+        assert all(name in normal for name in names)
+        assert len(normal.splitlines()) == len(names)
+        assert "CustomIndex" in normal
+
+        hint_chars = [line[2] for line in normal.splitlines()]
+
+        if len(names) <= 1:
+            assert hint_chars == [" "]
+        else:
+            assert hint_chars[0] == "┌" and hint_chars[-1] == "└"
+            assert len(names) == 2 or hint_chars[1:-1] == ["│"] * (len(names) - 2)
+
     def test_diff_array_repr(self) -> None:
         da_a = xr.DataArray(
             np.array([[1, 2, 3], [4, 5, 6]], dtype="int64"),
