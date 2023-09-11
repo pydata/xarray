3edd9978b4590666e83d8c0e4e8f574be09ff4c8
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-09-11 20:09:15 +0200

    Don't try to sort hashable, map to string (#8172)
    * Update dataset.py
    
    * Update dataset.py
    
    * Update dataset.py
    
    * Remove sorted, keep it simple.
    
    * Update dataset.py
    
    * tests fail if tweaking this too much, just map to str then.

--- xarray/core/dataset.py
+++ xarray/core/dataset.py
@@ -5284,7 +5284,7 @@ class Dataset(
             if missing_sample_dims:
                 raise ValueError(
                     "Variables in the dataset must contain all ``sample_dims`` "
-                    f"({sample_dims!r}) but '{key}' misses {sorted(missing_sample_dims)}"
+                    f"({sample_dims!r}) but '{key}' misses {sorted(map(str, missing_sample_dims))}"
                 )
 
         def stack_dataarray(da):
