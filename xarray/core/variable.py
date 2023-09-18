d68b95e1fd837fb5a9f1139bc5b1fd4c54958aef
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-09-18 21:39:50 +0200

    Update variable.py

--- xarray/core/variable.py
+++ xarray/core/variable.py
@@ -1085,9 +1085,9 @@ class Variable(AbstractArray, NdimSizeLenMixin, VariableArithmetic):
 
         else:
             ndata = as_compatible_data(data)
-            if self.shape != ndata.shape:
+            if self.shape != ndata.shape:  # type: ignore[attr-defined]
                 raise ValueError(
-                    f"Data shape {ndata.shape} must match shape of object {self.shape}"
+                    f"Data shape {ndata.shape} must match shape of object {self.shape}"  # type: ignore[attr-defined]
                 )
 
         attrs = copy.deepcopy(self._attrs, memo) if deep else copy.copy(self._attrs)
