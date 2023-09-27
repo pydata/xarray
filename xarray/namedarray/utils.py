dd65e3acf1d4b801b679b1eed1dc4badb8d2d203
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2023-09-27 08:23:20 -0700

    initial refactor for NamedArray (#8075)
    * initial prototype for NamedArray
    
    * move NDArrayMixin and NdimSizeLenMixin inside named_array
    
    * vendor is_duck_dask_array
    
    * vendor Frozen object
    
    * update import
    
    * move _default sentinel value
    
    * rename subpackage to namedarray per @TomNicholas suggestion
    
    * Remove NdimSizeLenMixin
    
    * fix typing
    
    * add annotations
    
    * Remove NDArrayMixin
    
    * Apply suggestions from code review
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    
    * fix typing
    
    * fix return type
    
    * revert NDArrayMixin
    
    * [WIP] as_compatible_data refactor
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * duplicate sentinel value and leave the original sentinel object alone
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephan Hoyer <shoyer@google.com>
    
    * use DuckArray
    
    * Apply suggestions from code review
    
    Co-authored-by: Stephan Hoyer <shoyer@google.com>
    
    * use sentinel value from xarray
    
    * remove unused code
    
    * fix variable constructor
    
    * fix as_compatible_data utility function
    
    * move _to_dense and _non_zero to NamedArray
    
    * more typing
    
    * add initial tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * attempt to fix some mypy errors
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update core.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update core.py
    
    * All input data can be arraylike
    
    * Update core.py
    
    * Update core.py
    
    * get and set attrs at the same level.
    
    * data doesn't have to be ndarray
    
    * avoid redefining typing use new variable names instead
    
    * import on runtime as well to be able to cast
    
    * requires ufunc and function to be a valid duck array
    
    * Add array_namespace
    
    * Update test_dataset.py
    
    * Update test_dataset.py
    
    * remove Frozen
    
    * update tests
    
    * update tests
    
    * switch to functional API
    
    * add fastpath
    
    * Test making sizes dict[Hashable, int]
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * A lot of errors... Try Mapping instead
    
    * Update groupby.py
    
    * Update types.py
    
    * Apply suggestions from code review
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update docstrings
    
    * update error messages
    
    * update tests
    
    * test explicitly index array
    
    * update tests
    
    * remove unused types
    
    * Update xarray/tests/test_namedarray.py
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * use Self
    
    ---------
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    Co-authored-by: dcherian <deepak@cherian.net>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Stephan Hoyer <shoyer@google.com>
    Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>

---
+++ xarray/namedarray/utils.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import importlib
+import sys
+import typing
+
+import numpy as np
+
+if typing.TYPE_CHECKING:
+    if sys.version_info >= (3, 10):
+        from typing import TypeGuard
+    else:
+        from typing_extensions import TypeGuard
+
+# temporary placeholder for indicating an array api compliant type.
+# hopefully in the future we can narrow this down more
+T_DuckArray = typing.TypeVar("T_DuckArray", bound=typing.Any)
+
+
+def module_available(module: str) -> bool:
+    """Checks whether a module is installed without importing it.
+
+    Use this for a lightweight check and lazy imports.
+
+    Parameters
+    ----------
+    module : str
+        Name of the module.
+
+    Returns
+    -------
+    available : bool
+        Whether the module is installed.
+    """
+    return importlib.util.find_spec(module) is not None
+
+
+def is_dask_collection(x: typing.Any) -> bool:
+    if module_available("dask"):
+        from dask.base import is_dask_collection
+
+        return is_dask_collection(x)
+    return False
+
+
+def is_duck_array(value: typing.Any) -> TypeGuard[T_DuckArray]:
+    if isinstance(value, np.ndarray):
+        return True
+    return (
+        hasattr(value, "ndim")
+        and hasattr(value, "shape")
+        and hasattr(value, "dtype")
+        and (
+            (hasattr(value, "__array_function__") and hasattr(value, "__array_ufunc__"))
+            or hasattr(value, "__array_namespace__")
+        )
+    )
+
+
+def is_duck_dask_array(x: typing.Any) -> bool:
+    return is_duck_array(x) and is_dask_collection(x)
+
+
+def to_0d_object_array(value: typing.Any) -> np.ndarray:
+    """Given a value, wrap it in a 0-D numpy.ndarray with dtype=object."""
+    result = np.empty((), dtype=object)
+    result[()] = value
+    return result
