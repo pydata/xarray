d8238b681e31ae5960efebc2d88029fa12eca307
Anderson Banihirwe <13301940+andersy005@users.noreply.github.com>
2023-10-04 11:14:24 -0700

    copy  the `dtypes` module to the `namedarray` package. (#8250)
    * move dtypes module to namedarray
    
    * keep original dtypes
    
    * revert utils changes
    
    * Update xarray/namedarray/dtypes.py
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix missing imports
    
    * update typing
    
    * fix return types
    
    * type fixes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * type fixes
    
    ---------
    
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>

--- xarray/namedarray/utils.py
+++ xarray/namedarray/utils.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import importlib
 import sys
+from collections.abc import Hashable
 from enum import Enum
 from typing import TYPE_CHECKING, Any, Final, Protocol, TypeVar
 
@@ -134,3 +135,29 @@ def to_0d_object_array(
     result = np.empty((), dtype=object)
     result[()] = value
     return result
+
+
+class ReprObject:
+    """Object that prints as the given value, for use with sentinel values."""
+
+    __slots__ = ("_value",)
+
+    _value: str
+
+    def __init__(self, value: str):
+        self._value = value
+
+    def __repr__(self) -> str:
+        return self._value
+
+    def __eq__(self, other: ReprObject | Any) -> bool:
+        # TODO: What type can other be? ArrayLike?
+        return self._value == other._value if isinstance(other, ReprObject) else False
+
+    def __hash__(self) -> int:
+        return hash((type(self), self._value))
+
+    def __dask_tokenize__(self) -> Hashable:
+        from dask.base import normalize_token
+
+        return normalize_token((type(self), self._value))  # type: ignore[no-any-return]
