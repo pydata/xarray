65c213ece2c527f34ecd19faa86552986f5466e7
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-10-13 21:17:53 +0200

    Create _array_api.py

---
+++ xarray/namedarray/_array_api.py
@@ -0,0 +1,72 @@
+from typing import Any, Callable, cast
+from types import ModuleType
+
+import numpy as np
+
+from xarray.namedarray.core import NamedArray, _new
+from xarray.namedarray.utils import (
+    _arrayapi,
+    _DType,
+    DuckArray,
+    _SupportsReal,
+    _SupportsImag,
+    _ScalarType,
+    _ShapeType,
+)
+
+
+def _get_data_namespace(x: NamedArray[Any, Any]) -> ModuleType:
+    if isinstance(x._data, _arrayapi):
+        return x._data.__array_namespace__()
+    else:
+        return np
+
+
+def astype(
+    x: NamedArray[Any, Any], dtype: _DType, /, *, copy: bool = True
+) -> NamedArray[Any, _DType]:
+    if isinstance(x._data, _arrayapi):
+        xp = x._data.__array_namespace__()
+
+        return _new(x, x._dims, xp.astype(x, dtype, copy=copy), x._attrs)  # type: ignore[no-any-return]
+
+    # np.astype doesn't exist yet:
+    return _new(x, x._dims, x.astype(dtype, copy=copy), x._attrs)  # type: ignore[no-any-return, attr-defined]
+
+
+def imag(
+    x: NamedArray[_ShapeType, np.dtype[_SupportsImag[_ScalarType]]], /  # type: ignore[type-var]
+) -> NamedArray[_ShapeType, np.dtype[_ScalarType]]:
+    xp = _get_data_namespace(x)
+    return _new(x, x._dims, xp.imag(x._data), x._attrs)
+
+
+def real(
+    x: NamedArray[_ShapeType, np.dtype[_SupportsReal[_ScalarType]]], /  # type: ignore[type-var]
+) -> NamedArray[_ShapeType, np.dtype[_ScalarType]]:
+    xp = _get_data_namespace(x)
+    return _new(x, x._dims, xp.real(x._data), x._attrs)
+
+
+a = NamedArray(("x",), np.array([1 + 3j, 2 + 2j, 3 + 3j], dtype=np.complex64))
+reveal_type(a)
+reveal_type(a.data)
+
+
+xp = _get_data_namespace(a)
+reveal_type(xp)
+
+# def _real
+# _func: Callable[
+#     NamedArray[_ShapeType, np.dtype[_SupportsReal[_ScalarType]]],
+#     NamedArray[_ShapeType, np.dtype[_ScalarType]],
+# ] = xp.imag
+# reveal_type(_func)
+
+b = real(a)
+reveal_type(b)
+reveal_type(b.data)
+
+b = imag(a)
+reveal_type(b)
+reveal_type(b.data)
