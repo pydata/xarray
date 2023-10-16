61d3a0213ab33b094ea57bdcdde1ee5a3c85fb16
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-10-16 18:43:33 +0200

    as_compatible_data lose the typing

--- xarray/core/variable.py
+++ xarray/core/variable.py
@@ -934,9 +934,9 @@ class Variable(NamedArray, AbstractArray, VariableArithmetic):
 
         else:
             ndata = as_compatible_data(data)
-            if self.shape != ndata.shape:
+            if self.shape != ndata.shape:  # type: ignore[attr-defined]
                 raise ValueError(
-                    f"Data shape {ndata.shape} must match shape of object {self.shape}"
+                    f"Data shape {ndata.shape} must match shape of object {self.shape}"  # type: ignore[attr-defined]
                 )
 
         attrs = copy.deepcopy(self._attrs, memo) if deep else copy.copy(self._attrs)
