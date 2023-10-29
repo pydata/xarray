eea2aeae784bcbaf11a1e7ebb4b5406ac673b063
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-10-29 12:08:33 +0100

    Update conf.py (#8390)

--- doc/conf.py
+++ doc/conf.py
@@ -237,7 +237,6 @@ html_theme_options = dict(
     use_repository_button=True,
     use_issues_button=True,
     home_page_in_toc=False,
-    navigation_with_keys=False,
     extra_footer="""<p>Xarray is a fiscally sponsored project of <a href="https://numfocus.org">NumFOCUS</a>,
     a nonprofit dedicated to supporting the open-source scientific computing community.<br>
     Theme by the <a href="https://ebp.jupyterbook.org">Executable Book Project</a></p>""",
