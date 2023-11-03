583dd95b2d4e37ca334d8e9bd37dcc0745767275
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-11-03 01:00:54 +0100

    Update _array_api.py

--- xarray/namedarray/_array_api.py
+++ xarray/namedarray/_array_api.py
@@ -8,11 +8,13 @@ import numpy as np
 
 from xarray.namedarray._typing import (
     _arrayapi,
+    _Dim,
     _DType,
     _ScalarType,
     _ShapeType,
     _SupportsImag,
     _SupportsReal,
+    Default,
 )
 from xarray.namedarray.core import NamedArray
 
@@ -143,4 +145,51 @@ def real(
     """
     xp = _get_data_namespace(x)
     out = x._new(data=xp.real(x._data))
+    return out
+
+
+# %% Manipulation functions
+def expand_dims(
+    x: NamedArray[Any, _DType],
+    /,
+    *,
+    dim: _Dim | Default = _default,
+    axis: _Axis = 0,
+) -> NamedArray[Any, _DType]:
+    """
+    Expands the shape of an array by inserting a new dimension of size one at the
+    position specified by dims.
+
+    Parameters
+    ----------
+    x :
+        Array to expand.
+    dim :
+        Dimension name. New dimension will be stored in the 0 position.
+    axis :
+        Axis position (zero-based). If x has rank (i.e, number of dimensions) N,
+        a valid axis must reside on the closed-interval [-N-1, N]. If provided a
+        negative axis, the axis position at which to insert a singleton dimension
+        must be computed as N + axis + 1. Hence, if provided -1, the resolved axis
+        position must be N (i.e., a singleton dimension must be appended to the
+        input array x). If provided -N-1, the resolved axis position must be 0
+        (i.e., a singleton dimension must be prepended to the input array x).
+
+    Returns
+    -------
+        out :
+            An expanded output array having the same data type as x.
+
+    Examples
+    --------
+    >>> x = NamedArray(("x", "y"), nxp.asarray([[1.0, 2.0], [3.0, 4.0]]))
+    >>> expand_dims(x, dims="new_dim")
+    <xarray.NamedArray (new_dim: 1, x: 2, y: 2)>
+    Array([[[1., 2.],
+            [3., 4.]]], dtype=float64)
+    """
+    xp = _get_data_namespace(x)
+    d = list(x.dims)
+    d.insert(axis, dim)
+    out = x._new(dims=tuple(d), data=xp.expand_dims(x._data, axis=axis))
     return out
