7205a32da5cb292eed754126b021b90a42c28b6e
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-11-03 16:22:59 +0100

    more

--- xarray/namedarray/core.py
+++ xarray/namedarray/core.py
@@ -25,6 +25,7 @@ from xarray.namedarray._typing import (
     _arrayapi,
     _arrayfunction_or_api,
     _chunkedarray,
+    _default,
     _dtype,
     _DType_co,
     _ScalarType_co,
@@ -33,7 +34,7 @@ from xarray.namedarray._typing import (
     _SupportsImag,
     _SupportsReal,
 )
-from xarray.namedarray.utils import _default, is_duck_dask_array, to_0d_object_array
+from xarray.namedarray.utils import is_duck_dask_array, to_0d_object_array
 
 if TYPE_CHECKING:
     from numpy.typing import ArrayLike, NDArray
