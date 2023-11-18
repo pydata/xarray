b97202ed0df54f5e9c53c89514bdfe74b766660e
Maximilian Roos <m@maxroos.com>
2023-11-17 17:37:17 -0800

    Consolidate `_get_alpha` func
    Am changing this a bit so starting with consolidating it rather than converting twice

--- xarray/core/rolling_exp.py
+++ xarray/core/rolling_exp.py
@@ -21,54 +21,37 @@ except ImportError:
 
 
 def _get_alpha(
-    com: float | None = None,
-    span: float | None = None,
-    halflife: float | None = None,
-    alpha: float | None = None,
-) -> float:
-    # pandas defines in terms of com (converting to alpha in the algo)
-    # so use its function to get a com and then convert to alpha
-
-    com = _get_center_of_mass(com, span, halflife, alpha)
-    return 1 / (1 + com)
-
-
-def _get_center_of_mass(
     comass: float | None,
     span: float | None,
     halflife: float | None,
     alpha: float | None,
 ) -> float:
     """
-    Vendored from pandas.core.window.common._get_center_of_mass
-
-    See licenses/PANDAS_LICENSE for the function's license
+    Convert comass, span, halflife to alpha.
     """
     valid_count = count_not_none(comass, span, halflife, alpha)
     if valid_count > 1:
         raise ValueError("comass, span, halflife, and alpha are mutually exclusive")
 
-    # Convert to center of mass; domain checks ensure 0 < alpha <= 1
+    # Convert to alpha
     if comass is not None:
         if comass < 0:
             raise ValueError("comass must satisfy: comass >= 0")
+        return 1 / (comass + 1)
     elif span is not None:
         if span < 1:
             raise ValueError("span must satisfy: span >= 1")
-        comass = (span - 1) / 2.0
+        return 2 / (span + 1)
     elif halflife is not None:
         if halflife <= 0:
             raise ValueError("halflife must satisfy: halflife > 0")
-        decay = 1 - np.exp(np.log(0.5) / halflife)
-        comass = 1 / decay - 1
+        return 1 - np.exp(np.log(0.5) / halflife)
     elif alpha is not None:
-        if alpha <= 0 or alpha > 1:
+        if not 0 < alpha <= 1:
             raise ValueError("alpha must satisfy: 0 < alpha <= 1")
-        comass = (1.0 - alpha) / alpha
+        return alpha
     else:
         raise ValueError("Must pass one of comass, span, halflife, or alpha")
-
-    return float(comass)
 
 
 class RollingExp(Generic[T_DataWithCoords]):
