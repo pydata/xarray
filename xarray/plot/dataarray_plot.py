bdd4d9dbb1524bb38910d60244d1963283e70bfe
Illviljan <14371165+Illviljan@users.noreply.github.com>
2023-12-10 23:54:47 +0100

    only filter on scatter plots as that is safe.

--- xarray/plot/dataarray_plot.py
+++ xarray/plot/dataarray_plot.py
@@ -944,15 +944,15 @@ def _plot1d(plotfunc):
         if plotfunc.__name__ == "scatter":
             size_ = kwargs.pop("_size", markersize)
             size_r = _MARKERSIZE_RANGE
+
+            # Remove any nulls, .where(m, drop=True) doesn't work when m is
+            # a dask array, so load the array to memory.
+            # It will have to be loaded to memory at some point anyway:
+            darray = darray.load()
+            darray = darray.where(darray.notnull(), drop=True)
         else:
             size_ = kwargs.pop("_size", linewidth)
             size_r = _LINEWIDTH_RANGE
-
-        # Remove any nulls, .where(m, drop=True) doesn't work when m is a dask array,
-        # so load the array to memory.
-        # It will have to be loaded to memory at some point anyway:
-        darray = darray.load()
-        darray = darray.where(darray.notnull(), drop=True)
 
         # Get data to plot:
         coords_to_plot: MutableMapping[str, Hashable | None] = dict(
