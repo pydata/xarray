7dc832d74b0ee7544feea14c049476b3d8a6e11b
TomNicholas <tom@cworthy.org>
2023-12-13 23:34:11 -0500

    remove old utilities

--- xarray/testing/utils.py
+++ xarray/testing/utils.py
@@ -8,29 +8,3 @@ def suppress_warning(category, message=""):
         warnings.filterwarnings("ignore", category=category, message=message)
 
         yield
-
-
-def create_dimension_names(ndim: int) -> list[str]:
-    return [f"dim_{n}" for n in range(ndim)]
-
-
-def valid_dims_from_axes(dims, axes):
-    if axes is None:
-        return None
-
-    if axes == 0 and len(dims) == 0:
-        return None
-
-    if isinstance(axes, int):
-        return dims[axes]
-
-    return [dims[axis] for axis in axes]
-
-
-def valid_axes_from_dims(all_dims, dims):
-    if dims is None:
-        return None
-    elif isinstance(dims, list):
-        return [all_dims.index(dim) for dim in dims]
-    else:
-        return all_dims.index(dims)
