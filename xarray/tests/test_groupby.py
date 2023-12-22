44e5a41f280f52b1f1efed10f3aab64dc2a9b0c0
Deepak Cherian <deepak@cherian.net>
2023-12-21 19:21:06 -0700

    minimize test mods

--- xarray/tests/test_groupby.py
+++ xarray/tests/test_groupby.py
@@ -1475,9 +1475,10 @@ class TestDataArrayGroupBy:
             ("a", ("a", "y")),
             ("b", ("x", "b")),
         ]:
-            result = array.groupby(by, squeeze=False, restore_coord_dims=True).map(
-                lambda x: x.squeeze()
-            )["c"]
+            with pytest.warns(UserWarning, match="The `squeeze` kwarg"):
+                result = array.groupby(by, restore_coord_dims=True).map(
+                    lambda x: x.squeeze()
+                )["c"]
             assert result.dims == expected_dims
 
     def test_groupby_first_and_last(self):
@@ -1585,7 +1586,7 @@ class TestDataArrayGroupBy:
         da = xr.DataArray(np.ones((2, 3, 4)))
         bins = [-1, 0, 1, 2]
         with xr.set_options(use_flox=False):
-            actual = da.groupby_bins("dim_0", bins, squeeze=False).mean(...)
+            actual = da.groupby_bins("dim_0", bins).mean(...)
         with xr.set_options(use_flox=True):
             expected = da.groupby_bins("dim_0", bins).mean(...)
         assert_allclose(actual, expected)
