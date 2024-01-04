6826bc136de57b3e2e68366134192ec1452146ef
Kai Mühlbauer <kai.muehlbauer@uni-bonn.de>
2024-01-04 07:41:35 +0100

    ENH: vendor SerializableLock from dask and use as default backend lock, adapt tests (#8571)
    * vendor SerializableLock from dask, adapt tests
    * Update doc/whats-new.rst
    
    ---------
    
    Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>

--- xarray/tests/test_distributed.py
+++ xarray/tests/test_distributed.py
@@ -27,7 +27,7 @@ from distributed.utils_test import (  # noqa: F401
 )
 
 import xarray as xr
-from xarray.backends.locks import HDF5_LOCK, CombinedLock
+from xarray.backends.locks import HDF5_LOCK, CombinedLock, SerializableLock
 from xarray.tests import (
     assert_allclose,
     assert_identical,
@@ -273,7 +273,7 @@ async def test_async(c, s, a, b) -> None:
 
 
 def test_hdf5_lock() -> None:
-    assert isinstance(HDF5_LOCK, dask.utils.SerializableLock)
+    assert isinstance(HDF5_LOCK, SerializableLock)
 
 
 @gen_cluster(client=True)
