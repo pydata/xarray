be6918e815a4616cabb6384836c5d65e2dc92aac
Justus Magin <keewis@posteo.de>
2024-01-12 12:29:25 +0100

    add a `minversion` kwarg to `module_available`

--- xarray/core/utils.py
+++ xarray/core/utils.py
@@ -74,6 +74,7 @@ from typing import (
 
 import numpy as np
 import pandas as pd
+from packaging.version import Version
 
 if TYPE_CHECKING:
     from xarray.core.types import Dims, ErrorOptionsWithWarn, OrderedDims, T_DuckArray
@@ -1198,7 +1199,7 @@ def contains_only_chunked_or_numpy(obj) -> bool:
     )
 
 
-def module_available(module: str) -> bool:
+def module_available(module: str, minversion: str | None = None) -> bool:
     """Checks whether a module is installed without importing it.
 
     Use this for a lightweight check and lazy imports.
@@ -1207,13 +1208,23 @@ def module_available(module: str) -> bool:
     ----------
     module : str
         Name of the module.
+    minversion : str, optional
+        Minimum version of the module
 
     Returns
     -------
     available : bool
         Whether the module is installed.
     """
-    return importlib.util.find_spec(module) is not None
+    if importlib.util.find_spec(module) is None:
+        return False
+
+    if minversion is not None:
+        version = importlib.metadata.version(module)
+
+        return Version(version) >= Version(minversion)
+
+    return True
 
 
 def find_stack_level(test_mode=False) -> int:
