72c0eddfed24a1f09c19f177abf178d0994f8000
Tom Vo <tomvothecoder@gmail.com>
2024-01-25 17:11:29 -0800

    Fix `variables` arg typo in `Dataset.sortby()` docstring (#8670)
    * Fix `variables` arg typo in `Dataset.sortby()` docstring
    
    * Add change to `whats-new.rst`
    
    * Remove whitespace in `whats-new.rst`
    
    * Update doc/whats-new.rst
    
    Co-authored-by: Maximilian Roos <5635139+max-sixty@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Maximilian Roos <5635139+max-sixty@users.noreply.github.com>

--- xarray/core/dataset.py
+++ xarray/core/dataset.py
@@ -7947,7 +7947,7 @@ class Dataset(
 
         Parameters
         ----------
-        kariables : Hashable, DataArray, sequence of Hashable or DataArray, or Callable
+        variables : Hashable, DataArray, sequence of Hashable or DataArray, or Callable
             1D DataArray objects or name(s) of 1D variable(s) in coords whose values are
             used to sort this array. If a callable, the callable is passed this object,
             and the result is used as the value for cond.
