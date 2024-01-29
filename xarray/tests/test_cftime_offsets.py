e82326f71ae6f17fd869e5743accb1624fc6cb99
Mathias Hauser <mathias.hauser@env.ethz.ch>
2024-01-29 17:39:16 +0100

    only test standard calendar

--- xarray/tests/test_cftime_offsets.py
+++ xarray/tests/test_cftime_offsets.py
@@ -1608,10 +1608,10 @@ def test_to_offset_deprecation_warning(freq):
 @pytest.mark.parametrize("start", ("2000", "2001"))
 @pytest.mark.parametrize("end", ("2000", "2001"))
 @pytest.mark.parametrize("freq", ("MS", "-1MS", "YS", "-1YS"))
-def test_cftime_range_same_as_pandas(calendar, start, end, freq):
-    result = date_range(start, end, freq=freq, calendar=calendar, use_cftime=True)
+def test_cftime_range_same_as_pandas(start, end, freq):
+    result = date_range(start, end, freq=freq, calendar="standard", use_cftime=True)
     result = result.to_datetimeindex()
-    expected = pd.date_range(start, end, freq=freq)
+    expected = date_range(start, end, freq=freq, use_cftime=False)
 
     np.testing.assert_array_equal(result, expected)
 
@@ -1619,9 +1619,9 @@ def test_cftime_range_same_as_pandas(calendar, start, end, freq):
 @pytest.mark.parametrize("start", ("2000", "2001"))
 @pytest.mark.parametrize("end", ("2000", "2001"))
 @pytest.mark.parametrize("freq", ("M", "-1M", "Y", "-1Y"))
-def test_cftime_range_end_same_len_pandas(calendar, start, end, freq):
-    result = date_range(start, end, freq=freq, calendar=calendar, use_cftime=True)
-    expected = pd.date_range(start, end, freq=freq)
+def test_cftime_range_end_same_len_pandas(start, end, freq):
+    result = date_range(start, end, freq=freq, calendar="standard", use_cftime=True)
+    expected = date_range(start, end, freq=freq, use_cftime=False)
 
     # end of month/ year is not convertable to datetimeindex
     assert result.size == expected.size
