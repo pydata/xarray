f84b3ec10570a1c8402d8bf8c7309ed9ab7f6d8f
Anderson Banihirwe <axbanihirwe@ualr.edu>
2024-02-02 13:37:27 -0800

    Add support for TypeAlias in Python 3.11 and fallback to typing_extensions in earlier versions

--- xarray/namedarray/_typing.py
+++ xarray/namedarray/_typing.py
@@ -5,6 +5,7 @@ from collections.abc import Hashable, Iterable, Mapping, Sequence
 from enum import Enum
 from types import ModuleType
 from typing import (
+    TYPE_CHECKING,
     Any,
     Callable,
     Final,
@@ -19,10 +20,16 @@ from typing import (
 
 import numpy as np
 
-if sys.version_info >= (3, 11):
-    from typing import TypeAlias
-else:
-    from typing_extensions import TypeAlias
+try:
+    if sys.version_info >= (3, 11):
+        from typing import TypeAlias
+    else:
+        from typing_extensions import TypeAlias
+except ImportError:
+    if TYPE_CHECKING:
+        raise
+    else:
+        Self: Any = None
 
 
 # Singleton type, as per https://github.com/python/typing/pull/240
