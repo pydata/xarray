98d3637280c62c8a701de8767e7eb46c09af41c7
Mathias Hauser <mathias.hauser@env.ethz.ch>
2024-02-09 18:12:30 +0100

    add freqs, invert ifs; add try block

--- xarray/coding/cftime_offsets.py
+++ xarray/coding/cftime_offsets.py
@@ -1265,31 +1265,37 @@ def _new_to_legacy_freq(freq):
     # we add logic to continue using the deprecated "M" and "Q" frequency
     # strings in these circumstances.
 
-    # "h" -> "H" conversion not required
+    # NOTE: other conversions ("h" -> "H", ..., "N" -> "ns") not required
 
     # TODO: remove once requiring pandas >= 2.2
+    if freq and Version(pd.__version__) >= Version("2.2"):
+        return freq
 
-    if freq and Version(pd.__version__) < Version("2.2"):
+    try:
         freq_as_offset = to_offset(freq)
-        if isinstance(freq_as_offset, MonthEnd) and "ME" in freq:
-            freq = freq.replace("ME", "M")
-        elif isinstance(freq_as_offset, QuarterEnd) and "QE" in freq:
-            freq = freq.replace("QE", "Q")
-        elif isinstance(freq_as_offset, YearBegin) and "YS" in freq:
-            freq = freq.replace("YS", "AS")
-        elif isinstance(freq_as_offset, YearEnd):
-            # testing for "Y" is required as this was valid in xarray 2023.11 - 2024.01
-            if "Y-" in freq:
-                # Check for and replace "Y-" instead of just "Y" to prevent
-                # corrupting anchored offsets that contain "Y" in the month
-                # abbreviation, e.g. "Y-MAY" -> "A-MAY".
-                freq = freq.replace("Y-", "A-")
-            elif "YE-" in freq:
-                freq = freq.replace("YE-", "A-")
-            elif "A-" not in freq and freq.endswith("Y"):
-                freq = freq.replace("Y", "A")
-            elif freq.endswith("YE"):
-                freq = freq.replace("YE", "A")
+    except ValueError:
+        # freq may be valid in pandas but not in xarray
+        return freq
+
+    if isinstance(freq_as_offset, MonthEnd) and "ME" in freq:
+        freq = freq.replace("ME", "M")
+    elif isinstance(freq_as_offset, QuarterEnd) and "QE" in freq:
+        freq = freq.replace("QE", "Q")
+    elif isinstance(freq_as_offset, YearBegin) and "YS" in freq:
+        freq = freq.replace("YS", "AS")
+    elif isinstance(freq_as_offset, YearEnd):
+        # testing for "Y" is required as this was valid in xarray 2023.11 - 2024.01
+        if "Y-" in freq:
+            # Check for and replace "Y-" instead of just "Y" to prevent
+            # corrupting anchored offsets that contain "Y" in the month
+            # abbreviation, e.g. "Y-MAY" -> "A-MAY".
+            freq = freq.replace("Y-", "A-")
+        elif "YE-" in freq:
+            freq = freq.replace("YE-", "A-")
+        elif "A-" not in freq and freq.endswith("Y"):
+            freq = freq.replace("Y", "A")
+        elif freq.endswith("YE"):
+            freq = freq.replace("YE", "A")
 
     return freq
 
@@ -1299,34 +1305,46 @@ def _legacy_to_new_freq(freq):
 
     # TODO: remove once requiring pandas >= 2.2
 
-    if freq and Version(pd.__version__) < Version("2.2"):
+    if freq and Version(pd.__version__) >= Version("2.2"):
+        return freq
+
+    try:
         freq_as_offset = to_offset(freq, warn=False)
-        if isinstance(freq_as_offset, MonthEnd) and "ME" not in freq:
-            freq = freq.replace("M", "ME")
-        elif isinstance(freq_as_offset, QuarterEnd) and "QE" not in freq:
-            freq = freq.replace("Q", "QE")
-        elif isinstance(freq_as_offset, YearBegin) and "YS" not in freq:
-            freq = freq.replace("AS", "YS")
-        elif isinstance(freq_as_offset, YearEnd):
-            if "A-" in freq:
-                # Check for and replace "A-" instead of just "A" to prevent
-                # corrupting anchored offsets that contain "Y" in the month
-                # abbreviation, e.g. "A-MAY" -> "YE-MAY".
-                freq = freq.replace("A-", "YE-")
-            elif "Y-" in freq:
-                freq = freq.replace("Y-", "YE-")
-            elif freq.endswith("A"):
-                # the "A-MAY" case is already handled above
-                freq = freq.replace("A", "YE")
-            elif "YE" not in freq and freq.endswith("Y"):
-                # the "Y-MAY" case is already handled above
-                freq = freq.replace("Y", "YE")
-        elif isinstance(freq_as_offset, Hour):
-            freq = freq.replace("H", "h")
-        elif isinstance(freq_as_offset, Minute):
-            freq = freq.replace("T", "min")
-        elif isinstance(freq_as_offset, Second):
-            freq = freq.replace("S", "s")
+    except ValueError:
+        # freq may be valid in pandas but not in xarray
+        return freq
+
+    if isinstance(freq_as_offset, MonthEnd) and "ME" not in freq:
+        freq = freq.replace("M", "ME")
+    elif isinstance(freq_as_offset, QuarterEnd) and "QE" not in freq:
+        freq = freq.replace("Q", "QE")
+    elif isinstance(freq_as_offset, YearBegin) and "YS" not in freq:
+        freq = freq.replace("AS", "YS")
+    elif isinstance(freq_as_offset, YearEnd):
+        if "A-" in freq:
+            # Check for and replace "A-" instead of just "A" to prevent
+            # corrupting anchored offsets that contain "Y" in the month
+            # abbreviation, e.g. "A-MAY" -> "YE-MAY".
+            freq = freq.replace("A-", "YE-")
+        elif "Y-" in freq:
+            freq = freq.replace("Y-", "YE-")
+        elif freq.endswith("A"):
+            # the "A-MAY" case is already handled above
+            freq = freq.replace("A", "YE")
+        elif "YE" not in freq and freq.endswith("Y"):
+            # the "Y-MAY" case is already handled above
+            freq = freq.replace("Y", "YE")
+    elif isinstance(freq_as_offset, Hour):
+        freq = freq.replace("H", "h")
+    elif isinstance(freq_as_offset, Minute):
+        freq = freq.replace("T", "min")
+    elif isinstance(freq_as_offset, Second):
+        freq = freq.replace("S", "s")
+    elif isinstance(freq_as_offset, Millisecond):
+        freq = freq.replace("L", "ms")
+    elif isinstance(freq_as_offset, Microsecond):
+        freq = freq.replace("U", "us")
+
     return freq
 
 
