34357ef30198a7ab4ef0aad6afb199506e48abaf
Mark Harfouche <mark.harfouche@gmail.com>
2024-02-12 21:16:57 -0500

    Don't break users that were already using ChunkManagerEntrypoint
    For example, you just broke cubed
    
    https://github.com/xarray-contrib/cubed-xarray/blob/main/cubed_xarray/cubedmanager.py#L15
    
    Not sure how much you care, it didn't seem like anybody other than me
    ever tried this module on github...

---
+++ xarray/core/parallelcompat.py
@@ -0,0 +1,6 @@
+from xarray.namedarray.parallelcompat import ChunkManagerEntrypoint  # noqa
+import warnings
+
+warnings.warn("xarray.core.parallelcompat.ChunkManagerEntrypoint is deprecated. "
+              "Use xarray.namedarray.parallel.ChunkManagerEntrypoint instead.",
+               DeprecationWarning, stacklevel=2)
