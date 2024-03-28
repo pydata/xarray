9cceadc77891c1bfd1625dc23a46704396798529
Ilan Gold <ilanbassgold@gmail.com>
2024-03-28 17:02:15 +0100

    Update xarray/core/extension_array.py
    Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>

--- xarray/core/extension_array.py
+++ xarray/core/extension_array.py
@@ -124,7 +124,7 @@ class PandasExtensionArray(Generic[T_ExtensionArray]):
 
     def __eq__(self, other):
         if np.isscalar(other):
-            other = type(self)(pd.Categorical([other]))
+            other = type(self)(type(self.array)([other]))
         if isinstance(other, PandasExtensionArray):
             return self.array == other.array
         return self.array == other
