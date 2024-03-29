d7e79bb5f3d7fa4545d018f5d9a2aa3f6585612b
Illviljan <14371165+Illviljan@users.noreply.github.com>
2024-03-29 12:16:06 +0100

    Update test_plot.py

--- xarray/tests/test_plot.py
+++ xarray/tests/test_plot.py
@@ -6,7 +6,7 @@ import math
 from collections.abc import Hashable
 from copy import copy
 from datetime import datetime
-from typing import Any, Callable, Literal
+from typing import Any, Callable, Literal, Generator
 
 import numpy as np
 import pandas as pd
@@ -150,7 +150,7 @@ def get_colorbar_label(colorbar) -> str:
 @requires_matplotlib
 class PlotTestCase:
     @pytest.fixture(autouse=True)
-    def setup(self) -> None:
+    def setup(self) -> Generator:
         yield
         # Remove all matplotlib figures
         plt.close("all")
