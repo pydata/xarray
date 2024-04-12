3cb20ceda7e473b875552b088779795bc41cf009
Justus Magin <keewis@posteo.de>
2024-04-12 20:52:01 +0200

    use the proper dtype kind

--- xarray/core/duck_array_ops.py
+++ xarray/core/duck_array_ops.py
@@ -528,7 +528,7 @@ def datetime_to_numeric(array, offset=None, datetime_unit=None, dtype=float):
     # This map_blocks call is for backwards compatibility.
     # dask == 2021.04.1 does not support subtracting object arrays
     # which is required for cftime
-    if is_duck_dask_array(array) and dtypes.isdtype(array.dtype, object):
+    if is_duck_dask_array(array) and dtypes.isdtype(array.dtype, "object"):
         array = array.map_blocks(lambda a, b: a - b, offset, meta=array._meta)
     else:
         array = array - offset
