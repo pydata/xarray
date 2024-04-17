84d23be58bb39be4eb896f5f0dbe0a8f956431fb
Matt Savoie <matthew.savoie@colorado.edu>
2024-04-17 09:57:53 -0600

    DAS-2108: stop pruning datatree_ directory
    Quick fixup of some typing.
    Removes mypy exclusions for datatree_

--- xarray/datatree_/datatree/tests/test_extensions.py
+++ xarray/datatree_/datatree/tests/test_extensions.py
@@ -18,16 +18,15 @@ class TestAccessor:
                 return "bar"
 
         dt: DataTree = DataTree()
-        assert dt.demo.foo == "bar"  # type: ignore
+        assert dt.demo.foo == "bar"
 
         # accessor is cached
-        assert dt.demo is dt.demo  # type: ignore
+        assert dt.demo is dt.demo
 
         # check descriptor
-        assert dt.demo.__doc__ == "Demo accessor."  # type: ignore
-        # TODO: typing doesn't seem to work with accessors
-        assert DataTree.demo.__doc__ == "Demo accessor."  # type: ignore
-        assert isinstance(dt.demo, DemoAccessor)  # type: ignore
+        assert dt.demo.__doc__ == "Demo accessor."
+        assert DataTree.demo.__doc__ == "Demo accessor." # type: ignore
+        assert isinstance(dt.demo, DemoAccessor)
         assert DataTree.demo is DemoAccessor  # type: ignore
 
         with pytest.warns(Warning, match="overriding a preexisting attribute"):
