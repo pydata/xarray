709f338752dbb75c9e5ed0c4702bdb6b028065eb
Deepak Cherian <dcherian@users.noreply.github.com>
2024-04-30 14:58:20 -0600

    Update xarray/core/dataset.py

--- xarray/core/dataset.py
+++ xarray/core/dataset.py
@@ -8922,7 +8922,7 @@ class Dataset(
         lhs = np.vander(x, order)
 
         if rcond is None:
-            rcond = x.shape[0] * np.finfo(x.dtype).eps  # type: ignore[attr-defined]
+            rcond = x.shape[0] * np.finfo(x.dtype).eps
 
         # Weights:
         if w is not None:
