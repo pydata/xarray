6c34e5f027a37a04fcf6366813c4eab70646fd78
Spencer Clark <spencerkclark@gmail.com>
2024-05-02 21:00:53 -0400

    xfail test_use_cftime_false_standard_calendar_in_range

--- xarray/tests/__init__.py
+++ xarray/tests/__init__.py
@@ -142,6 +142,7 @@ requires_numbagg_or_bottleneck = pytest.mark.skipif(
     not has_scipy_or_netCDF4, reason="requires scipy or netCDF4"
 )
 has_numpy_array_api, requires_numpy_array_api = _importorskip("numpy", "1.26.0")
+has_numpy_2, requires_numpy_2 = _importorskip("numpy", "2.0.0")
 
 
 def _importorskip_h5netcdf_ros3():
