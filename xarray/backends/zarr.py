6a759c03799cca636e21d20b7e1d0bb8b27646d1
aladinor <aladinor@unal.edu.co>
2024-05-29 14:10:49 -0500

    using the default generator instead of loading zarr groups in memory

--- xarray/backends/zarr.py
+++ xarray/backends/zarr.py
@@ -446,9 +446,7 @@ class ZarrStore(AbstractWritableDataStore):
             stacklevel=stacklevel,
             zarr_version=zarr_version,
         )
-        group_paths = [
-            str(group / node[1:]) for node in list(_iter_zarr_groups(zarr_group))
-        ]
+        group_paths = [str(group / node[1:]) for node in _iter_zarr_groups(zarr_group)]
         return {
             group: cls(
                 zarr_group.get(group),
