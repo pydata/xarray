bef3019f0c21c09bcaba1aa00eb3f55e7c1919a4
Deepak Cherian <deepak@cherian.net>
2024-06-24 22:47:42 -0400

    Better test

--- properties/test_properties.py
+++ properties/test_properties.py
@@ -7,7 +7,7 @@ import xarray.testing.strategies as xrst
 @given(attrs=xrst.attrs())
 def test_assert_identical(attrs):
     v = xr.Variable(dims=(), data=0, attrs=attrs)
-    xr.testing.assert_identical(v, v)
+    xr.testing.assert_identical(v, v.copy(deep=True))
 
     ds = xr.Dataset(attrs=attrs)
-    xr.testing.assert_identical(ds, ds)
+    xr.testing.assert_identical(ds, ds.copy(deep=True))
