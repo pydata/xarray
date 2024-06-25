4195e4af51e9baf96a54c4a92176b52e9defe83f
Deepak Cherian <deepak@cherian.net>
2024-06-24 22:40:52 -0400

    Add property test

---
+++ properties/test_properties.py
@@ -0,0 +1,13 @@
+from hypothesis import given
+
+import xarray as xr
+import xarray.testing.strategies as xrst
+
+
+@given(attrs=xrst.attrs())
+def test_assert_identical(attrs):
+    v = xr.Variable(dims=(), data=0, attrs=attrs)
+    xr.testing.assert_identical(v, v)
+
+    ds = xr.Dataset(attrs=attrs)
+    xr.testing.assert_identical(ds, ds)
