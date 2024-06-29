91b54e6947ffa3a61bd8a4c4e4ecfed5c62a0eca
Illviljan <14371165+Illviljan@users.noreply.github.com>
2024-06-29 23:14:53 +0200

    Add ASV benchmark

---
+++ asv_bench/benchmarks/datatree.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+import xarray as xr
+from xarray.core.datatree import DataTree
+
+from . import parameterized
+
+
+class Datatree:
+    def setup(self):
+        dat1 = xr.Dataset({"a": 1})
+        run1 = DataTree.from_dict({"run1": dat1})
+        self.d = {"run1": run1}
+
+    @parameterized(["copy"], [(True, False)])
+    def time_from_dict(self, copy: bool):
+        DataTree.from_dict(self.d, copy=copy)
