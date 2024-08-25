d4544c05bedc5c1ffc60562dd6d7c3b669cee87e
Paul Ockenfuss <42680748+Ockenfuss@users.noreply.github.com>
2024-08-25 17:46:44 +0200

    Fix accidental double pasting of arguments

--- xarray/core/missing.py
+++ xarray/core/missing.py
@@ -664,11 +664,6 @@ class GapMask(Generic[T_Xarray]):
                 use_coordinate=use_coordinate,
                 keep_attrs=keep_attrs,
                 **kwargs,
-                dim=dim,
-                method=method,
-                use_coordinate=use_coordinate,
-                keep_attrs=keep_attrs,
-                **kwargs,
             )
         )
 
