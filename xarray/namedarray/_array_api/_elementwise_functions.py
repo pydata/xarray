5622bd7e3aeedb40ead2962229fdcf234cf9ae58
Illviljan <14371165+Illviljan@users.noreply.github.com>
2024-08-25 18:44:06 +0200

    Update _elementwise_functions.py

--- xarray/namedarray/_array_api/_elementwise_functions.py
+++ xarray/namedarray/_array_api/_elementwise_functions.py
@@ -9,112 +9,117 @@ from xarray.namedarray._typing import (
     _SupportsImag,
     _SupportsReal,
 )
-from xarray.namedarray.core import (
-    NamedArray,
-)
+from xarray.namedarray.core import NamedArray
 
 
-def abs(x, /):
+def _atleast_0d(x, xp):
+    """
+    Workaround for numpy sometimes returning scalars instead of 0d arrays.
+    """
+    return xp.asarray(x)
+
+
+def abs(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.abs(x._data))
-    return out
+    _data = _atleast_0d(xp.abs(x._data), xp)
+    return x._new(data=_data)
 
 
-def acos(x, /):
+def acos(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.acos(x._data))
-    return out
+    _data = _atleast_0d(xp.acos(x._data), xp)
+    return x._new(data=_data)
 
 
-def acosh(x, /):
+def acosh(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.acosh(x._data))
-    return out
+    _data = _atleast_0d(xp.acosh(x._data), xp)
+    return x._new(data=_data)
 
 
-def add(x1, x2, /):
+def add(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.add(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.add(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def asin(x, /):
+def asin(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.asin(x._data))
-    return out
+    _data = _atleast_0d(xp.asin(x._data), xp)
+    return x._new(data=_data)
 
 
-def asinh(x, /):
+def asinh(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.asinh(x._data))
-    return out
+    _data = _atleast_0d(xp.asinh(x._data), xp)
+    return x._new(data=_data)
 
 
-def atan(x, /):
+def atan(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.atan(x._data))
-    return out
+    _data = _atleast_0d(xp.atan(x._data), xp)
+    return x._new(data=_data)
 
 
-def atan2(x1, x2, /):
+def atan2(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.atan2(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.atan2(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def atanh(x, /):
+def atanh(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.atanh(x._data))
-    return out
+    _data = _atleast_0d(xp.atanh(x._data), xp)
+    return x._new(data=_data)
 
 
-def bitwise_and(x1, x2, /):
+def bitwise_and(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.bitwise_and(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.bitwise_and(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def bitwise_invert(x, /):
+def bitwise_invert(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.bitwise_invert(x._data))
-    return out
+    _data = _atleast_0d(xp.bitwise_invert(x._data), xp)
+    return x._new(data=_data)
 
 
-def bitwise_left_shift(x1, x2, /):
+def bitwise_left_shift(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.bitwise_left_shift(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.bitwise_left_shift(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def bitwise_or(x1, x2, /):
+def bitwise_or(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.bitwise_or(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.bitwise_or(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def bitwise_right_shift(x1, x2, /):
+def bitwise_right_shift(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.bitwise_right_shift(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.bitwise_right_shift(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def bitwise_xor(x1, x2, /):
+def bitwise_xor(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.bitwise_xor(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.bitwise_xor(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def ceil(x, /):
+def ceil(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.ceil(x._data))
-    return out
+    _data = _atleast_0d(xp.ceil(x._data), xp)
+    return x._new(data=_data)
 
 
 def clip(
@@ -124,93 +129,93 @@ def clip(
     max: int | float | NamedArray | None = None,
 ) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.clip(x._data))
-    return out
+    _data = _atleast_0d(xp.clip(x._data, min=min, max=max), xp)
+    return x._new(data=_data)
 
 
-def conj(x, /):
+def conj(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.conj(x._data))
-    return out
+    _data = _atleast_0d(xp.conj(x._data), xp)
+    return x._new(data=_data)
 
 
 def copysign(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.copysign(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.copysign(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def cos(x, /):
+def cos(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.cos(x._data))
-    return out
+    _data = _atleast_0d(xp.cos(x._data), xp)
+    return x._new(data=_data)
 
 
-def cosh(x, /):
+def cosh(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.cosh(x._data))
-    return out
+    _data = _atleast_0d(xp.cosh(x._data), xp)
+    return x._new(data=_data)
 
 
-def divide(x1, x2, /):
+def divide(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.divide(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.divide(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def exp(x, /):
+def exp(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.exp(x._data))
-    return out
+    _data = _atleast_0d(xp.exp(x._data), xp)
+    return x._new(data=_data)
 
 
-def expm1(x, /):
+def expm1(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.expm1(x._data))
-    return out
+    _data = _atleast_0d(xp.expm1(x._data), xp)
+    return x._new(data=_data)
 
 
-def equal(x1, x2, /):
+def equal(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.equal(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.equal(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def floor(x, /):
+def floor(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.floor(x._data))
-    return out
+    _data = _atleast_0d(xp.floor(x._data), xp)
+    return x._new(data=_data)
 
 
-def floor_divide(x1, x2, /):
+def floor_divide(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.floor_divide(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.floor_divide(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def greater(x1, x2, /):
+def greater(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.greater(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.greater(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def greater_equal(x1, x2, /):
+def greater_equal(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.greater_equal(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.greater_equal(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
 def hypot(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.hypot(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.hypot(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
 def imag(
@@ -241,145 +246,145 @@ def imag(
     array([2., 4.])
     """
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.imag(x._data))
-    return out
+    _data = _atleast_0d(xp.imag(x._data), xp)
+    return x._new(data=_data)
 
 
-def isfinite(x, /):
+def isfinite(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.isfinite(x._data))
-    return out
+    _data = _atleast_0d(xp.isfinite(x._data), xp)
+    return x._new(data=_data)
 
 
-def isinf(x, /):
+def isinf(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.isinf(x._data))
-    return out
+    _data = _atleast_0d(xp.isinf(x._data), xp)
+    return x._new(data=_data)
 
 
-def isnan(x, /):
+def isnan(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.isnan(x._data))
-    return out
+    _data = _atleast_0d(xp.isnan(x._data), xp)
+    return x._new(data=_data)
 
 
-def less(x1, x2, /):
+def less(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.less(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.less(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def less_equal(x1, x2, /):
+def less_equal(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.less_equal(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.less_equal(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def log(x, /):
+def log(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.log(x._data))
-    return out
+    _data = _atleast_0d(xp.log(x._data), xp)
+    return x._new(data=_data)
 
 
-def log1p(x, /):
+def log1p(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.log1p(x._data))
-    return out
+    _data = _atleast_0d(xp.log1p(x._data), xp)
+    return x._new(data=_data)
 
 
-def log2(x, /):
+def log2(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.log2(x._data))
-    return out
+    _data = _atleast_0d(xp.log2(x._data), xp)
+    return x._new(data=_data)
 
 
-def log10(x, /):
+def log10(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.log10(x._data))
-    return out
+    _data = _atleast_0d(xp.log10(x._data), xp)
+    return x._new(data=_data)
 
 
-def logaddexp(x1, x2, /):
+def logaddexp(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.logaddexp(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.logaddexp(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def logical_and(x1, x2, /):
+def logical_and(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.logical_and(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.logical_and(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def logical_not(x, /):
+def logical_not(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.logical_not(x._data))
-    return out
+    _data = _atleast_0d(xp.logical_not(x._data), xp)
+    return x._new(data=_data)
 
 
-def logical_or(x1, x2, /):
+def logical_or(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.logical_or(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.logical_or(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def logical_xor(x1, x2, /):
+def logical_xor(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.logical_xor(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.logical_xor(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
 def maximum(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.maximum(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.maximum(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
 def minimum(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.minimum(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.minimum(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def multiply(x1, x2, /):
+def multiply(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.multiply(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.multiply(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def negative(x, /):
+def negative(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.negative(x._data))
-    return out
+    _data = _atleast_0d(xp.negative(x._data), xp)
+    return x._new(data=_data)
 
 
-def not_equal(x1, x2, /):
+def not_equal(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.not_equal(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.not_equal(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def positive(x, /):
+def positive(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.positive(x._data))
-    return out
+    _data = _atleast_0d(xp.positive(x._data), xp)
+    return x._new(data=_data)
 
 
-def pow(x1, x2, /):
+def pow(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.pow(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.pow(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
 def real(
@@ -410,79 +415,79 @@ def real(
     array([1., 2.])
     """
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.real(x._data))
-    return out
+    _data = _atleast_0d(xp.real(x._data), xp)
+    return x._new(data=_data)
 
 
-def remainder(x1, x2, /):
+def remainder(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.remainder(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.remainder(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def round(x, /):
+def round(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.round(x._data))
-    return out
+    _data = _atleast_0d(xp.round(x._data), xp)
+    return x._new(data=_data)
 
 
-def sign(x, /):
+def sign(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.sign(x._data))
-    return out
+    _data = _atleast_0d(xp.sign(x._data), xp)
+    return x._new(data=_data)
 
 
-def signbit(x, /):
+def signbit(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.signbit(x._data))
-    return out
+    _data = _atleast_0d(xp.signbit(x._data), xp)
+    return x._new(data=_data)
 
 
-def sin(x, /):
+def sin(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.sin(x._data))
-    return out
+    _data = _atleast_0d(xp.sin(x._data), xp)
+    return x._new(data=_data)
 
 
-def sinh(x, /):
+def sinh(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.sinh(x._data))
-    return out
+    _data = _atleast_0d(xp.sinh(x._data), xp)
+    return x._new(data=_data)
 
 
-def sqrt(x, /):
+def sqrt(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.sqrt(x._data))
-    return out
+    _data = _atleast_0d(xp.sqrt(x._data), xp)
+    return x._new(data=_data)
 
 
-def square(x, /):
+def square(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.square(x._data))
-    return out
+    _data = _atleast_0d(xp.square(x._data), xp)
+    return x._new(data=_data)
 
 
-def subtract(x1, x2, /):
+def subtract(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x1)
     # TODO: Handle attrs? will get x1 now
-    out = x1._new(data=xp.subtract(x1._data, x2._data))
-    return out
+    _data = _atleast_0d(xp.subtract(x1._data, x2._data), xp)
+    return x1._new(data=_data)
 
 
-def tan(x, /):
+def tan(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.tan(x._data))
-    return out
+    _data = _atleast_0d(xp.tan(x._data), xp)
+    return x._new(data=_data)
 
 
-def tanh(x, /):
+def tanh(x: NamedArray, /) -> NamedArray:
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.tanh(x._data))
-    return out
+    _data = _atleast_0d(xp.tanh(x._data), xp)
+    return x._new(data=_data)
 
 
 def trunc(x, /):
     xp = _get_data_namespace(x)
-    out = x._new(data=xp.trunc(x._data))
-    return out
+    _data = _atleast_0d(xp.trunc(x._data), xp)
+    return x._new(data=_data)
