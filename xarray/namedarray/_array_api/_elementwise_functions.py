aab7b22e86a8b97e9bc6423d5c39a7c54616af36
Illviljan <14371165+Illviljan@users.noreply.github.com>
2024-09-02 21:44:24 +0200

    Update _elementwise_functions.py

--- xarray/namedarray/_array_api/_elementwise_functions.py
+++ xarray/namedarray/_array_api/_elementwise_functions.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Any
 
 import numpy as np
 
@@ -8,6 +9,7 @@ from xarray.namedarray._array_api._utils import (
 )
 from xarray.namedarray._typing import (
     _ScalarType,
+    _DType,
     _ShapeType,
     _SupportsImag,
     _SupportsReal,
@@ -15,112 +17,124 @@ from xarray.namedarray._typing import (
 from xarray.namedarray.core import NamedArray
 
 
-def abs(x: NamedArray, /) -> NamedArray:
+def abs(x: NamedArray[_ShapeType, _DType], /) -> NamedArray[_ShapeType, _DType]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.abs(x._data)
     return NamedArray(_dims, _data)
 
 
-def acos(x: NamedArray, /) -> NamedArray:
+def acos(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.acos(x._data)
     return NamedArray(_dims, _data)
 
 
-def acosh(x: NamedArray, /) -> NamedArray:
+def acosh(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.acosh(x._data)
     return NamedArray(_dims, _data)
 
 
-def add(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def add(x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.add(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def asin(x: NamedArray, /) -> NamedArray:
+def asin(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.asin(x._data)
     return NamedArray(_dims, _data)
 
 
-def asinh(x: NamedArray, /) -> NamedArray:
+def asinh(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.asinh(x._data)
     return NamedArray(_dims, _data)
 
 
-def atan(x: NamedArray, /) -> NamedArray:
+def atan(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.atan(x._data)
     return NamedArray(_dims, _data)
 
 
-def atan2(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def atan2(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.atan2(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def atanh(x: NamedArray, /) -> NamedArray:
+def atanh(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.atanh(x._data)
     return NamedArray(_dims, _data)
 
 
-def bitwise_and(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def bitwise_and(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.bitwise_and(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def bitwise_invert(x: NamedArray, /) -> NamedArray:
+def bitwise_invert(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.bitwise_invert(x._data)
     return NamedArray(_dims, _data)
 
 
-def bitwise_left_shift(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def bitwise_left_shift(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.bitwise_left_shift(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def bitwise_or(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def bitwise_or(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.bitwise_or(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def bitwise_right_shift(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def bitwise_right_shift(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.bitwise_right_shift(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def bitwise_xor(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def bitwise_xor(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.bitwise_xor(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def ceil(x: NamedArray, /) -> NamedArray:
+def ceil(x: NamedArray[_ShapeType, _DType], /) -> NamedArray[_ShapeType, _DType]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.ceil(x._data)
@@ -128,102 +142,116 @@ def ceil(x: NamedArray, /) -> NamedArray:
 
 
 def clip(
-    x: NamedArray,
+    x: NamedArray[Any, Any],
     /,
-    min: int | float | NamedArray | None = None,
-    max: int | float | NamedArray | None = None,
-) -> NamedArray:
+    min: int | float | NamedArray[Any, Any] | None = None,
+    max: int | float | NamedArray[Any, Any] | None = None,
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.clip(x._data, min=min, max=max)
     return NamedArray(_dims, _data)
 
 
-def conj(x: NamedArray, /) -> NamedArray:
+def conj(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.conj(x._data)
     return NamedArray(_dims, _data)
 
 
-def copysign(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def copysign(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.copysign(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def cos(x: NamedArray, /) -> NamedArray:
+def cos(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.cos(x._data)
     return NamedArray(_dims, _data)
 
 
-def cosh(x: NamedArray, /) -> NamedArray:
+def cosh(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.cosh(x._data)
     return NamedArray(_dims, _data)
 
 
-def divide(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def divide(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.divide(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def exp(x: NamedArray, /) -> NamedArray:
+def exp(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.exp(x._data)
     return NamedArray(_dims, _data)
 
 
-def expm1(x: NamedArray, /) -> NamedArray:
+def expm1(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.expm1(x._data)
     return NamedArray(_dims, _data)
 
 
-def equal(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def equal(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.equal(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def floor(x: NamedArray, /) -> NamedArray:
+def floor(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.floor(x._data)
     return NamedArray(_dims, _data)
 
 
-def floor_divide(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def floor_divide(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.floor_divide(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def greater(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def greater(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.greater(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def greater_equal(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def greater_equal(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.greater_equal(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def hypot(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def hypot(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.hypot(x1._data, x2._data)
@@ -263,147 +291,165 @@ def imag(
     return NamedArray(_dims, _data)
 
 
-def isfinite(x: NamedArray, /) -> NamedArray:
+def isfinite(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.isfinite(x._data)
     return NamedArray(_dims, _data)
 
 
-def isinf(x: NamedArray, /) -> NamedArray:
+def isinf(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.isinf(x._data)
     return NamedArray(_dims, _data)
 
 
-def isnan(x: NamedArray, /) -> NamedArray:
+def isnan(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.isnan(x._data)
     return NamedArray(_dims, _data)
 
 
-def less(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def less(x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.less(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def less_equal(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def less_equal(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.less_equal(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def log(x: NamedArray, /) -> NamedArray:
+def log(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.log(x._data)
     return NamedArray(_dims, _data)
 
 
-def log1p(x: NamedArray, /) -> NamedArray:
+def log1p(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.log1p(x._data)
     return NamedArray(_dims, _data)
 
 
-def log2(x: NamedArray, /) -> NamedArray:
+def log2(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.log2(x._data)
     return NamedArray(_dims, _data)
 
 
-def log10(x: NamedArray, /) -> NamedArray:
+def log10(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.log10(x._data)
     return NamedArray(_dims, _data)
 
 
-def logaddexp(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def logaddexp(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.logaddexp(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def logical_and(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def logical_and(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.logical_and(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def logical_not(x: NamedArray, /) -> NamedArray:
+def logical_not(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.logical_not(x._data)
     return NamedArray(_dims, _data)
 
 
-def logical_or(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def logical_or(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.logical_or(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def logical_xor(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def logical_xor(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.logical_xor(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def maximum(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def maximum(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.maximum(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def minimum(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def minimum(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.minimum(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def multiply(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def multiply(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.multiply(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def negative(x: NamedArray, /) -> NamedArray:
+def negative(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.negative(x._data)
     return NamedArray(_dims, _data)
 
 
-def not_equal(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def not_equal(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.not_equal(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def positive(x: NamedArray, /) -> NamedArray:
+def positive(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.positive(x._data)
     return NamedArray(_dims, _data)
 
 
-def pow(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def pow(x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.pow(x1._data, x2._data)
@@ -443,84 +489,88 @@ def real(
     return NamedArray(_dims, _data)
 
 
-def remainder(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def remainder(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.remainder(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def round(x: NamedArray, /) -> NamedArray:
+def round(x: NamedArray[_ShapeType, _DType], /) -> NamedArray[_ShapeType, _DType]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.round(x._data)
     return NamedArray(_dims, _data)
 
 
-def sign(x: NamedArray, /) -> NamedArray:
+def sign(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.sign(x._data)
     return NamedArray(_dims, _data)
 
 
-def signbit(x: NamedArray, /) -> NamedArray:
+def signbit(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.signbit(x._data)
     return NamedArray(_dims, _data)
 
 
-def sin(x: NamedArray, /) -> NamedArray:
+def sin(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.sin(x._data)
     return NamedArray(_dims, _data)
 
 
-def sinh(x: NamedArray, /) -> NamedArray:
+def sinh(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.sinh(x._data)
     return NamedArray(_dims, _data)
 
 
-def sqrt(x: NamedArray, /) -> NamedArray:
+def sqrt(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.sqrt(x._data)
     return NamedArray(_dims, _data)
 
 
-def square(x: NamedArray, /) -> NamedArray:
+def square(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.square(x._data)
     return NamedArray(_dims, _data)
 
 
-def subtract(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def subtract(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _dims, _ = _get_broadcasted_dims(x1, x2)
     _data = xp.subtract(x1._data, x2._data)
     return NamedArray(_dims, _data)
 
 
-def tan(x: NamedArray, /) -> NamedArray:
+def tan(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.tan(x._data)
     return NamedArray(_dims, _data)
 
 
-def tanh(x: NamedArray, /) -> NamedArray:
+def tanh(x: NamedArray[_ShapeType, Any], /) -> NamedArray[_ShapeType, Any]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.tanh(x._data)
     return NamedArray(_dims, _data)
 
 
-def trunc(x, /):
+def trunc(x: NamedArray[_ShapeType, _DType], /) -> NamedArray[_ShapeType, _DType]:
     xp = _get_data_namespace(x)
     _dims = x.dims
     _data = xp.trunc(x._data)
