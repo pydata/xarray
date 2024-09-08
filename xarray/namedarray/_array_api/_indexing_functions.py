0328e449474e044ea01db11462d6c400f0acbe2f
Illviljan <14371165+Illviljan@users.noreply.github.com>
2024-09-08 21:00:12 +0200

    Update _indexing_functions.py

--- xarray/namedarray/_array_api/_indexing_functions.py
+++ xarray/namedarray/_array_api/_indexing_functions.py
@@ -1,22 +1,25 @@
 from __future__ import annotations
 
+from typing import Any
+
 from xarray.namedarray._array_api._utils import _dims_to_axis, _get_data_namespace
 from xarray.namedarray._typing import (
     Default,
     _default,
     _Dim,
+    _DType,
 )
 from xarray.namedarray.core import NamedArray
 
 
 def take(
-    x: NamedArray,
-    indices: NamedArray,
+    x: NamedArray[Any, _DType],
+    indices: NamedArray[Any, Any],
     /,
     *,
     dim: _Dim | Default = _default,
     axis: int | None = None,
-) -> NamedArray:
+) -> NamedArray[Any, _DType]:
     xp = _get_data_namespace(x)
     _axis = _dims_to_axis(x, dim, axis)[0]
     # TODO: Handle attrs? will get x1 now
