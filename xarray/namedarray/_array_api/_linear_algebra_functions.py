937724df6036f661aef55ca14c678e9b49bcd20f
Illviljan <14371165+Illviljan@users.noreply.github.com>
2024-09-08 20:34:42 +0200

    Update _linear_algebra_functions.py

--- xarray/namedarray/_array_api/_linear_algebra_functions.py
+++ xarray/namedarray/_array_api/_linear_algebra_functions.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 from collections.abc import Sequence
+from typing import Any
 
 from xarray.namedarray._array_api._utils import _get_data_namespace, _infer_dims
 from xarray.namedarray.core import NamedArray
 
 
-def matmul(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
+def matmul(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _data = xp.matmul(x1._data, x2._data)
     # TODO: Figure out a better way:
@@ -15,12 +18,12 @@ def matmul(x1: NamedArray, x2: NamedArray, /) -> NamedArray:
 
 
 def tensordot(
-    x1: NamedArray,
-    x2: NamedArray,
+    x1: NamedArray[Any, Any],
+    x2: NamedArray[Any, Any],
     /,
     *,
     axes: int | tuple[Sequence[int], Sequence[int]] = 2,
-) -> NamedArray:
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _data = xp.tensordot(x1._data, x2._data, axes=axes)
     # TODO: Figure out a better way:
@@ -28,7 +31,7 @@ def tensordot(
     return NamedArray(_dims, _data)
 
 
-def matrix_transpose(x: NamedArray, /) -> NamedArray:
+def matrix_transpose(x: NamedArray[Any, Any], /) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x)
     _data = xp.matrix_transpose(x._data)
     # TODO: Figure out a better way:
@@ -36,7 +39,9 @@ def matrix_transpose(x: NamedArray, /) -> NamedArray:
     return NamedArray(_dims, _data)
 
 
-def vecdot(x1: NamedArray, x2: NamedArray, /, *, axis: int = -1) -> NamedArray:
+def vecdot(
+    x1: NamedArray[Any, Any], x2: NamedArray[Any, Any], /, *, axis: int = -1
+) -> NamedArray[Any, Any]:
     xp = _get_data_namespace(x1)
     _data = xp.vecdot(x1._data, x2._data, axis=axis)
     # TODO: Figure out a better way:
