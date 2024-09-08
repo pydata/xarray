686ef2be2abb4632a85090082102d3c8a32c9a1a
Illviljan <14371165+Illviljan@users.noreply.github.com>
2024-09-08 20:34:36 +0200

    Update _data_type_functions.py

--- xarray/namedarray/_array_api/_data_type_functions.py
+++ xarray/namedarray/_array_api/_data_type_functions.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import Any, cast
 
 from xarray.namedarray._array_api._utils import (
     _get_data_namespace,
@@ -67,40 +67,42 @@ def astype(
     return x._new(data=x._data.astype(dtype, copy=copy))  # type: ignore[attr-defined]
 
 
-def can_cast(from_: _dtype | NamedArray, to: _dtype, /) -> bool:
+def can_cast(from_: _dtype[Any] | NamedArray[Any, Any], to: _dtype[Any], /) -> bool:
     if isinstance(from_, NamedArray):
         xp = _get_data_namespace(from_)
         from_ = from_.dtype
-        return xp.can_cast(from_, to)
+        return cast(bool, xp.can_cast(from_, to))  # TODO: Why is cast necessary?
     else:
         xp = _get_namespace_dtype(from_)
-        return xp.can_cast(from_, to)
+        return cast(bool, xp.can_cast(from_, to))  # TODO: Why is cast necessary?
 
 
-def finfo(type: _dtype | NamedArray[Any, Any], /) -> _FInfo:
+def finfo(type: _dtype[Any] | NamedArray[Any, Any], /) -> _FInfo:
     if isinstance(type, NamedArray):
         xp = _get_data_namespace(type)
-        return xp.finfo(type._data)
+        return cast(_FInfo, xp.finfo(type._data))  # TODO: Why is cast necessary?
     else:
         xp = _get_namespace_dtype(type)
-        return xp.finfo(type)
+        return cast(_FInfo, xp.finfo(type))  # TODO: Why is cast necessary?
 
 
-def iinfo(type: _dtype | NamedArray[Any, Any], /) -> _IInfo:
+def iinfo(type: _dtype[Any] | NamedArray[Any, Any], /) -> _IInfo:
     if isinstance(type, NamedArray):
         xp = _get_data_namespace(type)
-        return xp.iinfo(type._data)
+        return cast(_IInfo, xp.iinfo(type._data))  # TODO: Why is cast necessary?
     else:
         xp = _get_namespace_dtype(type)
-        return xp.iinfo(type)
+        return cast(_IInfo, xp.iinfo(type))  # TODO: Why is cast necessary?
 
 
-def isdtype(dtype: _dtype, kind: _dtype | str | tuple[_dtype | str, ...]) -> bool:
+def isdtype(
+    dtype: _dtype[Any], kind: _dtype[Any] | str | tuple[_dtype[Any] | str, ...]
+) -> bool:
     xp = _get_namespace_dtype(dtype)
-    return xp.isdtype(dtype, kind)
+    return cast(bool, xp.isdtype(dtype, kind))  # TODO: Why is cast necessary?
 
 
-def result_type(*arrays_and_dtypes: NamedArray[Any, Any] | _dtype) -> _dtype:
+def result_type(*arrays_and_dtypes: NamedArray[Any, Any] | _dtype[Any]) -> _dtype[Any]:
     # TODO: Empty arg?
     arr_or_dtype = arrays_and_dtypes[0]
     if isinstance(arr_or_dtype, NamedArray):
@@ -108,6 +110,9 @@ def result_type(*arrays_and_dtypes: NamedArray[Any, Any] | _dtype) -> _dtype:
     else:
         xp = _get_namespace_dtype(arr_or_dtype)
 
-    return xp.result_type(
-        *(a.dtype if isinstance(a, NamedArray) else a for a in arrays_and_dtypes)
-    )
+    return cast(
+        _dtype[Any],
+        xp.result_type(
+            *(a.dtype if isinstance(a, NamedArray) else a for a in arrays_and_dtypes)
+        ),
+    )  # TODO: Why is cast necessary?
