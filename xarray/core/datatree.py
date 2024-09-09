45a0027b2ddfef8c8b19ca518360838f4d9480c1
Tom Nicholas <tom@cworthy.org>
2024-09-09 09:31:23 -0600

    Implement `DataTree.__delitem__` (#9453)
    * test
    
    * improve error message
    
    * implement delitem
    
    * test KeyError
    
    * Remove special KeyError message
    
    Co-authored-by: Stephan Hoyer <shoyer@google.com>
    
    * remove another special KeyError message
    
    Co-authored-by: Stephan Hoyer <shoyer@google.com>
    
    * fix pytest raises expected error message
    
    ---------
    
    Co-authored-by: Stephan Hoyer <shoyer@google.com>

--- xarray/core/datatree.py
+++ xarray/core/datatree.py
@@ -910,6 +910,24 @@ class DataTree(
         else:
             raise ValueError("Invalid format for key")
 
+    def __delitem__(self, key: str) -> None:
+        """Remove a variable or child node from this datatree node."""
+        if key in self.children:
+            super().__delitem__(key)
+
+        elif key in self._node_coord_variables:
+            if key in self._node_indexes:
+                del self._node_indexes[key]
+            del self._node_coord_variables[key]
+            self._node_dims = calculate_dimensions(self.variables)
+
+        elif key in self._data_variables:
+            del self._data_variables[key]
+            self._node_dims = calculate_dimensions(self.variables)
+
+        else:
+            raise KeyError(key)
+
     @overload
     def update(self, other: Dataset) -> None: ...
 
