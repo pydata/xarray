199a5242df274b929b5edb32f7ee90f2d11c566f
Tom Nicholas <tom@cworthy.org>
2024-09-09 09:31:23 -0600

    Implement `DataTree.__delitem__` (#9453)
    * test
    
    * improve error message
    
    * implement delitem
    
    * test KeyError
    
    * Remove special KeyError message
    
    Co-authored-by: Stephan Hoyer <shoyer@google.com>
    
    * remove another special KeyError message
    
    Co-authored-by: Stephan Hoyer <shoyer@google.com>
    
    * fix pytest raises expected error message
    
    ---------
    
    Co-authored-by: Stephan Hoyer <shoyer@google.com>

--- xarray/core/treenode.py
+++ xarray/core/treenode.py
@@ -559,14 +559,14 @@ class TreeNode(Generic[Tree]):
         else:
             current_node._set(name, item)
 
-    def __delitem__(self: Tree, key: str):
+    def __delitem__(self: Tree, key: str) -> None:
         """Remove a child node from this tree object."""
         if key in self.children:
             child = self._children[key]
             del self._children[key]
             child.orphan()
         else:
-            raise KeyError("Cannot delete")
+            raise KeyError(key)
 
     def same_tree(self, other: Tree) -> bool:
         """True if other node is in the same tree as this node."""
