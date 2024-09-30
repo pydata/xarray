20c22bd4d601f1443503c45a4c83b422e86be2f0
Tom Augspurger <tom.w.augspurger@gmail.com>
2024-09-30 09:51:49 -0500

    fixup

--- xarray/tests/test_backends.py
+++ xarray/tests/test_backends.py
@@ -2273,6 +2273,7 @@ class ZarrBase(CFEncodedBase):
                     assert_identical(ds, expected)
 
     def test_non_existent_store(self) -> None:
+        exc: type[Exception]
         if have_zarr_v3:
             exc = ValueError
             msg = "store mode"
