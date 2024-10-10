0c2e260f7f7f2d9055047d967135e35cd1a6ca69
Tom Augspurger <tom.w.augspurger@gmail.com>
2024-10-10 15:09:48 -0500

    point upstream to v3 branch

--- ci/install-upstream-wheels.sh
+++ ci/install-upstream-wheels.sh
@@ -53,7 +53,7 @@ python -m pip install \
     git+https://github.com/dask/dask \
     git+https://github.com/dask/dask-expr \
     git+https://github.com/dask/distributed \
-    git+https://github.com/zarr-developers/zarr.git@main \
+    git+https://github.com/zarr-developers/zarr.git@v3 \
     git+https://github.com/Unidata/cftime \
     git+https://github.com/pypa/packaging \
     git+https://github.com/hgrecco/pint \
