14108e27ed3187c9c51467bd7420cf9c6373a81e
Illviljan <14371165+Illviljan@users.noreply.github.com>
2024-10-17 20:34:38 +0200

    ones_like

--- xarray/namedarray/_array_api/_creation_functions.py
+++ xarray/namedarray/_array_api/_creation_functions.py
@@ -506,6 +506,17 @@ def ones_like(
     dtype: _DType | None = None,
     device: _Device | None = None,
 ) -> NamedArray[_ShapeType, _DType]:
+    """
+    Returns a new array filled with ones and having the same shape as an input array x.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> ones_like(NamedArray(("x", "y"), np.array([[1, 2, 3], [4, 5, 6]])))
+    <xarray.NamedArray (x: 2, y: 3)> Size: 48B
+    array([[1, 1, 1],
+           [1, 1, 1]])
+    """
     xp = _get_data_namespace(x)
     _data = xp.ones_like(x._data, dtype=dtype, device=device)
     return x._new(data=_data)
