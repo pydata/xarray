437b3e0ed32af28826d6a81b1a24c01b690f750c
Illviljan <14371165+Illviljan@users.noreply.github.com>
2024-10-21 22:18:50 +0200

    all

--- xarray/namedarray/_array_api/_utility_functions.py
+++ xarray/namedarray/_array_api/_utility_functions.py
@@ -3,14 +3,17 @@ from __future__ import annotations
 from typing import Any
 
 from xarray.namedarray._array_api._utils import (
+    _dim_to_axis,
     _dims_to_axis,
     _get_data_namespace,
     _reduce_dims,
 )
 from xarray.namedarray._typing import (
     Default,
+    _Axis,
     _AxisLike,
     _default,
+    _Dim,
     _DimsLike2,
 )
 from xarray.namedarray.core import NamedArray
@@ -24,6 +27,29 @@ def all(
     keepdims: bool = False,
     axis: _AxisLike | None = None,
 ) -> NamedArray[Any, Any]:
+    """
+    Tests whether all input array elements evaluate to True along a specified axis.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> x = NamedArray(("x", "y"), np.array([[True, False],[True, True]]))
+    >>> all(x)
+    <xarray.NamedArray ()> Size: 1B
+    np.False_
+
+    >>> all(x, axis=0)
+    <xarray.NamedArray (y: 2)> Size: 2B
+    array([ True, False])
+    >>> all(x, dims="x")
+    <xarray.NamedArray (y: 2)> Size: 2B
+    array([ True, False])
+
+    >>> x = NamedArray(("x",), np.array([-1, 4, 5]))
+    >>> all(x)
+    <xarray.NamedArray ()> Size: 1B
+    np.True_
+    """
     xp = _get_data_namespace(x)
     _axis = _dims_to_axis(x, dims, axis)
     _data = xp.all(x._data, axis=_axis, keepdims=keepdims)
