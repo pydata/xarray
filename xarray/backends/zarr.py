fc3d4c3280f7190719f8e0baf703b783715bcb3b
Ryan Abernathey <ryan.abernathey@gmail.com>
2024-10-24 12:49:32 -0400

    Update xarray/backends/zarr.py
    Co-authored-by: Stephan Hoyer <shoyer@google.com>

--- xarray/backends/zarr.py
+++ xarray/backends/zarr.py
@@ -311,13 +311,15 @@ def _get_zarr_dims_and_attrs(zarr_obj, dimension_key, try_nczarr):
     # Zarr V3 explicitly stores the dimension names in the metadata
     try:
         # if this exists, we are looking at a Zarr V3 array
-        # convert None to empty tuple
-        dimensions = zarr_obj.metadata.dimension_names or ()
-        attributes = dict(zarr_obj.attrs)
-        return dimensions, attributes
+        metadata = zarr_obj.metadata
     except AttributeError:
         # continue to old code path
         pass
+    else:
+        # convert None to empty tuple
+        dimensions = metadata.dimension_names or ()
+        attributes = dict(zarr_obj.attrs)
+        return dimensions, attributes
 
     # Zarr arrays do not have dimensions. To get around this problem, we add
     # an attribute that specifies the dimension. We have to hide this attribute
