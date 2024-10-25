ad3246e10ac3ff5d71c6852f3b4ee4db4684ab6d
Sam Levang <slevang@salientpredictions.com>
2024-10-25 11:32:22 -0400

    add to api.rst

--- xarray/tests/test_datatree.py
+++ xarray/tests/test_datatree.py
@@ -2321,6 +2321,9 @@ class TestDask:
         assert_identical(actual, expected)
 
         assert actual.chunksizes == original_chunksizes, "chunksizes were modified"
+        assert (
+            tree.chunksizes == original_chunksizes
+        ), "original chunksizes were modified"
         assert all(
             d == 1 for d in actual_hlg_depths.values()
         ), "unexpected dask graph depth"
