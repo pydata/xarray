773cf863c090d421989a975599dfe5bf1daff489
Deepak Cherian <dcherian@users.noreply.github.com>
2024-11-18 13:30:15 -0700

    Bump minimum versions (#9796)
    * Bump minimum versions
    
    * tweak
    
    * Update doc/whats-new.rst
    
    Co-authored-by: Justus Magin <keewis@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Justus Magin <keewis@users.noreply.github.com>

--- xarray/core/nputils.py
+++ xarray/core/nputils.py
@@ -181,7 +181,6 @@ def _create_method(name, npmodule=np) -> Callable:
 
         if (
             module_available("numbagg")
-            and pycompat.mod_version("numbagg") >= Version("0.5.0")
             and OPTIONS["use_numbagg"]
             and isinstance(values, np.ndarray)
             # numbagg<0.7.0 uses ddof=1 only, but numpy uses ddof=0 by default
