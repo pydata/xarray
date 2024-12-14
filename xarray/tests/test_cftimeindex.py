566c08a1c8de5d786247b13bc5a449cb0c997427
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2024-12-14 16:02:19 +0100

    add test for microsecond

--- xarray/tests/test_cftimeindex.py
+++ xarray/tests/test_cftimeindex.py
@@ -100,6 +100,30 @@ ISO8601_LIKE_STRING_TESTS = {
             year="1999", month="01", day="01", hour="12", minute="34", second="56"
         ),
     ),
+    "microsecond-1": (
+        "19990101T123456.123456",
+        date_dict(
+            year="1999",
+            month="01",
+            day="01",
+            hour="12",
+            minute="34",
+            second="56",
+            microsecond="123456",
+        ),
+    ),
+    "microsecond-2": (
+        "19990101T123456.1",
+        date_dict(
+            year="1999",
+            month="01",
+            day="01",
+            hour="12",
+            minute="34",
+            second="56",
+            microsecond="1",
+        ),
+    ),
 }
 
 
@@ -112,8 +136,9 @@ def test_parse_iso8601_like(string, expected):
     result = parse_iso8601_like(string)
     assert result == expected
 
-    with pytest.raises(ValueError):
-        parse_iso8601_like(string + "3")
+    if result["microsecond"] is None:
+        with pytest.raises(ValueError):
+            parse_iso8601_like(string + "3")
     if result["second"] is None:
         with pytest.raises(ValueError):
             parse_iso8601_like(string + ".3")
@@ -317,6 +342,7 @@ def test_cftimeindex_days_in_month_accessor(index):
         ("19990202T01", (1999, 2, 2, 1), "hour"),
         ("19990202T0101", (1999, 2, 2, 1, 1), "minute"),
         ("19990202T010156", (1999, 2, 2, 1, 1, 56), "second"),
+        ("19990202T010156.123456", (1999, 2, 2, 1, 1, 56, 123456), "microsecond"),
     ],
 )
 def test_parse_iso8601_with_reso(date_type, string, date_args, reso):
