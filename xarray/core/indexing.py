7a63ed99b324515cabeea73fbbaf6f226fad1522
Tom White <tom.e.white@gmail.com>
2025-01-17 10:49:44 +0000

    Revert unnecessary replacement of `np.all` with duck array ops

--- xarray/core/indexing.py
+++ xarray/core/indexing.py
@@ -1271,9 +1271,7 @@ def _decompose_outer_indexer(
                 np_indexer.append(np_slice)
             elif isinstance(k, integer_types):
                 backend_indexer.append(k)
-            elif isinstance(k, np.ndarray) and duck_array_ops.array_all(
-                np.diff(k) >= 0
-            ):
+            elif isinstance(k, np.ndarray) and (np.diff(k) >= 0).all():
                 backend_indexer.append(k)
                 np_indexer.append(slice(None))
             else:
