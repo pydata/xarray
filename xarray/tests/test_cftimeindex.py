34b17e4743ec6799fe97ed08acfd557de39b4d45
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2025-01-29 09:25:30 +0100

    update infer_freq test

--- xarray/tests/test_cftimeindex.py
+++ xarray/tests/test_cftimeindex.py
@@ -1377,16 +1377,16 @@ def test_asi8_empty_cftimeindex():
 
 
 @requires_cftime
-def test_infer_freq_valid_types():
+def test_infer_freq_valid_types(time_unit):
     cf_indx = xr.cftime_range("2000-01-01", periods=3, freq="D")
     assert xr.infer_freq(cf_indx) == "D"
     assert xr.infer_freq(xr.DataArray(cf_indx)) == "D"
 
-    pd_indx = pd.date_range("2000-01-01", periods=3, freq="D")
+    pd_indx = pd.date_range("2000-01-01", periods=3, freq="D").as_unit(time_unit)
     assert xr.infer_freq(pd_indx) == "D"
     assert xr.infer_freq(xr.DataArray(pd_indx)) == "D"
 
-    pd_td_indx = pd.timedelta_range(start="1D", periods=3, freq="D")
+    pd_td_indx = pd.timedelta_range(start="1D", periods=3, freq="D").as_unit(time_unit)
     assert xr.infer_freq(pd_td_indx) == "D"
     assert xr.infer_freq(xr.DataArray(pd_td_indx)) == "D"
 
