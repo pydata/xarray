64e80bed668b93b52d881f697fca96e5423d2053
Mathias Hauser <mathias.hauser@env.ethz.ch>
2025-01-31 17:14:33 +0100

    newline for result

--- xarray/core/datatree_mapping.py
+++ xarray/core/datatree_mapping.py
@@ -161,7 +161,7 @@ def _check_single_set_return_values(path_to_node: str, obj: Any) -> int | None:
     ):
         raise TypeError(
             f"the result of calling func on the node at position '{path_to_node}' is"
-            f" not a Dataset or None or a tuple of such types: {obj!r}"
+            f" not a Dataset or None or a tuple of such types:\n{obj!r}"
         )
 
     return len(obj)
