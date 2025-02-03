403afa9f7fd1d63e7eac6c3202b9b42b16b45945
aladinor <aladinor@unal.edu.co>
2025-02-03 13:47:53 -0600

    fixing issue with dict creation

--- xarray/backends/zarr.py
+++ xarray/backends/zarr.py
@@ -656,10 +656,7 @@ class ZarrStore(AbstractWritableDataStore):
             zarr_format=zarr_format,
         )
 
-        group_members = {
-            path: store_group
-            for path, store_group in list(_iter_zarr_groups(zarr_group, parent=group))
-        }
+        group_members = dict(list(_iter_zarr_groups(zarr_group, parent=group)))
 
         return {
             group: cls(
