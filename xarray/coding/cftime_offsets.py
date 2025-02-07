7fbff45440e26f5fc414e9e58417f19e845bf327
maddogghoek <maddogghoek@penguin>
2025-02-07 15:01:37 -0700

    - Update cftime_range doctest to ignore deprecation warning.
    Issue #9886

--- xarray/coding/cftime_offsets.py
+++ xarray/coding/cftime_offsets.py
@@ -1108,12 +1108,10 @@ def cftime_range(
     This function returns a ``CFTimeIndex``, populated with ``cftime.datetime``
     objects associated with the specified calendar type, e.g.
 
-    >>> xr.cftime_range(
-    ...     start="2000", periods=6, freq="2MS", calendar="noleap"
-    ... )  # +doctest: ELLIPSIS
-    Traceback (most recent call last):
-      ...
-    DeprecationWarning: cftime_range() is deprecated, please use date_range(use_cftime=True) instead.
+    >>> with warnings.catch_warnings(DeprecationWarning):
+    ...     warnings.simplefilter("ignore")
+    ...     xr.cftime_range(start="2000", periods=6, freq="2MS", calendar="noleap")
+    ...
     CFTimeIndex([2000-01-01 00:00:00, 2000-03-01 00:00:00, 2000-05-01 00:00:00,
                  2000-07-01 00:00:00, 2000-09-01 00:00:00, 2000-11-01 00:00:00],
                 dtype='object', length=6, calendar='noleap', freq='2MS')
