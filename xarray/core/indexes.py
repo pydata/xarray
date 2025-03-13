528f3565d653b31f3bfa36160b3d204a616db58b
Benoit Bovy <benbovy@gmail.com>
2025-03-13 14:24:41 +0100

    cache Indexes.get_all_dims

--- xarray/core/indexes.py
+++ xarray/core/indexes.py
@@ -1568,6 +1568,7 @@ class Indexes(collections.abc.Mapping, Generic[T_PandasOrXarrayIndex]):
     """
 
     _index_type: type[Index] | type[pd.Index]
+    _index_dims: dict[Hashable, Mapping[Hashable, int]]
     _indexes: dict[Any, T_PandasOrXarrayIndex]
     _variables: dict[Any, Variable]
 
@@ -1576,6 +1577,7 @@ class Indexes(collections.abc.Mapping, Generic[T_PandasOrXarrayIndex]):
         "__id_coord_names",
         "__id_index",
         "_dims",
+        "_index_dims",
         "_index_type",
         "_indexes",
         "_variables",
@@ -1619,6 +1621,7 @@ class Indexes(collections.abc.Mapping, Generic[T_PandasOrXarrayIndex]):
             )
 
         self._index_type = index_type
+        self._index_dims = {}
         self._indexes = dict(**indexes)
         self._variables = dict(**variables)
 
@@ -1737,7 +1740,13 @@ class Indexes(collections.abc.Mapping, Generic[T_PandasOrXarrayIndex]):
         """
         from xarray.core.variable import calculate_dimensions
 
-        return calculate_dimensions(self.get_all_coords(key, errors=errors))
+        if key in self._index_dims:
+            return self._index_dims[key]
+        else:
+            dims = calculate_dimensions(self.get_all_coords(key, errors=errors))
+            if dims:
+                self._index_dims[key] = dims
+            return dims
 
     def group_by_index(
         self,
