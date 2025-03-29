ef466110bb84861c8f4efa9d21335cce419eaa69
Andrecho <37243793+andrewendlinger@users.noreply.github.com>
2025-03-29 13:34:22 +0000

    Add test to check units appear in FacetGrid plot
    - appended test to `TestFacetGrid` class inside test_plot.py
    - checks that units are added to the plot axis labelling

--- xarray/tests/test_plot.py
+++ xarray/tests/test_plot.py
@@ -2429,6 +2429,25 @@ class TestFacetGrid(PlotTestCase):
             col="z", subplot_kws=dict(projection="polar"), sharex=False, sharey=False
         )
 
+    @pytest.mark.slow
+    def test_units_appear_somewhere(self) -> None:
+
+        # assign coordinates to all dims so we can test for units
+        darray = self.darray.assign_coords({"x": np.arange(self.darray.x.size), "y": np.arange(self.darray.y.size)})
+
+        darray.x.attrs["units"] = "x_unit"
+        darray.y.attrs["units"] = "y_unit"
+
+        g = xplt.FacetGrid(darray, col="z")
+
+        g.map_dataarray(xplt.contourf, "x", "y")
+
+        alltxt = text_in_fig()
+
+        # unit should appear as e.g. 'x [x_unit]'
+        for unit_name in ["x_unit", "y_unit"]:
+            assert unit_name in "".join(alltxt)
+
 
 @pytest.mark.filterwarnings("ignore:tight_layout cannot")
 class TestFacetGrid4d(PlotTestCase):
