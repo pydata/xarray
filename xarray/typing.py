0567d73c0da177915e4bf41b2ddeee35c7cb3ecf
micguerr-bopen <m.guerreri@bopen.eu>
2025-04-10 18:13:41 +0200

    Removed TYPPE_CHECKING. Also removed DataArray, Dataset

--- xarray/typing.py
+++ xarray/typing.py
@@ -2,26 +2,16 @@
 Public typing utilities for use by external libraries.
 """
 
-from typing import (
-    TYPE_CHECKING,
+from xarray.computation.rolling import (
+    DataArrayCoarsen,
+    DataArrayRolling,
+    DatasetRolling,
 )
-
-
-if TYPE_CHECKING:
-    from xarray.core.dataarray import DataArray
-    from xarray.core.dataset import Dataset
-    from xarray.computation.rolling import (
-        DataArrayCoarsen,
-        DataArrayRolling,
-        DatasetRolling,
-    )
-    from xarray.computation.weighted import Weighted, DataArrayWeighted, DatasetWeighted
-    from xarray.core.groupby import DataArrayGroupBy
-    from xarray.core.resample import DataArrayResample
+from xarray.computation.weighted import Weighted, DataArrayWeighted, DatasetWeighted
+from xarray.core.groupby import DataArrayGroupBy
+from xarray.core.resample import DataArrayResample
 
 __all__ = [
-    "DataArray",
-    "Dataset",
     "DataArrayCoarsen",
     "DataArrayRolling",
     "DatasetRolling",
