3e30dc8a266508caf5fbc123b3553ed02d4aa28c
Illviljan <14371165+Illviljan@users.noreply.github.com>
2025-05-15 20:53:28 +0200

    Update _indexing_functions.py

--- xarray/namedarray/_array_api/_indexing_functions.py
+++ xarray/namedarray/_array_api/_indexing_functions.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 from typing import Any
 
-from xarray.namedarray._array_api._utils import _dims_to_axis, _get_data_namespace
+from xarray.namedarray._array_api._utils import (
+    _dim_to_axis,
+    _dim_to_optional_axis,
+    _get_data_namespace,
+)
 from xarray.namedarray._typing import (
     Default,
     _default,
@@ -21,7 +25,7 @@ def take(
     axis: int | None = None,
 ) -> NamedArray[Any, _DType]:
     xp = _get_data_namespace(x)
-    _axis = _dims_to_axis(x, dim, axis)[0]
+    _axis = _dim_to_optional_axis(x, dim, axis)
     _data = xp.take(x._data, indices._data, axis=_axis)
     _dims = x.dims
     return NamedArray(_dims, _data)
@@ -36,7 +40,7 @@ def take_along_axis(
     axis: int = -1,
 ) -> NamedArray[Any, _DType]:
     xp = _get_data_namespace(x)
-    _axis = _dims_to_axis(x, dim, axis, axis_default=-1)[0]
+    _axis = _dim_to_axis(x, dim, axis, axis_default=-1)
     _data = xp.take_along_axis(x._data, indices._data, axis=_axis)
     _dims = x.dims
     return NamedArray(_dims, _data)
