46f04ef8c960eeb8ba3d3a62dc4e37f0d275902d
Dimitri Papadopoulos Orfanos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2025-06-30 23:08:11 +0200

    More f-strings, less format() (#10474)

--- xarray/computation/rolling.py
+++ xarray/computation/rolling.py
@@ -131,13 +131,11 @@ class Rolling(Generic[T_Xarray]):
     def __repr__(self) -> str:
         """provide a nice str repr of our rolling object"""
 
-        attrs = [
+        attrs = ",".join(
             "{k}->{v}{c}".format(k=k, v=w, c="(center)" if c else "")
             for k, w, c in zip(self.dim, self.window, self.center, strict=True)
-        ]
-        return "{klass} [{attrs}]".format(
-            klass=self.__class__.__name__, attrs=",".join(attrs)
         )
+        return f"{self.__class__.__name__} [{attrs}]"
 
     def __len__(self) -> int:
         return math.prod(self.obj.sizes[d] for d in self.dim)
@@ -1106,14 +1104,12 @@ class Coarsen(CoarsenArithmetic, Generic[T_Xarray]):
     def __repr__(self) -> str:
         """provide a nice str repr of our coarsen object"""
 
-        attrs = [
+        attrs = ",".join(
             f"{k}->{getattr(self, k)}"
             for k in self._attributes
             if getattr(self, k, None) is not None
-        ]
-        return "{klass} [{attrs}]".format(
-            klass=self.__class__.__name__, attrs=",".join(attrs)
         )
+        return f"{self.__class__.__name__} [{attrs}]"
 
     def construct(
         self,
