pydata/xarray

commit 16b53ac0452f17ab61889aec4d3d27ce22cded98

drop support for `python=3.9` (#8937)
* bump python in the min versions environments

* bump python in ci

* package metadata

* docs

* typo

(This paragraph says it should be revisited after the bump)

* tell `ruff` the target version is 3.10

* apply `pre-commit` hooks

* automatically fix `UP038`

* remove obsolete version checks

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Ignore what's left of UP007

* Force numpy>=2

---------

Co-authored-by: Deepak Cherian <dcherian@users.noreply.github.com>
Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Deepak Cherian <deepak@cherian.net>

xarray/namedarray/_typing.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
 import sys
-from collections.abc import Hashable, Iterable, Mapping, Sequence
+from collections.abc import Callable, Hashable, Iterable, Mapping, Sequence
 from enum import Enum
 from types import ModuleType
 from typing import (
     TYPE_CHECKING,
     Any,
-    Callable,
     Final,
     Literal,
     Protocol,
@@ -24,7 +23,7 @@ try:
     if sys.version_info >= (3, 11):
         from typing import TypeAlias
     else:
-        from typing_extensions import TypeAlias
+        from typing import TypeAlias
 except ImportError:
     if TYPE_CHECKING:
         raise
@@ -79,9 +78,9 @@ _AxisLike = Union[_Axis, _Axes]
 _Chunks = tuple[_Shape, ...]
 _NormalizedChunks = tuple[tuple[int, ...], ...]
 # FYI in some cases we don't allow `None`, which this doesn't take account of.
-T_ChunkDim: TypeAlias = Union[int, Literal["auto"], None, tuple[int, ...]]
+T_ChunkDim: TypeAlias = int | Literal["auto"] | None | tuple[int, ...]
 # We allow the tuple form of this (though arguably we could transition to named dims only)
-T_Chunks: TypeAlias = Union[T_ChunkDim, Mapping[Any, T_ChunkDim]]
+T_Chunks: TypeAlias = T_ChunkDim | Mapping[Any, T_ChunkDim]
 
 _Dim = Hashable
 _Dims = tuple[_Dim, ...]
@@ -93,7 +92,7 @@ _DimsLike = Union[str, Iterable[_Dim]]
 # https://github.com/numpy/numpy/pull/25022
 # https://github.com/data-apis/array-api/pull/674
 _IndexKey = Union[int, slice, "ellipsis"]
-_IndexKeys = tuple[Union[_IndexKey], ...]  #  tuple[Union[_IndexKey, None], ...]
+_IndexKeys = tuple[_IndexKey, ...]  #  tuple[Union[_IndexKey, None], ...]
 _IndexKeyLike = Union[_IndexKey, _IndexKeys]
 
 _AttrsLike = Union[Mapping[Any, Any], None]

commit 5ff25e237f0fbf3e4c83eb5484c1de6bd8a3f249

Branches of the `if` statement have similar implementation
Issue introduced by 16b53ac / #8937.

It looks like the `sys.version_info >= (3, 11)` test is incorrect, as
`typing.TypeAlias`  was added in version 3.10, not 3.11:
	https://docs.python.org/3/library/typing.html#typing.TypeAlias

xarray/namedarray/_typing.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import sys
 from collections.abc import Callable, Hashable, Iterable, Mapping, Sequence
 from enum import Enum
 from types import EllipsisType, ModuleType
@@ -20,10 +19,7 @@ from typing import (
 import numpy as np
 
 try:
-    if sys.version_info >= (3, 11):
-        from typing import TypeAlias
-    else:
-        from typing import TypeAlias
+    from typing import TypeAlias
 except ImportError:
     if TYPE_CHECKING:
         raise