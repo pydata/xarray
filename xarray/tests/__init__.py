05c8aa60f2b64e98f33fdb361d97bf16a507e062
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2025-07-09 17:07:50 -0400

    update: zarr dtypes test

--- xarray/tests/__init__.py
+++ xarray/tests/__init__.py
@@ -130,7 +130,7 @@ has_bottleneck, requires_bottleneck = _importorskip("bottleneck")
 has_rasterio, requires_rasterio = _importorskip("rasterio")
 has_zarr, requires_zarr = _importorskip("zarr")
 has_zarr_v3, requires_zarr_v3 = _importorskip("zarr", "3.0.0")
-has_zarr_v3_dtypes, requires_zarr_v3_dtypes = _importorskip("zarr", "3.0.10")
+has_zarr_v3_dtypes, requires_zarr_v3_dtypes = _importorskip("zarr", "3.1.0")
 if has_zarr_v3:
     import zarr
 
@@ -141,7 +141,7 @@ if has_zarr_v3:
     has_zarr_v3_dtypes = hasattr(zarr.core, "dtype")
 
     requires_zarr_v3_dtypes = pytest.mark.skipif(
-        not has_zarr_v3_dtypes, reason="requires zarr>3.0.10 (including dev versions)"
+        not has_zarr_v3_dtypes, reason="requires zarr>3.1.0"
     )
 
 has_fsspec, requires_fsspec = _importorskip("fsspec")
