1ddb65372068645a8778c08da9f3b6fa09e76537
Illviljan <14371165+Illviljan@users.noreply.github.com>
2025-07-20 12:31:08 +0200

    Update _utils_inputs.py

--- xarray/namedarray/_array_api/_utils_inputs.py
+++ xarray/namedarray/_array_api/_utils_inputs.py
@@ -52,59 +52,59 @@ _py_scalars = (bool, int, float, complex)
 #     return x1, x2
 
 
-def _maybe_normalize_py_scalars(
-    x1: NamedArray[Any, Any] | bool | int | float | complex,
-    x2: NamedArray[Any, Any] | bool | int | float | complex,
-    dtype_category: str,
-    func_name: str,
-) -> tuple[NamedArray[Any, Any], NamedArray[Any, Any]]:
-    _allowed_dtypes = _dtype_categories[dtype_category]
+# def _maybe_normalize_py_scalars(
+#     x1: NamedArray[Any, Any] | bool | int | float | complex,
+#     x2: NamedArray[Any, Any] | bool | int | float | complex,
+#     dtype_category: str,
+#     func_name: str,
+# ) -> tuple[NamedArray[Any, Any], NamedArray[Any, Any]]:
+#     _allowed_dtypes = _dtype_categories[dtype_category]
 
-    if isinstance(x1, _py_scalars):
-        if isinstance(x2, _py_scalars):
-            raise TypeError(
-                f"Two scalars not allowed, got {type(x1) = } and {type(x2) =}"
-            )
-        # x2 must be an array
-        if x2.dtype not in _allowed_dtypes:
-            raise TypeError(
-                f"Only {dtype_category} dtypes are allowed {func_name}. Got {x2.dtype}."
-            )
-        x1 = _promote_scalar(x2, x1)
+#     if isinstance(x1, _py_scalars):
+#         if isinstance(x2, _py_scalars):
+#             raise TypeError(
+#                 f"Two scalars not allowed, got {type(x1) = } and {type(x2) =}"
+#             )
+#         # x2 must be an array
+#         if x2.dtype not in _allowed_dtypes:
+#             raise TypeError(
+#                 f"Only {dtype_category} dtypes are allowed {func_name}. Got {x2.dtype}."
+#             )
+#         x1 = _promote_scalar(x2, x1)
 
-    elif isinstance(x2, _py_scalars):
-        # x1 must be an array
-        if x1.dtype not in _allowed_dtypes:
-            raise TypeError(
-                f"Only {dtype_category} dtypes are allowed {func_name}. Got {x1.dtype}."
-            )
-        x2 = _promote_scalar(x1, x2)
-    elif x1.dtype not in _allowed_dtypes or x2.dtype not in _allowed_dtypes:
-        raise TypeError(
-            f"Only {dtype_category} dtypes are allowed in {func_name}(...). "
-            f"Got {x1.dtype} and {x2.dtype}."
-        )
-    return x1, x2
+#     elif isinstance(x2, _py_scalars):
+#         # x1 must be an array
+#         if x1.dtype not in _allowed_dtypes:
+#             raise TypeError(
+#                 f"Only {dtype_category} dtypes are allowed {func_name}. Got {x1.dtype}."
+#             )
+#         x2 = _promote_scalar(x1, x2)
+#     elif x1.dtype not in _allowed_dtypes or x2.dtype not in _allowed_dtypes:
+#         raise TypeError(
+#             f"Only {dtype_category} dtypes are allowed in {func_name}(...). "
+#             f"Got {x1.dtype} and {x2.dtype}."
+#         )
+#     return x1, x2
 
 
-def _maybe_asarray(
-    self, x: bool | int | float | complex | NamedArray[Any, Any]
-) -> NamedArray[Any, Any]:
-    """
-    If x is a scalar, use asarray with the same dtype as self.
-    If it is namedarray already, respect the dtype and return it.
+# def _maybe_asarray(
+#     self, x: bool | int | float | complex | NamedArray[Any, Any]
+# ) -> NamedArray[Any, Any]:
+#     """
+#     If x is a scalar, use asarray with the same dtype as self.
+#     If it is namedarray already, respect the dtype and return it.
 
-    Array API always promotes scalars to the same dtype as the other array.
-    Arrays are promoted according to result_types.
-    """
-    from xarray.namedarray._array_api import asarray
+#     Array API always promotes scalars to the same dtype as the other array.
+#     Arrays are promoted according to result_types.
+#     """
+#     from xarray.namedarray._array_api import asarray
 
-    if isinstance(x, NamedArray):
-        # x is proper array. Respect the chosen dtype.
-        return x
-    # x is a scalar. Use the same dtype as self.
-    # TODO: Is this a good idea? x[Any, int] + 1.4 => int result then.
-    return asarray(x, dtype=self.dtype)
+#     if isinstance(x, NamedArray):
+#         # x is proper array. Respect the chosen dtype.
+#         return x
+#     # x is a scalar. Use the same dtype as self.
+#     # TODO: Is this a good idea? x[Any, int] + 1.4 => int result then.
+#     return asarray(x, dtype=self.dtype)
 
 
 # def _split_args(
