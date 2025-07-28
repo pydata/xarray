938610862c5f0b0b9fe37427306ba09a964ccb5c
Justus Magin <keewis@posteo.de>
2025-07-28 22:08:15 +0200

    remove the old parametrized expected values

--- xarray/tests/test_indexing.py
+++ xarray/tests/test_indexing.py
@@ -306,17 +306,17 @@ class TestLazyArray:
                     assert_array_equal(expected, actual)
 
     @pytest.mark.parametrize(
-        ["old_slice", "array", "size", "expected"],
+        ["old_slice", "array", "size"],
         (
-            (slice(None, 8), np.arange(2, 6), 10, np.arange(2, 6)),
-            (slice(2, None), np.arange(2, 6), 10, np.arange(4, 8)),
-            (slice(1, 10, 2), np.arange(1, 4), 15, np.arange(3, 8, 2)),
-            (slice(10, None, -1), np.array([2, 5, 7]), 12, np.array([8, 5, 3])),
+            (slice(None, 8), np.arange(2, 6), 10),
+            (slice(2, None), np.arange(2, 6), 10),
+            (slice(1, 10, 2), np.arange(1, 4), 15),
+            (slice(10, None, -1), np.array([2, 5, 7]), 12),
         ),
     )
-    def test_slice_slice_by_array(self, old_slice, array, size, expected):
+    def test_slice_slice_by_array(self, old_slice, array, size):
         actual = indexing.slice_slice_by_array(old_slice, array, size)
-    expected = np.arange(size)[old_slice][array]
+        expected = np.arange(size)[old_slice][array]
         assert_array_equal(actual, expected)
 
     def test_lazily_indexed_array(self) -> None:
