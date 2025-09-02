pydata/xarray

commit 89c913a986f83cbfe39c2491be9afa964a005cf4

Support compute=False from DataTree.to_netcdf (#10625)
* Refactor to_netcdf() and to_zarr() internals

* Fixes per review

* Clean up comments

* Fix type for to_netcdf()

* Add test and whats-new for cross-group redundant computation

* Fix test failure on CI (and add a better test)

* grammar

xarray/core/datatree_io.py
@@ -1,10 +1,18 @@
 from __future__ import annotations
 
 import io
-from collections.abc import Mapping
+from collections.abc import Hashable, Mapping, MutableMapping
 from os import PathLike
 from typing import TYPE_CHECKING, Any, Literal, get_args
 
+from xarray.backends.api import (
+    delayed_close_after_writes,
+    dump_to_store,
+    get_writable_netcdf_store,
+    get_writable_zarr_store,
+)
+from xarray.backends.common import ArrayWriter
+from xarray.backends.locks import get_dask_scheduler
 from xarray.core.datatree import DataTree
 from xarray.core.types import NetcdfWriteModes, ZarrWriteModes
 
@@ -12,6 +20,9 @@ T_DataTreeNetcdfEngine = Literal["netcdf4", "h5netcdf", "pydap"]
 T_DataTreeNetcdfTypes = Literal["NETCDF4"]
 
 if TYPE_CHECKING:
+    from dask.delayed import Delayed
+
+    from xarray.backends import ZarrStore
     from xarray.core.types import ZarrStoreLike
 
 
@@ -26,8 +37,9 @@ def _datatree_to_netcdf(
     group: str | None = None,
     write_inherited_coords: bool = False,
     compute: bool = True,
-    **kwargs,
-) -> None | memoryview:
+    invalid_netcdf: bool = False,
+    auto_complex: bool | None = None,
+) -> None | memoryview | Delayed:
     """Implementation of `DataTree.to_netcdf`."""
 
     if format not in [None, *get_args(T_DataTreeNetcdfTypes)]:
@@ -45,9 +57,6 @@ def _datatree_to_netcdf(
         raise NotImplementedError(
             "specifying a root group for the tree has not been implemented"
         )
-
-    if not compute:
-        raise NotImplementedError("compute=False has not been implemented yet")
 
     if encoding is None:
         encoding = {}
@@ -70,26 +79,58 @@ def _datatree_to_netcdf(
     if unlimited_dims is None:
         unlimited_dims = {}
 
-    for node in dt.subtree:
-        at_root = node is dt
-        ds = node.to_dataset(inherit=write_inherited_coords or at_root)
-        group_path = None if at_root else "/" + node.relative_to(dt)
-        ds.to_netcdf(  # type: ignore[misc]  # Not all union combinations were tried because there are too many unions
-            target,
-            group=group_path,
-            mode=mode,
-            encoding=encoding.get(node.path),
-            unlimited_dims=unlimited_dims.get(node.path),
-            engine=engine,  # type: ignore[arg-type]
-            format=format,
-            compute=compute,
-            **kwargs,
-        )
-        mode = "a"
+    scheduler = get_dask_scheduler()
+    have_chunks = any(
+        v.chunks is not None for node in dt.subtree for v in node.variables.values()
+    )
+    autoclose = have_chunks and scheduler in ["distributed", "multiprocessing"]
+
+    root_store = get_writable_netcdf_store(
+        target,
+        engine,  # type: ignore[arg-type]
+        mode=mode,
+        format=format,
+        autoclose=autoclose,
+        invalid_netcdf=invalid_netcdf,
+        auto_complex=auto_complex,
+    )
+
+    writer = ArrayWriter()
+
+    # TODO: allow this work (setting up the file for writing array data)
+    # to be parallelized with dask
+    try:
+        for node in dt.subtree:
+            at_root = node is dt
+            dataset = node.to_dataset(inherit=write_inherited_coords or at_root)
+            node_store = (
+                root_store if at_root else root_store.get_child_store(node.path)
+            )
+            dump_to_store(
+                dataset,
+                node_store,
+                writer,
+                encoding=encoding.get(node.path),
+                unlimited_dims=unlimited_dims.get(node.path),
+            )
+
+        if autoclose:
+            root_store.close()
+
+        writes = writer.sync(compute=compute)
+
+    finally:
+        if compute:
+            root_store.close()
+        else:
+            root_store.sync()
 
     if filepath is None:
         assert isinstance(target, io.BytesIO)
         return target.getbuffer()
+
+    if not compute:
+        return delayed_close_after_writes(writes, root_store)
 
     return None
 
@@ -99,22 +140,31 @@ def _datatree_to_zarr(
     store: ZarrStoreLike,
     mode: ZarrWriteModes = "w-",
     encoding: Mapping[str, Any] | None = None,
-    consolidated: bool = True,
+    synchronizer=None,
     group: str | None = None,
     write_inherited_coords: bool = False,
+    *,
+    chunk_store: MutableMapping | str | PathLike | None = None,
     compute: bool = True,
-    **kwargs,
-):
+    consolidated: bool | None = None,
+    append_dim: Hashable | None = None,
+    region: Mapping[str, slice | Literal["auto"]] | Literal["auto"] | None = None,
+    safe_chunks: bool = True,
+    align_chunks: bool = False,
+    storage_options: dict[str, str] | None = None,
+    zarr_version: int | None = None,
+    zarr_format: int | None = None,
+    write_empty_chunks: bool | None = None,
+    chunkmanager_store_kwargs: dict[str, Any] | None = None,
+) -> ZarrStore | Delayed:
     """Implementation of `DataTree.to_zarr`."""
-
-    from zarr import consolidate_metadata
 
     if group is not None:
         raise NotImplementedError(
             "specifying a root group for the tree has not been implemented"
         )
 
-    if "append_dim" in kwargs:
+    if append_dim is not None:
         raise NotImplementedError(
             "specifying ``append_dim`` with ``DataTree.to_zarr`` has not been implemented"
         )
@@ -130,21 +180,50 @@ def _datatree_to_zarr(
             f"unexpected encoding group name(s) provided: {set(encoding) - set(dt.groups)}"
         )
 
-    for node in dt.subtree:
-        at_root = node is dt
-        ds = node.to_dataset(inherit=write_inherited_coords or at_root)
-        group_path = None if at_root else "/" + node.relative_to(dt)
-        ds.to_zarr(  # type: ignore[call-overload]
-            store,
-            group=group_path,
-            mode=mode,
-            encoding=encoding.get(node.path),
-            consolidated=False,
-            compute=compute,
-            **kwargs,
-        )
-        if "w" in mode:
-            mode = "a"
+    root_store = get_writable_zarr_store(
+        store,
+        chunk_store=chunk_store,
+        mode=mode,
+        synchronizer=synchronizer,
+        group=group,
+        consolidated=consolidated,
+        append_dim=append_dim,
+        region=region,
+        safe_chunks=safe_chunks,
+        align_chunks=align_chunks,
+        storage_options=storage_options,
+        zarr_version=zarr_version,
+        zarr_format=zarr_format,
+        write_empty_chunks=write_empty_chunks,
+    )
 
-    if consolidated:
-        consolidate_metadata(store)
+    writer = ArrayWriter()
+
+    try:
+        for node in dt.subtree:
+            at_root = node is dt
+            dataset = node.to_dataset(inherit=write_inherited_coords or at_root)
+            node_store = (
+                root_store if at_root else root_store.get_child_store(node.path)
+            )
+
+            dataset = node_store._validate_and_autodetect_region(dataset)
+            node_store._validate_encoding(encoding)
+
+            dump_to_store(
+                dataset,
+                node_store,
+                writer,
+                encoding=encoding.get(node.path),
+            )
+        writes = writer.sync(
+            compute=compute, chunkmanager_store_kwargs=chunkmanager_store_kwargs
+        )
+    finally:
+        if compute:
+            root_store.close()
+
+    if not compute:
+        return delayed_close_after_writes(writes, root_store)
+
+    return root_store

xarray/tests/test_backends_datatree.py
@@ -296,6 +296,20 @@ class DatatreeIOBase:
         assert list(root.dimensions) == ["x"]
         assert list(child.dimensions) == []
 
+    @requires_dask
+    def test_compute_false(self, tmpdir, simple_datatree):
+        filepath = tmpdir / "test.nc"
+        original_dt = simple_datatree.chunk()
+        result = original_dt.to_netcdf(filepath, engine=self.engine, compute=False)
+
+        with open_datatree(filepath, engine=self.engine) as in_progress_dt:
+            assert in_progress_dt.isomorphic(original_dt)
+            assert not in_progress_dt.equals(original_dt)
+
+        result.compute()
+        with open_datatree(filepath, engine=self.engine) as written_dt:
+            assert_identical(written_dt, original_dt)
+
 
 @requires_netCDF4
 class TestNetCDF4DatatreeIO(DatatreeIOBase):
@@ -606,6 +620,9 @@ class TestZarrDatatreeIO:
         with open_datatree(filepath, engine="zarr") as roundtrip_dt:
             assert_equal(original_dt, roundtrip_dt)
 
+    @pytest.mark.filterwarnings(
+        "ignore:Numcodecs codecs are not in the Zarr version 3 specification"
+    )
     def test_zarr_encoding(self, tmpdir, simple_datatree, zarr_format):
         filepath = str(tmpdir / "test.zarr")
         original_dt = simple_datatree
@@ -632,11 +649,10 @@ class TestZarrDatatreeIO:
 
             enc["/not/a/group"] = {"foo": "bar"}  # type: ignore[dict-item]
             with pytest.raises(ValueError, match="unexpected encoding group.*"):
-                original_dt.to_zarr(
-                    filepath, encoding=enc, engine="zarr", zarr_format=zarr_format
-                )
+                original_dt.to_zarr(filepath, encoding=enc, zarr_format=zarr_format)
 
     @pytest.mark.xfail(reason="upstream zarr read-only changes have broken this test")
+    @pytest.mark.filterwarnings("ignore:Duplicate name")
     def test_to_zarr_zip_store(self, tmpdir, simple_datatree, zarr_format):
         from zarr.storage import ZipStore
 
@@ -684,7 +700,9 @@ class TestZarrDatatreeIO:
 
         storepath = tmp_path / "test.zarr"
         original_dt = simple_datatree.chunk()
-        original_dt.to_zarr(str(storepath), compute=False, zarr_format=zarr_format)
+        result = original_dt.to_zarr(
+            str(storepath), compute=False, zarr_format=zarr_format
+        )
 
         def assert_expected_zarr_files_exist(
             arr_dir: Path,
@@ -754,6 +772,39 @@ class TestZarrDatatreeIO:
                     is_scalar=not bool(var.dims),
                     zarr_format=zarr_format,
                 )
+
+        with open_datatree(str(storepath), engine="zarr") as in_progress_dt:
+            assert in_progress_dt.isomorphic(original_dt)
+            assert not in_progress_dt.equals(original_dt)
+
+        result.compute()
+        with open_datatree(str(storepath), engine="zarr") as written_dt:
+            assert_identical(written_dt, original_dt)
+
+    @requires_dask
+    def test_to_zarr_no_redundant_computation(self, tmpdir, zarr_format):
+        import dask.array as da
+
+        eval_count = 0
+
+        def expensive_func(x):
+            nonlocal eval_count
+            eval_count += 1
+            return x + 1
+
+        base = da.random.random((), chunks=())
+        derived1 = da.map_blocks(expensive_func, base, meta=np.array((), np.float64))
+        derived2 = derived1 + 1  # depends on derived1
+        tree = DataTree.from_dict(
+            {
+                "group1": xr.Dataset({"derived": derived1}),
+                "group2": xr.Dataset({"derived": derived2}),
+            }
+        )
+
+        filepath = str(tmpdir / "test.zarr")
+        tree.to_zarr(filepath, zarr_format=zarr_format)
+        assert eval_count == 1  # not 2
 
     def test_to_zarr_inherited_coords(self, tmpdir, zarr_format):
         original_dt = DataTree.from_dict(

xarray/backends/api.py
@@ -31,13 +31,14 @@ from xarray import backends, conventions
 from xarray.backends import plugins
 from xarray.backends.common import (
     AbstractDataStore,
+    AbstractWritableDataStore,
     ArrayWriter,
     BytesIOProxy,
     T_PathFileOrDataStore,
     _find_absolute_paths,
     _normalize_path,
 )
-from xarray.backends.locks import _get_scheduler
+from xarray.backends.locks import get_dask_scheduler
 from xarray.coders import CFDatetimeCoder, CFTimedeltaCoder
 from xarray.core import dtypes, indexing
 from xarray.core.coordinates import Coordinates
@@ -310,10 +311,16 @@ def _protect_datatree_variables_inplace(tree: DataTree, cache: bool) -> None:
         _protect_dataset_variables_inplace(node.dataset, cache)
 
 
-def _finalize_store(write, store):
+def _finalize_store(writes, store):
     """Finalize this store by explicitly syncing and closing"""
-    del write  # ensure writing is done first
+    del writes  # ensure writing is done first
     store.close()
+
+
+def delayed_close_after_writes(writes, store):
+    import dask
+
+    return dask.delayed(_finalize_store)(writes, store)
 
 
 def _multi_file_closer(closers):
@@ -1858,6 +1865,39 @@ WRITEABLE_STORES: dict[T_NetcdfEngine, Callable] = {
 }
 
 
+def get_writable_netcdf_store(
+    target,
+    engine: T_NetcdfEngine,
+    *,
+    format: T_NetcdfTypes | None,
+    mode: NetcdfWriteModes,
+    autoclose: bool,
+    invalid_netcdf: bool,
+    auto_complex: bool | None,
+) -> AbstractWritableDataStore:
+    """Create a store for writing to a netCDF file."""
+    try:
+        store_open = WRITEABLE_STORES[engine]
+    except KeyError as err:
+        raise ValueError(f"unrecognized engine for to_netcdf: {engine!r}") from err
+
+    if format is not None:
+        format = format.upper()  # type: ignore[assignment]
+
+    kwargs = dict(autoclose=True) if autoclose else {}
+    if invalid_netcdf:
+        if engine == "h5netcdf":
+            kwargs["invalid_netcdf"] = invalid_netcdf
+        else:
+            raise ValueError(
+                f"unrecognized option 'invalid_netcdf' for engine {engine}"
+            )
+    if auto_complex is not None:
+        kwargs["auto_complex"] = auto_complex
+
+    return store_open(target, mode=mode, format=format, **kwargs)
+
+
 # multifile=True returns writer and datastore
 @overload
 def to_netcdf(
@@ -2043,16 +2083,8 @@ def to_netcdf(
     # sanitize unlimited_dims
     unlimited_dims = _sanitize_unlimited_dims(dataset, unlimited_dims)
 
-    try:
-        store_open = WRITEABLE_STORES[engine]
-    except KeyError as err:
-        raise ValueError(f"unrecognized engine for to_netcdf: {engine!r}") from err
-
-    if format is not None:
-        format = format.upper()  # type: ignore[assignment]
-
     # handle scheduler specific logic
-    scheduler = _get_scheduler()
+    scheduler = get_dask_scheduler()
     have_chunks = any(v.chunks is not None for v in dataset.variables.values())
 
     autoclose = have_chunks and scheduler in ["distributed", "multiprocessing"]
@@ -2067,18 +2099,17 @@ def to_netcdf(
     else:
         target = path_or_file  # type: ignore[assignment]
 
-    kwargs = dict(autoclose=True) if autoclose else {}
-    if invalid_netcdf:
-        if engine == "h5netcdf":
-            kwargs["invalid_netcdf"] = invalid_netcdf
-        else:
-            raise ValueError(
-                f"unrecognized option 'invalid_netcdf' for engine {engine}"
-            )
-    if auto_complex is not None:
-        kwargs["auto_complex"] = auto_complex
-
-    store = store_open(target, mode, format, group, **kwargs)
+    store = get_writable_netcdf_store(
+        target,
+        engine,
+        mode=mode,
+        format=format,
+        autoclose=autoclose,
+        invalid_netcdf=invalid_netcdf,
+        auto_complex=auto_complex,
+    )
+    if group is not None:
+        store = store.get_child_store(group)
 
     writer = ArrayWriter()
 
@@ -2099,17 +2130,18 @@ def to_netcdf(
         writes = writer.sync(compute=compute)
 
     finally:
-        if not multifile and compute:  # type: ignore[redundant-expr]
-            store.close()
+        if not multifile:
+            if compute:
+                store.close()
+            else:
+                store.sync()
 
     if path_or_file is None:
         assert isinstance(target, BytesIOProxy)  # created in this function
         return target.getvalue_or_getbuffer()
 
     if not compute:
-        import dask
-
-        return dask.delayed(_finalize_store)(writes, store)
+        return delayed_close_after_writes(writes, store)
 
     return None
 
@@ -2265,18 +2297,69 @@ def save_mfdataset(
     try:
         writes = [w.sync(compute=compute) for w in writers]
     finally:
-        if compute:
-            for store in stores:
+        for store in stores:
+            if compute:
                 store.close()
+            else:
+                store.sync()
 
     if not compute:
         import dask
 
         return dask.delayed(
-            list(
-                starmap(dask.delayed(_finalize_store), zip(writes, stores, strict=True))
-            )
+            list(starmap(delayed_close_after_writes, zip(writes, stores, strict=True)))
         )
+
+
+def get_writable_zarr_store(
+    store: ZarrStoreLike | None = None,
+    *,
+    chunk_store: MutableMapping | str | os.PathLike | None = None,
+    mode: ZarrWriteModes | None = None,
+    synchronizer=None,
+    group: str | None = None,
+    consolidated: bool | None = None,
+    append_dim: Hashable | None = None,
+    region: Mapping[str, slice | Literal["auto"]] | Literal["auto"] | None = None,
+    safe_chunks: bool = True,
+    align_chunks: bool = False,
+    storage_options: dict[str, str] | None = None,
+    zarr_version: int | None = None,
+    zarr_format: int | None = None,
+    write_empty_chunks: bool | None = None,
+) -> backends.ZarrStore:
+    """Create a store for writing to Zarr."""
+    from xarray.backends.zarr import _choose_default_mode, _get_mappers
+
+    kwargs, mapper, chunk_mapper = _get_mappers(
+        storage_options=storage_options, store=store, chunk_store=chunk_store
+    )
+    mode = _choose_default_mode(mode=mode, append_dim=append_dim, region=region)
+
+    if mode == "r+":
+        already_consolidated = consolidated
+        consolidate_on_close = False
+    else:
+        already_consolidated = False
+        consolidate_on_close = consolidated or consolidated is None
+
+    return backends.ZarrStore.open_group(
+        store=mapper,
+        mode=mode,
+        synchronizer=synchronizer,
+        group=group,
+        consolidated=already_consolidated,
+        consolidate_on_close=consolidate_on_close,
+        chunk_store=chunk_mapper,
+        append_dim=append_dim,
+        write_region=region,
+        safe_chunks=safe_chunks,
+        align_chunks=align_chunks,
+        zarr_version=zarr_version,
+        zarr_format=zarr_format,
+        write_empty=write_empty_chunks,
+        **kwargs,
+    )
 
 
 # compute=True returns ZarrStore
@@ -2353,7 +2436,6 @@ def to_zarr(
 
     See `Dataset.to_zarr` for full API docs.
     """
-    from xarray.backends.zarr import _choose_default_mode, _get_mappers
 
     # validate Dataset keys, DataArray names
     _validate_dataset_names(dataset)
@@ -2368,53 +2450,39 @@ def to_zarr(
     if encoding is None:
         encoding = {}
 
-    kwargs, mapper, chunk_mapper = _get_mappers(
-        storage_options=storage_options, store=store, chunk_store=chunk_store
-    )
-    mode = _choose_default_mode(mode=mode, append_dim=append_dim, region=region)
-
-    if mode == "r+":
-        already_consolidated = consolidated
-        consolidate_on_close = False
-    else:
-        already_consolidated = False
-        consolidate_on_close = consolidated or consolidated is None
-
-    zstore = backends.ZarrStore.open_group(
-        store=mapper,
+    zstore = get_writable_zarr_store(
+        store,
+        chunk_store=chunk_store,
         mode=mode,
         synchronizer=synchronizer,
         group=group,
-        consolidated=already_consolidated,
-        consolidate_on_close=consolidate_on_close,
-        chunk_store=chunk_mapper,
+        consolidated=consolidated,
         append_dim=append_dim,
-        write_region=region,
+        region=region,
         safe_chunks=safe_chunks,
         align_chunks=align_chunks,
+        storage_options=storage_options,
         zarr_version=zarr_version,
         zarr_format=zarr_format,
-        write_empty=write_empty_chunks,
-        **kwargs,
+        write_empty_chunks=write_empty_chunks,
     )
 
-    dataset = zstore._validate_and_autodetect_region(
-        dataset,
-    )
+    dataset = zstore._validate_and_autodetect_region(dataset)
     zstore._validate_encoding(encoding)
 
     writer = ArrayWriter()
+
     # TODO: figure out how to properly handle unlimited_dims
-    dump_to_store(dataset, zstore, writer, encoding=encoding)
-    writes = writer.sync(
-        compute=compute, chunkmanager_store_kwargs=chunkmanager_store_kwargs
-    )
+    try:
+        dump_to_store(dataset, zstore, writer, encoding=encoding)
+        writes = writer.sync(
+            compute=compute, chunkmanager_store_kwargs=chunkmanager_store_kwargs
+        )
+    finally:
+        if compute:
+            zstore.close()
 
-    if compute:
-        _finalize_store(writes, zstore)
-    else:
-        import dask
-
-        return dask.delayed(_finalize_store)(writes, zstore)
+    if not compute:
+        return delayed_close_after_writes(writes, zstore)
 
     return zstore

xarray/backends/common.py
@@ -12,6 +12,7 @@ from typing import (
     Any,
     ClassVar,
     Generic,
+    Self,
     TypeVar,
     Union,
     overload,
@@ -326,6 +327,10 @@ class BackendArray(NdimSizeLenMixin, indexing.ExplicitlyIndexed):
 class AbstractDataStore:
     __slots__ = ()
 
+    def get_child_store(self, group: str) -> Self:  # pragma: no cover
+        """Get a store corresponding to the indicated child group."""
+        raise NotImplementedError()
+
     def get_dimensions(self):  # pragma: no cover
         raise NotImplementedError()
 
@@ -605,6 +610,10 @@ class AbstractWritableDataStore(AbstractDataStore):
             elif dim not in existing_dims and length != parent_dims.get(dim):
                 is_unlimited = dim in unlimited_dims
                 self.set_dimension(dim, length, is_unlimited)
+
+    def sync(self):
+        """Write all buffered data to disk."""
+        raise NotImplementedError()
 
 
 def _infer_dtype(array, name=None):

xarray/backends/h5netcdf_.py
@@ -4,7 +4,7 @@ import functools
 import io
 import os
 from collections.abc import Iterable
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Self
 
 import numpy as np
 
@@ -149,6 +149,17 @@ class H5NetCDFStore(WritableCFDataStore):
         self.is_remote = is_remote_uri(self._filename)
         self.lock = ensure_lock(lock)
         self.autoclose = autoclose
+
+    def get_child_store(self, group: str) -> Self:
+        if self._group is not None:
+            group = os.path.join(self._group, group)
+        return type(self)(
+            self._manager,
+            group=group,
+            mode=self._mode,
+            lock=self.lock,
+            autoclose=self.autoclose,
+        )
 
     @classmethod
     def open(

xarray/backends/locks.py
@@ -134,7 +134,7 @@ def _get_lock_maker(scheduler=None):
         raise KeyError(scheduler)
 
 
-def _get_scheduler(get=None, collection=None) -> str | None:
+def get_dask_scheduler(get=None, collection=None) -> str | None:
     """Determine the dask scheduler that is being used.
 
     None is returned if no dask scheduler is active.
@@ -184,7 +184,7 @@ def get_write_lock(key):
     -------
     Lock object that can be used like a threading.Lock object.
     """
-    scheduler = _get_scheduler()
+    scheduler = get_dask_scheduler()
     lock_maker = _get_lock_maker(scheduler)
     return lock_maker(key)
 

xarray/backends/netCDF4_.py
@@ -5,7 +5,7 @@ import operator
 import os
 from collections.abc import Iterable
 from contextlib import suppress
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Self
 
 import numpy as np
 
@@ -400,6 +400,17 @@ class NetCDF4DataStore(WritableCFDataStore):
         self.is_remote = is_remote_uri(self._filename)
         self.lock = ensure_lock(lock)
         self.autoclose = autoclose
+
+    def get_child_store(self, group: str) -> Self:
+        if self._group is not None:
+            group = os.path.join(self._group, group)
+        return type(self)(
+            self._manager,
+            group=group,
+            mode=self._mode,
+            lock=self.lock,
+            autoclose=self.autoclose,
+        )
 
     @classmethod
     def open(

xarray/backends/zarr.py
@@ -5,7 +5,7 @@ import json
 import os
 import struct
 from collections.abc import Hashable, Iterable, Mapping
-from typing import TYPE_CHECKING, Any, Literal, cast
+from typing import TYPE_CHECKING, Any, Literal, Self, cast
 
 import numpy as np
 import pandas as pd
@@ -785,6 +785,22 @@ class ZarrStore(AbstractWritableDataStore):
             # on demand.
             self._members = self._fetch_members()
 
+    def get_child_store(self, group: str) -> Self:
+        zarr_group = self.zarr_group.require_group(group)
+        return type(self)(
+            zarr_group=zarr_group,
+            mode=self._mode,
+            consolidate_on_close=self._consolidate_on_close,
+            append_dim=self._append_dim,
+            write_region=self._write_region,
+            safe_chunks=self._safe_chunks,
+            write_empty=self._write_empty,
+            close_store_on_close=self._close_store_on_close,
+            use_zarr_fill_value_as_mask=self._use_zarr_fill_value_as_mask,
+            align_chunks=self._align_chunks,
+            cache_members=self._cache_members,
+        )
+
     @property
     def members(self) -> dict[str, ZarrArray | ZarrGroup]:
         """
@@ -1046,9 +1062,6 @@ class ZarrStore(AbstractWritableDataStore):
                 kwargs["zarr_format"] = self.zarr_group.metadata.zarr_format
             zarr.consolidate_metadata(self.zarr_group.store, **kwargs)
 
-    def sync(self):
-        pass
-
     def _open_existing_array(self, *, name) -> ZarrArray:
         import zarr
         from zarr import Array as ZarrArray
@@ -1265,6 +1278,9 @@ class ZarrStore(AbstractWritableDataStore):
                 )
 
             writer.add(v.data, zarr_array, region)
+
+    def sync(self) -> None:
+        pass
 
     def close(self) -> None:
         if self._close_store_on_close:

commit ed1f3a69d0c82a7e324187b069518927d4b4fa27

Fix unreleased bug in DataTree.to_zarr() with mode='r+'
Writing to zarr with `mode='r+'` was broken by #10625, due to a Zarr
bug (https://github.com/zarr-developers/zarr-python/pull/3428). This
add a work-around that works on older versions of Zarr.

The PR that introduced this bug has not yet appeared in an xarray
release, so there's no need for a release note.

xarray/core/datatree_io.py
@@ -211,12 +211,13 @@ def _datatree_to_zarr(
     writer = ArrayWriter()
 
     try:
-        for node in dt.subtree:
+        for rel_path, node in dt.subtree_with_keys:
             at_root = node is dt
             dataset = node.to_dataset(inherit=write_inherited_coords or at_root)
-            node_store = (
-                root_store if at_root else root_store.get_child_store(node.path)
-            )
+            # Use a relative path for group, because absolute paths are broken
+            # with consolidated metadata in zarr 3.1.2 and earlier:
+            # https://github.com/zarr-developers/zarr-python/pull/3428
+            node_store = root_store if at_root else root_store.get_child_store(rel_path)
 
             dataset = node_store._validate_and_autodetect_region(dataset)
             node_store._validate_encoding(encoding)

xarray/tests/test_backends_datatree.py
@@ -804,6 +804,16 @@ class TestZarrDatatreeIO:
         with open_datatree(str(storepath), engine="zarr") as written_dt:
             assert_identical(written_dt, original_dt)
 
+    def test_rplus_mode(
+        self, tmp_path: Path, simple_datatree: DataTree, zarr_format: Literal[2, 3]
+    ) -> None:
+        storepath = tmp_path / "test.zarr"
+        original_dt = simple_datatree.chunk()
+        original_dt.to_zarr(storepath, compute=False, zarr_format=zarr_format)
+        original_dt.to_zarr(storepath, mode="r+")
+        with open_datatree(str(storepath), engine="zarr") as written_dt:
+            assert_identical(written_dt, original_dt)
+
     @requires_dask
     def test_to_zarr_no_redundant_computation(self, tmpdir, zarr_format) -> None:
         import dask.array as da