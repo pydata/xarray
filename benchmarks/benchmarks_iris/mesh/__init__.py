4fd354aa95c9a41040575de71b692c244a3dbb68
Illviljan <14371165+Illviljan@users.noreply.github.com>
2025-09-17 23:06:48 +0200

    remove iris tests

--- benchmarks/benchmarks_iris/mesh/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright Iris contributors
-#
-# This file is part of Iris and is released under the BSD license.
-# See LICENSE in the root of the repository for full licensing details.
-"""Benchmark tests for the iris.mesh module."""
