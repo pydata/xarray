5cdc4941e6157a97c86c3a3287cbbb7624c18e39
Julia Signell <jsignell@gmail.com>
2025-12-02 16:21:36 +0100

    Coerce ``IndexVariable`` to ``Variable`` when assigning to data variables or coordinates (#10909)
    Co-authored-by: Illviljan <14371165+Illviljan@users.noreply.github.com>
    Co-authored-by: Benoit Bovy <benbovy@gmail.com>

--- xarray/tests/test_dataset.py
+++ xarray/tests/test_dataset.py
@@ -4311,9 +4311,11 @@ class TestDataset:
         # coordinate created from variables names should be of string dtype
         data = np.array(["a", "a", "a", "b"], dtype="<U1")
         expected_stacked_variable = DataArray(name="variable", data=data, dims="z")
+
+        # coerce from `IndexVariable` to `Variable` before comparing
         assert_identical(
-            stacked.coords["variable"].drop_vars(["z", "variable", "y"]),
-            expected_stacked_variable,
+            stacked["variable"].variable.to_base_variable(),
+            expected_stacked_variable.variable,
         )
 
     def test_to_stacked_array_transposed(self) -> None:
@@ -4778,6 +4780,18 @@ class TestDataset:
         actual = create_test_data()
         with pytest.raises(ValueError, match=error_regex):
             actual[var_list] = data
+
+    def test_setitem_uses_base_variable_class_even_for_index_variables(self) -> None:
+        ds = Dataset(coords={"x": [1, 2, 3]})
+        ds["y"] = ds["x"]
+
+        # explicit check
+        assert isinstance(ds["x"].variable, IndexVariable)
+        assert not isinstance(ds["y"].variable, IndexVariable)
+
+        # test internal invariant checks when comparing the datasets
+        expected = Dataset(data_vars={"y": ("x", [1, 2, 3])}, coords={"x": [1, 2, 3]})
+        assert_identical(ds, expected)
 
     def test_assign(self) -> None:
         ds = Dataset()
