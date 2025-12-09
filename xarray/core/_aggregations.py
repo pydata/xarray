5137fd8557281cb4338ae89730ff30f55ee78782
Illviljan <14371165+Illviljan@users.noreply.github.com>
2025-12-09 22:35:06 +0100

    Update _aggregations.py

--- xarray/core/_aggregations.py
+++ xarray/core/_aggregations.py
@@ -3808,6 +3808,17 @@ class DatasetGroupByAggregations:
     ) -> Dataset:
         raise NotImplementedError()
 
+    def _flox_scan(
+        self,
+        dim: Dims,
+        *,
+        func: str,
+        skipna: bool | None = None,
+        keep_attrs: bool | None = None,
+        **kwargs: Any,
+    ) -> Dataset:
+        raise NotImplementedError()
+
     def count(
         self,
         dim: Dims = None,
