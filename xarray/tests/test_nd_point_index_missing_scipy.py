ffe9a7a2a85067ed31ac1c6fa1a3ad522f6fe6e6
Sakshee_D <sak.dhormale@gmail.com>
2026-01-14 15:28:00 +0530

    Add test for missing scipy error in NDPointIndex

---
+++ xarray/tests/test_nd_point_index_missing_scipy.py
@@ -0,0 +1,30 @@
+import sys
+import numpy as np
+import pytest
+import xarray as xr
+
+from xarray.indexes import NDPointIndex
+
+
+def test_ndpointindex_missing_scipy(monkeypatch):
+    # Simulate scipy not being installed
+    monkeypatch.setitem(sys.modules, "scipy", None)
+
+    xx = xr.DataArray(
+        np.random.rand(2, 2),
+        dims=("y", "x"),
+        name="xx",
+    )
+    yy = xr.DataArray(
+        np.random.rand(2, 2),
+        dims=("y", "x"),
+        name="yy",
+    )
+
+    ds = xr.Dataset(coords={"xx": xx, "yy": yy})
+
+    with pytest.raises(
+        ImportError,
+        match="scipy.*NDPointIndex|NDPointIndex.*scipy",
+    ):
+        ds.set_xindex(("xx", "yy"), NDPointIndex)
