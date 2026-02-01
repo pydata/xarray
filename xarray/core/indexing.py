0ea47e1926aaf3e762457d79893f3ef5c66b6b05
Antonio Valentino <antonio.valentino@tiscali.it>
2026-02-01 11:41:03 +0100

    Fix _expand_slice for negative step

--- xarray/core/indexing.py
+++ xarray/core/indexing.py
@@ -285,7 +285,7 @@ def _expand_slice(slice_: slice, size: int) -> np.ndarray[Any, np.dtype[np.integ
     >>> _expand_slice(slice(0, -1), 10)
     array([0, 1, 2, 3, 4, 5, 6, 7, 8])
     """
-    sl = normalize_slice(slice_, size)
+    sl = slice(*slice_.indices(size))
     return np.arange(sl.start, sl.stop, sl.step)
 
 
