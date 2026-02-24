9bb47c6dea94f749a4d8f39fa54fcb4ab7bf1bd5
Justus Magin <keewis@posteo.de>
2026-02-24 23:35:11 +0100

    ignore the `_contents` directory

--- doc/conf.py
+++ doc/conf.py
@@ -218,7 +218,7 @@ today_fmt = "%Y-%m-%d"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "debug.ipynb", "**.ipynb_checkpoints"]
+exclude_patterns = ["_build", "debug.ipynb", "**.ipynb_checkpoints", "_contents"]
 
 
 # The name of the Pygments (syntax highlighting) style to use.
